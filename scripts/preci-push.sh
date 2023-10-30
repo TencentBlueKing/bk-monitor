58520898fcb49470a6c137262c91d39f04938d9a
lai <11598235+unique0lai@users.noreply.github.com>
2023-10-30 17:56:05 +0800

    chore: 增加pre-push检查

---
+++ scripts/preci-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Tencent is pleased to support the open source community by making
+# 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+# Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://opensource.org/licenses/MIT
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# shellcheck disable=SC2006
+preci info > /dev/null 2>&1
+if [ $? -ne "0" ]; then
+  echo "system have not preci，skip preci"
+else
+  preci run
+fi
