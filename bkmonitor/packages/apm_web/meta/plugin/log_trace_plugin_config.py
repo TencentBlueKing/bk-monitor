57387c3232bea638a38b87d47982ff09a23ee45c
12559 <1255990182@qq.com>
2023-10-31 18:04:24 +0800

    bugfix: log to trace test

--- bkmonitor/packages/apm_web/meta/plugin/log_trace_plugin_config.py
+++ bkmonitor/packages/apm_web/meta/plugin/log_trace_plugin_config.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+Copyright (C) 2017-2022 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
 from core.drf_resource import api
 
 
@@ -38,7 +49,7 @@ class LogTracePluginConfig(object):
                 "type": "output.bkcollector",
                 "param": {
                     "otlp_bk_data_token": output_param["bk_data_token"],
-                    "otlp_grpc_host": f"http://{host}:4317"
+                    "otlp_grpc_host": f"{host}:4317"
                 }
 
             }
