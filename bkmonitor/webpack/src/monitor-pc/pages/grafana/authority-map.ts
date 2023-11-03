af437f7f701347d24d84d92740df0b1873c8ce7f
liang ling <lingliang1100@gmail.com>
2023-11-03 15:35:41 +0800

    feat: 更新新版仪表盘权限 --story=114981275  (#93)
    feat: 更新新版仪表盘权限 --story=114981275

--- bkmonitor/webpack/src/monitor-pc/pages/grafana/authority-map.ts
+++ bkmonitor/webpack/src/monitor-pc/pages/grafana/authority-map.ts
@@ -23,6 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-export const VIEW_AUTH = 'view_dashboard_v2';
-export const MANAGE_AUTH = 'manage_dashboard_v2';
+// export const VIEW_AUTH = 'view_dashboard_v2';
+// export const MANAGE_AUTH = 'manage_dashboard_v2';
 export const DATASOURCE_AUTH = 'manage_datasource_v2';
+export const NEW_DASHBOARD_AUTH = 'new_dashboard';
