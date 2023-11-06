62300ee018859381349beb75f2249a89811f740f
chenguo <chenguo367@163.com>
2023-11-06 17:41:14 +0800

    chore: preci检查脚本优化

--- scripts/pre-commit/preci.sh
+++ scripts/pre-commit/preci.sh
@@ -8,6 +8,9 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+# add path env
+export PATH=~$PATH:~/PreCI
+
 # shellcheck disable=SC2006
 preci info > /dev/null 2>&1
 if [ $? -ne "0" ]; then
