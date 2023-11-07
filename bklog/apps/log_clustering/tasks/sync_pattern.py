3591a4e1b49194f4f428b2a13b507315c8a8f595
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2023-11-07 15:24:49 +0800

    feat: celery队列拆分
    Signed-off-by: yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>

--- bklog/apps/log_clustering/tasks/sync_pattern.py
+++ bklog/apps/log_clustering/tasks/sync_pattern.py
@@ -21,6 +21,9 @@ the project delivered to anyone in the future.
 """
 from typing import List
 
+from celery.schedules import crontab
+from celery.task import periodic_task, task
+
 from apps.log_clustering.constants import (
     CONTENT_PATTERN_INDEX,
     ORIGIN_LOG_INDEX,
@@ -36,8 +39,6 @@ from apps.log_clustering.models import (
     AiopsSignatureAndPattern,
     ClusteringConfig,
 )
-from celery.schedules import crontab
-from celery.task import periodic_task, task
 
 
 @periodic_task(run_every=crontab(minute="*/10"))
@@ -53,7 +54,7 @@ def sync_pattern():
             sync.delay(model_output_rt=clustering_config.model_output_rt)
 
 
-@task(ignore_result=True)
+@task(ignore_result=True, queue="high_priority")
 def sync(model_id=None, model_output_rt=None):
     if model_id:
         try:
