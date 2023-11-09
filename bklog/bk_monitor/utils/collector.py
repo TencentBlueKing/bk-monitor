8a01f376eac20143269479b52bba56bc948eb4a5
17 <66986742+yiqiwang-17@users.noreply.github.com>
2023-11-09 18:17:57 +0800

    Feat/measure quality opt/#1010158081114948423 (#158)
    Signed-off-by: yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
    Co-authored-by: lai <11598235+unique0lai@users.noreply.github.com>

--- bklog/bk_monitor/utils/collector.py
+++ bklog/bk_monitor/utils/collector.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-import importlib
-import time
-import logging
-import arrow
 
+import importlib
+import logging
+import time
+
+from apps.log_measure.utils.metric import build_metric_id
 from bk_monitor.utils.metric import REGISTERED_METRICS
 
 logger = logging.getLogger("bk_monitor")
@@ -20,13 +21,11 @@ class MetricCollector(object):
             for key in collector_import_paths:
                 importlib.reload(importlib.import_module(key))
 
-    def collect(self, namespaces=None, data_names=None, time_filter_enable=True):
+    def collect(self, namespaces=None, data_names=None):
         """
         采集入口
         """
-        metric_methods = self.metric_filter(
-            namespaces=namespaces, time_filter_enable=time_filter_enable, data_names=data_names
-        )
+        metric_methods = self.metric_filter(namespaces=namespaces, data_names=data_names)
         metric_groups = []
         for metric_method in metric_methods:
             try:
@@ -40,9 +39,12 @@ class MetricCollector(object):
                         "data_name": metric_method["data_name"],
                     }
                 )
+                metric_id = build_metric_id(
+                    metric_method["namespace"], metric_method["data_name"], metric_method["prefix"]
+                )
                 logger.info(
                     "[statistics_data] collect metric->[{}] took {} ms".format(
-                        metric_method["namespace"], int((time.time() - begin_time) * 1000)
+                        metric_id, int((time.time() - begin_time) * 1000)
                     ),
                 )
             except Exception as e:  # pylint: disable=broad-except
@@ -53,19 +55,13 @@ class MetricCollector(object):
         return metric_groups
 
     @classmethod
-    def metric_filter(cls, namespaces=None, data_names=None, time_filter_enable=True):
+    def metric_filter(cls, namespaces=None, data_names=None):
         metric_methods = []
-        time_now = arrow.now()
-        time_now_minute = 60 * time_now.hour + time_now.minute
         for metric_id, metric in REGISTERED_METRICS.items():
             if data_names and metric["data_name"] not in data_names:
                 continue
 
             if namespaces and metric["namespace"] not in namespaces:
-                continue
-
-            # 如果register_metric 有设置time_filter字段以及该字段符合当前时间所属周期才会被添加
-            if time_filter_enable and metric["time_filter"] and time_now_minute % metric["time_filter"]:
                 continue
             metric_methods.append(metric)
         return metric_methods
