cf0e95fbc837841c6000d986ce92a5ba9001643c
kiritoscs <kiritoscs@gmail.com>
2023-11-09 22:53:13 +0800

    chore: 移除debug日志

--- bklog/log_adapter/home/views.py
+++ bklog/log_adapter/home/views.py
@@ -56,8 +56,6 @@ class RequestProcessor:
         except Exception:
             logger.error(f"解析外部用户信息失败({external_user})")
             external_user = {"username": external_user}
-            return external_user
-        logger.debug(f"get_request_user_info, external_user: ({external_user})")
         return external_user
 
     @classmethod
