d487ade78c591f8dfecb336f6ccaebf42d5c951e
热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>
2023-11-10 15:08:29 +0800

    feature: 增加DeepFlow数据源发现&grafana自动接入逻辑 --story=114990012 (#165)
    Co-authored-by: redyfang <148301245+redyfang@users.noreply.github.com>
    Co-authored-by: liuwenping <haha.rober@gmail.com>

--- bkmonitor/apm/core/handlers/query/ebpf_query.py
+++ bkmonitor/apm/core/handlers/query/ebpf_query.py
@@ -15,11 +15,17 @@ specific language governing permissions and limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
+import logging
 
 from apm import constants
+from apm.core.deepflow.base import EBPFHandler
 from apm.core.handlers.query.base import EsQueryBuilderMixin
 from apm.utils.es_search import EsSearch
+from apm_ebpf.resource import TraceQueryResource
 from constants.apm import OtlpKey
+from core.drf_resource import api
+
+logger = logging.getLogger("apm")
 
 
 class EbpfQuery(EsQueryBuilderMixin):
@@ -41,3 +47,30 @@ class EbpfQuery(EsQueryBuilderMixin):
         query = query.extra(size=constants.DISCOVER_BATCH_SIZE).sort(OtlpKey.START_TIME)
 
         return [i.to_dict() for i in query.execute()]
+
+
+class DeepFlowQuery:
+    @classmethod
+    def get_ebpf(cls, trace_id: str, bk_biz_id: int):
+        """
+        获取ebpf数据
+        """
+        sql = (
+            "SELECT client_port, req_tcp_seq, resp_tcp_seq, l7_protocol, l7_protocol_str, version, Enum(type), "
+            "request_type, request_domain, request_resource, request_id, response_status, response_code, "
+            "response_exception, app_service, app_instance, endpoint, trace_id, span_id, parent_span_id, "
+            "Enum(span_kind) AS kind, http_proxy_client, syscall_trace_id_request, syscall_trace_id_response, "
+            "syscall_thread_0, syscall_thread_1, syscall_cap_seq_0, syscall_cap_seq_1, flow_id, signal_source, "
+            "tap, vtap, nat_source, tap_port, tap_port_name, tap_port_type, tap_side, tap_id, vtap_id, "
+            "toString(start_time) AS start_time, toString(end_time) AS end_time FROM l7_flow_log"
+        )
+        ebpf_spans = []
+        ebpf_param = {"trace_id": trace_id, "bk_biz_id": bk_biz_id, "sql": sql, "db": "flow_log"}
+        try:
+            res = TraceQueryResource().request(ebpf_param)
+            for item in res:
+                span_data = EBPFHandler.l7_flow_log_to_resource_span(item)
+                ebpf_spans.append(span_data)
+        except Exception as e:
+            logging.info("get_ebpf, {}".format(e))
+        return ebpf_spans
