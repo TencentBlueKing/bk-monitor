c1a01f92f51d2750d93f774a345261cfa853f593
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2023-11-10 15:27:14 +0800

    feat: 增加运营数据调度判定逻辑
    Signed-off-by: yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>

--- bklog/bk_monitor/utils/collector.py
+++ bklog/bk_monitor/utils/collector.py
@@ -5,6 +5,8 @@ import importlib
 import logging
 import time
 
+from django.core.cache import cache
+
 from apps.log_measure.utils.metric import build_metric_id
 from bk_monitor.utils.metric import REGISTERED_METRICS
 
@@ -17,6 +19,7 @@ class MetricCollector(object):
     """
 
     def __init__(self, collector_import_paths=None):
+        self.last_collect_time = None
         if collector_import_paths and not REGISTERED_METRICS:
             for key in collector_import_paths:
                 importlib.reload(importlib.import_module(key))
@@ -63,5 +66,28 @@ class MetricCollector(object):
 
             if namespaces and metric["namespace"] not in namespaces:
                 continue
+            # 根据上一次采集时间判定是否允许执行，避免队列堆积时重复采集
+            if not cls.is_allow_execute(metric):
+                continue
             metric_methods.append(metric)
         return metric_methods
+
+    @classmethod
+    def is_allow_execute(cls, metric):
+        now_collect_time = int(time.time())
+        COLLECT_METRIC_TIMESTAMP_KEY = "##".join([metric["namespace"], metric["data_name"]])
+        last_collect_time = cache.get(COLLECT_METRIC_TIMESTAMP_KEY)
+        # 未执行过则直接设置为当前采集时间戳
+        if last_collect_time is None:
+            cache.set(
+                COLLECT_METRIC_TIMESTAMP_KEY,
+                now_collect_time,
+            )
+            return True
+        # 未满足周期下一次执行时间，跳过当前轮次
+        elif last_collect_time + metric["time_filter"] * 60 > now_collect_time:
+            return False
+        # 满足下一次执行时间，更新采集时间戳
+        else:
+            cache.set(COLLECT_METRIC_TIMESTAMP_KEY, now_collect_time)
+            return True
