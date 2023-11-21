bdb668eafbc5b2e83dd6820965453a9e99b15624
liangling0628 <lingliang1100@gmail.com>
2023-11-21 14:43:56 +0800

    feat: 更新topo

--- bkmonitor/webpack/src/trace/pages/failure/failure-topo/failure-topo.tsx
+++ bkmonitor/webpack/src/trace/pages/failure/failure-topo/failure-topo.tsx
@@ -23,7 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { defineComponent, onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue';
 import { Arrow, Graph, registerEdge, registerLayout, registerNode } from '@antv/g6';
 import { addListener, removeListener } from 'resize-detector';
 import { debounce } from 'throttle-debounce';
