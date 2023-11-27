85fa4a206594514fc9a48712d0bdcb43ee4dd4f9
chenguo <chenguo367@163.com>
2023-11-27 14:31:39 +0800

    fix: 修复指标缓存获取app列表信息失败导致任务无法执行的问题 #1010158081117644103

--- bkmonitor/api/bk_paas/default.py
+++ bkmonitor/api/bk_paas/default.py
@@ -15,11 +15,9 @@ import logging
 
 import six
 from django.conf import settings
-from requests.exceptions import HTTPError, ReadTimeout
 from rest_framework import serializers
 
 from core.drf_resource import APIResource
-from core.errors.api import BKAPIError
 
 logger = logging.getLogger(__name__)
 
@@ -37,29 +35,16 @@ class BkPaaSAPIGWResource(six.with_metaclass(abc.ABCMeta, APIResource)):
     def get_request_url(self, validated_request_data):
         return super(BkPaaSAPIGWResource, self).get_request_url(validated_request_data).format(**validated_request_data)
 
-    def perform_request(self, validated_request_data):
-        request_url = self.get_request_url(validated_request_data)
-        headers = {
-            "x-bkapi-authorization": json.dumps({"bk_app_code": settings.APP_CODE, "bk_app_secret": settings.APP_TOKEN})
-        }
-        try:
-            result = self.session.get(
-                params=validated_request_data,
-                url=request_url,
-                headers=headers,
-                verify=False,
-                timeout=self.TIMEOUT,
-            )
-        except ReadTimeout:
-            raise BKAPIError(system_name=self.module_name, url=self.action, result="request timeout")
-
-        try:
-            result.raise_for_status()
-        except HTTPError as err:
-            logger.exception("【模块：{}】请求错误：{}，请求url: {} ".format(self.module_name, err, request_url))
-            raise BKAPIError(system_name=self.module_name, url=self.action, result=str(err.response.content))
-
-        return result.json()
+    def get_headers(self):
+        headers = super(BkPaaSAPIGWResource, self).get_headers()
+        headers.update(
+            {
+                "x-bkapi-authorization": json.dumps(
+                    {"bk_app_code": settings.APP_CODE, "bk_app_secret": settings.APP_TOKEN}
+                )
+            }
+        )
+        return headers
 
 
 class GetAppInfoResource(BkPaaSAPIGWResource):
