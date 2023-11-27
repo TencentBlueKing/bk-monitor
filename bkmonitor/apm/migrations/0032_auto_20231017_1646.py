c317b52611c1fb298a336786278bd1d456976185
IMBlues <bluesedenyu@gmail.com>
2023-11-27 15:24:37 +0800

    minor: remove useless content of migration

--- bkmonitor/apm/migrations/0032_auto_20231017_1646.py
+++ bkmonitor/apm/migrations/0032_auto_20231017_1646.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('apm', '0031_auto_20230829_1416'),
     ]
@@ -24,20 +23,5 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
-        ),
-        migrations.AlterField(
-            model_name='normaltypevalueconfig',
-            name='type',
-            field=models.CharField(
-                choices=[
-                    ('metrics_batch_size', '每批Metric发送大小'),
-                    ('traces_batch_size', '每批Trace发送大小'),
-                    ('logs_batch_size', '每批Log发送大小'),
-                    ('db_slow_command_config', 'db慢命令配置'),
-                    ('db_config', 'db配置'),
-                ],
-                max_length=32,
-                verbose_name='配置类型',
-            ),
-        ),
+        )
     ]
