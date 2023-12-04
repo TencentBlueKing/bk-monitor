8e280c00c65948878ccd54ea3605e8102c9b0586
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2023-12-04 17:22:46 +0800

    fix: migration merge
    Signed-off-by: yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>

--- bkmonitor/bkmonitor/migrations/0154_emailsubscription_subscriptionapplyrecord_subscriptionchannel_subscriptionsendrecord.py
+++ bkmonitor/bkmonitor/migrations/0154_emailsubscription_subscriptionapplyrecord_subscriptionchannel_subscriptionsendrecord.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('bkmonitor', '0149_monitormigration'),
+        ('bkmonitor', '0153_dutyrule_code_hash'),
     ]
 
     operations = [
