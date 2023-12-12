ffcf20eb2a6d774f120c1e7ba6ce4dd76bd78033
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2023-12-12 11:27:55 +0800

    feat: add report api to apigw
    Signed-off-by: yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>

---
+++ bklog/apps/log_clustering/views/report_views.py
@@ -0,0 +1,63 @@
+"""
+Tencent is pleased to support the open source community by making BK-LOG 蓝鲸日志平台 available.
+Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+BK-LOG 蓝鲸日志平台 is licensed under the MIT License.
+License for BK-LOG 蓝鲸日志平台:
+--------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF  OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+We undertake not to change the open source license (MIT license) applicable to the current version of
+the project delivered to anyone in the future.
+"""
+
+from rest_framework import serializers
+
+from apps.api import MonitorApi
+from apps.generic import APIViewSet
+from apps.iam import ActionEnum, ResourceEnum
+from apps.iam.handlers.drf import InstanceActionPermission
+from apps.log_clustering.serializers import (
+    CreateOrUpdateReportSerializer,
+    GetExistReportsSerlaizer,
+)
+from apps.utils.drf import detail_route
+
+
+class ReportViewSet(APIViewSet):
+    lookup_field = "index_set_id"
+    serializer_class = serializers.Serializer
+
+    def get_permissions(self):
+        return [InstanceActionPermission([ActionEnum.SEARCH_LOG], ResourceEnum.INDICES)]
+
+    @detail_route(methods=["GET"])
+    def get_exist_reports(self, request):
+        """
+        @api {post} /report/get_exist_reports/ 日志聚类-已存在订阅列表
+        @apiName get_exist_reports
+        @apiGroup log_clustering
+        """
+        params = self.params_valid(GetExistReportsSerlaizer)
+        return MonitorApi.create_or_update_report(params)
+
+    @detail_route(methods=["POST"], url_path="create_or_update_report")
+    def create_or_update_report(self, request):
+        """
+        @api {post} /report/create_or_update_report/ 日志聚类-创建或更新订阅报表
+        @apiName create_or_update_report
+        @apiGroup log_clustering
+        @apiParam {String} signature 数据指纹
+        @apiParam {String} label 标签内容
+        @apiSuccessExample {json} 成功返回: null
+        """
+        params = self.params_valid(CreateOrUpdateReportSerializer)
+        return MonitorApi.create_or_update_report(params)
---
+++ bkmonitor/kernel_api/views/v4/new_report.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from alarm_backends.service.new_report.factory import ReportFactory
+from bkmonitor.action.serializers.report import FrequencySerializer
+from bkmonitor.models import Report, ReportChannel
+from bkmonitor.report.serializers import (
+    ChannelSerializer,
+    ContentConfigSerializer,
+    ScenarioConfigSerializer,
+)
+from bkmonitor.views import serializers
+from core.drf_resource import Resource, resource
+from core.drf_resource.viewsets import ResourceRoute, ResourceViewSet
+
+
+class SendReport(Resource):
+    """
+    发送订阅报表
+    """
+
+    class RequestSerializer(serializers.Serializer):
+        report_id = serializers.IntegerField(required=False)
+        name = serializers.CharField(required=False)
+        bk_biz_id = serializers.IntegerField(required=False)
+        scenario = serializers.CharField(label="订阅场景", required=False)
+        channels = ChannelSerializer(many=True, required=False)
+        frequency = FrequencySerializer(required=False)
+        content_config = ContentConfigSerializer(required=False)
+        scenario_config = ScenarioConfigSerializer(required=False)
+        start_time = serializers.IntegerField(label="开始时间", required=False, default=None, allow_null=True)
+        end_time = serializers.IntegerField(label="结束时间", required=False, default=None, allow_null=True)
+        is_manager_created = serializers.BooleanField(required=False, default=False)
+        is_enabled = serializers.BooleanField(required=False, default=True)
+
+    def perform_request(self, validated_request_data):
+        channels = []
+        # 若订阅id不存在则为测试发送，使用缺省值绑定测试发送记录
+        report_id = validated_request_data.get("report_id", -1)
+        for channel in validated_request_data["channels"]:
+            channel["report_id"] = report_id
+            channels.append(ReportChannel(**channel))
+        if validated_request_data["report_id"]:
+            report = Report.objects.get(id=validated_request_data["report_id"])
+        else:
+            report = Report(validated_request_data)
+        ReportFactory.get_handler(report).run(channels)
+        return "success"
+
+
+class NewReportViewSet(ResourceViewSet):
+    """
+    订阅报表
+    """
+
+    resource_routes = [
+        ResourceRoute("POST", SendReport, endpoint="send_report"),
+        ResourceRoute("POST", resource.new_report.create_or_update_report, endpoint="create_or_update_report"),
+        ResourceRoute("GET", resource.new_report.get_exist_reports, endpoint="get_exist_reports"),
+    ]
