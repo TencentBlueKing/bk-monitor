ee2a02ffe8d8dd0d481934b4cba14f026e08b348
xlc <2933458919@qq.com>
2023-12-15 11:23:45 +0800

    fix: 告警事件缺少事件来源的统计和查询

--- bkmonitor/webpack/src/fta-solutions/pages/event/filter-input.tsx
+++ bkmonitor/webpack/src/fta-solutions/pages/event/filter-input.tsx
@@ -93,7 +93,12 @@ class FilterText {
   separator = ' ';
   fieldKey: FilterText;
   fieldValue: FilterText;
-  constructor(public text: string, public startOffset: number, public dataType: string, separator?: string) {
+  constructor(
+    public text: string,
+    public startOffset: number,
+    public dataType: string,
+    separator?: string
+  ) {
     this.endOffset = startOffset + text.length;
     this.separator = separator || ' ';
     if (dataType === 'field' && /\./.test(text)) {
@@ -274,7 +279,7 @@ export default class FilerInput extends tsc<IFilterInputProps, IFilterInputEvent
       },
       {
         id: 'plugin_id',
-        name: this.$t('告警源')
+        name: this.$t('告警来源')
       }
     ];
     // 事件建议字段列表
