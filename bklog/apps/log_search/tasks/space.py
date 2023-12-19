370c977aeaf794197adc26aefcd62b8b035f8e10
kiritoscs <kiritoscs@gmail.com>
2023-12-19 16:16:55 +0800

    chore: space_uid改为存储id，进一步减少内存使用

--- bklog/apps/log_search/tasks/space.py
+++ bklog/apps/log_search/tasks/space.py
@@ -56,10 +56,10 @@ def sync_spaces():
     """
     # 获取类型ID到类型名称的映射
     type_names = {t["type_id"]: t["type_name"] for t in TransferApi.list_space_types()}
-    # 记录本地同步所有的space_uid, 用于删除不存在的空间
-    space_uid_list: List[str] = []
+    # 记录本地同步所有的space_id, 用于删除不存在的空间
+    space_id_list: List[int] = []
     # 有关联的空间
-    have_related_space_uid_list: List[str] = []
+    have_related_space_id_list: List[int] = []
     total: int = TransferApi.list_spaces({"page": 1, "page_size": 1})["count"]
     for i in get_page_numbers(total=total, page_size=BATCH_SYNC_SPACE_COUNT):
         spaces = TransferApi.list_spaces(
@@ -89,15 +89,15 @@ def sync_spaces():
             )
 
             space_obj.save()
-            space_uid_list.append(space_uid)
+            space_id_list.append(space_pk)
             # 记录存在关联的空间, 因为只有非BKCC的业务会关联其他空间, 但是BKCC的业务不会知道他关联了哪些非BKCC业务, 所以需要记录
             if space_type_id == SpaceTypeEnum.BKCC.value or not space.get("resources", []):
                 continue
-            have_related_space_uid_list.append(space_obj.space_uid)
+            have_related_space_id_list.append(space_obj.id)
 
     # 将BKCC的业务的resources里也添加上其他空间类型的resource, 这样就可以通过BKCC的业务找到其他空间类型的业务
-    for _space_uid in have_related_space_uid_list:
-        _space = Space.objects.get(space_uid=_space_uid)
+    for _space_id in have_related_space_id_list:
+        _space = Space.objects.get(pk=_space_id)
         for resource in _space.properties["resources"]:
             need_relate_space_uid: str = SpaceApi.gen_space_uid(
                 space_type=resource["resource_type"], space_id=resource["resource_id"]
@@ -112,5 +112,5 @@ def sync_spaces():
             need_relate_space_obj.save()
 
     # 删除不存在的空间
-    deleted_rows = Space.origin_objects.exclude(space_uid__in=space_uid_list).delete()
-    logger.info("[sync_spaces] sync ({}), delete ({})".format(len(space_uid_list), deleted_rows))
+    deleted_rows = Space.origin_objects.exclude(id__in=space_id_list).delete()
+    logger.info("[sync_spaces] sync ({}), delete ({})".format(len(space_id_list), deleted_rows))
