bf81968bb35566b47f0549f9fcb636b19f789232
crayon <873217631@qq.com>
2023-12-20 14:31:20 +0800

    fix: 修复监控外部版不带业务ID访问提示无权限的问题 #1010158081118456101

--- bkmonitor/packages/monitor_adapter/home/views.py
+++ bkmonitor/packages/monitor_adapter/home/views.py
@@ -187,6 +187,17 @@ def dispatch_external_proxy(request):
 
         # transfer request.user 进行外部权限替换
         external_user = request.META.get("HTTP_USER", "") or request.META.get("USER", "")
+
+        # 如果参数不带 bk_biz_id，尝试从有权限的业务列表里选一个
+        if not bk_biz_id:
+            biz_id_list = (
+                ExternalPermission.objects.filter(authorized_user=external_user, expire_time__gt=timezone.now())
+                .values_list("bk_biz_id", flat=1)
+                .distinct()
+            )
+            if biz_id_list:
+                bk_biz_id = biz_id_list[0]
+
         if bk_biz_id:
             setattr(fake_request, "biz_id", bk_biz_id)
             setattr(request, "biz_id", bk_biz_id)
