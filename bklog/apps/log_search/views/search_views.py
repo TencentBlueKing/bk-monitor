e64b649b3746b00c9c64b488cc3fc0c32814ae09
positivelong <positivehua@outlook.com>
2023-12-21 13:51:49 +0800

    feat: 代码结构优化

--- bklog/apps/log_search/views/search_views.py
+++ bklog/apps/log_search/views/search_views.py
@@ -1166,10 +1166,10 @@ class SearchViewSet(APIViewSet):
         }
         """
         data = self.params_valid(UnionSearchAttrSerializer)
+        auth_info = Permission.get_auth_info(self.request, raise_exception=False)
         for info in data.get("union_configs", []):
             if not info.get("is_desensitize"):
                 # 只针对白名单中的APP_CODE开放不脱敏的权限
-                auth_info = Permission.get_auth_info(self.request, raise_exception=False)
                 if not auth_info or auth_info["bk_app_code"] not in settings.ESQUERY_WHITE_LIST:
                     info["is_desensitize"] = True
         return Response(UnionSearchHandler(data).union_search())
