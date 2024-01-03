ec2ab2bf3656daab87a96086be7191e1c640739f
liangling0628 <lingliang1100@gmail.com>
2024-01-03 14:59:53 +0800

    feat: 优化首屏加载脚本覆盖率 --story=115753663 # Reviewed, transaction id: 1353

--- bkmonitor/webpack/babel.config.js
+++ bkmonitor/webpack/babel.config.js
@@ -30,7 +30,7 @@ module.exports = function (api) {
       '@babel/preset-env',
       {
         targets: {
-          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
+          browsers: ['> 0.25%', 'last 1 versions', 'not ie <= 11'],
           node: 'current'
         },
         useBuiltIns: 'usage',
