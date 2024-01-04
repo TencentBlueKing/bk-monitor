65cce58d62ec0bde7cc924b9d51c73ee0c67ebf6
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2024-01-04 16:16:26 +0800

    feat: profiling开发 (#881)

--- bkmonitor/webpack/src/trace/pages/profiling/components/application-cascade.tsx
+++ bkmonitor/webpack/src/trace/pages/profiling/components/application-cascade.tsx
@@ -23,31 +23,163 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { defineComponent } from 'vue';
-import { Popover } from 'bkui-vue';
+import { defineComponent, reactive, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { Button, Form, Input, Popover } from 'bkui-vue';
 
 import './application-cascade.scss';
 
 export default defineComponent({
   name: 'ApplicationCascade',
-  setup() {},
+  setup() {
+    const { t } = useI18n();
+
+    const showPopover = ref(false);
+    function handlePopoverShowChange({ isShow }) {
+      showPopover.value = isShow;
+    }
+
+    const localValue = reactive({
+      first: 1,
+      second: 1,
+      hasData: true
+    });
+    function handleFirstClick(val) {
+      localValue.first = val.id;
+      localValue.hasData = val.children.length > 0;
+    }
+    function handleSecondClick(val) {
+      localValue.first = val.id;
+      localValue.hasData = false;
+    }
+
+    const list = {
+      hasData: [
+        { id: 1, name: 'app1', desc: 'app123', children: [{ id: 7, name: 'load-generator' }] },
+        { id: 2, name: 'rideshare-app', desc: 'app123', children: [{ id: 8, name: 'ride-sharing-app' }] }
+      ],
+      noData: [
+        { id: 3, name: 'nodata', desc: 'app123', children: [] },
+        { id: 4, name: 'nodata2', desc: 'app123', children: [] }
+      ]
+    };
+
+    return {
+      t,
+      list,
+      showPopover,
+      localValue,
+      handleFirstClick,
+      handleSecondClick,
+      handlePopoverShowChange
+    };
+  },
   render() {
     return (
       <div class='application-cascade-component'>
         <Popover
           placement='bottom-start'
           arrow={false}
-          theme='light'
+          theme='light application-cascade-popover'
           trigger='click'
+          onAfterShow={val => this.handlePopoverShowChange(val)}
+          onAfterHidden={val => this.handlePopoverShowChange(val)}
         >
           {{
             default: () => (
-              <div class='trigger-wrap'>
-                <div class='text-content'></div>
-                <span class='icon-monitor icon-arrow-down'></span>
+              <div class={['trigger-wrap', this.showPopover ? 'active' : '']}>
+                <Input placeholder={this.t('选择应用/服务')}>
+                  {{ suffix: () => <span class='icon-monitor icon-arrow-down'></span> }}
+                </Input>
               </div>
             ),
-            content: () => <div class='application-cascade-popover-content'>12312</div>
+            content: () => (
+              <div class='application-cascade-popover-content'>
+                <div class='search-wrap'>
+                  <i class='icon-monitor icon-mc-search search-icon'></i>
+                  <Input
+                    class='search-input'
+                    placeholder={this.t('输入关键字')}
+                  ></Input>
+                </div>
+                <div class='select-wrap'>
+                  <div class='first panel'>
+                    <div class='group-title'>{this.t('有数据应用')}</div>
+                    <div class='group-wrap'>
+                      {this.list.hasData.map(item => (
+                        <div
+                          class={{ 'group-item': true, active: item.id === this.localValue.first }}
+                          onClick={() => this.handleFirstClick(item)}
+                          key={item.id}
+                        >
+                          <div class='left'>
+                            <i class='icon-monitor icon-mc-menu-apm'></i>
+                            <span class='name'>{item.name}</span>
+                            <span class='desc'>{item.desc}</span>
+                          </div>
+                          <i class='icon-monitor icon-arrow-right'></i>
+                        </div>
+                      ))}
+                    </div>
+                    <div class='group-title'>{this.t('无数据应用')}</div>
+                    {this.list.noData.map(item => (
+                      <div
+                        class={{ 'group-item': true, active: item.id === this.localValue.first }}
+                        onClick={() => this.handleFirstClick(item)}
+                      >
+                        <i class='icon-monitor icon-mc-menu-apm'></i>
+                        <span class='name'>{item.name}</span>
+                      </div>
+                    ))}
+                  </div>
+                  <div class='second panel'>
+                    {this.localValue.hasData ? (
+                      <div class='has-data-wrap'>
+                        <div class='group-item active'>
+                          <i class='icon-monitor icon-mc-grafana-home'></i>
+                          <span class='name'>ride-sharing-app</span>
+                        </div>
+                        <div class='group-item'>
+                          <i class='icon-monitor icon-mc-grafana-home'></i>
+                          <span class='name'>ride-sharing-app</span>
+                        </div>
+                      </div>
+                    ) : (
+                      <div class='no-data-wrap'>
+                        <Form labelWidth={100}>
+                          <Form.FormItem label={this.t('应用名')}>trace_agg_scene</Form.FormItem>
+                          <Form.FormItem label={this.t('应用别名')}>应用1</Form.FormItem>
+                          <Form.FormItem label={this.t('描述')}>我是描述我是描述我是描述</Form.FormItem>
+                          <Form.FormItem label='Token'>
+                            <span class='password'>●●●●●●●●●●</span>
+                            <Button
+                              text
+                              theme='primary'
+                            >
+                              {this.t('点击查看')}
+                            </Button>
+                          </Form.FormItem>
+                        </Form>
+                        <div class='btn'>
+                          <span>{this.t('Profile 接入指引')}</span>
+                          <i class='icon-monitor icon-fenxiang'></i>
+                        </div>
+                        <div class='btn'>
+                          <span>{this.t('查看应用')}</span>
+                          <i class='icon-monitor icon-fenxiang'></i>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div class='footer-wrap'>
+                  <div class='jump-btn'>
+                    <i class='icon-monitor icon-jia'></i>
+                    <span class=''>{this.t('新增接入')}</span>
+                  </div>
+                </div>
+              </div>
+            )
           }}
         </Popover>
       </div>
--- bkmonitor/webpack/src/trace/pages/profiling/components/upload-retrieval-view.tsx
+++ bkmonitor/webpack/src/trace/pages/profiling/components/upload-retrieval-view.tsx
@@ -23,50 +23,184 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { defineComponent, ref } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { Button, Dialog, Upload } from 'bkui-vue';
-import { Upload as UploadIcon } from 'bkui-vue/lib/icon';
+import { Button, Dialog, Exception, Select, Upload } from 'bkui-vue';
+import { Spinner, Upload as UploadIcon } from 'bkui-vue/lib/icon';
+
+import { ConditionType, RetrievalFormData } from '../typings';
 
 import './upload-retrieval-view.scss';
 
 export default defineComponent({
   name: 'UploadRetrievalView',
+  props: {
+    formData: {
+      type: Object as PropType<RetrievalFormData>,
+      required: true
+    }
+  },
   setup() {
     const { t } = useI18n();
 
     const uploadDialogShow = ref(false);
+    const active = ref<ConditionType>(ConditionType.Where);
+    const selectFile = ref(null);
+
+    function handleUploadTypeChange(type: ConditionType) {
+      active.value = type;
+    }
+    function handleUploadConfirm() {
+      uploadDialogShow.value = false;
+    }
 
     return {
       t,
-      uploadDialogShow
+      uploadDialogShow,
+      active,
+      selectFile,
+      handleUploadTypeChange,
+      handleUploadConfirm
     };
   },
   render() {
     return (
       <div class='upload-retrieval-view-component'>
-        <div class='upload-wrap'>
+        <div class='header-wrap'>
           <Button
+            class='upload-btn'
             theme='primary'
             onClick={() => (this.uploadDialogShow = true)}
           >
-            <UploadIcon />
+            <UploadIcon class='upload-icon' />
             {this.t('上传文件')}
           </Button>
+
+          <div class='file-select'>
+            {this.formData.isComparison && <div class='label where'>{this.t('查询项')}</div>}
+            <Select
+              v-model={this.selectFile}
+              popoverOptions={{
+                extCls: 'upload-select-popover'
+              }}
+            >
+              <Select.Option
+                id='1'
+                name='Profile-2023-11-22-06-02-27.json （原文件名）'
+              >
+                <div class='upload-select-item'>
+                  <div class='left'>
+                    <div class='status'>
+                      <Spinner class='loading'></Spinner>
+                      <span class='label'>{this.t('解析中')}</span>
+                    </div>
+                    <div class='divider'></div>
+                    <div class='name'>Profile-2023-11-22-06-02-27.json （原文件名）</div>
+                  </div>
+                  <i class='icon-monitor icon-mc-detail'></i>
+                </div>
+              </Select.Option>
+              <Select.Option>
+                <div class='upload-select-item'>
+                  <div class='left'>
+                    <div class='status'>
+                      <div class='success circle'></div>
+                      <span class='label'>{this.t('解析成功')}</span>
+                    </div>
+                    <div class='divider'></div>
+                    <div class='name'>Profile-2023-11-22-06-02-27.json （原文件名）</div>
+                  </div>
+                  <i class='icon-monitor icon-mc-detail'></i>
+                </div>
+              </Select.Option>
+              <Select.Option>
+                <div class='upload-select-item'>
+                  <div class='left'>
+                    <div class='status'>
+                      <div class='error circle'></div>
+                      <span class='label'>{this.t('解析失败')}</span>
+                    </div>
+                    <div class='divider'></div>
+                    <div class='name'>Profile-2023-11-22-06-02-27.json （原文件名）</div>
+                  </div>
+                  <i class='icon-monitor icon-mc-detail'></i>
+                </div>
+              </Select.Option>
+            </Select>
+          </div>
+          {this.formData.isComparison && (
+            <div class='file-select'>
+              {this.formData.isComparison && <div class='label comparison'>{this.t('对比项')}</div>}
+              <Select></Select>
+            </div>
+          )}
+        </div>
+
+        <div class='chart-wrap'>
+          <Exception
+            class='loading-wrap'
+            type='search-empty'
+          >
+            <div class='text'>{this.t('文件解析中')}</div>
+            <div class='desc'>{this.t('文件解析可能耗费较长时间，可先选择已解析文件查看')}</div>
+          </Exception>
         </div>
 
         <Dialog
+          class={['upload-popover', this.formData.isComparison ? 'show-footer' : '']}
           is-show={this.uploadDialogShow}
           title={this.t('上传文件')}
           show-mask={false}
+          width={640}
+          onClosed={() => (this.uploadDialogShow = false)}
         >
           {{
             default: () => (
-              <div>
-                <Upload></Upload>
+              <div class='upload-popover-wrap'>
+                {this.formData.isComparison && (
+                  <Button.ButtonGroup class='upload-type-group'>
+                    <Button
+                      selected={this.active === ConditionType.Where}
+                      onClick={() => this.handleUploadTypeChange(ConditionType.Where)}
+                    >
+                      {this.t('查询项')}
+                    </Button>
+                    <Button
+                      selected={this.active === ConditionType.Comparison}
+                      onClick={() => this.handleUploadTypeChange(ConditionType.Comparison)}
+                    >
+                      {this.t('查询项')}
+                    </Button>
+                  </Button.ButtonGroup>
+                )}
+
+                <Upload>
+                  <div class='upload-content'>
+                    <UploadIcon class='upload-icon' />
+                    <span class='title'>{this.t('点击上传或将文件拖到此处')}</span>
+                    <span class='desc'>{this.t('支持 pprof, json 等文件格式')}</span>
+                    <Button
+                      theme='primary'
+                      class='upload-btn'
+                    >
+                      {this.t('上传')}
+                    </Button>
+                  </div>
+                </Upload>
               </div>
             ),
-            footer: () => <div class=''></div>
+            footer: () => (
+              <div class='upload-popover-footer'>
+                <Button
+                  theme='primary'
+                  disabled
+                  onClick={this.handleUploadConfirm}
+                >
+                  {this.t('确定')}
+                </Button>
+                <Button onClick={() => (this.uploadDialogShow = false)}>{this.t('取消')}</Button>
+              </div>
+            )
           }}
         </Dialog>
       </div>
