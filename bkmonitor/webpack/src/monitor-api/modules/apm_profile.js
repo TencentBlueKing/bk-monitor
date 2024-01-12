151f639201d9ba2cd491793519b784857978ffe7
热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>
2024-01-12 16:38:42 +0800

    feat：增加获取应用/服务接口&解决冲突文件 (#949)
    Co-authored-by: redyfang <148301245+redyfang@users.noreply.github.com>

--- bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
+++ bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
@@ -3,8 +3,12 @@ import { request } from '../base';
 export const query = request('POST', 'apm/profile_api/profiling/query/');
 export const upload = request('POST', 'apm/profile_api/profiling/upload/');
 export const listProfileUploadRecord = request('GET', 'apm/profile_api/profiling/list_profile_upload_records/');
+export const listApplicationServices = request('GET', 'apm/profile_api/query/services/');
+export const queryServicesDetail = request('GET', 'apm/profile_api/query/services_detail/');
 
 export default {
+  listApplicationServices,
+  queryServicesDetail,
   query,
   upload,
   listProfileUploadRecord
