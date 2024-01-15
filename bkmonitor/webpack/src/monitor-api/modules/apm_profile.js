112e4487f4df52538c375de4c97b1aba1d6c2674
布鲁斯搬砖鱼 <bluesedenyu@gmail.com>
2024-01-15 11:14:46 +0800

    feat: add profiling labels & values api (#959)

--- bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
+++ bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
@@ -1,15 +1,18 @@
 import { request } from '../base';
-
-export const query = request('POST', 'apm/profile_api/profiling/query/');
-export const upload = request('POST', 'apm/profile_api/profiling/upload/');
-export const listProfileUploadRecord = request('GET', 'apm/profile_api/profiling/list_profile_upload_records/');
-export const listApplicationServices = request('GET', 'apm/profile_api/query/services/');
+export const query = request('POST', 'apm/profile_api/query/samples/');
+export const queryLabels = request('GET', 'apm/profile_api/query/labels/');
+export const queryLabelValues = request('GET', 'apm/profile_api/query/label_values/');
 export const queryServicesDetail = request('GET', 'apm/profile_api/query/services_detail/');
+export const listApplicationServices = request('GET', 'apm/profile_api/query/services/');
+export const upload = request('POST', 'apm/profile_api/upload/upload/');
+export const listProfileUploadRecord = request('GET', 'apm/profile_api/query/records/');
 
 export default {
-  listApplicationServices,
-  queryServicesDetail,
-  query,
   upload,
+  query,
+  queryLabels,
+  queryLabelValues,
+  queryServicesDetail,
+  listApplicationServices,
   listProfileUploadRecord
 };
