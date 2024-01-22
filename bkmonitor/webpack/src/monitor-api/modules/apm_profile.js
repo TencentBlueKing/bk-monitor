336e7f2692c5eb746b1b6979e3acece24d1c1435
redyfang <148301245+redyfang@users.noreply.github.com>
2024-01-22 14:15:26 +0800

    minir: [apm] add export api in apm_profile.js (#1045)

--- bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
+++ bkmonitor/webpack/src/monitor-api/modules/apm_profile.js
@@ -1,6 +1,7 @@
 import { request } from '../base';
 
 export const query = request('POST', 'apm/profile_api/query/samples/');
+export const queryExport = request('POST', 'apm/profile_api/query/export/');
 export const queryLabels = request('GET', 'apm/profile_api/query/labels/');
 export const queryLabelValues = request('GET', 'apm/profile_api/query/label_values/');
 export const queryServicesDetail = request('GET', 'apm/profile_api/query/services_detail/');
@@ -11,6 +12,7 @@ export const listProfileUploadRecord = request('GET', 'apm/profile_api/upload/re
 export default {
   upload,
   query,
+  queryExport,
   queryLabels,
   queryLabelValues,
   queryServicesDetail,
