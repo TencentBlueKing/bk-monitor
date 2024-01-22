00e8b1ceffa2c92dfde66b8e65d27e690f389e5c
liangling0628 <lingliang1100@gmail.com>
2024-01-22 10:59:16 +0800

    fix: 修复智能策略算法展示错误问题 --bug=119750341

--- bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-set-new/detection-rules/components/intelligent-detect/intelligent-detect.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-set-new/detection-rules/components/intelligent-detect/intelligent-detect.tsx
@@ -343,7 +343,7 @@ export default class IntelligentDetect extends tsc<IntelligentDetectProps, Intel
             ></div>
           </bk-alert>
         )}
-        <bk-form
+        <Form
           ref='formRef'
           rules={this.rules}
           readonly={this.readonly}
@@ -351,7 +351,7 @@ export default class IntelligentDetect extends tsc<IntelligentDetectProps, Intel
           label-width={126}
           onChange={this.handleValueChange}
           class='time-serise-forecast-wrap'
-        ></bk-form>
+        ></Form>
       </div>
     );
   }
