9fb026d6c6df64a1e8121bfabc63eae5728a4c4c
IMBlues <bluesedenyu@gmail.com>
2024-02-01 16:54:33 +0800

    feat: add diff to tendency

--- bkmonitor/packages/apm_web/profile/diagrams/tendency.py
+++ bkmonitor/packages/apm_web/profile/diagrams/tendency.py
@@ -13,15 +13,20 @@ from apm_web.profile.constants import DESCRIBING_SAMPLE_UNIT
 from apm_web.profile.converter import Converter
 
 
+def get_statistics(c: Converter) -> dict:
+    statistics = defaultdict(int)
+    for s in c.raw_data:
+        if s["sample_type"].split("/")[1] != DESCRIBING_SAMPLE_UNIT:
+            continue
+        statistics[s["dtEventTimeStamp"]] += int(s["value"])
+
+    return statistics
+
+
 class TendencyDiagrammer:
     def draw(self, c: Converter, **options) -> dict:
         """statistics profile data by time"""
-
-        statistics = defaultdict(int)
-        for s in c.raw_data:
-            if s["sample_type"].split("/")[1] != DESCRIBING_SAMPLE_UNIT:
-                continue
-            statistics[s["dtEventTimeStamp"]] += int(s["value"])
+        statistics = get_statistics(c)
 
         # follow the structure of bk-ui plugin
         return {
@@ -36,4 +41,24 @@ class TendencyDiagrammer:
         }
 
     def diff(self, base_doris_converter: Converter, diff_doris_converter: Converter, **options) -> dict:
-        raise NotImplementedError
+        """diff two profile data by time"""
+        base_statistics = get_statistics(base_doris_converter)
+        diff_statistics = get_statistics(diff_doris_converter)
+
+        # follow the structure of bk-ui plugin
+        return {
+            "series": [
+                {
+                    "alias": "_result_",
+                    "datapoints": [[[v, k] for k, v in sorted(base_statistics.items())]],
+                    "type": "line",
+                    "unit": "",
+                },
+                {
+                    "alias": "_result_",
+                    "datapoints": [[[v, k] for k, v in sorted(diff_statistics.items())]],
+                    "type": "line",
+                    "unit": "",
+                },
+            ]
+        }
