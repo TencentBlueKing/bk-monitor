4f6d8c08aef4c27d2e6bfa5112b240ceb2a561e6
chenguo <chenguo367@163.com>
2024-03-19 14:36:48 +0800

    "feat: 智能异常检测，中间环节去掉存储节点 #1010158081116549706"

--- bkmonitor/bkmonitor/management/commands/migrate_cmdblevel_dataflow.py
+++ bkmonitor/bkmonitor/management/commands/migrate_cmdblevel_dataflow.py
@@ -8,6 +8,9 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+"""
+# 清理计算平台dataflow中，冗余的存储节点
+"""
 import time
 
 from django.conf import settings
