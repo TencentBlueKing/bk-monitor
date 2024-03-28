3796eb165dca60f40c60de9596368266e83c7ec4
ziijiawen <zijialoveplay@qq.com>
2024-03-28 19:50:54 +0800

    feat: 优化前端渲染及清理无用资源 #1010158081116726727

--- bklog/web/webpack.config.js
+++ bklog/web/webpack.config.js
@@ -173,16 +173,14 @@ module.exports = (baseConfig, { mobile, production, fta, email = false }) => {
         '@': path.resolve('src')
       }
     },
-    plugins: baseConfig.plugins
-      .filter(plugin => plugin.constructor.name !== 'ContextReplacementPlugin')
-      .map(plugin => {
-        return plugin instanceof wepack.ProgressPlugin
-          ? new WebpackBar({
-              profile: true,
-              name: `日志平台 ${production ? 'Production模式' : 'Development模式'} 构建`
-            })
-          : plugin;
-      }),
+    plugins: baseConfig.plugins.map(plugin => {
+      return plugin instanceof wepack.ProgressPlugin
+        ? new WebpackBar({
+            profile: true,
+            name: `日志平台 ${production ? 'Production模式' : 'Development模式'} 构建`
+          })
+        : plugin;
+    }),
     cache: typeof devConfig.cache === 'boolean' ? devConfig.cache : config.cache
   };
 };
