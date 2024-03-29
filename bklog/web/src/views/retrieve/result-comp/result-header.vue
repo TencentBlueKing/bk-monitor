c867bda0717061c8fca199bf6f4ddd58b8e7f760
Shahzad <68816290+ta0tao@users.noreply.github.com>
2024-03-29 11:04:39 +0800

    fix: 清洗分词符切换为自定义时未回填原始日志内的自定义分词 --bug=122241981 (#1513)
    # Reviewed, transaction id: 4879

--- bklog/web/src/views/retrieve/result-comp/result-header.vue
+++ bklog/web/src/views/retrieve/result-comp/result-header.vue
@@ -268,7 +268,8 @@ export default {
       ],
       settingMenuList: [
         // { id: 'index', name: '全文索引' },
-        { id: 'extract', name: this.$t('button-字段清洗').replace('button-', '') },
+        // !!TODO 先关闭字段清洗入口
+        // { id: 'extract', name: this.$t('button-字段清洗').replace('button-', '') },
         { id: 'clustering', name: this.$t('日志聚类') }
       ],
       accessList: [
