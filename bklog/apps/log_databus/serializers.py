d44f87d463bd0fe04037784123124fd7dc110e96
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2024-04-19 17:27:46 +0800

    fix: 优化邮件订阅调度发送逻辑 #1010158081123053233

--- bklog/apps/log_databus/serializers.py
+++ bklog/apps/log_databus/serializers.py
@@ -220,7 +220,6 @@ class PluginParamSerializer(serializers.Serializer):
         label=_("windows事件内容匹配操作符"),
         choices=PluginParamOpEnum.get_choices(),
         required=False,
-        default="",
     )
 
     # Redis慢日志相关参数
