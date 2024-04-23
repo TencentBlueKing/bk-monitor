b9e38cc4c2f99d19a7a5e47fcc8e3ab643eb7959
Tang <1270432252@qq.com>
2024-04-23 17:43:04 +0800

    feat: 代码优化修改 --story=116827538 # Reviewed, transaction id: 6589

--- bkmonitor/packages/monitor_web/statistics/v2/grafana.py
+++ bkmonitor/packages/monitor_web/statistics/v2/grafana.py
@@ -14,6 +14,7 @@ from typing import List, Dict, Tuple
 
 from django.utils.functional import cached_property
 from django.conf import settings
+from django.core.paginator import Paginator
 
 from core.drf_resource import api, resource
 from core.statistics.metric import Metric, register
@@ -25,6 +26,7 @@ class GrafanaCollector(BaseCollector):
     """
     仪表盘
     """
+    CHUNK_SIZE = 500
 
     @cached_property
     def organizations(self) -> List:
@@ -56,23 +58,37 @@ class GrafanaCollector(BaseCollector):
         return org_ids
 
     @cached_property
-    def org_id_mapping_name(self) -> Dict[int, str]:
-        org_id_mapping_name = {}
+    def org_id_mapping_biz_id(self) -> Dict[int, int]:
+        org_id_mapping_biz_id = {}
         for org in self.organizations:
-            org_id_mapping_name[org.id] = org.name
-        return org_id_mapping_name
+            org_id_mapping_biz_id[org.id] = int(org.name)
+        return org_id_mapping_biz_id
 
-    def get_org_id_mapping_dashboards(self, org_ids: List) -> Tuple[Dict[int, List[Dashboard]], List[int]]:
-        """
-        获取org_id对应的所有dashboard
-        """
-        dashboard_query = Dashboard.objects.filter(org_id__in=org_ids, is_folder__exact=0)
-        org_id_mapping_dashboards = defaultdict(list)
-        dashboard_ids = []
-        for dashboard in dashboard_query:
-            org_id_mapping_dashboards[dashboard.org_id].append(dashboard)
-            dashboard_ids.append(dashboard.id)
-        return org_id_mapping_dashboards, dashboard_ids
+    @staticmethod
+    def dashboard_data_iterator(fields: list, batch_size: int = 1000):
+        dashboards_query = Dashboard.objects.filter(is_folder=0).values_list(*fields)
+        paginator = Paginator(dashboards_query, batch_size)
+        for page_number in range(1, paginator.num_pages + 1):
+            page = paginator.page(page_number)
+            for item in page.object_list:
+                yield item
+
+    def increment_panel_metric(self, panels, biz_id, metric: Metric):
+        import pdb; pdb.set_trace()
+        for panel in panels:
+            if "type" not in panel:
+                continue
+            if panel["type"] == "row":
+                # 如果是行类型，需要统计嵌套数量
+                num = len(panel.get("panels", []))
+            else:
+                num = 1
+            metric.labels(bk_biz_id=biz_id, bk_biz_name=self.get_biz_name(biz_id)).inc(num)
+
+    @staticmethod
+    def chunked_list(big_list, chunk_size):
+        for i in range(0, len(big_list), chunk_size):
+            yield big_list[i:i + chunk_size]
 
     @register(labelnames=("bk_biz_id", "bk_biz_name", "type"))
     def grafana_datasource_count(self, metric: Metric):
@@ -91,18 +107,17 @@ class GrafanaCollector(BaseCollector):
                     ).inc()
         else:
             org_ids = self.org_ids
-            datasources = DataSource.objects.filter(org_id__in=org_ids)
-            org_id_mapping_datasources = defaultdict(list)
-            for datasource in datasources:
-                org_id_mapping_datasources[datasource.org_id].append(datasource)
+            for chunk in self.chunked_list(org_ids, self.CHUNK_SIZE):
+                datasources = DataSource.objects.filter(org_id__in=chunk).values_list("org_id", "type")
 
-            org_id_mapping_name = self.org_id_mapping_name
+            org_id_mapping_biz_id = self.org_id_mapping_biz_id
             for org_id in org_ids:
-                for datasource in org_id_mapping_datasources[org_id]:
-                    metric.labels(
-                        bk_biz_id=int(org_id_mapping_name[org_id]),
-                        bk_biz_name=self.get_biz_name(org_id_mapping_name[org_id]), type=datasource.type
-                    ).inc()
+                for org_id_, datasource_type in datasources:
+                    if org_id_ == org_id:
+                        metric.labels(
+                            bk_biz_id=org_id_mapping_biz_id[org_id],
+                            bk_biz_name=self.get_biz_name(org_id_mapping_biz_id[org_id]), type=datasource_type
+                        ).inc()
 
     @register(labelnames=("bk_biz_id", "bk_biz_name"))
     def grafana_dashboard_count(self, metric: Metric):
@@ -115,13 +130,14 @@ class GrafanaCollector(BaseCollector):
                 dashboards = api.grafana.search_folder_or_dashboard(type="dash-db", org_id=org["id"])["data"]
                 metric.labels(bk_biz_id=int(org_name), bk_biz_name=self.get_biz_name(org_name)).set(len(dashboards))
         else:
-            org_ids = self.org_ids
-            org_id_mapping_dashboards = self.get_org_id_mapping_dashboards(org_ids)[0]
-            org_id_mapping_name = self.org_id_mapping_name
-            for org_id in org_ids:
-                dashboards = org_id_mapping_dashboards[org_id]
-                metric.labels(bk_biz_id=int(org_id_mapping_name[org_id]),
-                              bk_biz_name=self.get_biz_name(org_id_mapping_name[org_id])).set(len(dashboards))
+            org_id_mapping_biz_id = self.org_id_mapping_biz_id
+            for org_id in self.org_ids:
+                num = 0
+                for org_id_, in self.dashboard_data_iterator(fields=["org_id", ]):
+                    if org_id_ == org_id:
+                        num += 1
+                metric.labels(bk_biz_id=org_id_mapping_biz_id[org_id],
+                              bk_biz_name=self.get_biz_name(org_id_mapping_biz_id[org_id])).set(num)
 
     @register(labelnames=("bk_biz_id", "bk_biz_name"))
     def grafana_dashboard_panel_count(self, metric: Metric):
@@ -137,32 +153,15 @@ class GrafanaCollector(BaseCollector):
                         "data"].get(
                         "dashboard", {}
                     )
-                    for panel in dashboard_info.get("panels", []):
-                        if "type" not in panel:
-                            continue
-                        if panel["type"] == "row":
-                            # 如果是行类型，需要统计嵌套数量
-                            num = len(panel.get("panels", []))
-                        else:
-                            num = 1
-                        metric.labels(bk_biz_id=int(org_name), bk_biz_name=self.get_biz_name(org_name)).inc(num)
+                    self.increment_panel_metric(dashboard_info.get("panels", []), int(org_name), metric)
         else:
-            org_ids = self.org_ids
-            org_id_mapping_dashboards = self.get_org_id_mapping_dashboards(org_ids)[0]
-            for org_id in org_ids:
-                panels = []
-                for dashboard in org_id_mapping_dashboards[org_id]:
-                    panels.extend(json.loads(dashboard.data)["panels"])
-                for panel in panels:
-                    if "type" not in panel:
-                        continue
-                    if panel["type"] == "row":
-                        # 如果是行类型，需要统计嵌套数量
-                        num = len(panel.get("panels", []))
-                    else:
-                        num = 1
-                    metric.labels(bk_biz_id=int(self.org_id_mapping_name[org_id]),
-                                  bk_biz_name=self.get_biz_name(self.org_id_mapping_name[org_id])).inc(num)
+            org_id_mapping_biz_id = self.org_id_mapping_biz_id
+            for org_id in self.org_ids:
+                for org_id_, data in self.dashboard_data_iterator(fields=["org_id", "data"]):
+                    if org_id_ == org_id:
+                        panels = json.loads(data)["panels"]
+                        biz_id = org_id_mapping_biz_id[org_id]
+                        self.increment_panel_metric(panels, biz_id, metric)
 
     @register(labelnames=("bk_biz_id", "bk_biz_name"))
     def grafana_dashboard_favorite_count(self, metric: Metric):
@@ -175,22 +174,15 @@ class GrafanaCollector(BaseCollector):
                 dashboards = resource.grafana.get_dashboard_list(bk_biz_id=org_name, is_starred=True)
                 metric.labels(bk_biz_id=int(org_name), bk_biz_name=self.get_biz_name(org_name)).set(len(dashboards))
         else:
-            org_ids = self.org_ids
-            org_id_mapping_dashboards, dashboard_ids = self.get_org_id_mapping_dashboards(org_ids)
-            starred_dashboards = Star.objects.filter(dashboard_id__in=dashboard_ids)
-            dashboard_mapping_starred_dashboards = defaultdict(list)
-            for starred_dashboard in starred_dashboards:
-                dashboard_mapping_starred_dashboards[starred_dashboard.dashboard_id].append(starred_dashboard)
-
-            org_id_mapping_star_dashboards = defaultdict(list)
-            for org_id, dashboards in org_id_mapping_dashboards.items():
-                starred_dashboard_set = set()
-                for dashboard in dashboards:
-                    starred_dashboard_set.update(dashboard_mapping_starred_dashboards[dashboard.id])
-                org_id_mapping_star_dashboards[org_id].extend(list(starred_dashboard_set))
-
-            for org_id in org_ids:
-                starred_dashboards = org_id_mapping_star_dashboards[org_id]
-                metric.labels(bk_biz_id=int(self.org_id_mapping_name[org_id]),
-                              bk_biz_name=self.get_biz_name(self.org_id_mapping_name[org_id]))\
-                    .set(len(starred_dashboards))
+            org_id_mapping_biz_id = self.org_id_mapping_biz_id
+            for org_id in self.org_ids:
+                dashboard_ids = []
+                for org_id_, id in self.dashboard_data_iterator(fields=["org_id", "id"]):
+                    if org_id_ == org_id:
+                        dashboard_ids.append(id)
+                num = 0
+                for chunk in self.chunked_list(dashboard_ids, self.CHUNK_SIZE):
+                    num += Star.objects.filter(dashboard_id__in=chunk).count()
+                metric.labels(bk_biz_id=org_id_mapping_biz_id[org_id],
+                              bk_biz_name=self.get_biz_name(org_id_mapping_biz_id[org_id]))\
+                    .set(num)
