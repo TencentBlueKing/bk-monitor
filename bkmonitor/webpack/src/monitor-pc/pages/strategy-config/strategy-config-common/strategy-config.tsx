561f41111719f91da35eb321b41521e5e5bf7ed5
xlc <2933458919@qq.com>
2024-04-28 17:54:36 +0800

    feat: 配置管理骨架屏改造

--- bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-common/strategy-config.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-common/strategy-config.tsx
@@ -45,6 +45,7 @@ import { debounce } from 'throttle-debounce';
 import EmptyStatus from '../../../components/empty-status/empty-status';
 import { EmptyStatusOperationType, EmptyStatusType } from '../../../components/empty-status/types';
 import { INodeType, TargetObjectType } from '../../../components/monitor-ip-selector/typing';
+import TableSkeleton from '../../../components/skeleton/table-skeleton';
 import SvgIcon from '../../../components/svg-icon/svg-icon.vue';
 import TableFilter from '../../../components/table-filter/table-filter.vue';
 // import StrategySetTarget from '../strategy-config-set/strategy-set-target/strategy-set-target.vue';
@@ -2505,7 +2506,7 @@ class StrategyConfig extends Mixins(commonPageSizeMixin) {
     return (
       <div
         class='strategy-config'
-        v-monitor-loading={{ isLoading: this.loading }}
+        // v-monitor-loading={{ isLoading: this.loading }}
       >
         <div class='content'>
           <div
@@ -2520,6 +2521,7 @@ class StrategyConfig extends Mixins(commonPageSizeMixin) {
               {...{ on: { 'update:show': val => (this.showFilterPanel = val) } }}
               checkedData={this.header.keywordObj}
               data={this.filterPanelData}
+              showSkeleton={this.loading}
               on-change={this.handleSearchSelectChange}
             ></FilterPanel>
             <div
@@ -2666,23 +2668,29 @@ class StrategyConfig extends Mixins(commonPageSizeMixin) {
                   </div>
                 </bk-popover>
               </div>
-              {this.getTableComponent()}
-              {this.table.data?.length ? (
-                <bk-pagination
-                  class='strategy-pagination list-pagination'
-                  v-show={this.tableInstance.total}
-                  align='right'
-                  count={this.pageCount}
-                  current={this.tableInstance.page}
-                  limit={this.tableInstance.pageSize}
-                  limit-list={this.tableInstance.pageList}
-                  size='small'
-                  pagination-able
-                  show-total-count
-                  on-change={this.handlePageChange}
-                  on-limit-change={this.handleLimitChange}
-                ></bk-pagination>
-              ) : undefined}
+              {this.table.loading || this.loading ? (
+                <TableSkeleton type={2}></TableSkeleton>
+              ) : (
+                [
+                  this.getTableComponent(),
+                  this.table.data?.length ? (
+                    <bk-pagination
+                      class='strategy-pagination list-pagination'
+                      v-show={this.tableInstance.total}
+                      align='right'
+                      count={this.pageCount}
+                      current={this.tableInstance.page}
+                      limit={this.tableInstance.pageSize}
+                      limit-list={this.tableInstance.pageList}
+                      size='small'
+                      pagination-able
+                      show-total-count
+                      on-change={this.handlePageChange}
+                      on-limit-change={this.handleLimitChange}
+                    ></bk-pagination>
+                  ) : undefined,
+                ]
+              )}
             </div>
           </div>
         </div>
