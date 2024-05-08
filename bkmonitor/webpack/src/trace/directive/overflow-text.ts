d7e79a706147b7175d630d006c6ff70f265466c5
xlc <2933458919@qq.com>
2024-05-08 15:39:16 +0800

    fix: 【监控】服务/应用列表文本溢出没处理

--- bkmonitor/webpack/src/trace/directive/overflow-text.ts
+++ bkmonitor/webpack/src/trace/directive/overflow-text.ts
@@ -23,7 +23,6 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-// import type { DirectiveBinding } from 'vue';
 import { $bkPopover } from 'bkui-vue';
 
 import type { DirectiveBinding } from 'vue';
