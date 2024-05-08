363e793222f435d0177e566817ea35d435b5c8b2
v_cwcgcui <1724427771@qq.com>
2024-05-08 15:29:16 +0800

    feat: 获取空间列表的接口优化 #1010158081117279634

--- bklog/apps/iam/handlers/permission.py
+++ bklog/apps/iam/handlers/permission.py
@@ -121,7 +121,6 @@ class Permission(object):
     def _make_application(
         self, action_ids: List[str], resources: List[Resource] = None, system_id: str = settings.BK_IAM_SYSTEM_ID
     ) -> Application:
-
         resources = resources or []
         actions = []
 
@@ -335,11 +334,6 @@ class Permission(object):
         """
         根据动作过滤用户有权限的业务列表
         """
-        if space_list is None:
-            # 获取业务列表
-            from apps.log_search.models import Space
-
-            space_list = Space.objects.all()
         # 跳过权限检验
         if settings.IGNORE_IAM_PERMISSION:
             return space_list
@@ -356,7 +350,7 @@ class Permission(object):
         if not policies:
             # 如果策略是空，则说明没有任何权限，若存在Demo业务，返回Demo业务，否则返回空
             for space in space_list:
-                if settings.DEMO_BIZ_ID == space.bk_biz_id:
+                if settings.DEMO_BIZ_ID == space["bk_biz_id"]:
                     return [space]
             return []
 
@@ -366,13 +360,13 @@ class Permission(object):
         results = []
         for space in space_list:
             obj_set = ObjectSet()
-            obj_set.add_object(_type=ResourceEnum.BUSINESS.id, obj={"id": str(space.bk_biz_id)})
+            obj_set.add_object(_type=ResourceEnum.BUSINESS.id, obj={"id": str(space["bk_biz_id"])})
 
             # 计算表达式
             is_allowed = self.iam_client._eval_expr(expr, obj_set)
 
             # 针对demo业务权限豁免
-            if is_allowed or str(settings.DEMO_BIZ_ID) == str(space.bk_biz_id):
+            if is_allowed or str(settings.DEMO_BIZ_ID) == str(space["bk_biz_id"]):
                 results.append(space)
 
         return results
