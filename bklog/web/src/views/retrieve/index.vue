96c10a14915b08ad179b4ced3a27fde56186ede4
ptaoapeng <1127949533@qq.com>
2024-05-09 16:48:41 +0800

    fix: 检索页‘更多’列表点击无法展开修复 --bug=123825989

--- bklog/web/src/views/retrieve/index.vue
+++ bklog/web/src/views/retrieve/index.vue
@@ -601,19 +601,19 @@ export default {
     },
     /** 索引集更变时的数据初始化 */
     initIndexSetChangeFn(val, isUnionSearch = false) {
-      const aloneSetItem = this.indexSetList.find(item => item.index_set_id === val);
-      this.isSearchAllowed = isUnionSearch
-        ? val?.every(
-            item =>
-              this.indexSetList.find(indexSet => indexSet.index_set_id === item)?.permission?.[
-                authorityMap.SEARCH_LOG_AUTH
-              ]
-          )
-        : !!aloneSetItem?.permission?.[authorityMap.SEARCH_LOG_AUTH];
-      if (this.isSearchAllowed) this.authPageInfo = null;
-      if (!isUnionSearch) {
+      if (isUnionSearch) {
+        const aloneSetItem = this.indexSetList.find(item => item.index_set_id === val);
         this.indexSetItem = aloneSetItem ?? { index_set_name: '', indexName: '', scenario_name: '', scenario_id: '' };
+        this.isSearchAllowed = !!aloneSetItem?.permission?.[authorityMap.SEARCH_LOG_AUTH];
+      } else {
+        this.isSearchAllowed = val?.every(
+          item =>
+            this.indexSetList.find(indexSet => indexSet.index_set_id === item)?.permission?.[
+              authorityMap.SEARCH_LOG_AUTH
+            ]
+        );
       }
+      if (this.isSearchAllowed) this.authPageInfo = null;
       this.resetRetrieveCondition();
       this.resetFavoriteValue();
       this.$store.commit('updateIndexId', val);
