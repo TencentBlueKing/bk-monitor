a20b0dfd5ccec689263187e3d0424e92a3fae33d
v_cwcgcui <1724427771@qq.com>
2024-05-21 09:26:46 +0800

    fix: 索引检索历史记录无法正常显示 #1010158081124107815

--- bklog/apps/log_search/handlers/search/search_handlers_esquery.py
+++ bklog/apps/log_search/handlers/search/search_handlers_esquery.py
@@ -1032,9 +1032,11 @@ class SearchHandler(object):
 
         # 获取当前空间关联空间的索引集
         space_uids = IndexSetHandler.get_all_related_space_uids(space_uid)
-        index_set_objs = LogIndexSet.objects.filter(index_set_id__in=index_set_id_all, space_uid__in=space_uids)
+        index_set_objs = LogIndexSet.objects.filter(index_set_id__in=index_set_id_all, space_uid__in=space_uids).values(
+            "index_set_id", "index_set_name"
+        )
 
-        effect_index_set_mapping = {obj.index_set_id: obj.index_set_name for obj in index_set_objs}
+        effect_index_set_mapping = {obj["index_set_id"]: obj["index_set_name"] for obj in index_set_objs}
 
         if not effect_index_set_mapping:
             return []
