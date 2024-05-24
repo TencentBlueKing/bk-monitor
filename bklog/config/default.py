943ac4a44d68ffa43102419fb24331d05daff0f2
jayjiahua <553544693@qq.com>
2024-05-24 13:28:29 +0800

    feat: 增加自定义的UserModelBackend

--- bklog/config/default.py
+++ bklog/config/default.py
@@ -527,7 +527,7 @@ LOCALE_PATHS = (os.path.join(PROJECT_ROOT, "locale"),)
 AUTH_USER_MODEL = "account.User"
 AUTHENTICATION_BACKENDS = (
     "apps.middleware.api_token_middleware.ApiTokenAuthBackend",
-    "apigw_manager.apigw.authentication.UserModelBackend",
+    "apps.middleware.apigw.UserModelBackend",
     "blueapps.account.backends.BkJwtBackend",
     "blueapps.account.backends.UserBackend",
     "django.contrib.auth.backends.ModelBackend",
