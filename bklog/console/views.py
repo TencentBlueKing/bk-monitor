4de8a312e3c54bd14a64a9cdca119fbe6d24d2b4
jayjiahua <553544693@qq.com>
2024-05-28 20:49:40 +0800

    fix: 修复退出登陆后凭证未失效的问题

--- bklog/console/views.py
+++ bklog/console/views.py
@@ -1,12 +1,24 @@
+from blueapps.account import ConfFixture
+from blueapps.account.handlers.response import ResponseHandler
+from django.conf import settings
 from django.contrib import auth
-from django.http import HttpResponseRedirect
 from rest_framework.views import APIView
 
 
 class LogOutView(APIView):
     def get(self, request):
+        def add_logout_slug():
+            return {"is_from_logout": "1"}
+
         auth.logout(request)
-        response = HttpResponseRedirect("/")
+
+        # 验证不通过，需要跳转至统一登录平台
+        request.path = "/"
+        handler = ResponseHandler(ConfFixture, settings)
+        handler._build_extra_args = add_logout_slug
+        response = handler.build_401_response(request)
+
+        # 清除 cookies
         cookie_keys = ["bk_ticket", "bk_token", "bk_uid"]
         host = "".join(request.headers["Host"].split(":")[:1])
         domain = f'.{".".join(host.split(".")[1:])}'
