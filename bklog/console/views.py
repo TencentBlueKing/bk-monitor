2b36f89e6aff549ad0ef93a00b287c021b0239b1
jayjiahua <553544693@qq.com>
2024-05-29 16:47:37 +0800

    fix: 修正登出逻辑

--- bklog/console/views.py
+++ bklog/console/views.py
@@ -14,13 +14,4 @@ def user_exit(request):
     request.path = request.path.replace("/console/accounts/logout", "")
     handler = ResponseHandler(ConfFixture, settings)
     handler._build_extra_args = add_logout_slug
-    response = handler.build_401_response(request)
-
-    # 清除 cookies
-    cookie_keys = ["bk_ticket", "bk_token", "bk_uid"]
-    host = "".join(request.headers["Host"].split(":")[:1])
-    domain = f'.{".".join(host.split(".")[1:])}'
-    for _domain in [host, domain]:
-        for key in cookie_keys:
-            response.delete_cookie(key, domain=_domain)
-    return response
+    return handler.build_401_response(request)
