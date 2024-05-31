d8ef9651c9d17226ea45ddc1865eca9e2404094e
v_cwcgcui <1724427771@qq.com>
2024-05-31 15:42:04 +0800

    feat: 检索历史增加收藏来源的记录 #1010158081117730336

--- bklog/apps/log_search/models.py
+++ bklog/apps/log_search/models.py
@@ -700,6 +700,7 @@ class UserIndexSetSearchHistory(SoftDeleteModel):
     index_set_type = models.CharField(
         _("索引集类型"), max_length=32, choices=IndexSetType.get_choices(), default=IndexSetType.SINGLE.value
     )
+    from_favorite_id = models.IntegerField(_("收藏夹ID"), null=True, default=None)
 
     class Meta:
         verbose_name = _("索引集用户检索记录")
