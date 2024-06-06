4004acd4a066dfa526d3ee30b9b30fef7f09a3a8
xlc <2933458919@qq.com>
2024-06-06 14:55:53 +0800

    fix: 【监控】重名校验，是否可以在策略名称失焦时检测

--- bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-set-new/strategy-config-set.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/strategy-config/strategy-config-set-new/strategy-config-set.tsx
@@ -2554,6 +2554,7 @@ export default class StrategyConfigSet extends tsc<IStrategyConfigSetProps, IStr
               title={this.$t('基本信息')}
             >
               <BaseConfig
+                id={this.id && !this.isClone ? this.id : ''}
                 ref='base-config'
                 scenarioReadonly={
                   this.monitorDataEditMode === 'Source' ? false : !!this.metricData.some(item => !!item.metric_id)
