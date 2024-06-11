aa3f07b005a4e27b935f86445b1bc9c5c3f42cf1
liuwenping <haha.rober@gmail.com>
2024-06-11 09:20:18 +0800

    feature: 对 APM 主要访问入口新增用户记录 --story=117882849 (#2039)

--- bkmonitor/packages/apm_web/models/visit_record.py
+++ bkmonitor/packages/apm_web/models/visit_record.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
 Copyright (C) 2017-2022 THL A29 Limited, a Tencent company. All rights reserved.
@@ -8,8 +7,19 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from .application import *  # noqa
-from .profile import *  # noqa
-from .service import *  # noqa
-from .visit_record import *  # noqa
-from .trace import *  # noqa
+
+from django.db import models
+
+
+class UserVisitRecord(models.Model):
+
+    bk_biz_id = models.BigIntegerField("业务 ID")
+    app_name = models.CharField("应用名称", max_length=50, null=True)
+    func_name = models.CharField("功能名称", max_length=128, default="")
+
+    created_at = models.DateTimeField("访问时间", auto_now_add=True, db_index=True)
+    created_by = models.CharField("访问者", max_length=32, default="", db_index=True)
+
+    class Meta:
+        verbose_name = "APM 用户访问记录"
+        verbose_name_plural = "APM 用户访问记录"
