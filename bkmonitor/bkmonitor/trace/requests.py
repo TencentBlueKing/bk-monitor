fab6bf05f81139cd29f8e3320d93d846fd1e23ba
rxwycdh <597701764@qq.com>
2024-06-12 20:29:42 +0800

    fix: 修复框架中状态码判断异常问题

--- bkmonitor/bkmonitor/trace/requests.py
+++ bkmonitor/bkmonitor/trace/requests.py
@@ -10,17 +10,51 @@ specific language governing permissions and limitations under the License.
 """
 import json
 
-from blueapps.opentelemetry.instrumentor import requests_callback
-from opentelemetry.trace import Span
+from opentelemetry.trace import Span, Status, StatusCode
 
 
 def requests_span_callback(span: Span, response):
-    requests_callback(span, response)
+    """将 requests 的请求和返回中的有用信息记录在 span 中"""
     if not response:
         return
 
-    req = response.request
+    try:
+        json_result = response.json()
+    except Exception:  # pylint: disable=broad-except # noqa
+        return
 
+    if not isinstance(json_result, dict):
+        return
+
+    code = json_result.get("code", 0)
+    errors = str(json_result.get("errors", ""))
+    if errors:
+        span.set_attribute("result_errors", errors)
+
+    request_id = (
+        response.headers.get("x-bkapi-request-id")
+        or response.headers.get("x-request-id")
+        or json_result.get("request_id", "")
+    )
+    if request_id:
+        span.set_attribute("request_id", request_id)
+
+    try:
+        code_int = int(code)
+        if code_int == 0 or code // 100 == 2:
+            span.set_status(Status(StatusCode.OK))
+        else:
+            span.set_status(Status(StatusCode.ERROR))
+    except ValueError:
+        if response.status_code // 100 == 2:
+            span.set_status(Status(StatusCode.OK))
+        else:
+            span.set_status(Status(StatusCode.ERROR))
+
+    span.set_attribute("result_code", code)
+    span.set_attribute("result_message", json_result.get("message", ""))
+
+    req = response.request
     body = None
     if req.body:
         try:
