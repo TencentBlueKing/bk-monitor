996df3faa1a42da9fd72ad32b3c5244302fa5b2d
bellke <bellkeyang@tencent.com>
2024-07-04 16:17:46 +0800

    feat: 调整查询计算平台结果表路由方法 --story=118351534 (#2225)

--- bkmonitor/kernel_api/views/v3/meta.py
+++ bkmonitor/kernel_api/views/v3/meta.py
@@ -392,7 +392,7 @@ class QueryDataSourceViewSet(MetaViewSet):
 
 
 class QueryBizByBkBaseViewSet(MetaViewSet):
-    resource_routes = [ResourceRoute("GET", resource.QueryBizByBkBase)]
+    resource_routes = [ResourceRoute("POST", resource.QueryBizByBkBase)]
 
 
 class QueryResultTableStorageDetailViewSet(MetaViewSet):
