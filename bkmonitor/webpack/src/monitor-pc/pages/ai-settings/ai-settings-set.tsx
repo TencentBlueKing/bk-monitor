7473497dfb3f7f0240859144be7c963fe137df57
闪烁 <2199202809@qq.com>
2024-07-09 11:06:49 +0800

    feat: ai设置默认方案ID为0认为没有选择方案

--- bkmonitor/webpack/src/monitor-pc/pages/ai-settings/ai-settings-set.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/ai-settings/ai-settings-set.tsx
@@ -91,7 +91,7 @@ export default class AiSettingsSet extends tsc<object> {
       type: AISettingType.IntelligentDetect,
       title: window.i18n.t('单指标异常检测'),
       data: {
-        default_plan_id: 0,
+        default_plan_id: '',
       },
       errorsMsg: {
         default_plan_id: '',
@@ -105,7 +105,7 @@ export default class AiSettingsSet extends tsc<object> {
           type: 'host',
           title: window.i18n.t('主机'),
           data: {
-            default_plan_id: 0,
+            default_plan_id: '',
             default_sensitivity: 1,
             is_enabled: true,
             exclude_target: [],
@@ -151,8 +151,16 @@ export default class AiSettingsSet extends tsc<object> {
     const aiSetting = await fetchAiSetting().catch(() => null);
     if (aiSetting) {
       this.aiSetting = aiSetting;
-      this.settingsData[0].data.default_plan_id = this.aiSetting.kpi_anomaly_detection.default_plan_id;
-      this.settingsData[1].data[0].data = this.aiSetting.multivariate_anomaly_detection.host;
+      const {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        kpi_anomaly_detection: { default_plan_id },
+        multivariate_anomaly_detection: { host },
+      } = this.aiSetting;
+      this.settingsData[0].data.default_plan_id = default_plan_id || '';
+      this.settingsData[1].data[0].data = {
+        ...host,
+        default_plan_id: host.default_plan_id || '',
+      };
     }
   }
 
