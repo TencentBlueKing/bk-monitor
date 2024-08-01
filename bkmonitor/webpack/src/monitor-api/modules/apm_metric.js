3514666254f7019b7bf0a695121fb571c613bfaf
unique0lai <11598235+unique0lai@users.noreply.github.com>
2024-08-01 15:25:45 +0800

    "fix: resource js api生成问题处理 #1010158081128272789"

--- bkmonitor/webpack/src/monitor-api/modules/apm_metric.js
+++ bkmonitor/webpack/src/monitor-api/modules/apm_metric.js
@@ -1,39 +1,39 @@
 import { request } from '../base';
 
 export const errorListByTraceIds = request('POST', 'apm/metric/metric_event/error_list_by_trace_ids/');
-export const unifyQuery = request('POST', 'apm/metric/metric/unify_query/');
-export const dynamicUnifyQuery = request('POST', 'apm/metric/metric/dynamic_unify_query/');
 export const serviceList = request('POST', 'apm/metric/metric/service_list/');
-export const serviceListAsync = request('POST', 'apm/metric/metric/service_list_async/');
-export const topNQuery = request('POST', 'apm/metric/metric/top_n_query/');
-export const apdexQuery = request('GET', 'apm/metric/metric/apdex_query/');
-export const alertQuery = request('GET', 'apm/metric/metric/alert_query/');
-export const instanceList = request('POST', 'apm/metric/metric/instance_list/');
 export const serviceInstances = request('POST', 'apm/metric/metric/service_instances/');
 export const errorList = request('POST', 'apm/metric/metric/error_list/');
-export const collectService = request('POST', 'apm/metric/metric/collect_service/');
 export const endpointList = request('POST', 'apm/metric/metric/endpoint_list/');
+export const hostInstanceDetailList = request('POST', 'apm/metric/metric/host_instance_detail_list/');
+export const apdexQuery = request('GET', 'apm/metric/metric/apdex_query/');
+export const alertQuery = request('GET', 'apm/metric/metric/alert_query/');
+export const unifyQuery = request('POST', 'apm/metric/metric/unify_query/');
+export const dynamicUnifyQuery = request('POST', 'apm/metric/metric/dynamic_unify_query/');
+export const serviceListAsync = request('POST', 'apm/metric/metric/service_list_async/');
+export const topNQuery = request('POST', 'apm/metric/metric/top_n_query/');
+export const instanceList = request('POST', 'apm/metric/metric/instance_list/');
+export const collectService = request('POST', 'apm/metric/metric/collect_service/');
 export const endpointDetailList = request('POST', 'apm/metric/metric/endpoint_detail_list/');
 export const exceptionDetailList = request('POST', 'apm/metric/metric/exception_detail_list/');
-export const hostInstanceDetailList = request('POST', 'apm/metric/metric/host_instance_detail_list/');
 export const serviceQueryException = request('POST', 'apm/metric/metric/service_query_exception/');
 
 export default {
   errorListByTraceIds,
-  unifyQuery,
-  dynamicUnifyQuery,
   serviceList,
-  serviceListAsync,
-  topNQuery,
-  apdexQuery,
-  alertQuery,
-  instanceList,
   serviceInstances,
   errorList,
-  collectService,
   endpointList,
+  hostInstanceDetailList,
+  apdexQuery,
+  alertQuery,
+  unifyQuery,
+  dynamicUnifyQuery,
+  serviceListAsync,
+  topNQuery,
+  instanceList,
+  collectService,
   endpointDetailList,
   exceptionDetailList,
-  hostInstanceDetailList,
   serviceQueryException,
 };
