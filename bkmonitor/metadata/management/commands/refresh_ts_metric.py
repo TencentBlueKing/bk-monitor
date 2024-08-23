34a92f31ae936a1b7591c1d33b9da69fa5359b55
EASYGOING45 <1505674928@qq.com>
2024-08-23 16:47:30 +0800

    feat: Metadata:重构刷新指标命令，支持根据data_id、table_id以及全量强制刷新 --story=119339776

--- bkmonitor/metadata/management/commands/refresh_ts_metric.py
+++ bkmonitor/metadata/management/commands/refresh_ts_metric.py
@@ -11,23 +11,57 @@ specific language governing permissions and limitations under the License.
 from django.core.management.base import BaseCommand, CommandError
 
 from metadata import models
+from metadata.models.space.space_table_id_redis import SpaceTableIDRedis
 
 
 class Command(BaseCommand):
     help = "refresh time series metric"
 
     def add_arguments(self, parser):
-        parser.add_argument("--data_id", type=int, required=True, help="data source id")
+        parser.add_argument("--data_id", type=int, required=False, help="data source id or table_id")
+        parser.add_argument("--table_id", type=str, required=False, help="table id")
+        parser.add_argument("--force", action="store_true", help="force refresh")
 
     def handle(self, *args, **options):
         data_id = options.get("data_id")
-        # 获取ts group
-        try:
-            ts_group = models.TimeSeriesGroup.objects.get(bk_data_id=data_id)
-        except models.TimeSeriesGroup.DoesNotExist:
-            raise CommandError("data id not found from TimeSeriesGroup")
-        # 从redis中同步数据到metadata
-        ts_group.update_time_series_metrics()
-        # TODO: 更新上游缓存或上游DB
+        table_id = options.get("table_id")
+        force = options.get("force")
+        client = SpaceTableIDRedis()
+        if data_id:
+            try:
+                self.stdout.write("data id: %s start to refresh metric router" % data_id)
+                ts_group = models.TimeSeriesGroup.objects.get(bk_data_id=data_id)
+                ts_group.update_time_series_metrics()
+                self.stdout.write("data id: %s start to push redis data" % data_id)
+                client.push_table_id_detail(table_id_list=[ts_group.table_id], is_publish=True)
+                self.stdout.write("data id: %s refresh metric router successfully" % data_id)
+                return
+            except models.TimeSeriesGroup.DoesNotExist:
+                raise CommandError("data id not found from TimeSeriesGroup")
 
-        print("update time series metric successfully")
+        if table_id:
+            try:
+                self.stdout.write("table id: %s start to refresh metric router" % table_id)
+                ts_group = models.TimeSeriesGroup.objects.get(table_id=table_id)
+                ts_group.update_time_series_metrics()
+                self.stdout.write("table id: %s start to push redis data" % table_id)
+                client.push_table_id_detail(table_id_list=[ts_group.table_id], is_publish=True)
+                self.stdout.write("table id: %s refresh metric router successfully" % table_id)
+                return
+            except models.TimeSeriesGroup.DoesNotExist:
+                raise CommandError("table id not found from TimeSeriesGroup")
+
+        if force:
+            self.stdout.write("start to force refresh metric router")
+            tid_list = []
+            for ts_group in models.TimeSeriesGroup.objects.all():
+                try:
+                    ts_group.update_time_series_metrics()
+                    tid_list.append(ts_group.table_id)
+                except models.TimeSeriesGroup.DoesNotExist:
+                    self.stdout.write("table id: %s refresh metric router failed" % ts_group.table_id)
+                    continue
+
+            client.push_table_id_detail(table_id_list=tid_list, is_publish=True)
+
+        self.stdout.write("update time series metric successfully")
