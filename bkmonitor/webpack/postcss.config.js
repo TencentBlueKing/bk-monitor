475f0288190f732c5593681ab1df81a2f1c57f4d
liang ling <lingliang1100@gmail.com>
2024-08-28 21:03:55 +0800

    fix: tailwindcss初始化样式影响部分icon展示 --Bug=129803939 (#2689)
    # Reviewed, transaction id: 16773

--- bkmonitor/webpack/postcss.config.js
+++ bkmonitor/webpack/postcss.config.js
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('tailwindcss/nesting'),
+    // require('tailwindcss'),
+    // require('tailwindcss/nesting'),
     require('postcss-preset-env')({
       stage: 2,
       features: {
