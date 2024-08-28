81efd14c156d88c71dc04355b006aa68df980295
jayjiahua <553544693@qq.com>
2024-08-28 20:09:52 +0800

    fix: 修改模型逻辑修复

--- bklog/apps/log_clustering/components/collections/flow_component.py
+++ bklog/apps/log_clustering/components/collections/flow_component.py
@@ -319,7 +319,7 @@ class UpdateOnlineModelService(BaseService):
         clustering_config = ClusteringConfig.get_by_index_set_id(index_set_id=index_set_id)
         if clustering_config.predict_flow_id:
             # 需要更新 flow中的预测节点 及 更新在线训练任务 训练参数的变动
-            DataFlowHandler().update_predict_nodes_and_online_tasks(index_set_id=self.index_set_id)
+            DataFlowHandler().update_predict_nodes_and_online_tasks(index_set_id=index_set_id)
         return True
 
 
