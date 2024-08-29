86983a65d7eabddd64a3df0f15bf07af67bbcdce
chenguo <chenguo367@163.com>
2024-08-29 15:53:12 +0800

    fix: 研发空间刷新页面弹框修复

--- bkmonitor/packages/monitor_web/commons/biz/space_api.py
+++ bkmonitor/packages/monitor_web/commons/biz/space_api.py
@@ -52,7 +52,7 @@ class InjectSpaceApi(space_api.AbstractSpaceApi):
             raise ValidationError(_("参数[space_uid]、和[id]不能同时为空"))
         # 尝试从缓存获取, 解决 bkcc 业务层面快速获取空间信息的场景
         ret: List[SpaceDefine] = local_mem.get("metadata:spaces_map", None)
-        if ret is not None and "space_uid" in params:
+        if ret is not None and "space_uid" in params and params["space_uid"].startswith("bkcc"):
             space = ret.get(params["space_uid"])
             if space is not None:
                 return space
