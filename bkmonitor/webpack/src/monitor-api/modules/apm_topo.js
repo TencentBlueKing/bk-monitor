f19ad057333142ea8578133522c03b0d289cd398
rxwycdh <597701764@qq.com>
2024-09-02 10:21:47 +0800

    fix: 提交 api

--- bkmonitor/webpack/src/monitor-api/modules/apm_topo.js
+++ bkmonitor/webpack/src/monitor-api/modules/apm_topo.js
@@ -2,12 +2,14 @@ import { request } from '../base';
 
 export const dataTypeBarQuery = request('GET', 'apm/topo/global/bar/');
 export const topoView = request('GET', 'apm/topo/global/topo/');
+export const nodeEndpointsTop = request('GET', 'apm/topo/global/topo/node/endpoints/');
 export const nodeRelation = request('GET', 'apm/topo/global/relation/');
 export const nodeRelationDetail = request('POST', 'apm/topo/global/relation/detail/');
 
 export default {
   dataTypeBarQuery,
   topoView,
+  nodeEndpointsTop,
   nodeRelation,
   nodeRelationDetail,
 };
