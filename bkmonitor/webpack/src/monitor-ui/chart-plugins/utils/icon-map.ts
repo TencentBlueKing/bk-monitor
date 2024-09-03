816f542ac5413c36c135d972a26be4c7c8a55f52
liangling0628 <lingliang1100@gmail.com>
2024-09-03 15:39:26 +0800

    feat: apm 资源topo图 # Reviewed, transaction id: 17220

--- bkmonitor/webpack/src/monitor-ui/chart-plugins/utils/icon-map.ts
+++ bkmonitor/webpack/src/monitor-ui/chart-plugins/utils/icon-map.ts
@@ -23,7 +23,6 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-
 export const NODE_TYPE_ICON = {
   // node
   apm_service: 'icon-mc-apm_service',
