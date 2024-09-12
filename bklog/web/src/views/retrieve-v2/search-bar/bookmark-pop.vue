0cfff4b1194c7a5613a1cd54cb0acdaab34d9a99
israelli <li18566230175@gmail.com>
2024-09-12 10:57:17 +0800

    feat: bookmark-pop数据处理 # Reviewed, transaction id: 18208

--- bklog/web/src/views/retrieve-v2/search-bar/bookmark-pop.vue
+++ bklog/web/src/views/retrieve-v2/search-bar/bookmark-pop.vue
@@ -1,4 +1,28 @@
-<script setup></script>
+<script setup>
+  import { computed } from 'vue';
+  import useStore from '@/hooks/use-store';
+
+  const props = defineProps({
+    sql: {
+      default: '',
+      type: String,
+      required: true,
+    },
+  });
+
+  const store = useStore();
+
+  // 用于展示索引集
+  // 这里返回数组，展示 index_set_name 字段
+  const indexSetItemList = computed(() => store.state.indexItem.items);
+  const collectGroupList = computed(() => [{ id: 'group1', name: '分组1' }]);
+
+  // 新建提交逻辑
+  const handleCreateRequest = () => {
+
+  }
+
+</script>
 <template>
   <span class="disabled bklog-icon bklog-star-line"></span>
 </template>
