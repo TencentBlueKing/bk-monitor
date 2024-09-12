5f3657391430a50e3dabc1bc7fb998065e64df15
dengyh <dengyh071@gmail.com>
2024-09-12 16:57:45 +0800

    refactor: 兼容不适用django cache作为logo缓存的情况 --story=119666122 (#2939)

--- bkmonitor/bkmonitor/commons/storage.py
+++ bkmonitor/bkmonitor/commons/storage.py
@@ -9,6 +9,7 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 from bkstorages.backends.bkrepo import BKGenericRepoClient, BKRepoStorage
+from django.conf import settings
 from django.core.cache import cache
 
 from constants.common import LOGO_IMAGE_TIMEOUT
@@ -60,3 +61,10 @@ class BKCacheRepoStorage(BKRepoStorage):
             username=self.client.username,
             password=self.client.password,
         )
+
+
+def get_default_image_storage():
+    if getattr(settings, "USE_CEPH", False):
+        return BKCacheRepoStorage
+
+    return None
