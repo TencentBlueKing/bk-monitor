861369fdcd91bfee27bc1a00241fd4a55c2fe1df
goodGai <1398281274@qq.com>
2024-09-25 18:09:08 +0800

    feat: 【产品功能】监控图表选中单个图表时不展示对比 --Story=119860071 # Reviewed, transaction id: 19408

--- bkmonitor/webpack/src/monitor-pc/constant/constant.ts
+++ bkmonitor/webpack/src/monitor-pc/constant/constant.ts
@@ -40,6 +40,7 @@ export const THRESHOLD_METHOD_LIST = [
 // 监控条件方法列表
 export const CONDITION_METHOD_LIST = [
   { id: 'eq', name: '=' },
+  { id: 'contains', name: '=' }, // 存在部分脏数据 等价 eq
   { id: 'gt', name: '>' },
   { id: 'gte', name: '>=' },
   { id: 'lt', name: '<' },
