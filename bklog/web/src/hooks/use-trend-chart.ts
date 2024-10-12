90fb984b868f9629c17ba3ee1c520a9062a35a00
israelli <li18566230175@gmail.com>
2024-10-12 14:48:34 +0800

    feat: 拆分趋势图渲染逻辑

---
+++ bklog/web/src/hooks/use-trend-chart.ts
@@ -0,0 +1,41 @@
+import { onMounted, Ref } from "vue"
+import * as echarts from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent
+} from 'echarts/components';
+// 标签自动布局、全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer
+]);
+
+
+export type TrandChartOption = {
+  target: Ref<HTMLElement | null>
+}
+
+export default ({ target }: TrandChartOption) => {
+  let chartInstance = null;
+  onMounted(() => {
+    if (target.value) {
+      chartInstance = echarts.init()
+    }
+  });
+}
