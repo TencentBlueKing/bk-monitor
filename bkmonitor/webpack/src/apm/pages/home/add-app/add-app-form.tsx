082721e49db30b4e25ada03570a7592badf6613b
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2024-10-17 20:05:05 +0800

    fix: 创建应用表单优化 (#3424)

--- bkmonitor/webpack/src/apm/pages/home/add-app/add-app-form.tsx
+++ bkmonitor/webpack/src/apm/pages/home/add-app/add-app-form.tsx
@@ -23,7 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { Component, Ref } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 
 import { checkDuplicateName, createApplication } from 'monitor-api/modules/apm_meta';
@@ -32,12 +32,6 @@ import { ETelemetryDataType } from '../../application/app-configuration/type';
 
 import './add-app-form.scss';
 
-interface FormData {
-  ID: string;
-  name: string;
-  desc: string;
-}
-
 interface IProps {
   onCancel?: () => void;
   onSuccess?: (v: string) => void;
@@ -45,7 +39,6 @@ interface IProps {
 
 @Component
 export default class AddAppForm extends tsc<IProps> {
-  @Ref('addForm') addFormRef: any;
   list = [
     {
       id: ETelemetryDataType.metric,
@@ -75,68 +68,94 @@ export default class AddAppForm extends tsc<IProps> {
     },
   ];
 
-  formData: FormData | Record<string, boolean> = {
-    ID: '',
-    name: '',
-    desc: '',
+  formData = {
+    appName: '',
+    appAlias: '',
+    description: '',
     [ETelemetryDataType.metric]: true,
     [ETelemetryDataType.log]: true,
     [ETelemetryDataType.trace]: true,
     [ETelemetryDataType.profiling]: false,
   };
-  rules = {
-    ID: [
-      {
-        required: true,
-        validator: val => val.length >= 1 && val.length <= 50,
-        message: window.i18n.tc('输入1-50个字符'),
-        trigger: 'blur',
-      },
-      {
-        validator: val => /^[a-z0-9_-]+$/.test(val),
-        message: window.i18n.t('仅支持小写字母、数字、_- 中任意一条件即可'),
-        trigger: ['change', 'blur'],
-      },
-    ],
-    name: [
-      {
-        validator: val => val.length >= 1 && val.length <= 50,
-        message: window.i18n.t('输入1-50个字符'),
-        trigger: 'blur',
-      },
-      {
-        validator: this.handleCheckDuplicateName,
-        message: window.i18n.tc('注意: 名字冲突'),
-        trigger: ['blur'],
-      },
-    ],
+  formDataErrMsg = {
+    appName: '',
+    appAlias: '',
   };
   saveLoading = false;
   saveToServiceLoading = false;
-
   isVerify = false;
-  isCheckDuplicateName = false;
+
+  initForm() {
+    this.formData = {
+      appName: '',
+      appAlias: '',
+      description: '',
+      [ETelemetryDataType.metric]: true,
+      [ETelemetryDataType.log]: true,
+      [ETelemetryDataType.trace]: true,
+      [ETelemetryDataType.profiling]: false,
+    };
+  }
+
+  emojiRegex(value: string) {
+    return /(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]/g.test(value);
+  }
 
   /** 检查 应用名 是否重名 */
   async handleCheckDuplicateName(val: string) {
     const pass = await checkDuplicateName({ app_name: val })
       .then(data => !data.exists)
       .catch(() => false);
-    this.isCheckDuplicateName = pass;
     return pass;
   }
 
-  initForm() {
-    this.formData = {
-      ID: '',
-      name: '',
-      desc: '',
-      [ETelemetryDataType.metric]: true,
-      [ETelemetryDataType.log]: true,
-      [ETelemetryDataType.trace]: true,
-      [ETelemetryDataType.profiling]: false,
-    };
-    this.addFormRef?.clearError?.();
+  commonRule(value) {
+    if (!value) {
+      return this.$tc('必填项');
+    }
+    if (this.emojiRegex(value)) {
+      return this.$tc('不能输入emoji表情');
+    }
+    if (!(value.length >= 1 && value.length <= 50)) {
+      return window.i18n.tc('输入1-50个字符');
+    }
+    if (!/^[a-z0-9_-]+$/.test(value)) {
+      return window.i18n.t('仅支持小写字母、数字、_- 中任意一条件即可');
+    }
+  }
+
+  clearErrorMsg() {
+    for (const key in this.formDataErrMsg) {
+      this.formDataErrMsg[key] = '';
+    }
+  }
+  isVerifyFn() {
+    let is = true;
+    for (const key in this.formDataErrMsg) {
+      if (this.formDataErrMsg[key]) {
+        is = false;
+        break;
+      }
+    }
+    return is;
+  }
+  async handleBlur(key) {
+    const value = this.formData[key];
+    let errMsg = this.commonRule(value);
+    if (key === 'appName' && !this.formDataErrMsg[key] && value) {
+      const pass = await this.handleCheckDuplicateName(value);
+      if (!pass) {
+        errMsg = this.$t('应用名已存在');
+      }
+    }
+    this.formDataErrMsg[key] = errMsg;
+    this.isVerify = this.isVerifyFn();
+  }
+
+  async validateForm() {
+    await this.handleBlur('appName');
+    await this.handleBlur('appAlias');
+    this.isVerify = this.isVerifyFn();
   }
 
   /* 保存 */
@@ -146,46 +165,44 @@ export default class AddAppForm extends tsc<IProps> {
     } else {
       this.saveLoading = true;
     }
-
-    const isPass = await this.addFormRef?.validate().catch(() => false);
-    setTimeout(async () => {
-      this.isVerify = isPass && this.isCheckDuplicateName;
-      if (isPass && this.isCheckDuplicateName) {
-        // 保存接口
-        const params = {
-          app_name: this.formData.ID,
-          app_alias: this.formData.name,
-          description: this.formData.desc,
-          enabled_profiling: this.formData[ETelemetryDataType.profiling],
-          enabled_trace: this.formData[ETelemetryDataType.trace],
-          enabled_metric: this.formData[ETelemetryDataType.metric],
-          enabled_log: this.formData[ETelemetryDataType.log],
-          es_storage_config: null,
-        };
-        const res = await createApplication(params)
-          .then(() => true)
-          .catch(() => false);
-        if (res) {
-          this.$bkMessage({
-            theme: 'success',
-            message: this.$t('保存成功'),
-          });
-          this.$emit('success', params.app_name as string);
-          if (isAccess) {
-            // 跳转到接入服务页面
-            this.$router.push({
-              name: 'service-add',
-              params: {
-                appName: params.app_name as string,
-              },
-            });
-          }
-          this.initForm();
-        }
-      }
+    await this.validateForm();
+    if (!this.isVerifyFn()) {
       this.saveLoading = false;
       this.saveToServiceLoading = false;
-    }, 200);
+      return;
+    }
+    const params = {
+      app_name: this.formData.appName,
+      app_alias: this.formData.appAlias,
+      description: this.formData.description,
+      enabled_profiling: this.formData[ETelemetryDataType.profiling],
+      enabled_trace: this.formData[ETelemetryDataType.trace],
+      enabled_metric: this.formData[ETelemetryDataType.metric],
+      enabled_log: this.formData[ETelemetryDataType.log],
+      es_storage_config: null,
+    };
+    const res = await createApplication(params)
+      .then(() => true)
+      .catch(() => false);
+    if (res) {
+      this.$bkMessage({
+        theme: 'success',
+        message: this.$t('保存成功'),
+      });
+      this.$emit('success', params.app_name as string);
+      if (isAccess) {
+        // 跳转到接入服务页面
+        this.$router.push({
+          name: 'service-add',
+          params: {
+            appName: params.app_name as string,
+          },
+        });
+      }
+      this.initForm();
+    }
+    this.saveLoading = false;
+    this.saveToServiceLoading = false;
   }
 
   handleCancel() {
@@ -193,87 +210,82 @@ export default class AddAppForm extends tsc<IProps> {
     this.$emit('cancel');
   }
 
-  async handleBlur() {
-    const isPass = await this.addFormRef?.validate().catch(() => false);
-    setTimeout(() => {
-      this.isVerify = isPass && this.isCheckDuplicateName;
-    }, 200);
+  formItem(label: any | string, content: any, cls = '', err = '') {
+    return (
+      <div class={['form-item', cls]}>
+        <span class={['form-item-label']}>{label}</span>
+        <div>
+          <span class='form-item-content'>{content}</span>
+          {!!err && <div class='err-msg'>{err}</div>}
+        </div>
+      </div>
+    );
   }
-
   render() {
     return (
       <div class='add-app-form-component'>
-        <bk-form
-          ref='addForm'
-          class='form-wrap'
-          form-type='vertical'
-          {...{
-            props: {
-              model: this.formData,
-              rules: this.rules,
-            },
-          }}
-        >
-          <bk-form-item
-            class='cluster-select-item input-width'
-            error-display-type='normal'
-            label={this.$t('应用名')}
-            property='ID'
-            required
-          >
-            <bk-input
-              class='input input-width'
-              v-model={this.formData.ID}
-              maxlength={50}
-              placeholder={this.$t('1-50字符，由小写字母、数字、下划线(_)、中划线(-)组成')}
-              onBlur={() => this.handleBlur()}
-            />
-          </bk-form-item>
-          <bk-form-item
-            error-display-type='normal'
-            label={this.$t('应用别名')}
-            property='name'
-            required
-          >
-            <bk-input
-              class='input input-width'
-              v-model={this.formData.name}
-              placeholder={this.$t('1-50字符')}
-              onBlur={() => this.handleBlur()}
-            />
-          </bk-form-item>
-          <bk-form-item label={this.$t('描述')}>
-            <bk-input
-              class='input input-width'
-              v-model={this.formData.desc}
-              maxlength='100'
-              type='textarea'
-            />
-          </bk-form-item>
-          <bk-form-item label={this.$t('数据上报')}>
-            {this.list.map(item => (
-              <div
-                key={item.id}
-                class='report-type-wrap'
-              >
-                <div class='report-left-content'>
-                  <i class={['icon-monitor', item.icon]} />
-                </div>
-                <div class='report-middle-content'>
-                  <span class='middle-content-title'>{item.title}</span>
-                  <span class='middle-content-text'>{item.content}</span>
-                </div>
-                <div class='report-right-content'>
-                  <bk-switcher
-                    v-model={this.formData[item.id]}
-                    size='small'
-                    theme='primary'
-                  />
-                </div>
+        {this.formItem(
+          this.$t('应用名'),
+          <bk-input
+            class='input input-width'
+            v-model={this.formData.appName}
+            placeholder={this.$t('1-50字符')}
+            onBlur={() => this.handleBlur('appName')}
+            onFocus={() => this.clearErrorMsg()}
+          />,
+          'required mb-24',
+          this.formDataErrMsg.appName
+        )}
+        {this.formItem(
+          this.$t('应用别名'),
+          <bk-input
+            class='input input-width'
+            v-model={this.formData.appAlias}
+            placeholder={this.$t('1-50字符')}
+            onBlur={() => this.handleBlur('appAlias')}
+            onFocus={() => this.clearErrorMsg()}
+          />,
+          'required mb-24',
+          this.formDataErrMsg.appAlias
+        )}
+        {this.formItem(
+          this.$t('描述'),
+          <bk-input
+            class='input input-width'
+            v-model={this.formData.description}
+            maxlength='100'
+            type='textarea'
+          />,
+          'mb-24'
+        )}
+        {this.formItem(
+          this.$t('上报类型'),
+          this.list.map(item => (
+            <div
+              key={item.id}
+              class='report-type-wrap'
+            >
+              <div class='report-left-content'>
+                <i class={['icon-monitor', item.icon]} />
               </div>
-            ))}
-          </bk-form-item>
-          <bk-form-item>
+              <div class='report-middle-content'>
+                <span class='middle-content-title'>{item.title}</span>
+                <span class='middle-content-text'>{item.content}</span>
+              </div>
+              <div class='report-right-content'>
+                <bk-switcher
+                  v-model={this.formData[item.id]}
+                  size='small'
+                  theme='primary'
+                />
+              </div>
+            </div>
+          )),
+          'mb-24'
+        )}
+        {this.formItem(
+          '',
+          <div>
             <bk-button
               class='mr-8'
               disabled={!this.isVerify}
@@ -293,8 +305,8 @@ export default class AddAppForm extends tsc<IProps> {
               {this.$t('保存并接入服务')}
             </bk-button>
             <bk-button onClick={this.handleCancel}>{this.$t('取消')}</bk-button>
-          </bk-form-item>
-        </bk-form>
+          </div>
+        )}
       </div>
     );
   }
