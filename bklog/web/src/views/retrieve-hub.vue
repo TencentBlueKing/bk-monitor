6e36d276121226491dd5a51eb45880ab8d1be2a3
israelli <li18566230175@gmail.com>
2024-10-23 16:25:58 +0800

    fix: 切换版本跟随session

--- bklog/web/src/views/retrieve-hub.vue
+++ bklog/web/src/views/retrieve-hub.vue
@@ -10,33 +10,12 @@
   const route = useRoute();
 
   const version = sessionStorage.getItem('retrieve_version') ?? 'v2';
-  // const version = computed(() => store.state.retrieve.activeVersion);
 
   const RetrieveComponent = computed(() => {
     if (route.name === 'retrieve') {
       if (version === 'v1') {
         return retrieve;
       }
-
-      // const isDebug = window.FEATURE_TOGGLE.bklog_search_new === 'debug';
-      // const isOn = window.FEATURE_TOGGLE.bklog_search_new === 'on';
-
-      // if (isDebug) {
-      //   const whiteList = (window.FEATURE_TOGGLE_WHITE_LIST?.bklog_search_new ?? []).map(id => `${id}`);
-      //   const spaceWhiteList = window.SPACE_UID_WHITE_LIST?.bklog_search_new ?? [];
-
-      //   const bkBizId = route.query.bizId;
-      //   const spaceUid = route.query.spaceUid;
-      //   if ((bkBizId && whiteList.includes(bkBizId)) || spaceWhiteList.includes(spaceUid)) {
-      //     return retrieveV2;
-      //   }
-      // }
-
-      // if (isOn) {
-      //   return retrieveV2;
-      // }
-
-      // return retrieveV2;
     }
 
     return retrieveV2;
