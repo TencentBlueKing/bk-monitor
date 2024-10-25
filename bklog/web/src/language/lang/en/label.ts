7125e18c6bc3fa97d34058d13be79b3686fb8edd
LeeXT <xintaoLi@users.noreply.github.com>
2024-10-25 16:26:20 +0800

    feat: 改造采集下发的步骤条逻辑为抽屉展示需求 (#3565)
    Co-authored-by: chenmanting1 <49743833+chenmanting1@users.noreply.github.com>

--- bklog/web/src/language/lang/en/label.ts
+++ bklog/web/src/language/lang/en/label.ts
@@ -706,6 +706,9 @@ export default {
   可见范围设置: 'Visible range settings',
   无: 'None',
   不分词: 'No word separation',
+  秒: 'Second',
+  '已耗时{0}秒': '{0} seconds have passed',
+  '正在下发...': 'Issuing...',
   回到旧版: 'Old Version',
   切换新版: 'New Version',
 };
