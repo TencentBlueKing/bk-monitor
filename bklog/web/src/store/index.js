103d1cf111404ceb2464e7076b606dbe32f51ae9
LeeXT <xintaoLi@users.noreply.github.com>
2024-10-28 10:40:39 +0800

    fix: 修复日志下载字段配置丢失问题 & url解析时间格式问题 (#3574)

--- bklog/web/src/store/index.js
+++ bklog/web/src/store/index.js
@@ -971,8 +971,16 @@ const store = new Vuex.Store({
       // 每次请求这里需要根据选择日期时间这里计算最新的timestamp
       // 最新的 start_time, end_time 也要记录下来，用于字段统计时，保证请求的参数一致
       const { datePickerValue } = state.indexItem;
-      const [start_time, end_time] = handleTransformToTimestamp(datePickerValue);
-      commit('updateIndexItem', { start_time, end_time });
+      const letterRegex = /[a-zA-Z]/;
+      const needTransform = datePickerValue.every(d => letterRegex.test(d));
+
+      const [start_time, end_time] = needTransform
+        ? handleTransformToTimestamp(datePickerValue)
+        : [state.indexItem.start_time, state.indexItem.end_time];
+
+      if (needTransform) {
+        commit('updateIndexItem', { start_time, end_time });
+      }
 
       if (!payload?.isPagination && payload.formChartChange) {
         store.commit('retrieve/updateChartKey');
@@ -1324,6 +1332,8 @@ const store = new Vuex.Store({
     },
     requestSearchTotal({ state, getters }) {
       state.searchTotal = 0;
+      const start_time = Math.floor(getters.retrieveParams.start_time);
+      const end_time = Math.ceil(getters.retrieveParams.end_time);
       http
         .request(
           'retrieve/fieldStatisticsTotal',
@@ -1332,6 +1342,8 @@ const store = new Vuex.Store({
               ...getters.retrieveParams,
               bk_biz_id: state.bkBizId,
               index_set_ids: state.indexItem.ids,
+              start_time,
+              end_time,
             },
           },
           {
