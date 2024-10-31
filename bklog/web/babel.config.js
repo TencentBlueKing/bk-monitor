5c26df7169caf8aac3bdb41c8c0d7edae2737917
liangling0628 <lingliang1100@gmail.com>
2024-10-31 20:03:26 +0800

    feat: 监控APM日志功能开发 # Reviewed, transaction id: 22485

--- bklog/web/babel.config.js
+++ bklog/web/babel.config.js
@@ -27,18 +27,18 @@
 module.exports = function (api) {
   api?.cache.never();
   const presets = [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
-          node: 'current',
-        },
-        useBuiltIns: 'usage',
-        corejs: 3,
-        debug: false,
-      },
-    ],
+    // [
+    //   '@babel/preset-env',
+    //   {
+    //     targets: {
+    //       browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
+    //       node: 'current',
+    //     },
+    //     useBuiltIns: 'usage',
+    //     corejs: 3,
+    //     debug: false,
+    //   },
+    // ],
     '@vue/babel-preset-jsx',
   ];
   const plugins = [
