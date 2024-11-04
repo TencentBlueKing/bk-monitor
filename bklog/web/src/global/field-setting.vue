e10aeb2d7c51ecde661c6ffb9f36d842c3c085bb
chenmanting1 <49743833+chenmanting1@users.noreply.github.com>
2024-11-04 16:39:27 +0800

    feat: 新增索引集信息快速展示需求 (#3707)

---
+++ bklog/web/src/global/field-setting.vue
@@ -0,0 +1,404 @@
+<template>
+  <div>
+    <div
+      @click="handleOpenSidebar"
+      class="field-setting-wrap"
+    >
+      <span class="bklog-icon bklog-setting"></span>{{ t('字段配置') }}
+    </div>
+    <bk-sideslider
+      :is-show.sync="showSlider"
+      :quick-close="true"
+      :title="$t('字段配置')"
+      :width="640"
+    >
+      <div slot="header">
+        {{ t('字段配置') }}
+        <bk-button
+          class="mt10 fr"
+          data-test-id="fieldSettingSlider_button_edit"
+          theme="default"
+          @click="handleEdit"
+        >
+          {{ isEdit ? $t('取消') : $t('编辑') }}
+        </bk-button>
+      </div>
+      <template #content>
+        <div
+          class="king-slider-content"
+          v-bkloading="{ isLoading: sliderLoading }"
+        >
+          <bk-form
+            v-if="!sliderLoading"
+            ref="validateForm"
+            class="king-form"
+            :label-width="100"
+            :model="formData"
+            :rules="basicRules"
+          >
+            <div class="add-collection-title">{{ $t('基础信息') }}</div>
+            <bk-form-item
+              ext-cls="en-bk-form"
+              :icon-offset="120"
+              :label="$t('采集名')"
+              :property="'collector_config_name'"
+              :required="isEdit || isEditConfigName"
+              :rules="basicRules.collector_config_name"
+            >
+              <bk-input
+                v-if="isEdit || isEditConfigName"
+                class="w520"
+                v-model="formData.collector_config_name"
+                maxlength="50"
+                show-word-limit
+                @blur="isEditConfigName = false"
+              >
+              </bk-input>
+              <div v-else>
+                {{ formData.collector_config_name }}
+                <i
+                  :class="['bk-icon icon-edit-line icons']"
+                  @click="isEditConfigName = true"
+                ></i>
+              </div>
+            </bk-form-item>
+            <bk-form-item
+              ext-cls="en-bk-form"
+              :icon-offset="120"
+              :label="$t('数据名')"
+              :property="'collector_config_name_en'"
+              :required="isEdit"
+              :rules="basicRules.collector_config_name_en"
+            >
+              <div class="en-name-box">
+                <bk-input
+                  v-if="isEdit"
+                  class="w520"
+                  v-model="formData.collector_config_name_en"
+                  disabled
+                  show-word-limit
+                >
+                </bk-input>
+                <div v-else>{{ formData.collector_config_name_en }}</div>
+              </div>
+            </bk-form-item>
+            <bk-form-item
+              ext-cls="en-bk-form"
+              :label="$t('数据ID')"
+              :property="'bk_data_id'"
+              :required="isEdit"
+            >
+              <bk-input
+                v-if="isEdit"
+                class="form-input"
+                v-model="formData.bk_data_id"
+                disabled
+              >
+              </bk-input>
+              <div v-else>{{ formData.bk_data_id }}</div>
+            </bk-form-item>
+            <div class="add-collection-title">{{ $t('存储配置') }}</div>
+            <bk-form-item
+              ext-cls="en-bk-form"
+              :label="$t('集群名称')"
+              :property="'storage_cluster_name'"
+              :required="isEdit"
+            >
+              <bk-input
+                v-if="isEdit"
+                v-model="formData.storage_cluster_name"
+              >
+              </bk-input>
+              <div v-else>{{ formData.storage_cluster_name }}</div>
+            </bk-form-item>
+            <bk-form-item
+              ext-cls="en-bk-form"
+              :label="$t('日志保存天数')"
+              :property="'retention'"
+              :required="isEdit || isEditRetention"
+            >
+              <bk-input
+                v-if="isEdit || isEditRetention"
+                v-model="formData.retention"
+                @blur="isEditRetention = false"
+              >
+                <template slot="append">
+                  <div>{{ $t('天') }}</div>
+                </template>
+              </bk-input>
+              <div v-else>
+                {{ formData.retention }}
+                <i
+                  :class="['bk-icon icon-edit-line icons']"
+                  @click="isEditRetention = true"
+                ></i>
+              </div>
+            </bk-form-item>
+            <div class="add-collection-title">{{ $t('索引配置') }}</div>
+            <div class="setting-title">{{ $t('原始日志配置') }}</div>
+            <setting-table
+              v-if="formData.etl_params.retain_original_text"
+              ref="originfieldTable"
+              :table-type="'originLog'"
+              :is-preview-mode="!isEdit"
+              :extract-method="cleanType"
+              :fields="copyBuiltField?.filter(item => item.field_name === 'data')"
+            >
+            </setting-table>
+            <div
+              v-else
+              class="setting-title"
+            >
+              {{ $t('暂未保留原始日志') }}
+            </div>
+
+            <div class="setting-title">{{ $t('索引日志配置') }}</div>
+            <setting-table
+              ref="indexfieldTable"
+              :table-type="'indexLog'"
+              :is-preview-mode="!isEdit"
+              :extract-method="cleanType"
+              :fields="tableField"
+              :collector-config-id="collectorConfigId"
+              :built-fields="copyBuiltField?.filter(item => item.field_name !== 'data')"
+            >
+            </setting-table>
+            <div
+              v-if="isShowAddFields"
+              class="add-field-container"
+            >
+              <div
+                class="text-btn"
+                @click="addNewField"
+              >
+                <i class="icon bk-icon icon-plus push"></i>
+                <span class="text">{{ $t('新增字段') }}</span>
+              </div>
+            </div>
+          </bk-form>
+        </div>
+      </template>
+    </bk-sideslider>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed, ref } from 'vue';
+  import useStore from '@/hooks/use-store';
+  import useLocale from '@/hooks/use-locale';
+  import { useRoute } from 'vue-router/composables';
+  import http from '@/api';
+  import { deepClone } from '@/common/util';
+
+  import settingTable from './setting-table.vue';
+
+  const { t } = useLocale();
+  const store = useStore();
+  const route = useRoute();
+
+  const showSlider = ref(false);
+  const sliderLoading = ref(false);
+  const isEdit = ref(false);
+  const isEditConfigName = ref(false);
+  const isEditRetention = ref(false);
+  const copyBuiltField = ref([]);
+  const tableField = ref([]);
+  const cleanType = ref('');
+  const collectorConfigId = ref('');
+
+  const formData = ref({
+    collector_config_name: '',
+    collector_config_name_en: '',
+    bk_data_id: '',
+    storage_cluster_name: '',
+    retention: '',
+    etl_params: {
+      retain_original_text: false,
+    },
+  });
+
+  /** 添加字段的基础数据 */
+  const baseFieldObj = ref({
+    value: '',
+    option: {
+      time_zone: '',
+      time_format: '',
+    },
+    is_time: false,
+    verdict: false,
+    is_delete: false,
+    alias_name: '',
+    field_name: '',
+    field_type: '',
+    description: '',
+    is_case_sensitive: false,
+    is_analyzed: false,
+    is_built_in: false,
+    is_dimension: false,
+    previous_type: '',
+    tokenize_on_chars: '',
+    participleState: 'default',
+    is_edit: true,
+  });
+
+  const basicRules = ref({
+    collector_config_name: [
+      {
+        required: true,
+        trigger: 'blur',
+      },
+    ],
+    collector_config_name_en: [
+      {
+        required: true,
+        trigger: 'blur',
+      },
+    ],
+  });
+
+  const isShowAddFields = computed(() => {
+    return cleanType.value === 'bk_log_json';
+  });
+
+  const indexfieldTable = ref(null);
+  const addNewField = () => {
+    const fields = deepClone(tableField.value);
+    const newBaseFieldObj = {
+      ...baseFieldObj.value,
+      field_index: tableField.value.length + 1,
+    };
+    // 获取table表格编辑的数据 新增新的字段对象
+    tableField.value.splice(0, fields.length, ...[...indexfieldTable.value.getData(), newBaseFieldObj]);
+  };
+  const handleEdit = () => {
+    isEdit.value = !isEdit.value;
+  };
+
+  const handleOpenSidebar = () => {
+    showSlider.value = true;
+    sliderLoading.value = true;
+    initFormData();
+  };
+
+  const initFormData = async () => {
+    const indexSetList = store.state.retrieve.indexSetList;
+    const indexSetId = route.params?.indexId;
+    const currentIndexSet = indexSetList.find(item => item.index_set_id === indexSetId);
+    if (!currentIndexSet.collector_config_id) return;
+    collectorConfigId.value = currentIndexSet.collector_config_id;
+    await http
+      .request('collect/details', {
+        params: {
+          collector_config_id: currentIndexSet.collector_config_id,
+        },
+      })
+      .then(res => {
+        const collectData = res?.data || {};
+        formData.value = collectData;
+        cleanType.value = collectData?.etl_config;
+        copyBuiltField.value = collectData?.fields.filter(item => item.is_built_in);
+      });
+
+    await http
+      .request('clean/getCleanStash', {
+        params: {
+          collector_config_id: currentIndexSet.collector_config_id,
+        },
+      })
+      .then(res => {
+        tableField.value = res?.data?.etl_fields.filter(item => !item.is_built_in && !item.is_delete);
+      });
+    sliderLoading.value = false;
+  };
+</script>
+
+<style lang="scss" scoped>
+  .field-setting-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 108px;
+    height: 32px;
+    margin-left: 10px;
+    font-size: 14px;
+    color: #63656e;
+    cursor: pointer;
+
+    span {
+      margin: 3px 6px 0 0;
+    }
+  }
+
+  .king-slider-content {
+    min-height: 394px;
+    overflow-y: auto;
+
+    .add-collection-title {
+      width: 100%;
+      padding-top: 18px;
+      font-size: 14px;
+      font-weight: 600;
+      color: #63656e;
+    }
+
+    .setting-title {
+      padding-top: 10px;
+      font-size: 12px;
+      color: #888a8e;
+    }
+
+    .king-form {
+      padding: 16px 36px 36px;
+
+      .form-flex-container {
+        display: flex;
+        align-items: center;
+        // height: 32px;
+        font-size: 12px;
+        color: #63656e;
+
+        .icon-info {
+          margin: 0 8px 0 24px;
+          font-size: 14px;
+          color: #3a84ff;
+        }
+      }
+
+      .bk-form-item {
+        margin-top: 18px;
+      }
+
+      .source-item {
+        display: flex;
+      }
+
+      .add-field-container {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        padding-left: 4px;
+        border: 1px solid #dcdee5;
+        border-top: none;
+        border-bottom: 1.5px solid #dcdee5;
+        border-radius: 0 0 2px 2px;
+        transform: translateY(-1px);
+
+        .text-btn {
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+
+          .text,
+          .icon {
+            font-size: 22px;
+            color: #3a84ff;
+          }
+
+          .text {
+            font-size: 12px;
+          }
+        }
+      }
+    }
+  }
+</style>
