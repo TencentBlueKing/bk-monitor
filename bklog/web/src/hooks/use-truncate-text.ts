5b220c58e817c44131985a6de23dbb268173e44d
LeeXT <xintaoLi@users.noreply.github.com>
2024-11-05 21:02:31 +0800

    fix: 截取之后mark标签被截断问题 (#3735)

--- bklog/web/src/hooks/use-truncate-text.ts
+++ bklog/web/src/hooks/use-truncate-text.ts
@@ -70,6 +70,18 @@ export default (options: ComputedRef<TextOption>) => {
       currentWidth += charWidth;
     }
 
+    const openingTagPattern = /<mark>/g;
+    const closingTagPattern = /<\/mark>/g;
+
+    // 计算截取文本中的 <mark> 和 </mark> 标签数量
+    const openCount = (truncatedText.match(openingTagPattern) || []).length;
+    const closeCount = (truncatedText.match(closingTagPattern) || []).length;
+
+    // 如果 <mark> 标签数量多于 </mark>，则追加一个 </mark>
+    if (openCount > closeCount) {
+      truncatedText += '</mark>';
+    }
+
     return truncatedText;
   };
 
