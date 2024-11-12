18178871f179a97de241746cfdca6917c96a62fa
xlc <2933458919@qq.com>
2024-11-12 11:17:06 +0800

    fix: 轮值代码复用性优化

---
+++ bkmonitor/webpack/src/monitor-common/rotation-utils/utils.ts
@@ -0,0 +1,57 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+export const colorList = [
+  '#3A84FF',
+  '#FF9C01',
+  '#18B456',
+  '#BC66E5',
+  '#FF5656',
+  '#699DF4',
+  '#FFB848',
+  '#51BE68',
+  '#D493F3',
+  '#FF6F6F',
+  '#61B2C2',
+  '#F5876C',
+  '#EAD550',
+  '#AEA5F2',
+  '#85CCA8',
+  '#FFA66B',
+  '#6FBCEA',
+  '#FFC685',
+  '#6EDAC1',
+  '#E787CB',
+];
+export const randomColor = (index: number) => {
+  if (index <= colorList.length - 1) {
+    return colorList[index];
+  }
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgba(${r},${g},${b},0.8)`;
+};
