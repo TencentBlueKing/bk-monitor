fe1c2ebf2aa92ba4412b58d60b384d65a875546b
热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>
2024-11-12 15:34:08 +0800

    fix: 修复拓扑关联无法查询问题 --bug=133784945 (#3843)

--- bkmonitor/packages/monitor_web/scene_view/builtin/apm.py
+++ bkmonitor/packages/monitor_web/scene_view/builtin/apm.py
@@ -157,12 +157,16 @@ class ApmBuiltinProcessor(BuiltinProcessor):
         # APM观测场景处
         # 主机场景
         if builtin_view == "apm_service-service-default-host":
-            if all(list(params.values())) and HostHandler.list_application_hosts(
-                view.bk_biz_id,
-                params.get("app_name"),
-                params.get("service_name"),
-                start_time=params.get("start_time"),
-                end_time=params.get("end_time"),
+            if (
+                app_name
+                and service_name
+                and HostHandler.list_application_hosts(
+                    view.bk_biz_id,
+                    app_name,
+                    service_name,
+                    start_time=params.get("start_time"),
+                    end_time=params.get("end_time"),
+                )
             ):
                 cls._add_config_from_host(view, view_config)
                 return view_config
