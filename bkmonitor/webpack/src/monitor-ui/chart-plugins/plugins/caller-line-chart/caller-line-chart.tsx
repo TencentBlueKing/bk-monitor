92251fe417fd9dfcfff501c74312e2b6406f847f
liang ling <lingliang1100@gmail.com>
2024-11-19 10:46:45 +0800

    fix: 修复APM日志体验问题 --Bug=134151925 (#3935)
    # Reviewed, transaction id: 24116

--- bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/caller-line-chart/caller-line-chart.tsx
+++ bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/caller-line-chart/caller-line-chart.tsx
@@ -450,6 +450,7 @@ class CallerLineChart extends CommonSimpleChart {
                 formatter: formatterFunc || '{value}',
               },
               ...xInterval,
+              splitNumber: 4,
             },
             series: seriesList,
             tooltip: {
