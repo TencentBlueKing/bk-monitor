0bdd5a5820838d259dff701ae957875c912bdcf1
chenguo <chenguo367@163.com>
2024-11-25 11:04:07 +0800

    minor: 优化gse进程托管事件平台侧监控插件范围

--- bkmonitor/alarm_backends/service/detect/strategy/os_restart.py
+++ bkmonitor/alarm_backends/service/detect/strategy/os_restart.py
@@ -11,7 +11,7 @@ specific language governing permissions and limitations under the License.
 """
 系统重启算法：基于时序数据 system.env.uptime 进行判断。
 uptime表示主机运行时长。
-该检测算法依赖basereport采集器被gse agent托管(机器重启后basereport自动拉起)否则无数据上报会导致该检测算法失效。
+该检测算法依赖bkmonitorbeat采集器被gse agent托管(机器重启后bkmonitorbeat自动拉起)否则无数据上报会导致该检测算法失效。
 """
 
 
@@ -23,7 +23,6 @@ from alarm_backends.service.detect.strategy.threshold import Threshold
 
 
 class OsRestart(SimpleRingRatio):
-
     expr_op = "and"
     desc_tpl = _("当前服务器在{{data_point.value}}秒前发生系统重启事件")
     config_serializer = None
