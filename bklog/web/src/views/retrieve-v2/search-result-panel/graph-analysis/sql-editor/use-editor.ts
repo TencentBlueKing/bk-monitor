83bf5d0e0b854f63669bf41b1ee9ed963e07f8a5
LeeXT <xintaoLi@users.noreply.github.com>
2024-11-26 09:52:00 +0800

    fix: 图表分析SQL编辑高度自适应修复和出图样式修复 (#4044)
    Co-authored-by: cjyscjys <136775313+cjyscjys@users.noreply.github.com>
    Co-authored-by: wencong1724427771 <82081580+wencong1724427771@users.noreply.github.com>
    Co-authored-by: chenmanting1 <49743833+chenmanting1@users.noreply.github.com>
    Co-authored-by: jayjiahua <553544693@qq.com>
    Co-authored-by: lai <11598235+unique0lai@users.noreply.github.com>
    Co-authored-by: NeoLing <pegasusljn@gmail.com>
    Co-authored-by: EmilyMei <447693773@qq.com>
    Co-authored-by: 热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>

--- bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/use-editor.ts
+++ bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/use-editor.ts
@@ -1,16 +1,48 @@
-import { debounce } from 'lodash';
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 import { computed, onMounted, ref } from 'vue';
-import * as monaco from 'monaco-editor';
-import { setDorisFields } from './lang';
+
 import useStore from '@/hooks/use-store';
+import { debounce } from 'lodash';
+import * as monaco from 'monaco-editor';
+
+import { setDorisFields } from './lang';
 
 export default ({ refRootElement, sqlContent, onValueChange }) => {
-  const editorInstance = ref();
+  const editorInstance = ref<monaco.editor.IStandaloneCodeEditor>();
   const store = useStore();
   const fieldList = computed(() => store.state.indexFieldInfo.fields);
 
+  const updateEditorHeight = () => {
+    editorInstance.value.layout(); // 重新布局编辑器
+  };
+
   const debounceUpdateSqlValue = debounce(() => {
     onValueChange?.(editorInstance.value.getValue());
+    updateEditorHeight();
   });
 
   const initEditorInstance = () => {
@@ -20,6 +52,9 @@ export default ({ refRootElement, sqlContent, onValueChange }) => {
       language: 'dorisSQL',
       theme: 'vs-dark',
       padding: { top: 10, bottom: 10 },
+      automaticLayout: true,
+      minimap: { enabled: false },
+      scrollBeyondLastLine: false, // 避免空白行
     });
 
     // 监听编辑器的键盘事件
