73b5758e55cd29c5e178cab599578b7d2427540e
LeeXT <xintaoLi@users.noreply.github.com>
2024-11-26 17:56:07 +0800

    fix: 处理合并冲突 (#4064)
    Co-authored-by: cjyscjys <136775313+cjyscjys@users.noreply.github.com>
    Co-authored-by: wencong1724427771 <82081580+wencong1724427771@users.noreply.github.com>
    Co-authored-by: chenmanting1 <49743833+chenmanting1@users.noreply.github.com>
    Co-authored-by: jayjiahua <553544693@qq.com>
    Co-authored-by: lai <11598235+unique0lai@users.noreply.github.com>
    Co-authored-by: NeoLing <pegasusljn@gmail.com>
    Co-authored-by: EmilyMei <447693773@qq.com>
    Co-authored-by: 热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>

--- bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/index.tsx
+++ bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/index.tsx
@@ -197,8 +197,6 @@ export default class GraphAnalysisIndex extends tsc<IProps> {
     return {
       favorite_type: 'chart',
       chart_params: {
-        // basicInfoTitle: this.basicInfoTitle.title,
-        // basicInfoDescription: this.basicInfoDescription.title,
         xFields: this.xAxis,
         yFields: this.yAxis,
         activeGraphCategory: this.activeGraphCategory,
@@ -212,20 +210,7 @@ export default class GraphAnalysisIndex extends tsc<IProps> {
   @Watch('storedChartParams', { deep: true })
   handleChartParamsChange() {
     if (this.storedChartParams) {
-      [
-<<<<<<< HEAD
-        // 'basicInfoTitle',
-        // 'basicInfoDescription',
-=======
-        'basicInfoTitle',
-        'basicInfoDescription',
->>>>>>> df415bdc7dafc1c7c300cbb52b7e0f55bed0be5f
-        'xFields',
-        'yFields',
-        'activeGraphCategory',
-        'chartActiveType',
-        'dimensions',
-      ].forEach(key => {
+      ['xFields', 'yFields', 'activeGraphCategory', 'chartActiveType', 'dimensions'].forEach(key => {
         if (this.storedChartParams[key]) {
           Object.assign(this, { [key]: this.storedChartParams[key] });
         }
