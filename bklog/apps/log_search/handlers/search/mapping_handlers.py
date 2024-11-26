abb38f4bcc352396d07f191690a241e38ce83406
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2024-11-26 11:36:50 +0800

    fix: 切换集群获取mappings兼容 #1010158081134496665

--- bklog/apps/log_search/handlers/search/mapping_handlers.py
+++ bklog/apps/log_search/handlers/search/mapping_handlers.py
@@ -27,6 +27,7 @@ from collections import defaultdict
 from typing import Any, Dict, List
 
 import arrow
+import pytz
 from django.conf import settings
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext as _
@@ -483,9 +484,20 @@ class MappingHandlers(object):
 
     @cache_one_minute("latest_mapping_key_{index_set_id}_{start_time}_{end_time}_{only_search}")
     def _get_latest_mapping(self, index_set_id, start_time, end_time, only_search=False):  # noqa
-        storage_cluster_record_objs = StorageClusterRecord.objects.filter(
-            index_set_id=int(self.index_set_id), created_at__gt=(start_time - datetime.timedelta(hours=1))
-        ).exclude(storage_cluster_id=self.storage_cluster_id)
+        storage_cluster_record_objs = StorageClusterRecord.objects.none()
+
+        if self.start_time:
+            try:
+                tz_info = pytz.timezone(get_local_param("time_zone", settings.TIME_ZONE))
+                if type(self.start_time) in [int, float]:
+                    start_time = arrow.get(self.start_time).to(tz=tz_info).datetime
+                else:
+                    start_time = arrow.get(self.start_time).replace(tzinfo=tz_info).datetime
+                storage_cluster_record_objs = StorageClusterRecord.objects.filter(
+                    index_set_id=int(self.index_set_id), created_at__gt=(start_time - datetime.timedelta(hours=1))
+                ).exclude(storage_cluster_id=self.storage_cluster_id)
+            except Exception as e:  # pylint: disable=broad-except
+                logger.exception(f"[_multi_search] parse time error -> e: {e}")
 
         params = {
             "indices": self.indices,
