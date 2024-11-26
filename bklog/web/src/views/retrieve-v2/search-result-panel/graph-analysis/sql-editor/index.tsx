c225f25a755c6dccd95d1053e1731556b1757181
jidanjuan <112756875+jidanjuan@users.noreply.github.com>
2024-11-26 19:07:55 +0800

    fix: 验收问题11问题修复 (#4071)

--- bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/index.tsx
+++ bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/index.tsx
@@ -31,6 +31,8 @@ import useStore from '@/hooks/use-store';
 import RequestPool from '@/store/request-pool';
 import axios from 'axios';
 import { debounce } from 'lodash';
+import screenfull from 'screenfull';
+import { format } from 'sql-formatter';
 
 import BookmarkPop from '../../../search-bar/bookmark-pop.vue';
 import useEditor from './use-editor';
@@ -48,10 +50,12 @@ export default defineComponent({
   setup(props, { emit, expose }) {
     const store = useStore();
     const refRootElement: Ref<HTMLElement> = ref();
+    const refSqlBox: Ref<HTMLElement> = ref();
     const isRequesting = ref(false);
     const isSyncSqlRequesting = ref(false);
     const isPreviewSqlShow = ref(false);
     const sqlContent = ref('');
+    const isFullscreen = ref(false);
     const onValueChange = (value: any) => {
       if (value !== sqlContent.value) {
         sqlContent.value = value;
@@ -141,6 +145,7 @@ export default defineComponent({
         .then(resp => {
           onValueChange(resp.data.sql);
           editorInstance.value.setValue(resp.data.sql);
+          formatMonacoSqlCode();
           editorInstance.value.focus();
           if (storeResult) {
             storeChartOptions();
@@ -150,7 +155,16 @@ export default defineComponent({
           isSyncSqlRequesting.value = false;
         });
     };
-
+    const handleFullscreenClick = () => {
+      if (!screenfull.isEnabled) return;
+      isFullscreen.value ? screenfull.exit() : screenfull.request(refSqlBox.value);
+      isFullscreen.value = !isFullscreen.value;
+      editorInstance.value.focus();
+    };
+    const formatMonacoSqlCode = () => {
+      const val = format(editorInstance.value.getValue(), { language: 'mysql' });
+      editorInstance.value.setValue([val].join('\n'));
+    };
     const renderTools = () => {
       return (
         <div class='sql-editor-tools'>
@@ -196,7 +210,41 @@ export default defineComponent({
         </div>
       );
     };
-
+    const renderHeadTools = () => {
+      return (
+        <div
+          v-if='toolsConfig.enabled'
+          class='bk-monaco-tools'
+        >
+          <span>SQL查询</span>
+          <div>
+            <div class='fr header-tool-right'>
+              <bk-popover
+                class='sqlFormat'
+                content='格式化'
+              >
+                <div onClick={formatMonacoSqlCode}>
+                  <span class='bk-icon icon-script-file'></span>
+                </div>
+              </bk-popover>
+              {isFullscreen.value ? (
+                <bk-popover content='取消全屏'>
+                  <div onClick={handleFullscreenClick}>
+                    <span class='bk-icon icon-un-full-screen'></span>
+                  </div>
+                </bk-popover>
+              ) : (
+                <bk-popover content='全屏'>
+                  <div onClick={handleFullscreenClick}>
+                    <span class='bk-icon icon-full-screen'></span>
+                  </div>
+                </bk-popover>
+              )}
+            </div>
+          </div>
+        </div>
+      );
+    };
     const handleUpdateIsContentShow = val => {
       isPreviewSqlShow.value = val;
     };
@@ -228,20 +276,27 @@ export default defineComponent({
 
     return {
       refRootElement,
+      refSqlBox,
       isPreviewSqlShow,
       sqlContent,
       renderTools,
+      renderHeadTools,
       handleUpdateIsContentShow,
       handleQueryBtnClick,
     };
   },
   render() {
     return (
-      <div class='bklog-sql-editor-root'>
+      <div
+        ref='refSqlBox'
+        class='bklog-sql-editor-root'
+      >
         <div
           ref='refRootElement'
           class='bklog-sql-editor'
-        ></div>
+        >
+          {this.renderHeadTools()}
+        </div>
         {this.renderTools()}
       </div>
     );
