16da46b3847c97c04f7ed9e30a262e247ea65a27
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2024-11-27 11:44:01 +0800

    feat: APM 日志索引及增加默认查询语句 --bug=134194297 (#4059)

--- bklog/web/src/store/index.js
+++ bklog/web/src/store/index.js
@@ -957,7 +957,10 @@ const store = new Vuex.Store({
           items: ids.map(val => (list || []).find(item => item.index_set_id === val)).filter(val => val !== undefined),
           isUnionIndex,
         };
-
+        if (!payload.keyword && payload.items.length === 1 && payload.items[0].query_string) {
+          payload.keyword = payload.items[0].query_string;
+          payload.search_mode = 'sql';
+        }
         commit('updateIndexId', isUnionIndex ? undefined : ids[0]);
         commit('updateIndexItem', payload);
       }
