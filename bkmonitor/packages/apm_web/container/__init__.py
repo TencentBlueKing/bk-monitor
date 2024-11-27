c297757d84d988b5bf5124cc13b9e0e712995956
热心网友陈德华 <47016498+rxwycdh@users.noreply.github.com>
2024-11-27 20:14:39 +0800

    feat: 新增服务容器页面 & 调整主机关联页面 --story=120782340 (#4081)

--- bkmonitor/packages/apm_web/container/__init__.py
+++ bkmonitor/packages/apm_web/container/__init__.py
@@ -8,22 +8,3 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-from django.conf.urls import include, url
-
-from apm_web.views import apm_home
-
-app_name = "apm_web"
-
-urlpatterns = [
-    url(r"^$", apm_home),
-    url(r"meta/", include("apm_web.meta.urls")),
-    url(r"^trace_api/", include("apm_web.trace.urls")),
-    url(r"^profile_api/", include("apm_web.profile.urls")),
-    url(r"^metric/", include("apm_web.metric.urls")),
-    url(r"^topo/", include("apm_web.topo.urls")),
-    url(r"^service/", include("apm_web.service.urls")),
-    url(r"^service_log/", include("apm_web.log.urls")),
-    url(r"^service_db/", include("apm_web.db.urls")),
-    url(r"^container/", include("apm_web.container.urls")),
-]
