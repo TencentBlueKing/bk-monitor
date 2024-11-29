667232d640a86cc2371c515cc8c6e784c42440dc
chenguo <chenguo367@163.com>
2024-11-29 17:27:37 +0800

    minor: k8s meta过滤查询生成优化

--- bkmonitor/packages/monitor_web/k8s/core/meta.py
+++ bkmonitor/packages/monitor_web/k8s/core/meta.py
@@ -56,7 +56,7 @@ class FilterCollection(object):
             # 字段映射， prometheus数据字段 映射到 ORM中的 模型字段
             field_name = resource_meta.column_mapping.get(field_name, field_name)
             # 重新组装特殊查询条件
-            new_key = field_name if field_name == key else f"{field_name}__{condition}"
+            new_key = field_name if len(parsed_token) == 1 else f"{field_name}__{condition}"
             orm_filter_dict[new_key] = value
         return orm_filter_dict
 
