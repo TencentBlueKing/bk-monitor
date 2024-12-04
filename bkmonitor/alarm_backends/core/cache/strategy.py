0c720d3633addd3d44817a46c83737f016262dcd
chenguo <chenguo367@163.com>
2024-12-04 18:09:40 +0800

    feat: 支持aiops sdk独立集群异常检测 #1010158081120977161

--- bkmonitor/alarm_backends/core/cache/strategy.py
+++ bkmonitor/alarm_backends/core/cache/strategy.py
@@ -1224,6 +1224,20 @@ class StrategyCacheManager(CacheManager):
         metrics.ALARM_CACHE_TASK_TIME.labels("0", "strategy", str(exc)).observe(duration)
         metrics.report_all()
 
+        # 推送aiops策略变更至 sdk 依赖的 历史数据维护服务
+        change_records = histories.values("operate", "strategy_id", "create_time")
+        for change_record in change_records:
+            changed_time = change_record["create_time"].timestamp()
+            if change_record["operate"] == "delete":
+                sync_aiops_strategy_signal("deleted", change_record["strategy_id"], changed_time)
+            else:
+                # check strategy use aiops
+                intelligent_detect = change_record["content"]["items"][0]["query_configs"][0].get(
+                    "intelligent_detect", {}
+                )
+                if intelligent_detect and intelligent_detect.get("use_sdk", False):
+                    sync_aiops_strategy_signal("modify", change_record["strategy_id"], changed_time)
+
 
 class TargetShieldProcessor:
     """
@@ -1370,3 +1384,19 @@ def smart_refresh():
 
 def main():
     StrategyCacheManager.refresh()
+
+
+def sync_aiops_strategy_signal(strategy_id, signal, update_time):
+    """
+    推送策略变更信号至rabbitmq
+    {
+        "sync_type": "modify", # "delete"
+        "sync_time": 1700000000,
+        "strategy_id": 1,
+        "strategy_info": {}
+    }
+    signal: modify, delete
+
+    delete: 已删除的策略（不论是否是aiops策略）, 修改后的策略不包含aiops算法
+    modify: 新增的，修改后的 策略包含了aiops算法
+    """
