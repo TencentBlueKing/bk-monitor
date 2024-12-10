ea3b64e05f70808c579eb9bbb524612a94bd2875
HACK-WU <2931580128@qq.com>
2024-12-10 06:40:38 +0000

    refactor: 变更force_text黄和smart_text

--- bkmonitor/bkmonitor/utils/common_utils.py
+++ bkmonitor/bkmonitor/utils/common_utils.py
@@ -31,7 +31,7 @@ from typing import Dict, List, Union
 from zipfile import ZipFile
 
 from django.conf import settings
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.functional import Promise
 from django.utils.timezone import is_aware
 from django.utils.translation import ugettext as _
@@ -100,7 +100,7 @@ class DatetimeEncoder(json.JSONEncoder):
         if isinstance(obj, set):
             return list(obj)
         if isinstance(obj, Promise):
-            return force_text(obj)
+            return force_str(obj)
         if issubclass(obj.__class__, DictObj):
             return obj.__dict__
         if isinstance(obj, bytes):
