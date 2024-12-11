413378323817a01a7c0e6902f8f5d5ad00116fb3
HACK-WU <2931580128@qq.com>
2024-12-11 06:46:50 +0000

    refactor: 新增periodic_task函数，用于兼容celery5

--- bkmonitor/alarm_backends/service/scheduler/app.py
+++ bkmonitor/alarm_backends/service/scheduler/app.py
@@ -18,7 +18,8 @@ import six.moves.urllib.error
 import six.moves.urllib.parse
 import six.moves.urllib.request
 from billiard import cpu_count
-from celery import Celery
+from celery import Celery, Task, current_app
+from celery.schedules import maybe_schedule
 from celery.signals import beat_init, setup_logging
 from django.conf import settings
 from django.db import close_old_connections
@@ -258,3 +259,35 @@ def config_loggers(*args, **kwags):
 @beat_init.connect
 def clean_db_connections(sender, **kwargs):
     close_old_connections()
+
+
+class PeriodicTask(Task):
+    """A task that adds itself to the :setting:`beat_schedule` setting. 兼容celery5"""
+
+    abstract = True
+    ignore_result = True
+    relative = False
+    options = None
+    compat = True
+
+    def __init__(self):
+        if not hasattr(self, 'run_every'):
+            raise NotImplementedError('Periodic tasks must have a run_every attribute')
+        self.run_every = maybe_schedule(self.run_every, self.relative)
+        super(PeriodicTask, self).__init__()
+
+    @classmethod
+    def on_bound(cls, app):
+        app.conf.beat_schedule[cls.name] = {
+            'task': cls.name,
+            'schedule': cls.run_every,
+            'args': (),
+            'kwargs': {},
+            'options': cls.options or {},
+            'relative': cls.relative,
+        }
+
+
+def periodic_task(*args, **options):
+    """Deprecated decorator, please use :setting:`beat_schedule`."""
+    return current_app.task(**dict({"base": PeriodicTask}, **options))
