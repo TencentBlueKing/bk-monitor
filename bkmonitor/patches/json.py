2b69aac127d497750156f5f97dfb44f498dcfbb6
unique0lai <11598235+unique0lai@users.noreply.github.com>
2024-12-16 20:38:56 +0800

    Revert "feat: ujson.loads不再额外添加precise_float参数 --story=121220132 (#4409)"
    This reverts commit 1d98266066b77f068dabfd15a3424f5f62e8564b.

--- bkmonitor/patches/json.py
+++ bkmonitor/patches/json.py
@@ -26,22 +26,23 @@ except ImportError:
 else:
     __implements__ = ["load", "dump", "loads", "dumps"]
 
+
 SAFE_OPTIONS = {
     "encode_html_chars",
     "ensure_ascii",
     "double_precision",
     "escape_forward_slashes",
     "indent",
+    "precise_float",
 }
 
 
 def load(*args, **kwargs):
-    try:
-        # ujson5.9.0 只支持一个参数，precise_float=True是默认行为
-        obj = kwargs.get("obj") or args[0]
-        return ujson.load(obj)
-    except ValueError:
-        pass
+    if SAFE_OPTIONS.issuperset(kwargs.keys()):
+        try:
+            return ujson.load(*args, **kwargs)
+        except ValueError:
+            pass
     return json_load(*args, **kwargs)
 
 
@@ -56,12 +57,13 @@ def dump(*args, **kwargs):
 
 
 def loads(*args, **kwargs):
-    try:
-        # ujson5.9.0 只支持一个参数，precise_float=True是默认行为
-        obj = kwargs.get("obj") or args[0]
-        return ujson.loads(obj)
-    except ValueError:
-        pass
+    if SAFE_OPTIONS.issuperset(kwargs.keys()):
+        try:
+            kwargs.update({"precise_float": True})
+            return ujson.loads(*args, **kwargs)
+        except ValueError:
+            kwargs.pop("precise_float")
+            pass
     return json_loads(*args, **kwargs)
 
 
