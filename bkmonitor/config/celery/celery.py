1a9d3a38b588396bc1730cbf4d6889cfca70b3cd
unique0lai <11598235+unique0lai@users.noreply.github.com>
2024-12-18 14:52:44 +0800

    refactor: celery配置定义调整

--- bkmonitor/config/celery/celery.py
+++ bkmonitor/config/celery/celery.py
@@ -22,10 +22,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 app = Celery("proj")
 
-# Using a string here means the worker don't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object("config.celery.config:Config")
 
 # Load task modules from all registered Django app configs.
