b1c69e723f36c80bea752c25fce1bcac56d69c78
jidanjuan <zjjdx123@163.com>
2024-12-25 20:24:16 +0800

    fix: object别名问题修复 # Reviewed, transaction id: 27811

--- bklog/web/src/components/collection-access/field-table.vue
+++ bklog/web/src/components/collection-access/field-table.vue
@@ -328,7 +328,7 @@
                 >
                   <bk-select
 
-                    v-model="props.row.type_conversion || props.row.field_type"
+                    v-model="props.row.field_type"
                     :clearable="false"
                     :disabled="props.row.is_delete || isSetDisabled || props.row.is_built_in"
                     @selected="
