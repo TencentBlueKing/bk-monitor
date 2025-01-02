e6bddfb87f4bd977b34ac3445489b7bf7211a7f7
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-01-02 12:19:42 +0800

    chore: 仪表盘列表接口支持过滤无权限仪表盘

--- bkmonitor/packages/monitor_web/grafana/resources/manage.py
+++ bkmonitor/packages/monitor_web/grafana/resources/manage.py
@@ -13,7 +13,6 @@ from collections import defaultdict
 from copy import deepcopy
 from typing import Any, Dict
 
-from blueapps.utils import get_request
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
@@ -21,6 +20,7 @@ from bk_dataview.api import get_or_create_org
 from bk_dataview.models import Dashboard
 from bk_dataview.permissions import GrafanaPermission, GrafanaRole
 from bkmonitor.models.strategy import QueryConfigModel, StrategyModel
+from bkmonitor.utils.request import get_request
 from constants.data_source import DataSourceLabel
 from core.drf_resource import Resource, api, resource
 from core.errors.dashboard import GetFolderOrDashboardError
@@ -82,10 +82,13 @@ class GetDirectoryTree(Resource):
 
     class RequestSerializer(serializers.Serializer):
         bk_biz_id = serializers.IntegerField(label="业务ID")
+        filter_no_permission = serializers.BooleanField(label="是否过滤无权限的仪表盘", default=False)
 
     def perform_request(self, params):
         org_id = get_or_create_org(params["bk_biz_id"])["id"]
-        request = get_request()
+        request = get_request(peaceful=True)
+        if not request:
+            raise GetFolderOrDashboardError(code=200, message="lack of request")
 
         result = api.grafana.search_folder_or_dashboard(org_id=org_id)
         if not result:
@@ -103,13 +106,16 @@ class GetDirectoryTree(Resource):
             {"id": 0, "uid": "", "title": "General", "uri": "", "url": "", "slug": "", "tags": [], "isStarred": False}
         )
 
+        # 是否过滤无权限的仪表盘
+        filter_no_permission = params.get("filter_no_permission", False) or getattr(request, "external_user", False)
+
         for record in result["data"]:
             _type = record.pop("type", "")
             if _type == "dash-folder":
                 folders[record["id"]].update(record)
             elif _type == "dash-db":
-                # 过滤仪表盘权限
-                if getattr(request, "external_user", None) and record["uid"] not in dashboard_permissions:
+                # 过滤无权限的仪表盘
+                if filter_no_permission and record["uid"] not in dashboard_permissions:
                     continue
                 # 仪表盘是否可编辑
                 record["editable"] = (
