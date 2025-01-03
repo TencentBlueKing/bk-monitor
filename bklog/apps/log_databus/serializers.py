cded6e94d7c9fc5b1ec6bb0a35c55445cf217630
v_cwcgcui <1724427771@qq.com>
2025-01-03 18:32:36 +0800

    feat: 自定义上报更新时增加sort_fields和target_fields字段 #1010158081121516342

--- bklog/apps/log_databus/serializers.py
+++ bklog/apps/log_databus/serializers.py
@@ -1481,7 +1481,8 @@ class CustomCreateSerializer(CustomCollectorBaseSerializer):
 
 
 class CustomUpdateSerializer(CustomCollectorBaseSerializer):
-    ...
+    sort_fields = serializers.ListField(label=_("排序字段"), required=False, default=list)
+    target_fields = serializers.ListField(label=_("目标字段"), required=False, default=list)
 
 
 class FastCollectorCreateSerializer(CollectorETLParamsFieldSerializer):
