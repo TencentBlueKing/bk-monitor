90651fca634e74d2492d2e1a26cc1c6aa7b81b55
chenmanting1 <1471184327@qq.com>
2025-01-03 17:29:10 +0800

    feat: 编辑自定义上报新增字段设置功能 # Reviewed, transaction id: 28501

---
+++ bklog/web/src/views/manage/manage-access/custom-report/components/field-setting.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="create-form">
+    <div class="form-title">
+      <span>{{ $t('字段设置') }}</span>
+      <span class="title-tips">
+        <i class="bk-icon icon-exclamation-circle"></i>
+        <span>{{ $t('未匹配到对应字段，请手动指定字段后提交') }}</span>
+      </span>
+    </div>
+    <!-- 目标字段 -->
+    <bk-form-item
+      :label="$t('目标字段')"
+      :property="'target_fields'"
+      :desc="$t('用于标识日志文件来源及唯一性')"
+    >
+      <bk-select
+        style="width: 500px"
+        v-model="targetFields"
+        :collapse-tag="false"
+        :is-tag-width-limit="false"
+        display-tag
+        multiple
+        searchable
+      >
+        <bk-option
+          v-for="option in targetFieldSelectList"
+          :id="option.id"
+          :key="option.id"
+          :name="option.name"
+        >
+        </bk-option>
+      </bk-select>
+    </bk-form-item>
+    <!-- 排序字段 -->
+    <bk-form-item
+      :label="$t('排序字段')"
+      :property="'sort_fields'"
+      :desc="$t('用于控制日志排序的字段')"
+    >
+      <div class="collection-select sort-box">
+        <vue-draggable
+          v-model="sortFields"
+          animation="150"
+          handle=".icon-grag-fill"
+        >
+          <transition-group>
+            <bk-tag
+              v-for="(item, index) in sortFields"
+              ext-cls="tag-items"
+              :key="item"
+              closable
+              @close="handleCloseSortFiled(item, index)"
+            >
+              <i class="bk-icon icon-grag-fill"></i>
+              {{ item }}
+            </bk-tag>
+          </transition-group>
+        </vue-draggable>
+        <bk-select
+          :ext-cls="`add-sort-btn ${!sortFields.length && 'not-sort'}`"
+          :popover-min-width="240"
+          searchable
+          style="width: 500px"
+          @selected="handleAddSortFields"
+        >
+          <template #trigger>
+            <bk-button
+              class="king-button"
+              icon="plus"
+            ></bk-button>
+          </template>
+          <bk-option
+            v-for="option in targetFieldSelectList"
+            :disabled="getSortDisabledState(option.id)"
+            :id="option.id"
+            :key="option.id"
+            :name="option.name"
+          >
+          </bk-option>
+        </bk-select>
+      </div>
+    </bk-form-item>
+  </div>
+</template>
+<script setup lang="ts">
+  import { ref, watch, defineProps, defineExpose } from 'vue';
+  import VueDraggable from 'vuedraggable';
+  import $http from '../../../../../api';
+  import { cloneDeep } from 'lodash';
+
+  const props = defineProps({
+    fieldSettingData: {
+      default: () => {},
+      type: Object,
+      required: true,
+    },
+  });
+  const sortFields = ref([]);
+  const targetFields = ref([]);
+  // 获取字段设置数据列表
+  const targetFieldSelectList = ref([]);
+
+  watch(
+    () => props.fieldSettingData,
+    newVal => {
+      if (newVal && JSON.stringify(newVal) !== '{}') {
+        sortFields.value = cloneDeep(newVal.sortFields) || [];
+        targetFields.value = cloneDeep(newVal.targetFields) || [];
+        initTargetFieldSelectList();
+      }
+    },
+  );
+
+  const initTargetFieldSelectList = async () => {
+    if (!props?.fieldSettingData?.indexSetId) return;
+    const res = await $http.request('retrieve/getLogTableHead', {
+      params: {
+        index_set_id: props?.fieldSettingData?.indexSetId,
+      },
+      query: {
+        is_realtime: 'True',
+      },
+    });
+    targetFieldSelectList.value = res?.data?.fields.map(item => {
+      return {
+        id: item.field_name,
+        name: item.field_name,
+      };
+    });
+  };
+
+  const getSortDisabledState = id => {
+    return sortFields.value.includes(id);
+  };
+  const handleAddSortFields = val => {
+    sortFields.value.push(val);
+  };
+  const handleCloseSortFiled = (item, index) => {
+    sortFields.value.splice(index, 1);
+  };
+
+  defineExpose({
+    sortFields,
+    targetFields,
+  });
+</script>
+<style lang="scss" scoped>
+  .sort-box {
+    display: inline-flex;
+    align-items: center;
+
+    .add-sort-btn {
+      display: inline-block;
+      margin-left: 6px;
+      border: none;
+      box-shadow: none;
+    }
+
+    .not-sort {
+      margin-left: 0;
+    }
+  }
+
+  .title-tips {
+    margin-left: 16px;
+    font-size: 12px;
+    font-weight: normal;
+
+    .icon-exclamation-circle {
+      font-size: 16px;
+      color: #ea3636;
+    }
+  }
+
+  .collection-select {
+    .tag-items {
+      height: 32px;
+      line-height: 32px;
+
+      .icon-grag-fill {
+        display: inline-block;
+        cursor: move;
+        transform: translateY(-1px);
+      }
+    }
+  }
+</style>
