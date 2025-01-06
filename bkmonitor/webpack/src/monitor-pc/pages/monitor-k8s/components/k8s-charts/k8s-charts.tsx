b7f5215ceb3a17ff3b7c0106ae770028f41782f4
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-01-06 17:22:52 +0800

    fix: 智能监控容器场景产品验收问题修复 (#4685)

--- bkmonitor/webpack/src/monitor-pc/pages/monitor-k8s/components/k8s-charts/k8s-charts.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/monitor-k8s/components/k8s-charts/k8s-charts.tsx
@@ -407,21 +407,22 @@ export default class K8SCharts extends tsc<
         },
       ];
     } else {
-      data = this.isDetailMode
-        ? this.resourceListData
-        : await listK8sResources({
-            ...this.filterCommonParams,
-            with_history: true,
-            page_size: Math.abs(this.limit),
-            page: 1,
-            page_type: 'scrolling',
-            order_by: this.limit > 0 ? '-cpu' : 'cpu',
-          })
-            .then(data => {
-              if (!data?.items?.length) return [];
-              return data.items;
+      data =
+        this.isDetailMode && this.resourceListData.length
+          ? this.resourceListData
+          : await listK8sResources({
+              ...this.filterCommonParams,
+              with_history: true,
+              page_size: Math.abs(this.limit),
+              page: 1,
+              page_type: 'scrolling',
+              order_by: this.limit > 0 ? '-cpu' : 'cpu',
             })
-            .catch(() => []);
+              .then(data => {
+                if (!data?.items?.length) return [];
+                return data.items;
+              })
+              .catch(() => []);
       if (data.length) {
         const container = new Set<string>();
         const pod = new Set<string>();
