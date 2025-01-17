7d8d0f76b2a038d7706018f4d3644c726d30385b
WuFantaotao <88392262+WuFantaotao@users.noreply.github.com>
2025-01-17 17:15:03 +0800

    fix: 解决topo_handler.discover()报错问题 --bug=137078903 (#4783)

--- bkmonitor/apm/core/discover/base.py
+++ bkmonitor/apm/core/discover/base.py
@@ -397,7 +397,14 @@ class TopoHandler:
         trace_id_count = 0
         span_count = 0
         filter_span_count = 0
-        max_result_count, per_trace_size, index_name = self._get_trace_task_splits()
+        try:
+            max_result_count, per_trace_size, index_name = self._get_trace_task_splits()
+        except Exception as e:
+            logger.error(
+                f"[TopoHandler] 业务id: {self.bk_biz_id}和应用名: {self.app_name}"
+                f"构建的TopoHandler对象在discover方法内发生异常, error({e})"
+            )
+            return
 
         for round_index, trace_ids in enumerate(self.list_trace_ids(index_name)):
             if not trace_ids:
