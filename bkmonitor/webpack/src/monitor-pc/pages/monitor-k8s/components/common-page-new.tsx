925d5dce07d772fa2db3c75e27c42c93a7d45c18
yzygyin <1446680116@qq.com>
2025-01-20 11:21:13 +0800

    fix: 【监控】图表列没有获取缓存的列数缓存 # Reviewed, transaction id: 29595

--- bkmonitor/webpack/src/monitor-pc/pages/monitor-k8s/components/common-page-new.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/monitor-k8s/components/common-page-new.tsx
@@ -631,7 +631,8 @@ export default class CommonPageNew extends tsc<ICommonPageProps, ICommonPageEven
   async initData() {
     this.localSceneType = this.sceneType;
     this.loading = true;
-    this.columns = +localStorage.getItem(DASHBOARD_PANEL_COLUMN_KEY) || 2;
+    const storedValue = localStorage.getItem(DASHBOARD_PANEL_COLUMN_KEY);
+    this.columns = storedValue === '0' ? 0 : +storedValue || 2;
     this.filtersReady = false;
     this.selectorReady = false;
     await this.$nextTick();
