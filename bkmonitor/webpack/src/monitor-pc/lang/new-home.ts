7de292ebbeb1786c2dff56273d96ce43847fceeb
EmilyMei <447693773@qq.com>
2025-01-20 14:51:11 +0800

    feat: 导航栏全局搜索输入框替换 --story=120798093

--- bkmonitor/webpack/src/monitor-pc/lang/new-home.ts
+++ bkmonitor/webpack/src/monitor-pc/lang/new-home.ts
@@ -54,7 +54,7 @@ export default {
   删除该业务: 'Delete this space',
   添加业务: 'Add a service',
   '请输入 IP / Trace ID / 容器集群 / 告警ID / 策略名 进行搜索':
-    'Please enter IP / Trace ID / container cluster / alarm ID / policy name to search',
+    'Please enter IP / Trace ID / Container cluster / Alarm ID / Policy name to search',
   清空搜索: 'Clear search',
   '当前正在加载 采集任务': 'Currently loading the collection task',
   相关功能: 'Related functions',
