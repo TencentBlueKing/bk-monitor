6c1f67407c2183ae2c8be0db872f22fcc7a3bb71
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-02-07 11:44:12 +0800

    "feat: python311升级 #1010158081121620224"

--- bkmonitor/.devcontainer/create_venv.sh
+++ bkmonitor/.devcontainer/create_venv.sh
@@ -6,12 +6,12 @@ pip install virtualenv
 # Change to the project directory
 cd /app/code/bkmonitor
 
-Create virtual environment
+# Create virtual environment
 if [ ! -d "venv" ]; then
   virtualenv venv
 fi
 
-Set up environment variables
+# Set up environment variables
 source /app/code/bkmonitor/venv/bin/activate
 
 python -m ensurepip --default-pip
