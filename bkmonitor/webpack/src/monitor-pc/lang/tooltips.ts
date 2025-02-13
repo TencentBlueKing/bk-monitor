463c5d6a54e6f6c08921f9254acbc4f64df2dcf0
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2025-02-13 17:43:05 +0800

    feat: 完成新版k8s导航栏和左侧面板样式的调整 (#4989)

--- bkmonitor/webpack/src/monitor-pc/lang/tooltips.ts
+++ bkmonitor/webpack/src/monitor-pc/lang/tooltips.ts
@@ -314,4 +314,6 @@ export default {
   清空整组筛选项: 'Clear all filter options in the group',
   请先删除子级维度: 'Please delete the child dimension first',
   '该指标在当前级别({0})不可用': 'This indicator is not available at the current level ({0})',
+  '新版容器监控尚未完全覆盖旧版功能，如需可切换到旧版查看':
+    'The new version of container monitoring does not fully cover the legacy features yet. You can switch to the legacy version to view them if necessary',
 };
