babbe85406ee2c9018dbe74cd7197a32c2e784f3
goodGai <1398281274@qq.com>
2025-02-14 15:53:55 +0800

    fix: 细节调整 # Reviewed, transaction id: 30312

--- bkmonitor/webpack/src/monitor-pc/pages/home/new-home/components/recent-favorites-tab.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/home/new-home/components/recent-favorites-tab.tsx
@@ -90,6 +90,10 @@ export default class RecentFavoritesTab extends Mixins(UserConfigMixin) {
     return this.categoriesConfig.map(item => modeNameMap[item.name]);
   }
 
+  get loading() {
+    return aiWhaleStore.loading;
+  }
+
   // 初始化最近使用的数据
   async initRecentUseData() {
     try {
@@ -213,6 +217,17 @@ export default class RecentFavoritesTab extends Mixins(UserConfigMixin) {
     if (event.key !== 'Enter') return;
     // 阻止默认行为，即在没有按下 Shift 或 Ctrl 时不插入换行符
     if (event.shiftKey || event.ctrlKey) return;
+    if (this.loading) {
+      event.preventDefault();
+      return;
+    }
+    // 获取实际输入内容（考虑换行符情况）
+    const hasContent = event.target.innerText.replace(/[\n\r]/g, '').trim().length > 0;
+
+    if (!hasContent) {
+      event.preventDefault();
+      return;
+    }
     event.preventDefault();
     // 打开小鲸聊天框
     aiWhaleStore.setShowAIBlueking(true);
