38e400eaff39bec7c782856a3bd115234368ac4c
SevenDreamer <945186289@qq.com>
2025-02-14 17:32:21 +0800

    fix: 采集详情-链路状态-数据采样实现方式优化 #1010158081121894251

--- bkmonitor/packages/monitor_web/datalink/resources.py
+++ bkmonitor/packages/monitor_web/datalink/resources.py
@@ -519,13 +519,15 @@ class TransferLatestMsgResource(BaseStatusResource):
         series = api.metadata.kafka_tail({"table_id": table_id, "size": size})
         msgs = []
         for s in series:
-            # 取最后一个数据点
-            latest_datapoint = s["data"][-1]
+            # 获取时间戳
+            timestamp = s["timestamp"]
+            if len(str(timestamp)) == 13:
+                timestamp = timestamp / 1000
 
             msgs.append(
                 {
                     "message": json.dumps(s),
-                    "time": datetime.fromtimestamp(latest_datapoint["timestamp"] / 1000).strftime("%Y-%m-%d %H:%M:%S"),
+                    "time": datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S"),
                     "raw": s,
                 }
             )
