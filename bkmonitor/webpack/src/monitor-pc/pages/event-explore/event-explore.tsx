066620054ee0ed06e31a1cee1d1f8ac9e2e67aba
gc_huwumiao <2199202809@qq.com>
2025-02-19 10:18:16 +0800

    feat: 完成左侧维度过滤面板的基础开发

--- bkmonitor/webpack/src/monitor-pc/pages/event-explore/event-explore.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/event-explore/event-explore.tsx
@@ -23,31 +23,133 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { Component } from 'vue-property-decorator';
+import { Component, ProvideReactive, Ref } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 
+import { getDataSourceConfig } from 'monitor-api/modules/grafana';
+import { random } from 'monitor-common/utils';
+
 import RetrievalFilter from '../../components/retrieval-filter/retrieval-filter';
+import { DEFAULT_TIME_RANGE, handleTransformToTimestamp } from '../../components/time-range/utils';
+import { getDefaultTimezone } from '../../i18n/dayjs';
+import DimensionFilterPanel from './components/dimension-filter-panel';
 import EventRetrievalHeader from './components/event-retrieval-header';
 import EventRetrievalLayout from './components/event-retrieval-layout';
+
+import type { TimeRangeType } from '../../components/time-range/time-range';
+import type { IFormData } from './typing';
 
 import './event-explore.scss';
 Component.registerHooks(['beforeRouteEnter', 'beforeRouteLeave']);
 
 @Component
 export default class EventRetrievalNew extends tsc<object> {
+  // 数据时间间隔
+  @ProvideReactive('timeRange') timeRange: TimeRangeType = DEFAULT_TIME_RANGE;
+  // 时区
+  @ProvideReactive('timezone') timezone: string = getDefaultTimezone();
+  // 刷新间隔
+  @ProvideReactive('refleshInterval') refreshInterval = -1;
+  // 是否立即刷新
+  @ProvideReactive('refleshImmediate') refreshImmediate = '';
+
+  @ProvideReactive('formatTimeRange')
+  get formatTimeRange() {
+    return handleTransformToTimestamp(this.timeRange);
+  }
+
+  @Ref('eventRetrievalLayout') eventRetrievalLayoutRef: EventRetrievalLayout;
+
+  timer = null;
+
+  formData: IFormData = {
+    data_source_label: 'custom',
+    data_type_label: 'event',
+    result_table_id: '',
+    query_string: '',
+  };
+
+  dataIdList = [];
+
+  handleDataIdChange(dataId: string) {
+    this.formData.result_table_id = dataId;
+  }
+
+  handleEventTypeChange(dataType: { data_source_label: string; data_type_label: string }) {
+    this.formData.data_source_label = dataType.data_source_label;
+    this.formData.data_type_label = dataType.data_type_label;
+    this.formData.result_table_id = '';
+    this.getDataIdList();
+  }
+
+  handleImmediateRefresh() {
+    this.refreshImmediate = random(4);
+  }
+
+  handleRefreshChange(value: number) {
+    this.refreshInterval = value;
+    this.timer && clearInterval(this.timer);
+    if (value > -1) {
+      this.timer = setInterval(() => {
+        this.handleImmediateRefresh();
+      }, value);
+    }
+  }
+
+  handleTimeRangeChange(timeRange: TimeRangeType) {
+    this.timeRange = timeRange;
+  }
+
+  handleTimezoneChange(timezone: string) {
+    this.timezone = timezone;
+  }
+
+  async getDataIdList(init = true) {
+    const list = await getDataSourceConfig({
+      data_source_label: this.formData.data_source_label,
+      data_type_label: this.formData.data_type_label,
+    }).catch(() => []);
+    this.dataIdList = list;
+    if (init) {
+      this.formData.result_table_id = list[0]?.id || '';
+    }
+  }
+
+  handleCloseDimensionPanel() {
+    this.eventRetrievalLayoutRef.handleClickShrink(false);
+  }
+
+  mounted() {
+    this.getDataIdList(!this.formData.result_table_id);
+  }
+
   render() {
     return (
       <div class='event-explore'>
         <div class='left-favorite-panel' />
         <div class='right-main-panel'>
-          <EventRetrievalHeader />
+          <EventRetrievalHeader
+            dataIdList={this.dataIdList}
+            formData={this.formData}
+            onDataIdChange={this.handleDataIdChange}
+            onEventTypeChange={this.handleEventTypeChange}
+            onImmediateRefresh={this.handleImmediateRefresh}
+            onRefreshChange={this.handleRefreshChange}
+            onTimeRangeChange={this.handleTimeRangeChange}
+            onTimezoneChange={this.handleTimezoneChange}
+          />
           <div class='event-retrieval-content'>
             <RetrievalFilter />
-            <EventRetrievalLayout class='content-container'>
+            <EventRetrievalLayout
+              ref='eventRetrievalLayout'
+              class='content-container'
+            >
               <div
                 class='dimension-filter-panel'
                 slot='aside'
-              />
+              >
+                <DimensionFilterPanel onClose={this.handleCloseDimensionPanel} />
+              </div>
               <div class='result-content-panel' />
             </EventRetrievalLayout>
           </div>
