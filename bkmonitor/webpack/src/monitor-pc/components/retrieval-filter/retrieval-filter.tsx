9a8dc89d8b72a077406a257035f04b7c9da761a7
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2025-02-19 18:39:34 +0800

    feat: 维度过滤面板开发 (#5088)
    Co-authored-by: crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
    Co-authored-by: yamltdg <13126400746@163.com>
    Co-authored-by: xlc <33691777+xiaolicheng-github@users.noreply.github.com>
    Co-authored-by: liang ling <lingliang1100@gmail.com>

---
+++ bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/retrieval-filter.tsx
@@ -0,0 +1,99 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+import { Component } from 'vue-property-decorator';
+import { Component as tsc } from 'vue-tsx-support';
+
+import ResidentSetting from './resident-setting';
+import UiSelector from './ui-selector';
+import { EMode, MODE_LIST } from './utils';
+
+import './retrieval-filter.scss';
+
+@Component
+export default class RetrievalFilter extends tsc<object> {
+  /* 当前查询模式 */
+  mode = EMode.ui;
+  /* 是否展开常驻设置 */
+  residentSettingActive = false;
+
+  handleChangeMode() {
+    this.mode = this.mode === EMode.ui ? EMode.ql : EMode.ui;
+  }
+  handleShowResidentSetting() {
+    this.residentSettingActive = !this.residentSettingActive;
+  }
+  render() {
+    return (
+      <div class='retrieval-filter__component'>
+        <div class='retrieval-filter__component-main'>
+          <div
+            class='component-left'
+            onClick={() => this.handleChangeMode()}
+          >
+            {MODE_LIST.filter(item => item.id === this.mode).map(item => [
+              <span
+                key={`${item.id}_0`}
+                class='text'
+              >
+                {item.name}
+              </span>,
+              <div
+                key={`${item.id}_1`}
+                class='mode-icon'
+              >
+                <span class='icon-monitor icon-switch' />
+              </div>,
+            ])}
+          </div>
+          <div class='filter-content'>{this.mode === EMode.ui ? <UiSelector /> : undefined}</div>
+          <div class='component-right'>
+            {this.mode === EMode.ui && (
+              <div
+                class={['setting-btn', { 'btn-active': this.residentSettingActive }]}
+                v-bk-tooltips={{
+                  content: window.i18n.tc('常驻筛选'),
+                  delay: 300,
+                }}
+                onClick={() => this.handleShowResidentSetting()}
+              >
+                <span class='icon-monitor icon-tongyishezhi' />
+              </div>
+            )}
+            {false && (
+              <div class='favorite-btn'>
+                <span class='icon-monitor icon-mc-uncollect' />
+              </div>
+            )}
+            <div class='search-btn'>
+              <span class='icon-monitor icon-mc-search' />
+            </div>
+          </div>
+        </div>
+        {this.residentSettingActive && <ResidentSetting />}
+      </div>
+    );
+  }
+}
---
+++ bkmonitor/webpack/src/monitor-pc/pages/event-explore/components/event-retrieval-header.tsx
@@ -0,0 +1,177 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+import { Component, Emit, Prop } from 'vue-property-decorator';
+import { Component as tsc } from 'vue-tsx-support';
+
+import { DEFAULT_TIME_RANGE } from '../../../components/time-range/utils';
+import DashboardTools from '../../monitor-k8s/components/dashboard-tools';
+
+import type { TimeRangeType } from '../../../components/time-range/time-range';
+import type { IDataIdItem, IFormData } from '../typing';
+
+import './event-retrieval-header.scss';
+interface EventRetrievalNavBarProps {
+  timeRange?: TimeRangeType;
+  timezone?: string;
+  refreshInterval?: number;
+  dataIdList?: IDataIdItem[];
+  formData: IFormData;
+}
+
+interface EventRetrievalNavBarEvents {
+  onTimezoneChange(val: string): void;
+  onTimeRangeChange(val: TimeRangeType): void;
+  onImmediateRefresh(): void;
+  onRefreshChange(val: number): void;
+  onDataIdChange(val: string): void;
+  onEventTypeChange(val: { data_source_label: string; data_type_label: string }): void;
+}
+
+@Component
+export default class EventRetrievalHeader extends tsc<EventRetrievalNavBarProps, EventRetrievalNavBarEvents> {
+  @Prop({ required: true }) formData: IFormData;
+  @Prop({ default: () => [] }) dataIdList: IDataIdItem[];
+  // 数据间隔
+  @Prop({ default: () => DEFAULT_TIME_RANGE, type: Array }) timeRange: TimeRangeType;
+  // 自动刷新数据间隔
+  @Prop({ default: -1 }) readonly refreshInterval: number;
+  // 时区
+  @Prop({ type: String }) timezone: string;
+
+  dataIdToggle = false;
+
+  get selectDataIdName() {
+    return this.dataIdList.find(item => item.id === this.formData.result_table_id)?.name || '';
+  }
+
+  handleDataIdToggle(toggle: boolean) {
+    this.dataIdToggle = toggle;
+  }
+
+  @Emit('timeRangeChange')
+  handleTimeRangeChange(val: TimeRangeType) {
+    return val;
+  }
+
+  @Emit('timezoneChange')
+  handleTimezoneChange(v: string) {
+    return v;
+  }
+
+  @Emit('immediateRefresh')
+  handleImmediateRefresh() {}
+
+  @Emit('refreshChange')
+  handleRefreshChange(v: number) {
+    return v;
+  }
+
+  @Emit('dataIdChange')
+  handleDataIdChange(dataId: string) {
+    return dataId;
+  }
+
+  handleEventTypeChange(type: 'bk_monitor' | 'custom') {
+    if (this.formData.data_source_label === type) return;
+    if (type === 'custom') {
+      this.$emit('eventTypeChange', { data_source_label: 'custom', data_type_label: 'event' });
+      return;
+    }
+    this.$emit('eventTypeChange', { data_source_label: 'bk_monitor', data_type_label: 'log' });
+  }
+
+  render() {
+    return (
+      <div class='event-retrieval-header'>
+        <div class='header-left'>
+          <div class='favorite-btn'>
+            <i class='icon-monitor icon-back-right' />
+            <span class='text'>{this.$t('收藏夹')}</span>
+          </div>
+          <div class='event-type-select'>
+            <div
+              class={{ item: true, active: this.formData.data_source_label === 'custom' }}
+              onClick={() => this.handleEventTypeChange('custom')}
+            >
+              {this.$t('自定义上报事件')}
+            </div>
+            <div
+              class={{ item: true, active: this.formData.data_source_label === 'bk_monitor' }}
+              onClick={() => this.handleEventTypeChange('bk_monitor')}
+            >
+              {this.$t('日志关键字')}
+            </div>
+          </div>
+          <bk-select
+            class='data-id-select'
+            clearable={false}
+            value={this.formData.result_table_id}
+            searchable
+            onChange={this.handleDataIdChange}
+            onToggle={this.handleDataIdToggle}
+          >
+            <div
+              class='data-id-select-trigger'
+              slot='trigger'
+            >
+              <span>
+                <span class='prefix'>{this.$t('数据ID')}:</span>
+                <span class='name'>{this.selectDataIdName}</span>
+              </span>
+              <span class={`icon-monitor icon-mc-arrow-down ${this.dataIdToggle ? 'expand' : ''}`} />
+            </div>
+            {this.dataIdList.map(item => (
+              <bk-option
+                id={item.id}
+                key={item.id}
+                name={item.name}
+              />
+            ))}
+          </bk-select>
+        </div>
+        <div class='header-tools'>
+          <DashboardTools
+            isSplitPanel={false}
+            menuList={[]}
+            refleshInterval={this.refreshInterval}
+            showDownSampleRange={false}
+            showFullscreen={false}
+            showListMenu={false}
+            showSplitPanel={false}
+            timeRange={this.timeRange}
+            timezone={this.timezone}
+            onImmediateReflesh={this.handleImmediateRefresh}
+            onRefleshChange={this.handleRefreshChange}
+            onTimeRangeChange={this.handleTimeRangeChange}
+            onTimezoneChange={this.handleTimezoneChange}
+          >
+            {this.$slots.dashboardTools}
+          </DashboardTools>
+        </div>
+      </div>
+    );
+  }
+}
