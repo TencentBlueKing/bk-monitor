204727fd5b0324b0dcdce70ba44640c6a9bb0612
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-02-24 10:21:01 +0800

    feat: 事件检索过滤栏开发 (#5133)

--- bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/resident-setting.tsx
+++ bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/resident-setting.tsx
@@ -23,22 +23,119 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { Component } from 'vue-property-decorator';
+import { Component, Prop, Ref } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 
+import ResidentSettingTransfer from './resident-setting-transfer';
+import SettingKvSelector from './setting-kv-selector';
+import { defaultWhereItem, type IFilterField, type IWhereItem } from './utils';
+
 import './resident-setting.scss';
+interface IProps {
+  fields: IFilterField[];
+}
+
+interface ILocalValue {
+  field: IFilterField;
+  value: IWhereItem;
+}
 
 @Component
-export default class ResidentSetting extends tsc<object> {
+export default class ResidentSetting extends tsc<IProps> {
+  @Prop({ type: Array, default: () => [] }) fields: IFilterField[];
+  @Ref('selector') selectorRef: HTMLDivElement;
+  popoverInstance = null;
+
+  localValue: ILocalValue[] = [];
+
+  async handleShowSelect(event: MouseEvent) {
+    if (this.popoverInstance) {
+      this.destroyPopoverInstance();
+      return;
+    }
+    this.popoverInstance = this.$bkPopover(event.target, {
+      content: this.selectorRef,
+      trigger: 'click',
+      placement: 'bottom-start',
+      theme: 'light common-monitor',
+      arrow: false,
+      interactive: true,
+      boundary: 'window',
+      distance: 15,
+      zIndex: 998,
+      animation: 'slide-toggle',
+      followCursor: false,
+      onHidden: () => {
+        this.destroyPopoverInstance();
+      },
+    });
+    await this.$nextTick();
+    this.popoverInstance?.show();
+  }
+
+  destroyPopoverInstance() {
+    this.popoverInstance?.hide?.();
+    this.popoverInstance?.destroy?.();
+    this.popoverInstance = null;
+  }
+
+  handleShowSettingTransfer(event: MouseEvent) {
+    event.stopPropagation();
+    this.handleShowSelect({
+      target: this.$el,
+    } as any);
+  }
+
+  /**
+   * @description 点击弹层取消
+   */
+  handleCancel() {
+    this.destroyPopoverInstance();
+  }
+  /**
+   * @description 点击弹层确认
+   */
+  handleConfirm(fields: IFilterField[]) {
+    this.localValue = fields.map(item => ({
+      field: item,
+      value: defaultWhereItem({
+        key: item.name,
+      }),
+    }));
+    this.destroyPopoverInstance();
+  }
+
   render() {
     return (
       <div class='retrieval-filter__resident-setting-component'>
-        <span class='left-btn'>
+        <span
+          class='left-btn'
+          onClick={this.handleShowSettingTransfer}
+        >
           <span class='icon-monitor icon-setting' />
           <span class='setting-text'>{this.$t('设置筛选')}</span>
         </span>
         <div class='right-content'>
-          <span class='placeholder-text'>{`（${this.$t('暂未设置常驻筛选，请点击左侧设置按钮')}）`}</span>
+          {this.localValue.length ? (
+            this.localValue.map((item, index) => (
+              <SettingKvSelector
+                key={index}
+                class='mb-4 mr-4'
+                field={item.field}
+                value={item.value}
+              />
+            ))
+          ) : (
+            <span class='placeholder-text'>{`（${this.$t('暂未设置常驻筛选，请点击左侧设置按钮')}）`}</span>
+          )}
+        </div>
+        <div style='display: none;'>
+          <div ref='selector'>
+            <ResidentSettingTransfer
+              fields={this.fields}
+              onConfirm={this.handleConfirm}
+            />
+          </div>
         </div>
       </div>
     );
