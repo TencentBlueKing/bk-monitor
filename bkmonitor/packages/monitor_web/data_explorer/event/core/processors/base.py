e65064927e4015a0a3f45a73908693c1eee72b97
crayon <873217631@qq.com>
2025-02-25 11:47:20 +0800

    feat: 事件丰富（提升可读性）基础框架 #1010158081122091641 # Reviewed, transaction id: 31270

--- bkmonitor/packages/monitor_web/data_explorer/event/core/processors/base.py
+++ bkmonitor/packages/monitor_web/data_explorer/event/core/processors/base.py
@@ -8,3 +8,21 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
+import abc
+from typing import Any, Dict, List
+
+
+class BaseEventProcessor(abc.ABC):
+    """事件处理基类"""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def process(self, origin_events: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        """丰富原始事件，输出丰富后的事件数据列表
+        :param origin_events: 原始事件列表
+        :return:
+        """
+        raise NotImplementedError
