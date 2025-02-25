05c4766ef729581da8fb15b1a49bd075ecca32ff
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-02-25 19:40:20 +0800

    feat: 事件检索过滤栏开发 (#5175)

--- bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/ui-selector-options.tsx
+++ bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/ui-selector-options.tsx
@@ -24,8 +24,10 @@
  * IN THE SOFTWARE.
  */
 
-import { Component, Prop, Watch } from 'vue-property-decorator';
+import { Component, Prop, Ref, Watch } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
+
+import { Debounce } from 'monitor-common/utils';
 
 import {
   fieldTypeMap,
@@ -35,6 +37,7 @@ import {
   type IFilterField,
   ECondition,
   type IFilterItem,
+  EMethod,
 } from './utils';
 import ValueTagSelector, { type IValue } from './value-tag-selector';
 
@@ -44,6 +47,7 @@ interface IProps {
   fields: IFilterField[];
   value?: IFilterItem;
   show?: boolean;
+  keyword?: string; // 上层传的关键字，用于搜索
   getValueFn?: (params: IGetValueFnParams) => Promise<IWhereValueOptionsItem>;
   onConfirm?: (v: IFilterItem) => void;
   onCancel?: () => void;
@@ -62,12 +66,16 @@ export default class UiSelectorOptions extends tsc<IProps> {
   getValueFn: (params: IGetValueFnParams) => Promise<IWhereValueOptionsItem>;
   @Prop({ type: Object, default: () => null }) value: IFilterItem;
   @Prop({ type: Boolean, default: false }) show: boolean;
+  @Prop({ type: String, default: '' }) keyword: string;
+
+  @Ref('allInput') allInputRef;
   /* 搜索值 */
   searchValue = '';
   /* 当前 */
   localFields: IFilterField[] = [];
+  searchLocalFields: IFilterField[] = [];
   /* 当前光标选中项 */
-  cursorIndex = -1;
+  cursorIndex = 0;
   /* 当前选中项 */
   checkedItem: IFilterField = null;
   /* 全文检索检索内容 */
@@ -82,31 +90,62 @@ export default class UiSelectorOptions extends tsc<IProps> {
   valueLoading = false;
   /* 检索值候选项 */
   valueOptions = [];
+  /* 光标是否移至右边 */
+  cursorRight = false;
 
   get wildcardItem() {
     return this.checkedItem?.supported_operations?.find(item => item.value === this.method)?.options;
   }
 
+  mounted() {
+    document.addEventListener('keydown', this.handleKeydownEvent);
+  }
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.handleKeydownEvent);
+  }
+
   @Watch('fields', { immediate: true })
   handleWatchFields() {
-    this.localFields = this.fields;
-    if (this.localFields.length) {
+    const ids = this.fields.map(item => item.name);
+    const localIds = this.localFields.map(item => item.name);
+    if (JSON.stringify(ids) !== JSON.stringify(localIds)) {
+      this.localFields = this.fields.slice();
       this.handleCheck(this.localFields[0]);
     }
+    this.handleSearchChange();
   }
 
   @Watch('show', { immediate: true })
   handleWatchShow() {
-    if (this.show && this.value) {
-      const id = this.value.key.id;
-      for (const item of this.localFields) {
-        if (item.name === id) {
-          const checkedItem = JSON.parse(JSON.stringify(item));
-          this.handleCheck(checkedItem, this.value.method.id, this.value.value);
-          break;
+    if (this.show) {
+      if (this.value) {
+        const id = this.value.key.id;
+        for (const item of this.localFields) {
+          if (item.name === id) {
+            const checkedItem = JSON.parse(JSON.stringify(item));
+            this.handleCheck(checkedItem, this.value.method.id, this.value.value);
+            break;
+          }
         }
       }
+    } else {
+      this.initData();
     }
+  }
+
+  initData() {
+    this.searchValue = '';
+    this.localFields = [];
+    this.searchLocalFields = [];
+    this.cursorIndex = 0;
+    this.checkedItem = null;
+    this.queryString = '';
+    this.method = '';
+    this.values = [];
+    this.isWildcard = false;
+    this.valueLoading = false;
+    this.valueOptions = [];
+    this.cursorRight = false;
   }
 
   /**
@@ -116,28 +155,41 @@ export default class UiSelectorOptions extends tsc<IProps> {
   handleCheck(item: IFilterField, method = '', value = []) {
     this.checkedItem = JSON.parse(JSON.stringify(item));
     this.values = value || [];
+    this.method = method || item?.supported_operations?.[0]?.value || '';
+    this.getValueData(this.checkedItem, method, value);
+    const index = this.searchLocalFields.findIndex(f => f.name === item.name) || 0;
+    this.cursorIndex = index;
+  }
+
+  /**
+   * @description 搜索接口
+   * @param item
+   * @param method
+   * @param value
+   */
+  async getValueData(item: IFilterField, method = '', value = []) {
     this.valueOptions = [];
     this.valueLoading = true;
-    if (this.checkedItem?.name !== '*') {
-      this.method = method || this.checkedItem?.supported_operations?.[0]?.value || '';
-      this.getValueFn({
+    if (item.name !== '*') {
+      const data = await this.getValueFn({
         where: [
           {
-            key: this.checkedItem.name,
-            method: this.method,
-            value: this.values,
+            key: item.name,
+            method: method,
+            value: value,
             condition: ECondition.and,
+            options: this.isWildcard
+              ? {
+                  is_wildcard: true,
+                }
+              : undefined,
           },
         ],
-        fields: [this.checkedItem.name],
+        fields: [item.name],
         limit: 5,
-      })
-        .then(data => {
-          this.valueOptions = data.list;
-        })
-        .finally(() => {
-          this.valueLoading = false;
-        });
+      });
+      this.valueOptions = data.list;
+      this.valueLoading = false;
     }
   }
 
@@ -145,6 +197,16 @@ export default class UiSelectorOptions extends tsc<IProps> {
    * @description 点击确定
    */
   handleConfirm() {
+    if (this.checkedItem.name === '*' && this.queryString) {
+      const value: IFilterItem = {
+        key: { id: this.checkedItem.name, name: this.$tc('全文') },
+        method: { id: EMethod.include, name: this.$tc('包含') },
+        value: [{ id: this.queryString, name: this.queryString }],
+        condition: { id: ECondition.and, name: 'AND' },
+      };
+      this.$emit('confirm', value);
+      return;
+    }
     if (this.values.length) {
       const methodName = this.checkedItem.supported_operations.find(item => item.value === this.method)?.alias;
       const value: IFilterItem = {
@@ -162,8 +224,19 @@ export default class UiSelectorOptions extends tsc<IProps> {
     this.$emit('cancel');
   }
 
+  /**
+   * @description 检索值更新
+   * @param v
+   */
   handleValueChange(v: IValue[]) {
     this.values = v;
+  }
+
+  /**
+   * @description 是否通配符
+   */
+  handleIsWildcardChange() {
+    this.handleChange();
   }
 
   handleChange() {
@@ -174,9 +247,116 @@ export default class UiSelectorOptions extends tsc<IProps> {
         method: { id: this.method as any, name: methodName },
         value: this.values,
         condition: { id: ECondition.and, name: 'AND' },
+        options: this.isWildcard
+          ? {
+              is_wildcard: true,
+            }
+          : undefined,
       };
       this.$emit('change', value);
     }
+  }
+
+  /**
+   * @description 搜索
+   * @param v
+   */
+  @Debounce(500)
+  handleValueSearch(v: string) {
+    this.getValueData(this.checkedItem, this.method, [v]);
+  }
+
+  /**
+   * @description 监听键盘事件
+   * @param event
+   * @returns
+   */
+  handleKeydownEvent(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      event.preventDefault();
+      this.handleCancel();
+    } else if (event.key === 'Enter' && event.ctrlKey) {
+      event.preventDefault();
+      this.handleConfirm();
+      return;
+    }
+    if (this.cursorRight) {
+      return;
+    }
+    switch (event.key) {
+      case 'ArrowUp': {
+        // 按下上箭头键
+        event.preventDefault();
+        this.cursorIndex -= 1;
+        if (this.cursorIndex < 0) {
+          this.cursorIndex = this.searchLocalFields.length - 1;
+        }
+        this.updateSelection();
+        break;
+      }
+
+      case 'ArrowDown': {
+        event.preventDefault();
+        this.cursorIndex += 1;
+        if (this.cursorIndex > this.searchLocalFields.length) {
+          this.cursorIndex = 0;
+        }
+        this.updateSelection();
+        break;
+      }
+      case 'Enter': {
+        event.preventDefault();
+        this.enterSelection();
+        break;
+      }
+    }
+  }
+  /**
+   * @description 聚焦选中项
+   */
+  updateSelection() {
+    this.$nextTick(() => {
+      const listEl = this.$el.querySelector('.component-top-left .options-wrap');
+      const el = listEl?.children?.[this.cursorIndex];
+      if (el) {
+        el.scrollIntoView(false);
+      }
+    });
+  }
+  /**
+   * @description 回车选中项
+   */
+  enterSelection() {
+    const item = this.searchLocalFields[this.cursorIndex];
+    if (item) {
+      if (item.name === '*') {
+        if (!this.keyword) this.allInputRef?.focus();
+      } else {
+        this.queryString = '';
+        this.handleCheck(item);
+      }
+      this.cursorRight = true;
+    }
+  }
+
+  @Debounce(300)
+  handleSearchChange() {
+    this.cursorIndex = 0;
+    if (!this.searchValue) {
+      this.searchLocalFields = this.localFields.slice();
+    }
+    this.searchLocalFields = this.localFields.filter(item => {
+      if (!this.searchValue) {
+        return true;
+      }
+      if (item.alias.toLocaleLowerCase().includes(this.searchValue.toLocaleLowerCase())) {
+        return true;
+      }
+      if (item.name.toLocaleLowerCase().includes(this.searchValue.toLocaleLowerCase())) {
+        return true;
+      }
+      return false;
+    });
   }
 
   render() {
@@ -190,6 +370,7 @@ export default class UiSelectorOptions extends tsc<IProps> {
             <div class='form-item-label mt-16'>{this.$t('检索内容')}</div>
             <div class='form-item-content mt-8'>
               <bk-input
+                ref={'allInput'}
                 v-model={this.queryString}
                 placeholder={this.$t('请输入')}
                 rows={15}
@@ -233,15 +414,22 @@ export default class UiSelectorOptions extends tsc<IProps> {
               <div class='form-item-label'>
                 <span class='left'>{this.$t('检索值')}</span>
                 <span class='right'>
-                  <bk-checkbox v-model={this.isWildcard}>{this.wildcardItem?.label || '使用通配符'}</bk-checkbox>
+                  <bk-checkbox
+                    v-model={this.isWildcard}
+                    onChange={this.handleIsWildcardChange}
+                  >
+                    {this.wildcardItem?.label || '使用通配符'}
+                  </bk-checkbox>
                 </span>
               </div>
               <div class='form-item-content mt-6'>
                 <ValueTagSelector
+                  cursorActive={this.cursorRight}
                   loading={this.valueLoading}
                   options={this.valueOptions}
                   value={this.values}
                   onChange={this.handleValueChange}
+                  onSearch={this.handleValueSearch}
                 />
               </div>
             </div>,
@@ -258,15 +446,20 @@ export default class UiSelectorOptions extends tsc<IProps> {
                 behavior='simplicity'
                 left-icon='bk-icon icon-search'
                 placeholder={this.$t('请输入关键字')}
+                onChange={this.handleSearchChange}
               />
             </div>
             <div class='options-wrap'>
-              {this.localFields.map(item => {
+              {this.searchLocalFields.map((item, index) => {
                 const { title, subtitle } = getTitleAndSubtitle(item.alias);
                 return (
                   <div
                     key={item.name}
-                    class={['option', { checked: this.checkedItem?.name === item.name }]}
+                    class={[
+                      'option',
+                      { checked: this.checkedItem?.name === item.name },
+                      { cursor: index === this.cursorIndex },
+                    ]}
                     onClick={() => this.handleCheck(item)}
                   >
                     <span
