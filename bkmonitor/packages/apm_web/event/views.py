e4ddc1934bd725b4643123a46f1cf025417f8c8b
crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
2025-02-27 14:35:07 +0800

    feat: 新增 TopK 下载接口协议 --story=122138050 (#5211)

--- bkmonitor/packages/apm_web/event/views.py
+++ bkmonitor/packages/apm_web/event/views.py
@@ -8,12 +8,20 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from io import StringIO
+from typing import Any, Dict
+from urllib import parse
+
+from django.http import HttpResponse
+from rest_framework.decorators import action
 
 from apm_web.event import resources
+from apm_web.event.serializers import EventTopKRequestSerializer
 from apm_web.models import Application
 from bkmonitor.iam import ActionEnum, ResourceEnum
 from bkmonitor.iam.drf import InstanceActionForDataPermission
 from core.drf_resource.viewsets import ResourceRoute, ResourceViewSet
+from monitor_web.data_explorer.event.mock_data import API_TOPK_RESPONSE
 
 
 class EventViewSet(ResourceViewSet):
@@ -36,3 +44,24 @@ class EventViewSet(ResourceViewSet):
         ResourceRoute("POST", resources.EventViewConfigResource, endpoint="view_config"),
         ResourceRoute("POST", resources.EventTimeSeriesResource, endpoint="time_series"),
     ]
+
+    @action(methods=["POST"], detail=False, url_path="download_topk")
+    def download_topk(self, request, *args, **kwargs):
+        # TODO 和数据探索侧的事件逻辑重合，这里需要抽象公共逻辑。
+        s = EventTopKRequestSerializer(data=request.data)
+        s.is_valid(raise_exception=True)
+        validated_data: Dict[str, Any] = s.validated_data
+
+        output = StringIO()
+        for item in API_TOPK_RESPONSE[0]["list"]:
+            output.write(f"{item['value']},{item['count']},{item['proportions']:.2f}%\n")
+
+        table: str = validated_data["query_configs"][0]["table"]
+        file_name = f"{table}_{validated_data['fields'][0]}.txt"
+        file_name = parse.quote(file_name, encoding="utf8")
+        file_name = parse.unquote(file_name, encoding="ISO8859_1")
+
+        response = HttpResponse(output.getvalue())
+        response["Content-Type"] = "application/x-msdownload"
+        response["Content-Disposition"] = 'attachment;filename="{}"'.format(file_name)
+        return response
