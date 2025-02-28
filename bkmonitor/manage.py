3e450253b4c800bc993e1bf47fda16355a8707ba
chenguo <chenguo367@163.com>
2025-02-28 11:05:05 +0800

    fix: 修复请求并发时QueryString 语法解析可能会出现错乱的问题 #1010158081138787027 # Reviewed, transaction id: 31895

--- bkmonitor/manage.py
+++ bkmonitor/manage.py
@@ -10,11 +10,31 @@ specific language governing permissions and limitations under the License.
 """
 import os
 import sys
+
 import dotenv
 
 dotenv.load_dotenv()
 
 if __name__ == "__main__":
+    # 我希望判断当前是否有ai_agent模块，如果没有，执行ln软链接命令，将当面目录上层的ai_agent目录链接到当前目录，同时需要支持跨平台的能力
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    target_dir = os.path.join(current_dir, "ai_agent")
+    source_path = os.path.normpath(os.path.join(current_dir, "../ai_agent"))
+
+    if not os.path.exists(target_dir):
+        try:
+            # 跨平台创建符号链接
+            if os.name == 'nt':  # Windows
+                import _winapi
+
+                if not os.path.exists(source_path):
+                    raise FileNotFoundError(f"source path {source_path} does not exist")
+                _winapi.CreateJunction(source_path, target_dir)
+            else:  # POSIX
+                os.symlink(source_path, target_dir)
+        except OSError as e:
+            sys.stderr.write(f"create symlink failed: {str(e)}\n")
+            sys.exit(1)
 
     if "celery" in sys.argv and "gevent" in sys.argv:
         from gevent import monkey
