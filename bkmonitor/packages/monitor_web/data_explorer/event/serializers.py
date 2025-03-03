e85c41406c4dfd9a225148565b79179931b5c0ad
yamltdg <13126400746@163.com>
2025-03-03 15:36:52 +0800

    fix: 【事件 / 数据探索】修复 DownloadTopK 接口序列化器字段重复定义的问题#1010158081122171525

--- bkmonitor/packages/monitor_web/data_explorer/event/serializers.py
+++ bkmonitor/packages/monitor_web/data_explorer/event/serializers.py
@@ -78,10 +78,6 @@ class EventTotalRequestSerializer(BaseEventRequestSerializer):
 
 
 class EventDownloadTopKRequestSerializer(EventTopKRequestSerializer):
-    limit = serializers.IntegerField(label="数量限制", required=False, default=0)
-    fields = serializers.ListField(label="维度字段列表", child=serializers.CharField(label="维度字段"), allow_empty=False)
-    query_configs = serializers.ListField(label="查询配置列表", child=EventFilterSerializer(), allow_empty=False)
-
     def validate(self, attrs):
         attrs = super(BaseEventRequestSerializer, self).validate(attrs)
         attrs = super(EventTopKRequestSerializer, self).validate(attrs)
