b0bc7eaa8ecabb3564d5b66d44f03d8b63784e65
xlc <2933458919@qq.com>
2025-03-03 17:38:27 +0800

    fix: apm拓扑服务概览日志趋势图优化

--- bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/related-log-chart/related-log-chart.tsx
+++ bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/related-log-chart/related-log-chart.tsx
@@ -233,6 +233,31 @@ class RelatedLogChart extends CommonSimpleChart {
     }
     return formatterFunc;
   }
+
+  /**
+   * @description: 在图表数据没有单位或者单位不一致时则不做单位转换 y轴label的转换用此方法做计数简化
+   * @param {number} num
+   * @return {*}
+   */
+  handleYxisLabelFormatter(num: number): string {
+    const si = [
+      { value: 1, symbol: '' },
+      { value: 1e3, symbol: 'K' },
+      { value: 1e6, symbol: 'M' },
+      { value: 1e9, symbol: 'G' },
+      { value: 1e12, symbol: 'T' },
+      { value: 1e15, symbol: 'P' },
+      { value: 1e18, symbol: 'E' },
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    let i: number;
+    for (i = si.length - 1; i > 0; i--) {
+      if (num >= si[i].value) {
+        break;
+      }
+    }
+    return (num / si[i].value).toFixed(3).replace(rx, '$1') + si[i].symbol;
+  }
   /**
    * @desc 更新柱状图数据
    */
@@ -306,6 +331,9 @@ class RelatedLogChart extends CommonSimpleChart {
                           splitNumber: 3,
                         },
                         yAxis: {
+                          axisLabel: {
+                            formatter: (v: number) => this.handleYxisLabelFormatter(v),
+                          },
                           type: 'value',
                           splitNumber: 2,
                           splitLine: {
