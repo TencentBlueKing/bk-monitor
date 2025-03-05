6ce3a402700a2f79c8119d0a66c629526c7e8f0f
jidanjuan <zjjdx123@163.com>
2025-03-05 14:25:28 +0800

    fix: 修复设置筛选过滤功能 # Reviewed, transaction id: 32547

--- bklog/web/src/views/retrieve-v2/search-bar/common-filter-select.vue
+++ bklog/web/src/views/retrieve-v2/search-bar/common-filter-select.vue
@@ -83,49 +83,16 @@
     return operatorMapping[item.operator] ?? operatorDictionary.value[key]?.label ?? item.operator;
   };
 
-  const rquestFieldEgges = (() => {
-    return (field, index, operator?, value?, callback?) => {
-      const getConditionValue = () => {
-        if (['keyword'].includes(field.field_type)) {
-          return [`*${value}*`];
-        }
-
-        return [];
-      };
-      commonFilterAddition.value[index].list.splice(0, commonFilterAddition.value[index].list.length);
-
-      if (value !== undefined && value !== null && !['keyword', 'text'].includes(field.field_type)) {
-        return;
-      }
-
-      const size = ['keyword'].includes(field.field_type) && value?.length > 0 ? 10 : 100;
-      isRequesting.value = true;
-
-      requestTimer && clearTimeout(requestTimer);
-      requestTimer = setTimeout(() => {
-        const targetAddition = value
-          ? [{ field: field.field_name, operator: '=~', value: getConditionValue() }].map(val => {
-              const instance = new ConditionOperator(val);
-              return instance.getRequestParam();
-            })
-          : [];
-        store
-          .dispatch('requestIndexSetValueList', { fields: [field], targetAddition, force: true, size })
-          .then(res => {
-            const arr = res.data?.aggs_items?.[field.field_name] || [];
-            commonFilterAddition.value[index].list = arr.filter(item => item);
-          })
-          .finally(() => {
-            isRequesting.value = false;
-          });
-      }, 300);
-    };
-  })();
-
   const handleToggle = (visable, item, index) => {
     if (visable) {
       activeIndex.value = index;
-      rquestFieldEgges(item, index, null, null, () => {});
+      const { requestFieldEgges } = useFieldEgges();
+      requestFieldEgges(item, null, resp => {
+        if (typeof resp === 'boolean') {
+          return;
+        }
+        commonFilterAddition.value[index].list = store.state.indexFieldInfo.aggs_items[item.field_name] ?? [];
+      });
     }
   };
 
