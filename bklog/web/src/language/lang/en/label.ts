8089eb04e4550c9a70c34217f3c81f6bd7430557
jidanjuan <112756875+jidanjuan@users.noreply.github.com>
2025-03-05 14:38:24 +0800

    fix: 修复设置筛选过滤功能 (#5292)
    # Reviewed, transaction id: 32527

--- bklog/web/src/language/lang/en/label.ts
+++ bklog/web/src/language/lang/en/label.ts
@@ -734,4 +734,6 @@ export default {
   '日用量/总用量': 'Daily Usage/Total Usage',
   'JSON 字段动态新增': 'JSON field dynamically added',
   对象: 'Object',
+  待选列表: 'List to be selected:',
+  常驻筛选: 'Persistent filtering:',
 };
