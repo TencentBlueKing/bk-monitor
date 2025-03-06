b5d11ba93d6354c712b227297f8ad33c622da075
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2025-03-06 20:56:04 +0800

    feat: 1. 完成收藏功能调整 2. 维度统计面板抽离组件 (#5335)

--- bkmonitor/webpack/src/monitor-pc/pages/event-explore/components/dimension-filter-panel.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/event-explore/components/dimension-filter-panel.tsx
@@ -28,12 +28,13 @@ import { Component, Emit, Ref, InjectReactive, Prop, Watch } from 'vue-property-
 import { Component as tsc } from 'vue-tsx-support';
 
 import EmptyStatus from '../../../components/empty-status/empty-status';
-import { getDownloadTopK, getEventTopK } from '../api-utils';
+import { getEventTopK } from '../api-utils';
 import FieldTypeIcon from './field-type-icon';
+import StatisticsList from './statistics-list';
 
 import type { EmptyStatusType } from '../../../components/empty-status/types';
 import type { IWhereItem } from '../../../components/retrieval-filter/utils';
-import type { IDimensionField, ITopKField } from '../typing';
+import type { IDimensionField } from '../typing';
 
 import './dimension-filter-panel.scss';
 
@@ -55,6 +56,7 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
   @Prop({ default: false }) listLoading!: boolean;
 
   @Ref('dimensionPopover') dimensionPopoverRef!: HTMLDivElement;
+  @Ref('statisticsList') statisticsListRef!: StatisticsList;
 
   @InjectReactive('formatTimeRange') formatTimeRange;
   @InjectReactive('commonParams') commonParams;
@@ -69,21 +71,11 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
   searchResultList: IDimensionField[] = [];
   /** 已选择的字段 */
   selectField = '';
-  popoverLoading = true;
   /** popover实例 */
   popoverInstance = null;
-  /** 统计列表 */
-  statisticsList: ITopKField[] = [];
-
-  sliderShow = false;
-  sliderLoading = false;
-  sliderDimensionList: ITopKField[] = [];
-
-  downloadLoading = false;
 
   @Watch('condition')
   async watchConditionChange() {
-    console.log(123);
     await this.getFieldCount();
   }
 
@@ -109,12 +101,11 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
 
   /** 维度项点击 */
   async handleDimensionItemClick(e: Event, item) {
-    this.popoverLoading = true;
     this.destroyPopover();
     this.selectField = item.name;
-    if (!item.is_option_enabled) return;
+    if (!item.is_option_enabled || this.fieldListCount[item.name] === 0) return;
     this.popoverInstance = this.$bkPopover(e.currentTarget, {
-      content: this.dimensionPopoverRef,
+      content: this.statisticsListRef.$refs.dimensionPopover,
       placement: 'right',
       width: 400,
       distance: -5,
@@ -125,12 +116,6 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
       interactive: true,
     });
     this.popoverInstance?.show(100);
-    const list = await this.getFieldTopK({
-      limit: 5,
-      fields: [item.name],
-    });
-    this.statisticsList = list;
-    this.popoverLoading = false;
   }
 
   destroyPopover() {
@@ -140,11 +125,8 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
   }
 
   @Emit('conditionChange')
-  handleConditionChange(type, item: ITopKField['list'][0]) {
-    if (type === 'eq') {
-      return [{ condition: 'and', key: this.selectField, method: 'eq', value: [item.value] }];
-    }
-    return [{ condition: 'and', key: this.selectField, method: 'ne', value: [item.value] }];
+  handleConditionChange(value) {
+    return value;
   }
 
   async getFieldCount() {
@@ -174,100 +156,23 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
   handleClose() {}
 
   // 渲染骨架屏
-  renderSkeleton(type: 'page' | 'topKList') {
-    if (type === 'page')
-      return (
-        <div class='dimension-filter-panel-skeleton'>
-          <div class='skeleton-element title' />
-          <div class='skeleton-element search-input' />
-          {new Array(10).fill(null).map((item, index) => (
-            <div
-              key={index}
-              class='skeleton-element list-item'
-            />
-          ))}
-        </div>
-      );
+  renderSkeleton() {
     return (
-      <div class='skeleton-wrap'>
-        {new Array(5).fill(null).map((_, index) => (
+      <div class='dimension-filter-panel-skeleton'>
+        <div class='skeleton-element title' />
+        <div class='skeleton-element search-input' />
+        {new Array(10).fill(null).map((item, index) => (
           <div
             key={index}
-            class='skeleton-element'
+            class='skeleton-element list-item'
           />
         ))}
       </div>
     );
   }
 
-  /** 渲染TopK字段行 */
-  renderTopKField(list: ITopKField['list'] = []) {
-    return (
-      <div class='top-k-list'>
-        {list.map(item => (
-          <div
-            key={item.value}
-            class='top-k-list-item'
-          >
-            <div class='filter-tools'>
-              <i
-                class='icon-monitor icon-a-sousuo'
-                onClick={() => this.handleConditionChange('eq', item)}
-              />
-              <i
-                class='icon-monitor icon-sousuo-'
-                onClick={() => this.handleConditionChange('ne', item)}
-              />
-            </div>
-            <div class='progress-content'>
-              <div class='info-text'>
-                <span class='field-name'>{item.alias}</span>
-                <span class='counts'>
-                  <span class='total'>{this.$t('{0}条', [item.count])}</span>
-                  <span class='progress-count'>{item.proportions}%</span>
-                </span>
-              </div>
-              <bk-progress
-                color='#5AB8A8'
-                percent={item.proportions / 100}
-                show-text={false}
-                stroke-width={6}
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-
-  async showMore() {
-    this.sliderShow = true;
-    this.sliderLoading = true;
-    this.destroyPopover();
-    this.sliderDimensionList = await this.getFieldTopK({
-      limit: this.statisticsList[0].distinct_count,
-      fields: [this.selectField],
-    }).catch(() => []);
-    this.sliderLoading = false;
-  }
-
-  handleSliderShowChange(show: boolean) {
-    this.sliderShow = show;
-  }
-
-  async handleDownload() {
-    this.downloadLoading = true;
-    await getDownloadTopK({
-      limit: this.statisticsList[0].distinct_count,
-      fields: [this.selectField],
-      ...this.commonParams,
-    }).finally(() => {
-      this.downloadLoading = false;
-    });
-  }
-
   render() {
-    if (this.listLoading) return this.renderSkeleton('page');
+    if (this.listLoading) return this.renderSkeleton();
 
     return (
       <div class='dimension-filter-panel-comp'>
@@ -319,73 +224,13 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
         )}
 
         <div style={{ display: 'none' }}>
-          <div
-            ref='dimensionPopover'
-            class='event-retrieval-dimension-filter-content'
-          >
-            <div class='popover-header'>
-              <div class='title'>
-                {this.selectField}
-                {this.$t('去重后的字段统计')}
-              </div>
-              <div class='count'>{this.statisticsList[0]?.distinct_count || 0}</div>
-            </div>
-            {this.popoverLoading
-              ? this.renderSkeleton('topKList')
-              : [
-                  this.renderTopKField(this.statisticsList[0]?.list),
-                  this.statisticsList[0].distinct_count > 2 && (
-                    <div
-                      class='load-more'
-                      onClick={this.showMore}
-                    >
-                      {this.$t('更多')}
-                    </div>
-                  ),
-                ]}
-          </div>
+          <StatisticsList
+            ref='statisticsList'
+            selectField={this.selectField}
+            onConditionChange={this.handleConditionChange}
+            onShowMore={this.destroyPopover}
+          />
         </div>
-
-        <bk-sideslider
-          width='480'
-          ext-cls='dimension-top-k-slider'
-          is-show={this.sliderShow}
-          show-mask={false}
-          quick-close
-          {...{ on: { 'update:isShow': this.handleSliderShowChange } }}
-        >
-          <div
-            class='dimension-slider-header'
-            slot='header'
-          >
-            <div class='title'>
-              <span class='field-name'>{this.selectField}</span>
-              <span class='divider' />
-              <span class='desc'>
-                {this.$t('去重后的字段统计')} ({this.sliderDimensionList[0]?.distinct_count || 0})
-              </span>
-            </div>
-            {this.downloadLoading ? (
-              <bk-spin size='mini' />
-            ) : (
-              <div
-                class='download-tool'
-                onClick={this.handleDownload}
-              >
-                <i class='icon-monitor icon-xiazai1' />
-                <span class='text'>{this.$t('下载')}</span>
-              </div>
-            )}
-          </div>
-          <div
-            class='dimension-slider-content'
-            slot='content'
-          >
-            {this.sliderLoading
-              ? this.renderSkeleton('topKList')
-              : this.renderTopKField(this.sliderDimensionList[0]?.list)}
-          </div>
-        </bk-sideslider>
       </div>
     );
   }
