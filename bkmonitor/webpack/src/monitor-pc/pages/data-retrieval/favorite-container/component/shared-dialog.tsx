a4fc6f7a1c0617a1abcf6e626c8602d1d272d3ce
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2025-03-06 20:56:04 +0800

    feat: 1. 完成收藏功能调整 2. 维度统计面板抽离组件 (#5335)

--- bkmonitor/webpack/src/monitor-pc/pages/data-retrieval/favorite-container/component/shared-dialog.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/data-retrieval/favorite-container/component/shared-dialog.tsx
@@ -119,7 +119,7 @@ export default class SharedDialog extends tsc<IProps> {
       }
     });
     const allList: IListItem[] = [];
-    if (!!list.children.length) {
+    if (list.children.length) {
       list.children = list.children.slice(0, 500);
       allList.push(list);
     }
