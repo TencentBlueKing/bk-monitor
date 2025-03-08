75f9dbcdf1b297ee1786d221ce43bf6249a3eea0
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-03-08 13:41:28 +0800

    feat: 事件检索过滤栏语句模式开发 (#5350)

--- bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/qs-selector-options.tsx
+++ bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/qs-selector-options.tsx
@@ -28,55 +28,56 @@ import { Component as tsc } from 'vue-tsx-support';
 
 import { Debounce } from 'monitor-common/utils';
 
+import EmptyStatus from '../empty-status/empty-status';
 import QsSelectorHelp from './qs-selector-help';
+import { getQueryStringMethods, QUERY_STRING_CONDITIONS, queryStringColorMap } from './query-string-utils';
 import TextHighlighter from './text-highlighter';
-import { EQueryStringTokenType, queryStringColorMap, type IFilterField } from './utils';
+import { EQueryStringTokenType, type IGetValueFnParams, type IWhereValueOptionsItem, type IFilterField } from './utils';
 
 import './qs-selector-options.scss';
 
 interface IOptions {
   id: string;
   name: string;
+  desc?: string;
 }
 
 interface IProps {
   fields: IFilterField[];
+  field?: string;
   search?: string;
   type?: EQueryStringTokenType;
   show?: boolean;
   onSelect?: (v: string) => void;
+  getValueFn?: (params: IGetValueFnParams) => Promise<IWhereValueOptionsItem>;
 }
 
 @Component
 export default class QsSelectorSelector extends tsc<IProps> {
+  /* 当前输入的值 */
   @Prop({ type: String, default: '' }) search: string;
+  /* 所有字段 */
   @Prop({ type: Array, default: () => [] }) fields: IFilterField[];
+  /* 当前选项类型 */
   @Prop({ type: String, default: '' }) type: EQueryStringTokenType;
+  /* 是否展示 */
   @Prop({ type: Boolean, default: false }) show: boolean;
+  /* 获取value选项时的当前的key */
+  @Prop({ type: String, default: '' }) field: string;
+  @Prop({
+    type: Function,
+    default: () =>
+      Promise.resolve({
+        count: 0,
+        list: [],
+      }),
+  })
+  getValueFn: (params: IGetValueFnParams) => Promise<IWhereValueOptionsItem>;
+
+  loading = false;
 
   localOptions: IOptions[] = [];
-  favoriteOptions = [
-    {
-      title: '个人收藏 / 审计数据',
-      content: '* AND (xxxx =[xxxxxxxx] AND hostID = [2342345] AND bbaq = [cw3rsdfaasd])',
-      keyword: 'xxxx =[xxxxxxxx]',
-    },
-    {
-      title: '个人收藏 / 审计数据',
-      content: '* AND (xxxx =[xxxxxxxx] AND hostID = [2342345] AND bbaq = [cw3rsdfaasd])',
-      keyword: 'xxxx =[xxxxxxxx]',
-    },
-    {
-      title: '个人收藏 / 审计数据',
-      content: '* AND (xxxx =[xxxxxxxx] AND hostID = [2342345] AND bbaq = [cw3rsdfaasd])',
-      keyword: 'xxxx =[xxxxxxxx]',
-    },
-    {
-      title: '个人收藏 / 审计数据',
-      content: '* AND (xxxx =[xxxxxxxx] AND hostID = [2342345] AND bbaq = [cw3rsdfaasd])',
-      keyword: 'xxxx =[xxxxxxxx]',
-    },
-  ];
+  favoriteOptions: { title: string; content: string; keyword: string }[] = [];
 
   cursorIndex = 0;
 
@@ -86,10 +87,12 @@ export default class QsSelectorSelector extends tsc<IProps> {
 
   @Watch('type', { immediate: true })
   handleWatchFieldType() {
+    console.log(this.type, 'type------------');
     this.handleGetOptions();
   }
   @Watch('search', { immediate: true })
   handleWatchSearch() {
+    console.log(this.search, 'search--------------');
     this.handleGetOptions();
   }
   @Watch('show', { immediate: true })
@@ -101,13 +104,55 @@ export default class QsSelectorSelector extends tsc<IProps> {
       document.removeEventListener('keydown', this.handleKeydownEvent);
     }
   }
-  @Debounce(200)
-  handleGetOptions() {
+  @Debounce(300)
+  async handleGetOptions() {
     this.cursorIndex = 0;
+    let fieldItem: IFilterField = null;
+    for (const item of this.fields) {
+      if (item.name === this.field) {
+        fieldItem = item;
+        break;
+      }
+    }
+    console.log(this.field, this.type, fieldItem);
     if (this.type === EQueryStringTokenType.key) {
-      this.localOptions = this.fields.map(item => ({
-        id: item.name,
-        name: item.name,
+      this.localOptions = this.fields
+        .filter(item => {
+          if (!this.search) {
+            return true;
+          }
+          if (item.alias.toLocaleLowerCase().includes(this.search.toLocaleLowerCase())) {
+            return true;
+          }
+          if (item.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase())) {
+            return true;
+          }
+          return false;
+        })
+        .map(item => ({
+          id: item.name,
+          name: item.name,
+        }));
+    } else if (this.type === EQueryStringTokenType.method) {
+      this.localOptions = getQueryStringMethods(fieldItem?.type).map(item => ({
+        id: item.id,
+        name: item.id,
+        desc: item.name,
+      }));
+    } else if (this.type === EQueryStringTokenType.value) {
+      if (fieldItem?.is_option_enabled) {
+        if (!this.loading) {
+          const data = await this.getValueData();
+          this.localOptions = data;
+        }
+      } else {
+        this.localOptions = [];
+      }
+    } else if (this.type === EQueryStringTokenType.condition) {
+      this.localOptions = QUERY_STRING_CONDITIONS.map(item => ({
+        id: item.id,
+        name: item.id,
+        desc: item.name,
       }));
     }
   }
@@ -166,50 +211,89 @@ export default class QsSelectorSelector extends tsc<IProps> {
     });
   }
 
+  /**
+   * @description 搜索接口
+   * @param item
+   * @param method
+   * @param value
+   */
+  async getValueData() {
+    let list = [];
+    this.loading = true;
+    if (this.field) {
+      const data = await this.getValueFn({
+        queryString: `${this.field} : *`,
+        fields: [this.field],
+        limit: 5,
+      });
+      list = data.list;
+    }
+    this.loading = false;
+    return list;
+  }
+
   render() {
     return (
       <div class='retrieval-filter__qs-selector-options-component'>
         <div class='wrap-left'>
           <div class='options-wrap'>
-            {this.localOptions.map((item, index) => (
-              <div
-                key={item.id}
-                class={['option-item', { 'cursor-active': index === this.cursorIndex }]}
-                onClick={() => this.handleSelect(item.id)}
-              >
-                <span
-                  style={{
-                    backgroundColor: queryStringColorMap[this.type]?.background || '#E6F2F1',
-                    color: queryStringColorMap[this.type]?.color || '#02776E',
-                  }}
-                  class='option-item-icon'
-                >
-                  <span class={['icon-monitor', queryStringColorMap[this.type]?.icon || '']} />
-                </span>
-                <span class='option-item-name'>{item.name}</span>
-              </div>
-            ))}
+            {this.loading
+              ? new Array(3).fill(null).map((_item, index) => (
+                  <div
+                    key={index}
+                    class='option-item skeleton-item'
+                  >
+                    <div class='skeleton-element h-16' />
+                  </div>
+                ))
+              : this.localOptions.map((item, index) => (
+                  <div
+                    key={item.id}
+                    class={['option-item main-item', { 'cursor-active': index === this.cursorIndex }]}
+                    onClick={() => this.handleSelect(item.id)}
+                  >
+                    <span
+                      style={{
+                        backgroundColor: queryStringColorMap[this.type]?.background || '#E6F2F1',
+                        color: queryStringColorMap[this.type]?.color || '#02776E',
+                      }}
+                      class='option-item-icon'
+                    >
+                      <span class={['icon-monitor', queryStringColorMap[this.type]?.icon || '']} />
+                    </span>
+                    <span class='option-item-name'>{item.name}</span>
+                  </div>
+                ))}
           </div>
           <div class='favorite-wrap'>
             <div class='favorite-wrap-title'>
               <i18n path={'联想到以下 {0} 个收藏：'}>
-                <span class='favorite-count'>2</span>
+                <span class='favorite-count'>{this.favoriteOptions.length}</span>
               </i18n>
             </div>
-            {this.favoriteOptions.map((item, index) => (
-              <div
-                key={index}
-                class='favorite-item'
-              >
-                <span class='favorite-item-name'>{item.title}</span>
-                <span class='favorite-item-content'>
-                  <TextHighlighter
-                    content={item.content}
-                    keyword={item.keyword}
-                  />
-                </span>
-              </div>
-            ))}
+            {this.favoriteOptions.length ? (
+              this.favoriteOptions.map((item, index) => (
+                <div
+                  key={index}
+                  class='favorite-item'
+                >
+                  <span class='favorite-item-name'>{item.title}</span>
+                  <span class='favorite-item-content'>
+                    <TextHighlighter
+                      content={item.content}
+                      keyword={item.keyword}
+                    />
+                  </span>
+                </div>
+              ))
+            ) : (
+              <EmptyStatus
+                textMap={{
+                  empty: this.$tc('暂未匹配到符合条件的收藏项'),
+                }}
+                type={'empty'}
+              />
+            )}
           </div>
           <div class='key-help'>
             <span class='desc-item'>
