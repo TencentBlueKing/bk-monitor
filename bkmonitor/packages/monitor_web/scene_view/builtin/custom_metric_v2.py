965c7063306150189624ed9bb3f3fd9978bdfe55
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-03-11 11:15:17 +0800

    feat: 自定义指标页面优化 #1010158081121971892

--- bkmonitor/packages/monitor_web/scene_view/builtin/custom_metric_v2.py
+++ bkmonitor/packages/monitor_web/scene_view/builtin/custom_metric_v2.py
@@ -50,29 +50,6 @@ class CustomMetricV2BuiltinProcessor(BuiltinProcessor):
     ) -> Optional[SceneViewModel]:
         """
         创建或更新视图
-        options: {
-            # 显示的指标
-            "show_metrics": [],
-            # 指标过滤条件，[{"condition": "and", "key": "xxx", "method": "eq", "value": ["v1"]}]
-            "condition": [],
-            # 聚合维度，[{"field": "xxxx", "split": True}, {"field": "yyy", "split": False}]
-            "group_by": [],
-            # 限制返回的series数量，top/bottom，limit=0表示不限制
-            "limit": {"function": "top", "limit": 0},
-            # 对比配置, 对比模式, time: 时间对比, metric: 指标对比。offset: 对比偏移量
-            "compare": {"type": "time", "offset": "1d"},
-            # 图表配置
-            "chart_config": {
-                # 是否展示统计信息
-                "show_statistic": False,
-                # 是否展示峰值和谷值
-                "show_peakand_valley": False,
-                # 是否展示常用维度
-                "show_common_dimensions": False,
-                # 图表列数
-                "column_number": 1,
-            },
-        }
         """
         view, _ = SceneViewModel.objects.update_or_create(
             bk_biz_id=bk_biz_id,
