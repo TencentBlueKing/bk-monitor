3a332d79eb7d9284115b483388fff519594e4650
JayCC1 <85379453+JayCC1@users.noreply.github.com>
2025-03-17 17:13:06 +0800

    feat: 事件检索开发 (#5479)

--- bkmonitor/webpack/src/monitor-pc/pages/event-explore/api-utils.ts
+++ bkmonitor/webpack/src/monitor-pc/pages/event-explore/api-utils.ts
@@ -37,6 +37,7 @@ import {
   eventViewConfig,
   eventLogs,
 } from 'monitor-api/modules/data_explorer';
+import { bkMessage, makeMessage } from 'monitor-api/utils';
 
 import type { ExploreTableRequestParams, ExploreTotalRequestParams, ITopKField, ITopKRequestParams } from './typing';
 export enum APIType {
@@ -92,11 +93,18 @@ export const getEventTotal = (params: ExploreTotalRequestParams, type = APIType.
  * @param {ExploreTableRequestParams} params
  * @param {APIType} type
  */
-export const getEventLogs = (params: ExploreTableRequestParams, type = APIType.MONITOR) => {
+export const getEventLogs = (params: ExploreTableRequestParams, type = APIType.MONITOR, requestConfig = {}) => {
   const apiFunc = type === APIType.APM ? apmEventLogs : eventLogs;
-  return apiFunc(params).catch(() => ({
-    list: [],
-  }));
+  const config = { needMessage: false, ...requestConfig };
+  return apiFunc(params, config).catch(err => {
+    const message = makeMessage(err.error_details || err.message);
+    if (message && err?.message !== 'canceled') {
+      if (message) {
+        bkMessage(message);
+      }
+    }
+    return { list: [] };
+  });
 };
 
 /**
