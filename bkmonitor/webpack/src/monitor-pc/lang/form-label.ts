0b166b55ddfe3eb3fcd677cec910d5f3c9f03d0f
byron <14138993@qq.com>
2025-03-21 10:01:13 +0800

    fix: 故障详情页样式改版优化 --Bug=121992229 (#5570)

--- bkmonitor/webpack/src/monitor-pc/lang/form-label.ts
+++ bkmonitor/webpack/src/monitor-pc/lang/form-label.ts
@@ -1771,6 +1771,8 @@ export default {
   投递到Influxdb: 'Post to Influxdb',
   备注说明: 'Note',
   空间筛选: 'Spaces',
+  '业务筛选:': 'Business:',
+  从属关系: 'Dependency Relationship',
   集群属性: 'set',
   模块属性: 'module',
   主机属性: 'host',
