13add7c0429e631a96320468732ef6f2d6024887
chenguo <chenguo367@163.com>
2025-03-26 17:44:07 +0800

    feat: 容器-容量场景 #1010158081122566491 # Reviewed, transaction id: 36248

--- bkmonitor/packages/monitor_web/k8s/scenario/capacity.py
+++ bkmonitor/packages/monitor_web/k8s/scenario/capacity.py
@@ -28,22 +28,24 @@ def get_metrics() -> List:
             children=[
                 Metric(
                     id="node_cpu_seconds_total",
-                    name=_lazy("CPU使用量"),
+                    name=_lazy("节点CPU使用量"),
                     unit="core",
                     unsupported_resource=[],
                 ),
+                # sum(rate(node_cpu_seconds_total{mode!="idle"}[1m])) by (node)
                 Metric(
                     id="node_cpu_capacity_ratio",
-                    name=_lazy("CPU 装箱率"),
+                    name=_lazy("节点CPU装箱率"),
                     unit="percentunit",
                     unsupported_resource=[],
                 ),
                 Metric(
                     id="node_cpu_usage_ratio",
-                    name=_lazy("CPU 使用率"),
+                    name=_lazy("节点CPU使用率"),
                     unit="percentunit",
                     unsupported_resource=[],
                 ),
+                # (1 - avg(rate(node_cpu_seconds_total{mode="idle"}[1m])) by (node)) * 100
             ],
         ),
         Category(
@@ -51,40 +53,83 @@ def get_metrics() -> List:
             name=_lazy("内存"),
             children=[
                 Metric(
-                    id="container_memory_working_set_bytes",
-                    name=_lazy("内存使用量(Working Set)"),
+                    id="node_memory_working_set_bytes",
+                    name=_lazy("节点内存使用量"),
                     unit="bytes",
                     unsupported_resource=[],
                 ),
+                # sum by (node)(node_memory_MemTotal_bytes) - sum by (node) (node_memory_MemAvailable_bytes)
                 Metric(
-                    id="kube_pod_memory_requests_ratio",
-                    name=_lazy("内存 request使用率"),
+                    id="node_memory_capacity_ratio",
+                    name=_lazy("节点内存装箱率"),
                     unit="percentunit",
                     unsupported_resource=["namespace"],
                 ),
                 Metric(
-                    id="kube_pod_memory_limits_ratio",
-                    name=_lazy("内存 limit使用率"),
+                    id="node_memory_usage_ratio",
+                    name=_lazy("节点内存使用率"),
                     unit="percentunit",
                     unsupported_resource=["namespace"],
                 ),
+                # (1 - (sum by (node)(node_memory_MemAvailable_bytes) / sum by (node)(node_memory_MemTotal_bytes)))
+            ],
+        ),
+        Category(
+            id="capacity",
+            name=_lazy("容量"),
+            children=[
+                Metric(
+                    id="master_node_count",
+                    name=_lazy("集群Master节点计数"),
+                    unit="none",
+                    unsupported_resource=["container"],
+                ),
+                # count(sum by (node)(kube_node_role{role=~"master|control-plane"}))
+                Metric(
+                    id="worker_node_count",
+                    name=_lazy("集群Worker节点计数"),
+                    unit="none",
+                    unsupported_resource=["container"],
+                ),
+                # count(kube_node_labels) - count(sum by (node)(kube_node_role{role=~"master|control-plane"}))
+                Metric(
+                    id="node_pod_usage",
+                    name=_lazy("节点Pod个数使用率"),
+                    unit="none",
+                    unsupported_resource=["container"],
+                ),
+                # sum by (node)(kubelet_running_pods) / sum by (node)(kube_node_status_capacity_pods)
             ],
         ),
         Category(
             id="network",
-            name=_lazy("流量"),
+            name=_lazy("网络"),
             children=[
                 Metric(
-                    id="container_network_receive_bytes_total",
-                    name=_lazy("网络入带宽"),
+                    id="node_network_receive_bytes_total",
+                    name=_lazy("节点网络入带宽"),
                     unit="Bps",
-                    unsupported_resource=["container"],
+                    unsupported_resource=[],
+                ),
+                # sum(rate(node_network_receive_bytes_total{device!~"lo|veth.*"}[5m])) by (node)
+                Metric(
+                    id="node_network_transmit_bytes_total",
+                    name=_lazy("节点网络出带宽"),
+                    unit="Bps",
+                    unsupported_resource=[],
+                ),
+                # sum(rate(node_network_transmit_bytes_total{device!~"lo|veth.*"}[5m])) by (node)
+                Metric(
+                    id="node_network_receive_packets_total",
+                    name=_lazy("节点网络入包量"),
+                    unit="pps",
+                    unsupported_resource=[],
                 ),
                 Metric(
-                    id="container_network_transmit_bytes_total",
-                    name=_lazy("网络出带宽"),
-                    unit="Bps",
-                    unsupported_resource=["container"],
+                    id="node_network_transmit_packets_total",
+                    name=_lazy("节点网络出包量"),
+                    unit="pps",
+                    unsupported_resource=[],
                 ),
             ],
         ),
