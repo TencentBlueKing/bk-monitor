193b4ee057cfbe753318bf008283afa222199b41
chenguo <chenguo367@163.com>
2025-03-26 17:49:47 +0800

    feat: 采集配置支持注入自定义值的维度 #1010158081122596897 # Reviewed, transaction id: 36250

--- bkmonitor/packages/monitor_web/plugin/manager/script.py
+++ bkmonitor/packages/monitor_web/plugin/manager/script.py
@@ -104,10 +104,13 @@ class ScriptPluginManager(PluginManager):
                 for dms_key, dms_value in list(param_value.items()):
                     if param["type"] == "host":
                         dms_insert_params[dms_key] = "{{ " + f"cmdb_instance.host.{dms_value} or '-'" + " }}"
-                    else:
+                    elif param["type"] == "service":
                         dms_insert_params[dms_key] = (
                             "{{ " + f"cmdb_instance.service.labels['{dms_value}'] or '-'" + " }}"
                         )
+                    elif param["type"] == "custom":
+                        # 自定义维度k， v 注入
+                        dms_insert_params[dms_key] = dms_value
 
         context["env.yaml"]["cmd_args"] = cmd_args
 
