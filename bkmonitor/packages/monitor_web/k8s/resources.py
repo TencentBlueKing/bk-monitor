30f2198eb1210f00f77a5ead0e8f8c82b19f990b
SevenDreamer <945186289@qq.com>
2025-04-09 14:47:11 +0800

    fix: 修复网络场景pod维度报KeyError('workload') # Reviewed, transaction id: 38038

--- bkmonitor/packages/monitor_web/k8s/resources.py
+++ bkmonitor/packages/monitor_web/k8s/resources.py
@@ -457,10 +457,12 @@ class ListK8SResources(Resource):
             resource_id_set.add(tuple(sorted(record.items())))
         # promql 查询数据量不足，从db中补充
         try:
-            meta_resource_list = [k8s_resource.to_meta_dict() for k8s_resource in resource_meta.get_from_meta()]
+            meta_resource_list: List[dict] = [
+                k8s_resource.to_meta_dict() for k8s_resource in resource_meta.get_from_meta()
+            ]
             if resource_meta.resource_field == "pod_name" and scenario == "network":
                 # 网络场景，pod不需要workload相关信息
-                [rs.pop("workload") for rs in meta_resource_list]
+                [rs.pop("workload") for rs in meta_resource_list if rs.get("workload")]
         except FieldError:
             meta_resource_list = []
         all_resource_id_set = {tuple(sorted(rs.items())) for rs in meta_resource_list} | resource_id_set
