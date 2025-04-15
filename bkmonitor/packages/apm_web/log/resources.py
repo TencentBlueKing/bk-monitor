dd2ceb65806b4207a59203904209169d357e513e
WuFantaotao <88392262+WuFantaotao@users.noreply.github.com>
2025-04-15 12:05:55 +0800

    feat: apm模块，观测场景的应用详情内的日志切页查询优化 --story=122454199 (#5510)

--- bkmonitor/packages/apm_web/log/resources.py
+++ bkmonitor/packages/apm_web/log/resources.py
@@ -9,6 +9,7 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 import functools
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from rest_framework import serializers
 
@@ -30,35 +31,38 @@ def overwrite_with_span_addition(info, overwrite_key=None):
     return res
 
 
-def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_time=None, end_time=None):
-    index_set_ids = []
+# 缓存增强：对 search_index_set 添加缓存
+@using_cache(CacheType.APM(5 * 60))
+def _cached_search_index_set(_bk_biz_id):
+    return api.log_search.search_index_set(bk_biz_id=_bk_biz_id)
 
-    def _retrieve_base_info(_bk_biz_id, _app_name, _span_id):
-        # 获取当前业务的索引集和 span 详情 减少耗时
-        return (
-            api.log_search.search_index_set(bk_biz_id=_bk_biz_id),
-            api.apm_api.query_span_detail(bk_biz_id=_bk_biz_id, app_name=_app_name, span_id=_span_id)
-            if _span_id
-            else None,
-        )
 
-    biz_indices, span_detail = using_cache(CacheType.APM(10 * 60))(_retrieve_base_info)(bk_biz_id, app_name, span_id)
-    indexes_mapping = {
-        bk_biz_id: biz_indices,
-    }
+@using_cache(CacheType.APM(5 * 60))
+def _cached_query_index_set(_bk_biz_id, _app_name, _span_id):
+    return api.apm_api.query_span_detail(bk_biz_id=_bk_biz_id, app_name=_app_name, span_id=_span_id)
 
-    overwrite_method = None
-    if span_id:
-        info = {"span_id": span_id}
-        if span_detail and span_detail.get("trace_id"):
-            info["trace_id"] = span_detail["trace_id"]
-        overwrite_method = functools.partial(overwrite_with_span_addition, info=info)
 
-    # Resource: 从服务关联中找日志
+# 并行获取基础信息
+def _retrieve_base_info(_bk_biz_id, _app_name, _span_id):
+    with ThreadPoolExecutor(max_workers=10) as _executor:
+        search_future = _executor.submit(_cached_search_index_set, _bk_biz_id)
+        query_future = _executor.submit(_cached_query_index_set, _bk_biz_id, _app_name, _span_id) if _span_id else None
+
+        try:
+            search_result = search_future.result()
+            query_result = query_future.result() if query_future else None
+            return search_result, query_result
+        except Exception:
+            return [], None
+
+
+# 从服务关联中找日志
+def process_service_relation(bk_biz_id, app_name, service_name, indexes_mapping, overwrite_method=None):
+    result = []
     relation = ServiceLogHandler.get_log_relation(bk_biz_id, app_name, service_name)
-    if relation and relation.value not in index_set_ids:
+    if relation:
         if relation.related_bk_biz_id != bk_biz_id:
-            relation_full_indexes = api.log_search.search_index_set(bk_biz_id=relation.related_bk_biz_id)
+            relation_full_indexes = _cached_search_index_set(_bk_biz_id=relation.related_bk_biz_id)
             indexes_mapping[relation.related_bk_biz_id] = relation_full_indexes
             index_info = next(
                 (i for i in relation_full_indexes if str(i["index_set_id"]) == relation.value),
@@ -73,42 +77,42 @@ def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_tim
             if overwrite_method:
                 index_info["addition"] = overwrite_method(overwrite_key="log")
 
-            index_set_ids.append(relation.value)
-            yield index_info
+            result.append(index_info)
+    return result
 
-    # Resource: 从 SpanId 关联主机 / 关联容器中找
-    if span_id:
-        host_indexes = ServiceLogHandler.list_host_indexes_by_span(
-            bk_biz_id,
-            app_name,
-            span_id,
-            span_detail=span_detail,
+
+def process_span_host(bk_biz_id, app_name, span_id, span_detail, indexes_mapping, overwrite_method=None):
+    result = []
+    host_indexes = ServiceLogHandler.list_host_indexes_by_span(
+        bk_biz_id,
+        app_name,
+        span_id,
+        span_detail=span_detail,
+    )
+    for item in host_indexes:
+        index_info = next(
+            (i for i in indexes_mapping.get(bk_biz_id, []) if str(i["index_set_id"]) == str(item["index_set_id"])),
+            None,
         )
-        for item in host_indexes:
-            if str(item["index_set_id"]) not in index_set_ids:
-                index_info = next(
-                    (
-                        i
-                        for i in indexes_mapping.get(bk_biz_id, [])
-                        if str(i["index_set_id"]) == str(item["index_set_id"])
-                    ),
-                    None,
-                )
-                if index_info:
-                    # 默认查询: 机器 IP
-                    index_info["addition"] = item.get("addition", [])
-                    if overwrite_method:
-                        index_info["addition"] = overwrite_method(overwrite_key="log")
-                    index_set_ids.append(str(item["index_set_id"]))
-                    yield index_info
+        if index_info:
+            # 默认查询: 机器 IP
+            index_info["addition"] = item.get("addition", [])
+            if overwrite_method:
+                index_info["addition"] = overwrite_method(overwrite_key="log")
+        result.append(index_info)
+    return result
 
+
+# 任务3: 自定义上报
+def process_datasource(bk_biz_id, app_name, service_name, indexes_mapping, overwrite_method=None):
+    result = []
     # Resource: 从自定义上报中找日志
     datasource_index_set_id = ServiceLogHandler.get_and_check_datasource_index_set_id(
         bk_biz_id,
         app_name,
         full_indexes=indexes_mapping.get(bk_biz_id, []),
     )
-    if datasource_index_set_id and str(datasource_index_set_id) not in index_set_ids:
+    if datasource_index_set_id:
         index_info = next(
             (i for i in indexes_mapping.get(bk_biz_id, []) if str(i["index_set_id"]) == str(datasource_index_set_id)),
             None,
@@ -125,9 +129,15 @@ def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_tim
             if overwrite_method:
                 index_info["addition"] = overwrite_method()
 
-            index_set_ids.append(str(datasource_index_set_id))
-            yield index_info
+            result.append(index_info)
+    return result
 
+
+# 任务4: 关联指标
+def process_metric_relations(
+    bk_biz_id, app_name, service_name, start_time, end_time, indexes_mapping, overwrite_method=None
+):
+    result = []
     # Resource: 从关联指标中找
     relations = ServiceLogHandler.list_indexes_by_relation(
         bk_biz_id,
@@ -138,17 +148,92 @@ def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_tim
     )
     if relations:
         for r in relations:
-            if str(r["index_set_id"]) not in index_set_ids:
-                index_info = next(
-                    (j for j in indexes_mapping.get(bk_biz_id, []) if str(j["index_set_id"]) == str(r["index_set_id"])),
-                    None,
+            index_info = next(
+                (j for j in indexes_mapping.get(bk_biz_id, []) if str(j["index_set_id"]) == str(r["index_set_id"])),
+                None,
+            )
+            if index_info:
+                index_info["addition"] = r["addition"]
+                if overwrite_method:
+                    index_info["addition"] = overwrite_method(overwrite_key="log")
+                result.append(index_info)
+    return result
+
+
+def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_time=None, end_time=None):
+    if start_time and end_time:
+        dt_start = (start_time // 300) * 300
+        dt_end = (end_time // 300) * 300
+        cache_key = f"{bk_biz_id}-{app_name}-{service_name}-{span_id}-{dt_start}-{dt_end}-log_relation_list"
+    else:
+        cache_key = f"{bk_biz_id}-{app_name}-{service_name}-{span_id}-log_relation_list"
+
+    cache_call = using_cache(CacheType.APM(5 * 60))
+    index_info_list = cache_call.get_value(cache_key)
+    if index_info_list:
+        for index_info in index_info_list:
+            yield index_info
+    else:
+        # 使用缓存获取基础信息
+        biz_indices, span_detail = _retrieve_base_info(bk_biz_id, app_name, span_id)
+        indexes_mapping = {bk_biz_id: biz_indices}
+
+        overwrite_method = None
+        if span_id:
+            info = {"span_id": span_id}
+            if span_detail and span_detail.get("trace_id"):
+                info["trace_id"] = span_detail["trace_id"]
+            overwrite_method = functools.partial(overwrite_with_span_addition, info=info)
+
+        # 并行执行所有任务
+        index_set_ids = set()
+        index_info_list = []
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [
+                executor.submit(
+                    process_service_relation, bk_biz_id, app_name, service_name, indexes_mapping, overwrite_method
                 )
-                if index_info:
-                    index_info["addition"] = r["addition"]
-                    if overwrite_method:
-                        index_info["addition"] = overwrite_method(overwrite_key="log")
-                    index_set_ids.append(str(r["index_set_id"]))
-                    yield index_info
+            ]
+
+            if span_id:
+                futures.append(
+                    executor.submit(
+                        process_span_host, bk_biz_id, app_name, span_id, span_detail, indexes_mapping, overwrite_method
+                    )
+                )
+
+            futures.append(
+                executor.submit(
+                    process_datasource, bk_biz_id, app_name, service_name, indexes_mapping, overwrite_method
+                )
+            )
+
+            futures.append(
+                executor.submit(
+                    process_metric_relations,
+                    bk_biz_id,
+                    app_name,
+                    service_name,
+                    start_time,
+                    end_time,
+                    indexes_mapping,
+                    overwrite_method,
+                )
+            )
+
+            for future in as_completed(futures):
+                try:
+                    for item in future.result():
+                        index_id = str(item["index_set_id"])
+                        if index_id not in index_set_ids:
+                            index_set_ids.add(index_id)
+                            index_info_list.append(item)
+                except Exception:
+                    continue
+
+        cache_call.set_value(cache_key, index_info_list)
+        for index_info in index_info_list:
+            yield index_info
 
 
 class ServiceLogInfoResource(Resource, HostIndexQueryMixin):
