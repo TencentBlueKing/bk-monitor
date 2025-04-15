518b08387e1671275c48879096fd35bca7f49b68
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2025-04-15 17:17:19 +0800

    fix: 聚合字段列表不为空过滤 # Reviewed, transaction id: 38859

--- bklog/apps/log_unifyquery/handler/field.py
+++ bklog/apps/log_unifyquery/handler/field.py
@@ -154,6 +154,10 @@ class UnifyQueryFieldHandler(UnifyQueryHandler):
         for query in search_dict["query_list"]:
             query["limit"] = limit
             query["function"] = [{"method": "count", "dimensions": [self.search_params["agg_field"]]}]
+            # 增加字段不为空的条件
+            query["conditions"]["field_list"].append(
+                {"field_name": self.search_params["agg_field"], "value": [""], "op": "ne"}
+            )
             reference_list.append(query["reference_name"])
         search_dict.update({"order_by": ["-_value"], "metric_merge": " or ".join(reference_list)})
         data = self.query_ts_reference(search_dict)
