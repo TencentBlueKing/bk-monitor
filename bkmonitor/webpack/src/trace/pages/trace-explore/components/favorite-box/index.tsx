7e10e609b010296859dc4730de7deb94f05d0ddb
hLinx <327159425@qq.com>
2025-04-16 20:15:31 +0800

    feat: 收藏功能 # Reviewed, transaction id: 41092

---
+++ bkmonitor/webpack/src/trace/pages/trace-explore/components/favorite-box/index.tsx
@@ -0,0 +1,181 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import { defineComponent, type PropType, shallowRef, watch, useTemplateRef, computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { useStorage } from '@vueuse/core';
+import { Input, Checkbox, Loading } from 'bkui-vue';
+import _ from 'lodash';
+
+import CreateGroupPopover from './components/create-group-popover';
+import GroupFavoriteSort from './components/group-favorite-sort';
+import GroupManage from './components/group-manage/index';
+import GroupTree from './components/group-tree/index';
+import { VIEW_DATA_ID_KEY } from './constants';
+import useEventFavoriteDataId from './hooks/use-event-favorite-data-id';
+import useFavoriteType from './hooks/use-favorite-type';
+import useGroupList from './hooks/use-group-list';
+
+import type { IFavorite, IFavoriteGroup } from './types';
+
+import './index.scss';
+
+export type { IFavorite, IFavoriteGroup };
+
+export default defineComponent({
+  name: 'FavoriteBox',
+  props: {
+    type: {
+      type: String as PropType<IFavorite>,
+      required: true,
+    },
+    dataId: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['openBlank', 'change'],
+  setup(props, context) {
+    const { t } = useI18n();
+    const { loading: isGroupListLoading, data: groupList, wholeFavoriteList } = useGroupList(props.type);
+    const favoriteType = useFavoriteType();
+    const eventFavoriteDataId = useEventFavoriteDataId();
+
+    const viewDataId = useStorage(VIEW_DATA_ID_KEY, false);
+
+    const groupTreeRef = useTemplateRef<any>('groupTreeRef');
+    const favoritSelectedId = shallowRef<number>();
+    const favoriteSearchKey = shallowRef('');
+    const isShowGroupManage = shallowRef(false);
+    const isAllExpaned = shallowRef(false);
+
+    const isEventFilterDataId = computed(() => props.type === 'event');
+
+    if (isEventFilterDataId.value) {
+      eventFavoriteDataId.value = props.dataId;
+    }
+
+    watch(
+      () => props.type,
+      () => {
+        favoriteType.value = props.type;
+      },
+      {
+        immediate: true,
+      }
+    );
+
+    watch(favoritSelectedId, () => {
+      context.emit(
+        'change',
+        _.find(wholeFavoriteList.value, item => item.id === favoritSelectedId.value)
+      );
+    });
+
+    const handleShowGroupManage = () => {
+      isShowGroupManage.value = true;
+    };
+
+    const handleGroupManageClose = () => {
+      isShowGroupManage.value = false;
+    };
+
+    const handleExpandAll = (value: boolean) => {
+      groupTreeRef.value?.expandAll(value);
+      isAllExpaned.value = value;
+    };
+
+    const handleFavoriteOpenBlank = (favoriteData: IFavoriteGroup['favorites'][number]) => {
+      context.emit('openBlank', favoriteData, props.type);
+    };
+
+    return () => (
+      <div class='bk-monitor-favorite-box'>
+        <div class='header-wrapper'>
+          <div>{t('收藏夹')}</div>
+          <div class='number-tag'>{groupList.value.length}</div>
+          <div class='extend-action'>
+            <span
+              class='icon-monitor icon-shezhi1'
+              v-bk-tooltips={t('收藏管理')}
+              onClick={handleShowGroupManage}
+            />
+            <span class='icon-monitor icon-gongneng-shouqi' />
+          </div>
+        </div>
+        <div class='favorite-wrapper'>
+          <div class='favorite-search'>
+            <Input
+              v-model={favoriteSearchKey.value}
+              placeholder={t('搜索收藏名')}
+            />
+          </div>
+          <div class='favorite-action'>
+            {isEventFilterDataId.value && <Checkbox v-model={viewDataId.value}>{t('仅查看当前数据 ID')}</Checkbox>}
+            <CreateGroupPopover style='margin-left: auto'>
+              <i class='icon-monitor icon-xinjianwenjianjia' />
+            </CreateGroupPopover>
+            {isAllExpaned.value ? (
+              <span
+                style='margin-left: 12px'
+                class='icon-monitor icon-zhankai-2'
+                v-bk-tooltips={t('全部收起')}
+                onClick={() => handleExpandAll(false)}
+              />
+            ) : (
+              <span
+                style='margin-left: 12px'
+                class='icon-monitor icon-shouqi3'
+                v-bk-tooltips={t('全部展开')}
+                onClick={() => handleExpandAll(true)}
+              />
+            )}
+            <GroupFavoriteSort style='margin-left: 12px' />
+          </div>
+        </div>
+        <Loading
+          style='height: calc(100% - 117px)'
+          loading={isGroupListLoading.value}
+        >
+          <GroupTree
+            ref='groupTreeRef'
+            v-model={favoritSelectedId.value}
+            favoriteSearch={favoriteSearchKey.value}
+            type={props.type}
+            onOpenBlank={handleFavoriteOpenBlank}
+          />
+          {isShowGroupManage.value && (
+            <GroupManage
+              type={props.type}
+              onClose={handleGroupManageClose}
+            />
+          )}
+        </Loading>
+      </div>
+    );
+  },
+});
