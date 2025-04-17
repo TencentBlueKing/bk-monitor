46ffa05a990d9bc01f9fbf9af1a3d1450f39e1de
EmilyMei <447693773@qq.com>
2025-04-17 20:48:06 +0800

    feat: 新版自定义指标-维度下钻相关展示优化 (#6000)
    Co-authored-by: hlinx <327159425@qq.com>

--- bkmonitor/webpack/src/monitor-pc/pages/custom-escalation/new-metric-view/metric-chart-view/metric-chart.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/custom-escalation/new-metric-view/metric-chart-view/metric-chart.tsx
@@ -289,7 +289,7 @@ class NewMetricChart extends CommonSimpleChart {
       );
       if (item.name) {
         for (const key in legendItem) {
-          if (['min', 'max', 'avg', 'total'].includes(key)) {
+          if (['min', 'max', 'avg', 'total', 'latest'].includes(key)) {
             const val = legendItem[key];
             legendItem[`${key}Source`] = val;
             const set: any = unitFormatter(val, item.unit !== 'none' && precision < 1 ? 2 : precision);
@@ -358,7 +358,7 @@ class NewMetricChart extends CommonSimpleChart {
     const { metric = {} } = item;
     const timeOffset = time_offset ? `${this.formatTimeStr(time_offset)}` : '';
     const output = this.convertJsonObject({ ...dimensions, ...dimensions_translation }, metric.name);
-    const outputStr = output ? `{${output}}` : '';
+    const outputStr = output ? `${output}` : '';
     if (!timeOffset && !outputStr) {
       return metric.alias || metric.name;
     }
