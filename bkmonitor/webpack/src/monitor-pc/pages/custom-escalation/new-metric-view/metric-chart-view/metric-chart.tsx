9e3c1edc330e0324fb93738b2a4fe4070d82b3ed
EmilyMei <447693773@qq.com>
2025-04-17 14:41:36 +0800

    feat: 新版自定义指标-相关图例文案展示 (#5990)
    Co-authored-by: hlinx <327159425@qq.com>

--- bkmonitor/webpack/src/monitor-pc/pages/custom-escalation/new-metric-view/metric-chart-view/metric-chart.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/custom-escalation/new-metric-view/metric-chart-view/metric-chart.tsx
@@ -359,8 +359,8 @@ class NewMetricChart extends CommonSimpleChart {
     const timeOffset = time_offset ? `${this.formatTimeStr(time_offset)}` : '';
     const output = this.convertJsonObject({ ...dimensions, ...dimensions_translation }, metric.name);
     const outputStr = output ? `{${output}}` : '';
-    if (isFull) {
-      return `${timeOffset}${this.method}(${metric?.alias || metric?.name})${outputStr}`;
+    if (!timeOffset && !outputStr) {
+      return metric.alias || metric.name;
     }
     return `${timeOffset}${time_offset && output ? '-' : ''}${outputStr}`;
   }
@@ -443,7 +443,7 @@ class NewMetricChart extends CommonSimpleChart {
             res.series &&
               series.push(
                 ...res.series.map(set => {
-                  const name = this.handleSeriesName(item, set, true) || set.target;
+                  const name = this.handleSeriesName(item, set) || set.target;
                   const tipsName = this.handleSeriesName(item, set) || set.target;
                   this.legendSorts.push({
                     name,
