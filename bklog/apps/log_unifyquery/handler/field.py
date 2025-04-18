eb0309064d10cf3d4b9912d32badf29b5074a0c6
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2025-04-18 15:04:15 +0800

    fix: 接入doris问题若干 # Reviewed, transaction id: 39388

--- bklog/apps/log_unifyquery/handler/field.py
+++ bklog/apps/log_unifyquery/handler/field.py
@@ -158,6 +158,9 @@ class UnifyQueryFieldHandler(UnifyQueryHandler):
             query["conditions"]["field_list"].append(
                 {"field_name": self.search_params["agg_field"], "value": [""], "op": "ne"}
             )
+
+            if len(query["conditions"]["field_list"]) > 0:
+                query["conditions"]["condition_list"].append("and")
             reference_list.append(query["reference_name"])
         search_dict.update({"order_by": ["-_value"], "metric_merge": " or ".join(reference_list)})
         data = self.query_ts_reference(search_dict)
