65b1b196f8cc2aea83e8a2024e6d123230ce7e17
hlinx <327159425@qq.com>
2025-04-30 18:25:20 +0800

    Revert "feat: 收藏功能 (#6156)"
    This reverts commit aefa70fe92ce72cc50a9cf7f85f6631df7fa8fdf.

--- bkmonitor/webpack/src/trace/pages/trace-explore/trace-explore.tsx
+++ bkmonitor/webpack/src/trace/pages/trace-explore/trace-explore.tsx
@@ -42,7 +42,6 @@ import {
 import { DEFAULT_TIME_RANGE, handleTransformToTimestamp } from '../../components/time-range/utils';
 import { useTraceExploreStore } from '../../store/modules/explore';
 import DimensionFilterPanel from './components/dimension-filter-panel';
-import FavoriteBox from './components/favorite-box';
 import TraceExploreHeader from './components/trace-explore-header';
 import TraceExploreLayout from './components/trace-explore-layout';
 import TraceExploreView from './components/trace-explore-view/trace-explore-view';
@@ -410,17 +409,7 @@ export default defineComponent({
   render() {
     return (
       <div class='trace-explore'>
-        <div class='favorite-panel'>
-          <FavoriteBox
-            type='event'
-            onChange={(data: any) => {
-              console.log('favorit change', data);
-            }}
-            onOpenBlank={(data: any) => {
-              console.log('favorit open blank', data);
-            }}
-          />
-        </div>
+        <div class='favorite-panel' />
         <div class='main-panel'>
           <div class='header-panel'>
             <TraceExploreHeader
