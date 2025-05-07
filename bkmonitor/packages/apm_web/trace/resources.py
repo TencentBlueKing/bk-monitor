d256c3ca1f036b9bdcdc5e51f612b8adfe09c39d
joker-joker-yuan <blackpig163@163.com>
2025-05-07 07:36:33 +0000

    fix: trace 平铺 trace/span 列表代码逻辑优化 # Reviewed, transaction id: 41358

--- bkmonitor/packages/apm_web/trace/resources.py
+++ bkmonitor/packages/apm_web/trace/resources.py
@@ -1350,8 +1350,10 @@ class ListFlattenTraceResource(Resource):
 
     def perform_request(self, data):
         response = ListTraceResource().get_trace_list_api_data(data)
+        data_list = []
         for trace_data_dict in response["data"]:
             if PreCalculateSpecificField.COLLECTIONS in trace_data_dict:
                 trace_data_dict.update(trace_data_dict.pop(PreCalculateSpecificField.COLLECTIONS))
-        response["data"] = [flatten_es_dict_data(data_dict) for data_dict in response["data"]]
+            data_list.append(flatten_es_dict_data(trace_data_dict))
+        response["data"] = data_list
         return response
