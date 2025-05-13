81c0183fac4e3dbd4f947a9c842e61c7fa976052
LeeXT <xintaoLi@users.noreply.github.com>
2025-05-13 20:43:14 +0800

    feat: 趋势图分组支持 (#6263)

--- bklog/web/src/views/retrieve-v3/use-app-init.tsx
+++ bklog/web/src/views/retrieve-v3/use-app-init.tsx
@@ -178,8 +178,8 @@ export default () => {
         // 拉取完毕根据当前路由参数回填默认选中索引集
         store.dispatch('updateIndexItemByRoute', { route, list: resp[1] }).then(() => {
           setDefaultIndexsetId();
-          const type = route.params.indexId ? 'single' : 'union';
-          RetrieveHelper.setIndexsetId(store.state.indexItem.ids, type);
+          // const type = route.params.indexId ? 'single' : 'union';
+          // RetrieveHelper.setIndexsetId(store.state.indexItem.ids, type);
 
           store.dispatch('requestIndexSetFieldInfo').then(() => {
             store.dispatch('requestIndexSetQuery').then(() => {
