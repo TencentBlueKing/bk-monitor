411d93c762eba3fd206219c6540fd89a46ea33bf
LeeXT <xintaoLi@users.noreply.github.com>
2025-05-14 09:27:16 +0800

    feat: 索引集选择组件实现 & 索引ID初始化相关问题修复优化 (#6267)
    Co-authored-by: jidanjuan <112756875+jidanjuan@users.noreply.github.com>

--- bklog/web/src/components/monitor-echarts/trend-chart.vue
+++ bklog/web/src/components/monitor-echarts/trend-chart.vue
@@ -209,7 +209,7 @@
 </script>
 <template>
   <div
-    v-bkloading="{ isLoading: isLoading }"
+    v-bkloading="{ isLoading: isLoading, zIndex: 10, size: 'mini' }"
     class="monitor-echart-wrap"
   >
     <div
