7f2385c07ca4422f92f0857a858ec18eb6bcf8fa
xintaoLi <li18566230175@gmail.com>
2025-05-16 14:15:56 +0800

    fix: 修复联合查询URL地址解析参数调用错误

--- bklog/web/src/store/default-values.ts
+++ bklog/web/src/store/default-values.ts
@@ -195,7 +195,7 @@ export const IndexFieldInfo = {
 export const IndexsetItemParams = { ...DEFAULT_RETRIEVE_PARAMS };
 
 export const IndexItem = {
-  ids: URL_ARGS.unionList?.length ? URL_ARGS.unionList : [URL_ARGS.index_id],
+  ids: URL_ARGS.unionList?.length ? [...URL_ARGS.unionList] : [URL_ARGS.index_id],
   isUnionIndex: URL_ARGS.unionList?.length ?? false,
   items: [],
   catchUnionBeginList: [],
