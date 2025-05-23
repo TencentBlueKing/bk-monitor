b5ca535391b42c02fce1e0c761621b3592abcf64
liangling0628 <lingliang1100@gmail.com>
2025-05-23 15:38:28 +0800

    feat: 前端全局字段及词条优化 --Story=124239174 # Reviewed, transaction id: 43687

--- bkmonitor/webpack/src/fta-solutions/index.ts
+++ bkmonitor/webpack/src/fta-solutions/index.ts
@@ -36,6 +36,7 @@ import Api from 'monitor-api/api';
 import { setVue } from 'monitor-api/utils/index';
 import { immediateRegister } from 'monitor-common/service-worker/service-wroker';
 import { getUrlParam, mergeSpaceList, setGlobalBizId } from 'monitor-common/utils';
+import { asignWindowField } from 'monitor-common/utils/asign-window';
 
 import App from './pages/app';
 import router from './router/router';
@@ -84,9 +85,7 @@ if (window.__BK_WEWEB_APP_KEY__) {
       context_type: 'basic',
     })
     .then(data => {
-      Object.keys(data).forEach(key => {
-        window[key.toLocaleLowerCase()] = data[key];
-      });
+      asignWindowField(data);
       mergeSpaceList(window.space_list);
       window.username = window.uin;
       window.user_name = window.uin;
@@ -120,9 +119,7 @@ if (window.__BK_WEWEB_APP_KEY__) {
           context_type: 'extra',
         })
         .then(data => {
-          Object.keys(data).forEach(key => {
-            window[key.toLocaleLowerCase()] = data[key];
-          });
+          asignWindowField(data);
         });
     })
     .catch(e => console.error(e))
