995e5d0836b1a7264077648a29155c0322717687
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-05-30 16:18:58 +0800

    chore: 清理cmdb缓存任务控制开关

--- bkmonitor/alarm_backends/core/cache/cmdb/topo.py
+++ bkmonitor/alarm_backends/core/cache/cmdb/topo.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
 Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
@@ -8,9 +7,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from typing import List
-
-from django.conf import settings
 
 from alarm_backends.core.cache.cmdb.base import CMDBCacheManager, RefreshByBizMixin
 from api.cmdb.define import TopoNode, TopoTree
@@ -24,11 +20,11 @@ class TopoManager(RefreshByBizMixin, CMDBCacheManager):
 
     ObjectClass = TopoNode
     type = "topo"
-    CACHE_KEY = "{prefix}.cmdb.topo".format(prefix=CMDBCacheManager.CACHE_KEY_PREFIX)
+    CACHE_KEY = f"{CMDBCacheManager.CACHE_KEY_PREFIX}.cmdb.topo"
 
     @classmethod
     def key_to_internal_value(cls, bk_obj_id, bk_inst_id):
-        return "{bk_obj_id}|{bk_inst_id}".format(bk_obj_id=bk_obj_id, bk_inst_id=bk_inst_id)
+        return f"{bk_obj_id}|{bk_inst_id}"
 
     @classmethod
     def get(cls, bk_obj_id, bk_inst_id):
@@ -36,7 +32,7 @@ class TopoManager(RefreshByBizMixin, CMDBCacheManager):
         :param bk_obj_id: 对象ID
         :param bk_inst_id: 实例ID
         """
-        return super(TopoManager, cls).get(bk_obj_id, bk_inst_id)
+        return super().get(bk_obj_id, bk_inst_id)
 
     @classmethod
     def refresh_by_biz(cls, bk_biz_id):
@@ -49,7 +45,7 @@ class TopoManager(RefreshByBizMixin, CMDBCacheManager):
         for node in topo_tree.get_all_nodes_with_relation().values():
             key = cls.key_to_internal_value(node.bk_obj_id, node.bk_inst_id)
 
-            parents: List[str] = []
+            parents: list[str] = []
             parent: TopoTree = node._parent
             while parent:
                 parents.append(f"{parent.bk_obj_id}|{parent.bk_inst_id}")
@@ -58,9 +54,3 @@ class TopoManager(RefreshByBizMixin, CMDBCacheManager):
             data[key] = TopoNode(**node.__dict__)
             data[key].parents = parents
         return data
-
-
-def main():
-    if "topo" in settings.DISABLE_ALARM_CMDB_CACHE_REFRESH:
-        return
-    TopoManager.refresh()
