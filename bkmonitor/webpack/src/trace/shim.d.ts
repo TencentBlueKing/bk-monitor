a72a06671c9322ad7bc34e2b639856d7d0917302
liangling0628 <lingliang1100@gmail.com>
2025-05-31 16:41:56 +0800

    feat: 更新 @blueking/bk-user-display-name 依赖版本至 1.0.3-beta.3，并调整相关文件中的关键词格式 # Reviewed, transaction id: 44655

--- bkmonitor/webpack/src/trace/shim.d.ts
+++ bkmonitor/webpack/src/trace/shim.d.ts
@@ -30,6 +30,7 @@ import 'vue/jsx.d.ts';
 import type i18n from './i18n/i18n';
 import type { IBizItem, ISpaceItem } from './typings';
 import type { Dayjs } from 'dayjs';
+import type { HTMLAttributes, ReservedProps } from 'vue/dist/vue.d.ts';
 
 declare global {
   interface Window {
@@ -67,11 +68,15 @@ declare global {
     BK_DOC_URL?: string;
     FEATURE_TOGGLE?: Record<string, 'off' | 'on'>;
   }
+}
+
+declare global {
   namespace JSX {
     interface IntrinsicElements {
       'bk-user-display-name': {
         'user-id': string;
-      };
+      } & ReservedProps &
+        HTMLAttributes;
     }
   }
 }
