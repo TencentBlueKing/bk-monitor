b208696381c4d7f3820a9e751161ad16b125a045
liangling0628 <lingliang1100@gmail.com>
2025-05-31 15:00:31 +0800

    feat: 前端多租户基础设施建设 # Reviewed, transaction id: 44651

---
+++ bkmonitor/webpack/src/trace/components/user-selector/user-selector.tsx
@@ -0,0 +1,110 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+import { type PropType, defineComponent, onMounted, shallowRef } from 'vue';
+
+import { BkUserSelector } from '@blueking/bk-user-selector/vue3';
+import { type ConfigOptions, getUserComponentConfig } from 'monitor-pc/common/user-display-name';
+
+import type { IUserGroup } from './user-group';
+
+import '@blueking/bk-user-selector/vue3/vue3.css';
+
+export default defineComponent({
+  name: 'UserSelector',
+  props: {
+    /**
+     * 选中的用户ID列表
+     */
+    modelValue: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    /**
+     * 用户组列表
+     */
+    userGroupList: {
+      type: Array as PropType<IUserGroup[]>,
+      default: () => [],
+    },
+    /**
+     * 用户组（备用字段）
+     */
+    userGroup: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    /**
+     * 是否支持多选
+     */
+    multiple: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 是否支持拖拽
+     */
+    draggable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: {
+    'update:modelValue': (value: string[]) => Array.isArray(value),
+    change: (value: string[]) => Array.isArray(value),
+  },
+  setup(props, { emit }) {
+    const componentConfig = shallowRef<Partial<ConfigOptions>>({});
+    onMounted(() => {
+      componentConfig.value = getUserComponentConfig();
+    });
+    const handleChange = (value: string[]) => {
+      emit('update:modelValue', value);
+      emit('change', value);
+    };
+
+    return {
+      componentConfig,
+      handleChange,
+    };
+  },
+  render() {
+    if (!this.componentConfig.apiBaseUrl) {
+      return undefined;
+    }
+
+    return (
+      <BkUserSelector
+        apiBaseUrl={this.componentConfig.apiBaseUrl}
+        draggable={this.draggable}
+        modelValue={this.modelValue}
+        multiple={this.multiple}
+        tenantId={this.componentConfig.tenantId}
+        userGroup={this.userGroupList}
+        onChange={this.handleChange}
+      />
+    );
+  },
+});
