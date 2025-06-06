2ef7c18dfbb242f02f58c3ecd383881a28a30c56
xlc <2933458919@qq.com>
2025-06-06 18:26:19 +0800

    fix: trace检索耗时组件代码优化

---
+++ bkmonitor/webpack/src/trace/components/retrieval-filter/duration-input.tsx
@@ -0,0 +1,233 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import { defineComponent, shallowRef, watch, type PropType } from 'vue';
+import { shallowReactive } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { Input, type InputValue, Slider, type SliderValue } from 'tdesign-vue-next';
+
+import { formatDuration, isValidTimeFormat, parseDuration } from './duration-input-utils';
+
+import './duration-input.scss';
+
+export default defineComponent({
+  name: 'DurationInput',
+  props: {
+    modelValue: {
+      type: Array as PropType<number[]>,
+      default: () => [],
+    },
+    value: {
+      type: Array as PropType<number[]>,
+      default: () => [],
+    },
+    styleType: {
+      type: String as PropType<'default' | 'form'>,
+      default: 'default',
+    },
+  },
+  emits: {
+    'update:modelValue': (_val: number[]) => true,
+    change: (_val: number[]) => true,
+  },
+  setup(props, { emit }) {
+    const { t } = useI18n();
+    const sliderValue = shallowReactive<Record<string, number | number[]>>({
+      min: 0,
+      max: 1000,
+      value: [0, 1000],
+    });
+    const startInput = shallowRef('');
+    const endInput = shallowRef('');
+
+    watch(
+      () => props.modelValue,
+      val => {
+        watchPropValue(val);
+      },
+      { immediate: true }
+    );
+    watch(
+      () => props.value,
+      val => {
+        watchPropValue(val);
+      },
+      { immediate: true }
+    );
+    function watchPropValue(val: number[]) {
+      const startVal = formatDuration(val[0]);
+      const endVal = formatDuration(val[1]);
+      const isStartNE = startVal !== startInput.value;
+      const isEndNE = endVal !== endInput.value;
+      if (isStartNE) {
+        startInput.value = startVal;
+        sliderInit(startVal, endVal);
+      }
+      if (isEndNE) {
+        endInput.value = endVal;
+        sliderInit(startVal, endVal);
+      }
+      if (isEndNE || isEndNE) {
+        sliderInit(val[0], val[1]);
+      }
+    }
+    /**
+     * 处理开始时间输入框变更事件
+     * @param val - 输入框的值
+     */
+    function handleStartInputChange(val: InputValue) {
+      const isValid = isValidTimeFormat(val as string);
+      if (isValid || val === '') {
+        startInput.value = val as string;
+        handleChange();
+      } else {
+        startInput.value = '';
+      }
+    }
+    /**
+     * 处理开始结束输入框变更事件
+     * @param val - 输入框的值
+     */
+    function handleEndInputChange(val: InputValue) {
+      const isValid = isValidTimeFormat(val as string);
+      if (isValid || val === '') {
+        endInput.value = val as string;
+        handleChange();
+      } else {
+        endInput.value = '';
+      }
+    }
+    /**
+     * 处理滑块拖动结束事件
+     * @param val - 滑块当前值，格式为[最小值, 最大值]
+     */
+    function handleSliderChangeEnd(val: SliderValue) {
+      sliderValue.value = val;
+      const startVal = formatDuration(val[0]);
+      const endVal = formatDuration(val[1]);
+      startInput.value = startVal;
+      endInput.value = endVal;
+      handleChange(false);
+    }
+    /**
+     * 处理时间范围变更事件
+     * 将输入框的时间字符串转换为数值并触发change事件
+     */
+    function handleChange(isInput = true) {
+      const startVal = parseDuration(startInput.value);
+      const endVal = parseDuration(endInput.value);
+      if (startVal === props.value[0] && endVal === props.value[1]) {
+        return;
+      }
+      if (isInput) {
+        sliderInit(startVal, endVal);
+      }
+      emit('change', [startVal, endVal]);
+      emit('update:modelValue', [startVal, endVal]);
+    }
+
+    function sliderInit(startVal, endVal) {
+      if (startVal > endVal || startVal === endVal) {
+        sliderValue.max = 1000;
+        sliderValue.value = [0, 1000];
+      } else {
+        if (endVal > 1000) {
+          sliderValue.max = endVal;
+        }
+        sliderValue.value = [startVal, endVal];
+      }
+    }
+
+    return {
+      startInput,
+      endInput,
+      sliderValue,
+      t,
+      handleStartInputChange,
+      handleEndInputChange,
+      handleSliderChangeEnd,
+    };
+  },
+  render() {
+    return (
+      <div class={['duration-input-component', this.styleType]}>
+        <div
+          class='input-wrap'
+          v-bk-tooltips={{
+            placement: 'bottom',
+            content: (
+              <div>
+                {this.t('支持')}
+                ns, μs, ms, s, m, h, d
+              </div>
+            ),
+          }}
+        >
+          <Input
+            v-model={this.startInput}
+            autoWidth={true}
+            placeholder={'0ns'}
+            size={this.styleType === 'default' ? 'small' : 'medium'}
+            onBlur={this.handleStartInputChange}
+            onEnter={this.handleStartInputChange}
+          />
+        </div>
+
+        <div class='duration-slider'>
+          <Slider
+            max={this.sliderValue.max as number}
+            min={this.sliderValue.min as number}
+            range={true}
+            value={this.sliderValue.value}
+            onChangeEnd={this.handleSliderChangeEnd}
+          />
+        </div>
+        <div
+          class='input-wrap'
+          v-bk-tooltips={{
+            placement: 'bottom',
+            content: (
+              <div>
+                {this.t('支持')}
+                ns, μs, ms, s, m, h, d
+              </div>
+            ),
+          }}
+        >
+          <Input
+            v-model={this.endInput}
+            autoWidth={true}
+            placeholder={'+∞'}
+            size={this.styleType === 'default' ? 'small' : 'medium'}
+            onBlur={this.handleEndInputChange}
+            onEnter={this.handleEndInputChange}
+          />
+        </div>
+      </div>
+    );
+  },
+});
--- bkmonitor/webpack/src/trace/components/retrieval-filter/time-consuming.tsx
+++ bkmonitor/webpack/src/trace/components/retrieval-filter/time-consuming.tsx
@@ -27,24 +27,10 @@
 import { defineComponent, shallowRef, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import { useDebounceFn } from '@vueuse/core';
-import { Input as BkInput, Slider } from 'bkui-vue';
-import { Input as TInput } from 'tdesign-vue-next';
-
-import { formatDuration } from '../trace-view/utils/date';
+import DurationInput from './duration-input';
 import { TIME_CONSUMING_EMITS, TIME_CONSUMING_PROPS } from './typing';
-import { TIME_CONSUMING_REGEXP } from './utils';
 
 import './time-consuming.scss';
-
-const DEFAULT_SLIDER_VALUE = {
-  curValue: [0, 1000], // 当前选择范围
-  scaleRange: [0, 0], // 刻度范围
-  min: 0, // 最小值
-  max: 1000, // 最大值
-  step: 10, // 步长
-  disable: false,
-};
 
 const GTE = 'gte'; // 大于等于
 const LTE = 'lte'; // 小于等于
@@ -57,19 +43,8 @@ export default defineComponent({
   emits: TIME_CONSUMING_EMITS,
   setup(props, { emit }) {
     const { t } = useI18n();
-    const durationSlider = shallowRef({ ...DEFAULT_SLIDER_VALUE });
-    const startValue = shallowRef('');
-    const endValue = shallowRef('');
-    const startError = shallowRef(false);
-    const endError = shallowRef(false);
-    const errMsg = shallowRef('');
-
+    const localValue = shallowRef([0, 0]);
     let stopWatch = false;
-    let sliderInit = false;
-
-    setTimeout(() => {
-      sliderInit = true;
-    }, 1000);
 
     watch(
       () => props.value,
@@ -78,19 +53,15 @@ export default defineComponent({
           const value = val.value;
           if (value.length === 1) {
             if (val?.method === GTE) {
-              startValue.value = `${Number(value[0]) / 1000}ms`;
-              endValue.value = '';
-              handleSetSlider(false);
+              localValue.value = [Number(value[0]), 0];
             } else if (val?.method === LTE) {
-              endValue.value = `${Number(value[0]) / 1000}ms`;
-              startValue.value = '';
-              handleSetSlider(false);
+              localValue.value = [0, Number(value[0])];
+            } else if (val?.method === EQUAL) {
+              localValue.value = [Number(value[0]), Number(value[0])];
             }
           } else {
             const [start, end] = value;
-            startValue.value = `${Number(start) / 1000}ms`;
-            endValue.value = `${Number(end) / 1000}ms`;
-            handleSetSlider(false);
+            localValue.value = [Number(start), Number(end)];
           }
           stopWatch = true;
         }
@@ -98,158 +69,52 @@ export default defineComponent({
       { immediate: true }
     );
 
-    const handleChangeDebounce = useDebounceFn(() => {
-      const where = getWhere();
+    function handleChange(val) {
+      localValue.value = val;
+      const where = getWhere(val);
       emit('change', {
         key: props.fieldInfo.field,
         ...where,
       });
-    }, 300);
-    const handleSliderChange = useDebounceFn((rangeValue: number[] = []) => {
-      const [start, end] = rangeValue;
-      startValue.value = formatDuration(start * 1000) === '0μs' ? '0ns' : formatDuration(start * 1000);
-      endValue.value = formatDuration(end * 1000) === '0μs' ? '0ns' : formatDuration(end * 1000);
-      startError.value = false;
-      endError.value = false;
-      errMsg.value = '';
-    }, 300);
+    }
 
-    const handleInputChangeDebounce = useDebounceFn((val: string, type: 'end' | 'start') => {
-      handleInputChange(val, type);
-    }, 300);
-
-    function getWhere() {
-      const start = formatToMs(startValue.value);
-      const end = formatToMs(endValue.value);
-      if (startValue.value || endValue.value) {
-        if (startValue.value && !endValue.value) {
+    function getWhere(val: number[]) {
+      const [startVal, endVal] = val;
+      if (startVal || endVal) {
+        if (startVal && !endVal) {
           return {
             method: GTE,
-            value: [Number(start * 1000)],
+            value: [startVal],
           };
         }
-        if (!startValue.value && endValue.value) {
+        if (!startVal && endVal) {
           return {
             method: LTE,
-            value: [Number(end * 1000)],
+            value: [endVal],
           };
         }
-        if (startValue.value === endValue.value) {
+        if (startVal === endVal) {
           return {
             method: EQUAL,
-            value: [Number(start * 1000)],
+            value: [startVal],
           };
         }
+        return {
+          method: BETWEEN,
+          value: val,
+        };
       }
       return {
-        method: BETWEEN,
-        value: [Number(start * 1000), Number(end * 1000)],
+        method: '',
+        value: [],
       };
     }
-
-    function handleInputChange(val: string, type: 'end' | 'start') {
-      const isError = val?.trim()?.length ? !TIME_CONSUMING_REGEXP.test(val) : false;
-      if (type === 'start') {
-        startError.value = isError;
-      } else {
-        endError.value = isError;
-      }
-      if (!startValue.value && !endValue.value) {
-        errMsg.value = '';
-        startError.value = false;
-        endError.value = false;
-        durationSlider.value = { ...DEFAULT_SLIDER_VALUE };
-        emit('change', {
-          key: props.fieldInfo.field,
-          method: BETWEEN,
-          value: [],
-        });
-      } else if (!startError.value || !endError.value) {
-        errMsg.value = '';
-        if (startValue.value.trim() !== '' || endValue.value.trim() !== '') {
-          // 起始时间均有值
-          handleSetSlider();
-        }
-      } else {
-        errMsg.value = t('单位仅支持ns, μs, ms, s, m, h, d');
-      }
-    }
-
-    function handleSetSlider(isUpdate = true) {
-      const start = formatToMs(startValue.value);
-      const end = formatToMs(endValue.value);
-      // if (start > end) {
-      //   // 当最小耗时大于最大耗时
-      //   errMsg.value = t('最小耗时不能大于最大耗时，');
-      // } else {
-      // }
-      if (startValue.value && endValue.value) {
-        durationSlider.value = {
-          ...durationSlider.value,
-          curValue: [start, end],
-          min: start,
-          max: end,
-          step: Math.abs(start - end) / 100,
-          disable: start === end,
-        };
-        errMsg.value = '';
-      }
-      isUpdate && handleChangeDebounce();
-    }
-    /** 将输入框内容转化为 ms 单位 */
-    function formatToMs(str: string) {
-      if (!str) {
-        return 0;
-      }
-      let totalMs = 0;
-      const unitMap = {
-        ns: 1 / 10 ** 6,
-        μs: 1 / 10 ** 3,
-        ms: 1,
-        s: 10 ** 3,
-        m: 10 ** 3 * 60,
-        h: 10 ** 3 * 3600,
-        d: 10 ** 3 * 3600 * 24,
-      };
-      for (const part of str.split(' ')) {
-        const parseStr = part.split(/(ns|μs|ms|s|m|h|d)$/);
-        const [value, unit] = parseStr;
-        totalMs += Number(value) * (unitMap[unit] || 0);
-      }
-      return totalMs;
-    }
-
-    function handleRangeChange(value: number[]) {
-      if (!sliderInit) {
-        return;
-      }
-      const [start, end] = value;
-      // 默认不赋值 解决组件api初始化问题
-      if (startValue.value === '' && endValue.value === '' && start === 0 && end === 1000) return;
-
-      handleSliderChange(value);
-    }
-
-    // function handleSliderChange(rangeValue: number[] = []) {
-    //   const [start, end] = rangeValue;
-    //   startValue.value = formatDuration(start * 1000) === '0μs' ? '0ns' : formatDuration(start * 1000);
-    //   endValue.value = formatDuration(end * 1000) === '0μs' ? '0ns' : formatDuration(end * 1000);
-    //   startError.value = false;
-    //   endError.value = false;
-    //   errMsg.value = '';
-    // }
 
     return {
-      startValue,
-      endValue,
-      durationSlider,
-      startError,
-      endError,
-      handleInputChange,
-      handleChangeDebounce,
-      handleRangeChange,
-      handleInputChangeDebounce,
+      localValue,
+
       t,
+      handleChange,
     };
   },
   render() {
@@ -266,79 +131,11 @@ export default defineComponent({
             {this.t('耗时')}
           </span>
         )}
-        <div
-          class={['input-wrap', { 'is-error': this.startError }]}
-          v-bk-tooltips={{
-            placement: 'bottom',
-            content: (
-              <div>
-                {this.t('支持')}
-                ns, μs, ms, s, m, h, d
-              </div>
-            ),
-          }}
-        >
-          {this.styleType === 'form' ? (
-            <BkInput
-              v-model={this.startValue}
-              placeholder={'0ns'}
-              size={this.styleType !== 'form' ? 'small' : 'default'}
-              onBlur={() => this.handleInputChange(this.startValue as string, 'start')}
-              // onInput={val => this.handleInputChangeDebounce(val, 'start')}
-            />
-          ) : (
-            <TInput
-              v-model={this.startValue}
-              autoWidth={true}
-              placeholder={'0ns'}
-              size={'small'}
-              onBlur={val => this.handleInputChangeDebounce(val as string, 'start')}
-            />
-          )}
-        </div>
-        <div class='slider-wrap'>
-          <Slider
-            v-model={this.durationSlider.curValue}
-            disable={this.durationSlider.disable}
-            maxValue={this.durationSlider.max}
-            minValue={this.durationSlider.min}
-            step={this.durationSlider.step}
-            range
-            onChange={() => this.handleChangeDebounce()}
-            onUpdate:modelValue={this.handleRangeChange}
-          />
-        </div>
-
-        <div
-          class={['input-wrap', { 'is-error': this.endError }]}
-          v-bk-tooltips={{
-            placement: 'bottom',
-            content: (
-              <div>
-                {this.t('支持')}
-                ns, μs, ms, s, m, h, d
-              </div>
-            ),
-          }}
-        >
-          {this.styleType === 'form' ? (
-            <BkInput
-              v-model={this.endValue}
-              placeholder={'+∞'}
-              size={this.styleType !== 'form' ? 'small' : 'default'}
-              onBlur={() => this.handleInputChange(this.endValue as string, 'end')}
-              // onInput={val => this.handleInputChangeDebounce(val, 'end')}
-            />
-          ) : (
-            <TInput
-              v-model={this.endValue}
-              autoWidth={true}
-              placeholder={'+∞'}
-              size={'small'}
-              onBlur={val => this.handleInputChangeDebounce(val as string, 'end')}
-            />
-          )}
-        </div>
+        <DurationInput
+          styleType={this.styleType || 'default'}
+          value={this.localValue}
+          onChange={this.handleChange}
+        />
       </div>
     );
   },
