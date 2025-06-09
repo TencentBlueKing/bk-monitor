4edce80f2872e97f51fd4c734a4cbd213ffe0bc0
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-06-09 15:04:49 +0800

    feat: 告警后台支持多租户 #1010158081124374676

--- bkmonitor/apm/core/discover/host.py
+++ bkmonitor/apm/core/discover/host.py
@@ -8,16 +8,16 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+from itertools import chain
 import logging
 from datetime import datetime
 
 from opentelemetry.semconv.trace import SpanAttributes
 
+from alarm_backends.core.cache.cmdb.host import HostIPManager
 from apm.core.discover.base import DiscoverBase, extract_field_value
 from apm.models import HostInstance
-from bkmonitor.utils.common_utils import safe_int
 from constants.apm import OtlpKey
-from constants.common import DEFAULT_TENANT_ID
 
 logger = logging.getLogger("apm")
 
@@ -87,32 +87,16 @@ class HostDiscover(DiscoverBase):
         self.clear_if_overflow()
         self.clear_expired()
 
-    def list_bk_cloud_id(self, ips):
+    def list_bk_cloud_id(self, ips: list[str]) -> dict[str, tuple[int, int]]:
         from alarm_backends.core.cache.cmdb.host import HostManager
 
-        # TODO: 需要改为使用HostIpManager获取ip信息，下面的代码需要重构
-        host_keys = []
-        if host_keys is None:
+        # 获取ip对应的host_key
+        host_keys = HostIPManager.mget(bk_tenant_id=self.bk_tenant_id, ips=ips)
+        if not host_keys:
             return {}
 
-        ip_to_host_key = {}
-        for host_key in host_keys:
-            if not host_key:
-                continue
+        # 获取host_key对应的host信息
+        hosts = HostManager.mget(bk_tenant_id=self.bk_tenant_id, host_keys=list(chain(*host_keys.values())))
 
-            split_keys = host_key.split("|")
-            if len(split_keys) == 2:
-                ip_to_host_key[split_keys[0]] = safe_int(split_keys[1], 0)
-
-        ret = {}
-        host_keys = []
-        for ip in ips:
-            if ip not in ip_to_host_key:
-                continue
-            host_keys.append(HostManager.get_host_key(ip, ip_to_host_key[ip]))
-
-        hosts = HostManager.mget(bk_tenant_id=DEFAULT_TENANT_ID, host_keys=host_keys)
-        for host in hosts.values():
-            ret[host.ip] = (host.bk_cloud_id, host.bk_host_id)
-
-        return ret
+        # 返回ip对应的host信息
+        return {host.ip: (host.bk_cloud_id, host.bk_host_id) for host in hosts.values()}
