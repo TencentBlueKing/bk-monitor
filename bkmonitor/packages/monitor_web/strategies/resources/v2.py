d22612391d42a3aae73dd92b9dfe5124d4dbc76c
lai <11598235+unique0lai@users.noreply.github.com>
2025-06-12 11:44:21 +0800

    feat: 指标缓存支持多租户 --story=124593012 (#6729)

--- bkmonitor/packages/monitor_web/strategies/resources/v2.py
+++ bkmonitor/packages/monitor_web/strategies/resources/v2.py
@@ -586,7 +586,9 @@ class GetStrategyListV2Resource(Resource):
         """
         # 过滤指标别名
         if filter_dict["metric_field_name"]:
-            metric_qs = MetricListCache.objects.filter(metric_field_name__in=filter_dict["metric_field_name"])
+            metric_qs = MetricListCache.objects.filter(
+                bk_tenant_id=get_request_tenant_id(), metric_field_name__in=filter_dict["metric_field_name"]
+            )
             if bk_biz_id is not None:
                 metric_qs = metric_qs.filter(bk_biz_id__in=[0, bk_biz_id])
             metric_fields = metric_qs.values_list("metric_field", flat=True).distinct()
@@ -1093,9 +1095,9 @@ class GetStrategyListV2Resource(Resource):
         if not queries:
             return {}
 
-        metrics = MetricListCache.objects.filter(bk_biz_id__in=[bk_biz_id, 0]).filter(
-            reduce(lambda x, y: x | y, queries)
-        )
+        metrics = MetricListCache.objects.filter(
+            bk_tenant_id=get_request_tenant_id(), bk_biz_id__in=[bk_biz_id, 0]
+        ).filter(reduce(lambda x, y: x | y, queries))
 
         metric_dicts = {get_metric_id(**metric.__dict__): metric for metric in metrics}
 
@@ -1928,7 +1930,9 @@ class GetMetricListV2Resource(Resource):
 
     def perform_request(self, params):
         # 从指标选择器缓存表根据业务查询指标
-        metrics = MetricListCache.objects.filter(bk_biz_id__in=[0, params["bk_biz_id"]])
+        metrics = MetricListCache.objects.filter(
+            bk_tenant_id=get_request_tenant_id(), bk_biz_id__in=[0, params["bk_biz_id"]]
+        )
 
         if get_source_app() == SourceApp.FTA:
             metrics = metrics.filter(
@@ -3177,14 +3181,15 @@ class PromqlToQueryConfig(Resource):
             data_type_label = DataTypeLabel.TIME_SERIES
             # 根据data_label查找对应指标缓存结果表
             if not result_table_id:
-                qs = MetricListCache.objects.filter(
+                metric = MetricListCache.objects.filter(
+                    bk_tenant_id=get_request_tenant_id(),
                     data_label=data_label,
                     data_source_label=data_source_label,
                     data_type_label=data_type_label,
                     metric_field=query["field_name"],
-                )
-                if qs.exists():
-                    result_table_id = qs.first().result_table_id
+                ).first()
+                if metric:
+                    result_table_id = metric.result_table_id
 
             query_config = {
                 "data_source_label": data_source_label,
