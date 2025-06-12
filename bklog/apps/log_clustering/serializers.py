fa09abf712eb136e4c28f8c537266d92579178e5
lyjpy <jay0613@163.com>
2025-06-12 14:21:01 +0800

    feat: 聚类调试支持最大长度等条件

--- bklog/apps/log_clustering/serializers.py
+++ bklog/apps/log_clustering/serializers.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making BK-LOG 蓝鲸日志平台 available.
 Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
@@ -19,6 +18,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 We undertake not to change the open source license (MIT license) applicable to the current version of
 the project delivered to anyone in the future.
 """
+
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
@@ -94,6 +94,8 @@ class ClusteringConfigSerializer(serializers.Serializer):
 class ClusteringDebugSerializer(serializers.Serializer):
     input_data = serializers.CharField()
     predefined_varibles = serializers.CharField()
+    delimeter = serializers.CharField(required=False)
+    max_log_length = serializers.IntegerField(required=False)
 
 
 class SetRemarkSerializer(serializers.Serializer):
