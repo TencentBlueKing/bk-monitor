8edf37e915057b7c58e3a3fb5f01f41ab5b136d0
xintaoLi <li18566230175@gmail.com>
2025-06-20 16:33:40 +0800

    fix: sql editor 默认渲染执行逻辑修复

--- bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/index.tsx
+++ bklog/web/src/views/retrieve-v2/search-result-panel/graph-analysis/sql-editor/index.tsx
@@ -23,7 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { computed, defineComponent, Ref, ref } from 'vue';
+import { computed, defineComponent, Ref, ref, onMounted } from 'vue';
 
 import $http from '@/api/index.js';
 import useLocale from '@/hooks/use-locale';
@@ -325,6 +325,10 @@ export default defineComponent({
       onRefereceChange,
     );
     useResizeObserve(refSqlPreviewElement, debounceUpdateHeight);
+
+    onMounted(async () => {
+      debounceSyncAdditionToSQL(debounceQuery);
+    });
 
     expose({
       handleQueryBtnClick,
