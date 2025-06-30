15ef18829d8208b54281b9e379c5a223f2177ddf
v_cwcgcui <1724427771@qq.com>
2025-06-30 09:53:07 +0800

    feat: 检索别名拼接长度优化 #1010158081125320007

--- bklog/apps/log_esquery/esquery/builder/query_index_optimizer.py
+++ bklog/apps/log_esquery/esquery/builder/query_index_optimizer.py
@@ -47,7 +47,6 @@ class QueryIndexOptimizer:
         indices = indices.replace(" ", "")
         result_table_id_list: list[str] = map_if(indices.split(","))
         # 根据查询场景优化index
-        _index = ""
         if scenario_id in [Scenario.BKDATA, Scenario.LOG]:
             # 日志采集使用0时区区分index入库,数据平台使用服务器所在时区
             time_zone = "GMT" if scenario_id == Scenario.LOG else tz.gettz()
