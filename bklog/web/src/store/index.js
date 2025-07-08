c19b630bf6381b482e5e226801d00e565ed7de68
LeeXT <xintaoLi@users.noreply.github.com>
2025-07-08 11:37:17 +0800

    fix: 修复安全问题 (#7064)

--- bklog/web/src/store/index.js
+++ bklog/web/src/store/index.js
@@ -42,6 +42,7 @@ import {
 } from '@/common/util';
 import { handleTransformToTimestamp } from '@/components/time-range/utils';
 import { builtInInitHiddenList } from '@/const/index.js';
+import DOMPurify from 'dompurify';
 import Vuex from 'vuex';
 
 import { deepClone } from '../components/monitor-echarts/utils';
@@ -52,7 +53,6 @@ import {
   IndexSetQueryResult,
   IndexFieldInfo,
   IndexItem,
-  logSourceField,
   indexSetClusteringData,
   getDefaultRetrieveParams,
   getStorageOptions,
@@ -1602,7 +1602,7 @@ const store = new Vuex.Store({
 
       const formatJsonString = formatResult => {
         if (typeof formatResult === 'string') {
-          return formatResult.replace(/"/g, '\\"');
+          return DOMPurify.sanitize(formatResult);
         }
 
         return formatResult;
@@ -1708,29 +1708,10 @@ const store = new Vuex.Store({
       return Promise.resolve([filterQueryList, searchMode, isNewSearchPage]);
     },
 
-    changeShowUnionSource({ commit, dispatch, state }) {
+    changeShowUnionSource({ commit, state }) {
       commit('updateIndexSetOperatorConfig', { isShowSourceField: !state.indexSetOperatorConfig.isShowSourceField });
-      // dispatch('showShowUnionSource', { keepLastTime: false });
     },
 
-    /** 日志来源显隐操作 */
-    // showShowUnionSource({ state }, { keepLastTime = false }) {
-    //   // 非联合查询 或者清空了所有字段 不走逻辑
-    //   if (!state.indexItem.isUnionIndex || !state.visibleFields.length) return;
-    //   const isExist = state.visibleFields.some(item => item.tag === 'union-source');
-    //   // 保持之前的逻辑
-    //   if (keepLastTime) {
-    //     const isShowSourceField = state.indexSetOperatorConfig.isShowSourceField;
-    //     if (isExist) {
-    //       !isShowSourceField && state.visibleFields.shift();
-    //     } else {
-    //       isShowSourceField && state.visibleFields.unshift(logSourceField());
-    //     }
-    //     return;
-    //   }
-
-    //   isExist ? state.visibleFields.shift() : state.visibleFields.unshift(logSourceField());
-    // },
     requestSearchTotal({ state, getters }) {
       state.searchTotal = 0;
       const start_time = Math.floor(getters.retrieveParams.start_time);
