12874cb12509aefdeff1cf57ef141df96b7a8053
v_cwcgcui <1724427771@qq.com>
2025-07-11 17:04:29 +0800

    fix: grep_query语法解析问题修复 #1010158081145302159

--- bklog/apps/tests/log_search/test_grep_logic.py
+++ bklog/apps/tests/log_search/test_grep_logic.py
@@ -147,11 +147,23 @@ HIGHLIGHT_PARAMS = [
         "match_field": "__ext.labels.app",
         "pattern": "s",
     },
+    {
+        "data_list": [{"host_id": 21, "log": "123sasname age, des 1 ssa"}],
+        "match_field": "log",
+        "pattern": "name age",
+    },
+    {
+        "data_list": [{"host_id": 21, "log": "123sasname age, 1 des ssa"}],
+        "match_field": "log",
+        "pattern": "name age, 1 des",
+    },
 ]
 HIGHLIGHT_RESULT = [
     [{"host_id": "2<mark>1</mark>", "__ext": '{"container_id":"7df2fe1","labels":{"app":"dsa","component":"se"}}'}],
     [{"host_id": 21, "__ext": '{"container_id": "7<mark>df</mark>2fe1", "labels": {"app": "dsa", "component": "se"}}'}],
     [{"host_id": 21, "__ext": '{"container_id": "7df2fe1", "labels": {"app": "d<mark>s</mark>a", "component": "se"}}'}],
+    [{"host_id": 21, "log": "123sas<mark>name age</mark>, des 1 ssa"}],
+    [{"host_id": 21, "log": "123sas<mark>name age, 1 des</mark> ssa"}],
 ]
 
 
