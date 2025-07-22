25ff6fc538a4efb7eee13531a1fa4ad2545846f5
EASYGOING45 <1505674928@qq.com>
2025-07-22 15:22:02 +0800

    feat: AIAgents: 小鲸快捷指令上新,新增PromQL Helper指令功能 --story=126039911 # Reviewed, transaction id: 51311

--- bkmonitor/ai_agents/tests/resources/test_switch_agent_by_command.py
+++ bkmonitor/ai_agents/tests/resources/test_switch_agent_by_command.py
@@ -114,3 +114,109 @@ def test_switch_agent_by_command_metadata():
     assert result
 
     DestroyChatSessionResource().request(session_code=session_code)
+
+
+def test_switch_agent_by_command_promql_helper():
+    """
+    测试通过快捷指令方式切换Agent
+    案例: PromQL Helper
+    """
+    session_code = generate_uuid()
+
+    settings.AIDEV_COMMAND_AGENT_MAPPING = {
+        "metadata_diagnosis": "promql_helper_xxx",
+    }
+
+    # 默认API Client
+    api_client = AidevApiClientBuilder.get_client(
+        bk_app_code=settings.AIDEV_AGENT_APP_CODE, bk_app_secret=settings.AIDEV_AGENT_APP_SECRET
+    )
+
+    basic_config = AgentConfigManager.get_config(agent_code=settings.AIDEV_AGENT_APP_CODE, api_client=api_client)
+
+    create_res = CreateChatSessionResource().request(
+        session_code=session_code, session_name="test_switch_agent_by_command"
+    )
+
+    assert create_res
+
+    # 初始化System Prompt
+    create_sys_content_res = CreateChatSessionContentResource().request(
+        session_code=session_code, role="hidden-system", content=basic_config.role_prompt
+    )
+
+    assert create_sys_content_res
+
+    # 创建问题 -- 大Agent
+    create_question_res = CreateChatSessionContentResource().request(
+        session_code=session_code, role="user", content="告警包含哪几个级别？"
+    )
+
+    assert create_question_res
+
+    # 获取对话上下文
+    session_context_data = api_client.api.get_chat_session_context(path_params={"session_code": session_code}).get(
+        "data", []
+    )
+
+    assert session_context_data
+
+    params = {"session_code": session_code, "execute_kwargs": {"stream": True}}
+
+    # 流式响应拼接
+    generator = CreateChatCompletionResource().request(params)
+    result = collect_streaming_response(generator)
+
+    # 插入AI回答
+    create_ai_answer_res = CreateChatSessionContentResource().request(
+        session_code=session_code, role="ai", content=result["full_content"]
+    )
+
+    assert create_ai_answer_res
+
+    promql_helper_command_params = {
+        "session_code": session_code,
+        "role": "user",
+        "content": "链路元数据排障",
+        "property": {
+            "extra": {
+                "command": "promql_helper",
+                "context": [
+                    {
+                        "__key": "promql",
+                        "__value": "bkmonitor:system:disk:in_use",
+                        "promql": "bkmonitor:system:disk:in_use",
+                        "context_type": "textarea",
+                    },
+                    {
+                        "__key": "user_demand",
+                        "__value": "请帮我按照目标IP进行聚合,找到最大值对应的IP",
+                        "user_demand": "请帮我按照目标IP进行聚合,找到最大值对应的IP",
+                        "context_type": "textarea",
+                    },
+                ],
+                "anchor_path_resources": {},
+            }
+        },
+    }
+
+    create_command_res = CreateChatSessionContentResource().request(**promql_helper_command_params)
+
+    assert create_command_res
+
+    # 获取对话上下文
+    session_context_data = api_client.api.get_chat_session_context(path_params={"session_code": session_code}).get(
+        "data", []
+    )
+
+    assert session_context_data
+
+    params = {"session_code": session_code, "execute_kwargs": {"stream": True}}
+
+    # 流式响应拼接
+    generator = CreateChatCompletionResource().request(params)
+    result = collect_streaming_response(generator)
+
+    assert result
+
+    DestroyChatSessionResource().request(session_code=session_code)
