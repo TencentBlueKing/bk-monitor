93fedb5324b5713fbf857ee1ea7e960cbb86b322
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-08-01 17:28:20 +0800

    feat: 告警后台支持多租户 #1010158081124374676

--- bkmonitor/api/bk_login/default.py
+++ bkmonitor/api/bk_login/default.py
@@ -290,4 +290,12 @@ class BatchQueryUserDisplayInfoResource(BkUserApiResource):
     method = "GET"
 
     class RequestSerializer(serializers.Serializer):
-        bk_usernames = serializers.CharField(required=True)
+        bk_usernames = serializers.ListField(required=True)
+
+    def perform_request(self, params: dict):
+        if not settings.ENABLE_MULTI_TENANT_MODE:
+            return [{"bk_username": username, "display_name": username} for username in params["bk_usernames"]]
+
+        # 参数格式转换
+        params["bk_usernames"] = ",".join(params["bk_usernames"])
+        return super().perform_request(params)
