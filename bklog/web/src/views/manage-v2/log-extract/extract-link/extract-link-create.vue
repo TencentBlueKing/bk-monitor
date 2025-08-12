e4acaff9eec3c8032f07b7c4a7e548dd77b0fd73
XiaohuTalk <2540760998@qq.com>
2025-08-12 16:03:23 +0800

    feat: 重构提取链路管理list组件

---
+++ bklog/web/src/views/manage-v2/log-extract/extract-link/extract-link-create.vue
@@ -0,0 +1,639 @@
+<!--
+* Tencent is pleased to support the open source community by making
+* 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+*
+* Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+*
+* 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+*
+* License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+*
+* ---------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+* documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+* to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+* the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+* THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+-->
+
+<template>
+  <div
+    class="extract-link-create-container"
+    v-bkloading="{ isLoading: basicLoading }"
+    data-test-id="extractLinkCreate_div_extractLinkCreateBox"
+  >
+    <article
+      class="article"
+      data-test-id="extractLinkCreateBox_article_basicInformation"
+    >
+      <h3 class="title">{{ $t('基础信息') }}</h3>
+      <bk-form
+        ref="formRef"
+        class="king-form"
+        :label-width="160"
+        :model="formData"
+        :rules="formRules"
+      >
+        <bk-form-item
+          :label="$t('链路名称')"
+          property="name"
+          required
+        >
+          <bk-input
+            v-model="formData.name"
+            data-test-id="basicInformation_input_linkName"
+          >
+          </bk-input>
+        </bk-form-item>
+        <bk-form-item
+          :label="$t('链路类型')"
+          property="link_type"
+          required
+        >
+          <bk-select
+            v-model="formData.link_type"
+            :clearable="false"
+            data-test-id="basicInformation_select_selectLinkType"
+          >
+            <bk-option
+              v-if="!isK8sDeploy || isShowCommon"
+              id="common"
+              :disabled="isDisableCommon"
+              :name="$t('内网链路')"
+            ></bk-option>
+            <bk-option
+              id="qcloud_cos"
+              :name="$t('腾讯云链路')"
+            ></bk-option>
+            <bk-option
+              id="bk_repo"
+              :name="$t('蓝鲸制品库')"
+            ></bk-option>
+          </bk-select>
+        </bk-form-item>
+        <bk-form-item
+          :label="$t('执行人')"
+          property="operator"
+          required
+        >
+          <bk-user-selector
+            :api="userApi"
+            :class="isAdminError && 'is-error'"
+            :empty-text="$t('无匹配人员')"
+            :placeholder="$t('请选择用户')"
+            :value="formData.operator"
+            data-test-id="basicInformation_input_executive"
+            @blur="handleBlur"
+            @change="handleUserChange"
+            @focus="handleClearOperator"
+          >
+          </bk-user-selector>
+        </bk-form-item>
+        <bk-form-item
+          :label="$t('执行bk_biz_id')"
+          property="op_bk_biz_id"
+          required
+        >
+          <bk-input
+            v-model="formData.op_bk_biz_id"
+            data-test-id="basicInformation_input_executivebk_biz_id"
+          ></bk-input>
+        </bk-form-item>
+        <template v-if="formData.link_type === 'qcloud_cos'">
+          <bk-form-item
+            :label="$t('腾讯云SecretId')"
+            property="qcloud_secret_id"
+            required
+          >
+            <bk-input
+              v-model="formData.qcloud_secret_id"
+              data-test-id="basicInformation_input_SecretId"
+            >
+            </bk-input>
+          </bk-form-item>
+          <bk-form-item
+            :label="$t('腾讯云SecretKey')"
+            property="qcloud_secret_key"
+            required
+          >
+            <bk-input
+              v-model="formData.qcloud_secret_key"
+              data-test-id="basicInformation_input_SecretKey"
+              type="password"
+            ></bk-input>
+          </bk-form-item>
+          <bk-form-item
+            :label="$t('腾讯云Cos桶名称')"
+            property="qcloud_cos_bucket"
+            required
+          >
+            <bk-input
+              v-model="formData.qcloud_cos_bucket"
+              data-test-id="basicInformation_input_cosBucket"
+            ></bk-input>
+          </bk-form-item>
+          <bk-form-item
+            :label="$t('腾讯云Cos区域')"
+            property="qcloud_cos_region"
+            required
+          >
+            <bk-input
+              v-model="formData.qcloud_cos_region"
+              data-test-id="basicInformation_input_cosRegion"
+            ></bk-input>
+          </bk-form-item>
+        </template>
+        <bk-form-item
+          :label="$t('是否启用')"
+          property="is_enable"
+          required
+        >
+          <bk-radio-group
+            v-model="formData.is_enable"
+            data-test-id="basicInformation_radio_whetherToEnable"
+          >
+            <bk-radio
+              style="margin-right: 16px"
+              :value="true"
+              >{{ $t('是') }}</bk-radio
+            >
+            <bk-radio :value="false">{{ $t('否') }}</bk-radio>
+          </bk-radio-group>
+        </bk-form-item>
+      </bk-form>
+    </article>
+    <article
+      class="article"
+      data-test-id="extractLinkCreateBox_article_linkTransfer"
+    >
+      <h3 class="title">{{ $t('链路中转机') }}</h3>
+      <div class="custom-form">
+        <div class="custom-label">{{ $t('中转机') }}</div>
+        <div class="custom-content">
+          <ul
+            ref="hostListRef"
+            class="host-list"
+          >
+            <li class="host-item header">
+              <div
+                class="min-box dir-container"
+                :title="$t('挂载目录')"
+              >
+                {{ $t('挂载目录') }}
+              </div>
+              <div
+                class="min-box id-container"
+                :title="$t('主机管控区域ID')"
+              >
+                {{ $t('主机管控区域ID') }}
+              </div>
+              <div
+                class="min-box ip-container"
+                :title="$t('主机IP')"
+              >
+                {{ $t('主机IP') }}
+              </div>
+              <div
+                class="min-box operation-container"
+                :title="$t('操作')"
+              >
+                {{ $t('操作') }}
+              </div>
+            </li>
+            <li
+              v-for="(item, index) in formData.hosts"
+              class="host-item"
+              :key="item.keyId"
+            >
+              <div class="min-box dir-container">
+                <bk-input
+                  class="king-input"
+                  v-model="item.target_dir"
+                  @blur="handleInputBlur"
+                >
+                </bk-input>
+              </div>
+              <div class="min-box id-container">
+                <bk-input
+                  class="king-input"
+                  v-model="item.bk_cloud_id"
+                  @blur="handleInputBlur"
+                >
+                </bk-input>
+              </div>
+              <div class="min-box ip-container">
+                <bk-input
+                  class="king-input"
+                  v-model="item.ip"
+                  @blur="handleInputBlur"
+                >
+                </bk-input>
+              </div>
+              <div class="min-box operation-container">
+                <bk-button
+                  style="padding: 0"
+                  :disabled="formData.hosts.length === 1"
+                  size="small"
+                  text
+                  @click="deleteHost(index)"
+                  >{{ $t('删除') }}
+                </bk-button>
+              </div>
+            </li>
+          </ul>
+          <bk-button
+            class="king-button"
+            @click="addHost"
+            >{{ $t('添加链路中转机') }}</bk-button
+          >
+        </div>
+      </div>
+    </article>
+    <bk-button
+      style="width: 86px"
+      :loading="submitLoading"
+      data-test-id="basicInformation_button_submitFrom"
+      theme="primary"
+      @click="submitForm"
+    >
+      {{ $t('提交') }}
+    </bk-button>
+  </div>
+</template>
+
+<script>
+  import BkUserSelector from '@blueking/user-selector';
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'ExtractLinkCreate',
+    components: {
+      BkUserSelector,
+    },
+    data() {
+      return {
+        basicLoading: false,
+        submitLoading: false,
+        isSubmit: false,
+        userApi: window.BK_LOGIN_URL,
+        formData: {
+          name: '',
+          link_type: 'common',
+          operator: [],
+          op_bk_biz_id: '',
+          qcloud_secret_id: '', // 腾讯云SecretId
+          qcloud_secret_key: '', // 腾讯云SecretKey
+          qcloud_cos_bucket: '', // 腾讯云Cos桶名称
+          qcloud_cos_region: '', // 腾讯云Cos区域
+          is_enable: true, // 是否启用
+          hosts: [
+            {
+              keyId: Date.now(),
+              target_dir: '', // 挂载目录
+              bk_cloud_id: '', // 主机管控区域id
+              ip: '', // 主机ip
+            },
+          ], // 中转机列表
+        },
+        formRules: {
+          name: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+          op_bk_biz_id: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+          qcloud_secret_key: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+          qcloud_cos_bucket: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+          qcloud_secret_id: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+          qcloud_cos_region: [
+            {
+              required: true,
+              trigger: 'blur',
+            },
+          ],
+        },
+        isAdminError: false, // 人员是否为空
+        cacheOperator: [], // 缓存的人员
+        isDisableCommon: false, // 是否禁用内网链路
+        editInitLinkType: '', // 编辑初始化时的链路类型
+      };
+    },
+    computed: {
+      ...mapState({
+        showRouterLeaveTip: state => state.showRouterLeaveTip,
+      }),
+      isK8sDeploy() {
+        // 是否要禁用内网链路
+        return this.$store.getters['globals/globalsData']?.is_k8s_deploy;
+      },
+      isShowCommon() {
+        // 禁用内网链路且 编辑时初始化是内网链路则展示内网链路且禁用选项
+        return this.$route.params.linkId && this.editInitLinkType === 'common';
+      },
+    },
+    created() {
+      this.init();
+    },
+
+    beforeRouteLeave(to, from, next) {
+      if (!this.isSubmit && !this.showRouterLeaveTip) {
+        this.$bkInfo({
+          title: this.$t('是否放弃本次操作？'),
+          confirmFn: () => {
+            next();
+          },
+        });
+        return;
+      }
+      next();
+    },
+    methods: {
+      async init() {
+        const { linkId } = this.$route.params;
+        if (linkId) {
+          try {
+            this.basicLoading = true;
+            const res = await this.$http.request('extractManage/getLogExtractLinkDetail', {
+              params: {
+                link_id: linkId,
+              },
+            });
+            const formData = res.data;
+            delete formData.link_id;
+            formData.hosts.forEach((item, index) => {
+              item.keyId = index;
+            });
+            // 字符串转成数组展示
+            formData.operator = [formData.operator];
+            if (formData.link_type === 'qcloud_cos') {
+              formData.qcloud_secret_key = res.qcloud_secret_key || '******';
+            }
+            this.formData = Object.assign({}, this.formData, formData);
+            this.basicLoading = false;
+            this.editInitLinkType = this.formData.link_type;
+          } catch (e) {
+            console.warn(e);
+            this.$router.push({
+              name: 'extract-link-list',
+              query: {
+                spaceUid: this.$store.state.spaceUid,
+              },
+            });
+          } finally {
+            if (this.isK8sDeploy && this.editInitLinkType === 'common') {
+              this.isDisableCommon = true; // 禁用内网链路选项
+            }
+          }
+        } else {
+          // 新建时 内网链路禁用则更改初始值
+          if (this.isK8sDeploy) this.formData.link_type = 'qcloud_cos';
+        }
+      },
+      addHost() {
+        this.formData.hosts.push({
+          keyId: Date.now(),
+          target_dir: '', // 挂载目录
+          bk_cloud_id: '', // 主机管控区域id
+          ip: '', // 主机ip
+        });
+      },
+      deleteHost(index) {
+        this.formData.hosts.splice(index, 1);
+      },
+      handleInputBlur(value, event) {
+        if (value) {
+          event.target.classList.remove('error');
+        } else {
+          event.target.classList.add('error');
+        }
+      },
+      async submitForm() {
+        try {
+          let isError = false;
+          const inputList = this.$refs.hostListRef.getElementsByClassName('bk-form-input');
+          for (const inputEl of inputList) {
+            if (!inputEl.value) {
+              isError = true;
+              inputEl.classList.add('error');
+            } else {
+              inputEl.classList.remove('error');
+            }
+          }
+          await this.$refs.formRef.validate();
+          if (isError || this.isAdminError) return;
+          this.submitLoading = true;
+          const requestData = { ...this.formData };
+          if (requestData.link_type === 'common') {
+            delete requestData.qcloud_cos_bucket;
+            delete requestData.qcloud_cos_region;
+            delete requestData.qcloud_secret_id;
+            delete requestData.qcloud_secret_key;
+          }
+          if (requestData.link_type === 'qcloud_cos' && requestData.qcloud_secret_key === '******') {
+            requestData.qcloud_secret_key = '';
+          }
+          requestData.hosts.forEach(host => {
+            delete host.keyId;
+          });
+          // 数组修改为字符串传参
+          requestData.operator = requestData.operator[0];
+          const { linkId } = this.$route.params;
+          if (linkId) {
+            await this.$http.request('extractManage/updateLogExtractLink', {
+              params: {
+                link_id: linkId,
+              },
+              data: requestData,
+            });
+            this.messageSuccess(this.$t('保存成功'));
+          } else {
+            await this.$http.request('extractManage/createLogExtractLink', {
+              data: requestData,
+            });
+            this.messageSuccess(this.$t('创建成功'));
+          }
+          this.isSubmit = true;
+          this.$router.push({
+            name: 'extract-link-list',
+            query: {
+              spaceUid: this.$store.state.spaceUid,
+            },
+          });
+        } catch (e) {
+          console.warn(e);
+          this.submitLoading = false;
+        }
+      },
+      handleUserChange(val) {
+        const realVal = val.filter(item => item !== undefined);
+        this.isAdminError = !realVal.length;
+        this.formData.operator = realVal;
+        this.cacheOperator = realVal;
+      },
+      handleClearOperator() {
+        if (this.formData.operator.length) {
+          this.cacheOperator = this.formData.operator;
+          this.formData.operator = [];
+        }
+      },
+      handleBlur() {
+        if (this.cacheOperator.length) {
+          this.formData.operator = this.cacheOperator;
+        }
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../../scss/mixins/scroller';
+
+  .extract-link-create-container {
+    height: 100%;
+    padding: 20px 24px;
+    overflow: auto;
+
+    @include scroller($backgroundColor: #adadad, $width: 4px);
+
+    .article {
+      padding: 22px 24px;
+      margin-bottom: 20px;
+      background-color: #fff;
+      border: 1px solid #dcdee5;
+      border-radius: 3px;
+
+      .title {
+        margin: 0 0 10px;
+        font-size: 14px;
+        font-weight: bold;
+        line-height: 20px;
+        color: #63656e;
+      }
+
+      .king-form {
+        width: 680px;
+
+        :deep(.bk-form-item) {
+          padding: 10px 0;
+          margin: 0;
+        }
+      }
+
+      .custom-form {
+        display: flex;
+        font-size: 14px;
+        color: #63656e;
+
+        .custom-label {
+          position: relative;
+          width: 160px;
+          padding: 10px 24px 10px 0;
+          line-height: 32px;
+          text-align: right;
+
+          &:after {
+            position: absolute;
+            top: 12px;
+            right: 16px;
+            display: inline-block;
+            font-size: 12px;
+            color: #ea3636;
+            content: '*';
+          }
+        }
+
+        .custom-content {
+          width: calc(100% - 160px);
+          max-width: 1000px;
+          padding: 5px 24px 5px 0;
+
+          .king-button {
+            margin-top: 10px;
+          }
+        }
+
+        .host-list {
+          font-size: 12px;
+          border: 1px solid #dcdee5;
+          border-bottom: none;
+
+          .host-item {
+            display: flex;
+            align-content: center;
+            width: 100%;
+            padding: 5px 0;
+            line-height: 32px;
+            background-color: #fff;
+            border-bottom: 1px solid #dcdee5;
+
+            &.header {
+              background-color: #fafbfd;
+            }
+
+            .min-box {
+              padding: 0 10px;
+
+              .king-input {
+                width: 86%;
+
+                :deep(.bk-form-input.error) {
+                  border-color: #ea3636;
+                }
+              }
+
+              &.dir-container {
+                width: 40%;
+              }
+
+              &.id-container,
+              &.ip-container {
+                width: 25%;
+              }
+
+              &.operation-container {
+                width: 10%;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  :deep(.user-selector) {
+    width: 100%;
+  }
+
+  :deep(.is-error .user-selector-container) {
+    border-color: #ff5656;
+  }
+</style>
---
+++ bklog/web/src/views/manage-v2/log-extract/extract-link/link-list.tsx
@@ -0,0 +1,270 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import { defineComponent, ref, computed, onMounted } from 'vue';
+import useStore from '@/hooks/use-store';
+import useRouter from '@/hooks/use-router';
+import useLocale from '@/hooks/use-locale';
+import http from '@/api';
+import EmptyStatus from '@/components/empty-status/index.vue';
+import * as authorityMap from '@/common/authority-map';
+import { Message, InfoBox } from 'bk-magic-vue';
+
+import './link-list.scss';
+
+export default defineComponent({
+  name: 'ExtractLinkList',
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const { t } = useLocale();
+
+    const isLoading = ref(true); // 加载状态
+    const extractLinkList = ref<any[]>([]); // 链路数据
+    const isAllowedManage = ref<null | boolean>(null); // 是否有管理权限
+    const isButtonLoading = ref(false); // 新增按钮 loading
+    const emptyType = ref('empty'); // 空状态类型
+    const linkNameMap = computed(() => ({
+      // 链路类型映射
+      common: t('内网链路'),
+      qcloud_cos: t('腾讯云链路'),
+      bk_repo: t('蓝鲸制品库'),
+    }));
+
+    const spaceUid = computed(() => store.getters.spaceUid); // 空间UID
+
+    // 权限校验，校验通过后拉取链路列表
+    const checkManageAuth = async () => {
+      try {
+        const res = await store.dispatch('checkAllowed', {
+          action_ids: [authorityMap.MANAGE_EXTRACT_AUTH],
+          resources: [
+            {
+              type: 'space',
+              id: spaceUid.value,
+            },
+          ],
+        });
+        isAllowedManage.value = res.isAllowed;
+        if (res.isAllowed) {
+          await initList();
+        } else {
+          isLoading.value = false;
+        }
+      } catch (err) {
+        console.warn(err);
+        isLoading.value = false;
+        isAllowedManage.value = false;
+      }
+    };
+
+    // 初始化链路列表
+    const initList = async () => {
+      try {
+        const res = await http.request('extractManage/getLogExtractLinks');
+        extractLinkList.value = res.data;
+      } catch (e) {
+        console.warn(e);
+        emptyType.value = '500';
+      } finally {
+        isLoading.value = false;
+      }
+    };
+
+    // 新增链路
+    const handleCreate = async () => {
+      if (!isAllowedManage.value) {
+        try {
+          isButtonLoading.value = true;
+          const res = await store.dispatch('getApplyData', {
+            action_ids: [authorityMap.MANAGE_EXTRACT_AUTH],
+            resources: [
+              {
+                type: 'space',
+                id: spaceUid.value,
+              },
+            ],
+          });
+          store.commit('updateAuthDialogData', res.data);
+        } catch (err) {
+          console.warn(err);
+        } finally {
+          isButtonLoading.value = false;
+        }
+      } else {
+        router.push({
+          name: 'extract-link-create',
+          query: {
+            spaceUid: spaceUid.value,
+          },
+        });
+      }
+    };
+
+    // 编辑链路
+    const handleEditStrategy = (row: any) => {
+      router.push({
+        name: 'extract-link-edit',
+        params: {
+          linkId: row.link_id,
+        },
+        query: {
+          spaceUid: spaceUid.value,
+          editName: row.name,
+        },
+      });
+    };
+
+    // 删除链路
+    const handleDeleteStrategy = (row: any) => {
+      InfoBox({
+        title: `${t('确定要删除')}【${row.name}】？`,
+        confirmLoading: true,
+        confirmFn: () => confirmDeleteStrategy(row.link_id),
+      });
+    };
+
+    // 确认删除策略
+    const confirmDeleteStrategy = async (id: number) => {
+      try {
+        isLoading.value = true;
+        await http.request('extractManage/deleteLogExtractLink', {
+          params: {
+            link_id: id,
+          },
+        });
+        Message({
+          theme: 'success',
+          message: t('删除成功'),
+        });
+        await initList();
+      } catch (e) {
+        console.warn(e);
+        isLoading.value = false;
+      }
+    };
+
+    // 空状态操作
+    const handleOperation = (type: string) => {
+      if (type === 'refresh') {
+        emptyType.value = 'empty';
+        initList();
+      }
+    };
+
+    // 表头渲染
+    const renderHeader = (_: any, { column }: any) => <span>{column.label}</span>;
+
+    onMounted(() => {
+      checkManageAuth();
+    });
+
+    // 渲染函数
+    return () => (
+      <div
+        class='extract-link-list-container'
+        v-bkloading={{ isLoading: isLoading.value }}
+        data-test-id='extractLinkList_div_extractLinkListBox'
+      >
+        {/* 新增按钮 */}
+        <div>
+          <bk-button
+            style='width: 120px; margin: 20px 0'
+            class='king-button'
+            v-cursor={{ active: isAllowedManage.value === false }}
+            disabled={isAllowedManage.value === null || isLoading.value}
+            loading={isButtonLoading.value}
+            data-test-id='extractLinkListBox_button_addNewLinkList'
+            theme='primary'
+            onClick={handleCreate}
+          >
+            {t('新增')}
+          </bk-button>
+        </div>
+        {/* 链路表格 */}
+        <bk-table
+          class='king-table'
+          data={extractLinkList.value}
+          data-test-id='extractLinkListBox_table_LinkListTableBox'
+          row-key='strategy_id'
+          scopedSlots={{
+            empty: () => (
+              <div>
+                <EmptyStatus
+                  empty-type={emptyType.value}
+                  on-operation={handleOperation}
+                />
+              </div>
+            ),
+          }}
+        >
+          <bk-table-column
+            label={t('链路名称')}
+            renderHeader={renderHeader}
+            scopedSlots={{
+              default: ({ row }: any) => (
+                <div class='table-ceil-container'>
+                  <span v-bk-overflow-tips>{row.name}</span>
+                </div>
+              ),
+            }}
+          />
+          <bk-table-column
+            label={t('链路类型')}
+            renderHeader={renderHeader}
+            prop='created_at'
+            scopedSlots={{
+              default: ({ row }: any) => <div>{linkNameMap.value[row.link_type]}</div>,
+            }}
+          />
+          <bk-table-column
+            width='200'
+            label={t('操作')}
+            renderHeader={renderHeader}
+            scopedSlots={{
+              default: ({ row }: any) => (
+                <div class='task-operation-container'>
+                  <span
+                    class='task-operation'
+                    onClick={() => handleEditStrategy(row)}
+                  >
+                    {t('编辑')}
+                  </span>
+                  <span
+                    class='task-operation'
+                    onClick={() => handleDeleteStrategy(row)}
+                  >
+                    {t('删除')}
+                  </span>
+                </div>
+              ),
+            }}
+          />
+        </bk-table>
+      </div>
+    );
+  },
+});
