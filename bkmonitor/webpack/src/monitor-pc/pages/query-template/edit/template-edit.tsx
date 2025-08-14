c93813e30a51bc85b31343b5cf8fd0127edbb5c9
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-08-14 09:32:19 +0800

    feat: 查询模版配置开发 (#7568)

--- bkmonitor/webpack/src/monitor-pc/pages/query-template/edit/template-edit.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/query-template/edit/template-edit.tsx
@@ -26,17 +26,11 @@
 import { Component } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 
-import TemplateConfig from '../components/template-config/template-config';
-
 import './template-edit.scss';
 
 @Component
 export default class TemplateEdit extends tsc<object> {
   render() {
-    return (
-      <div class='template-edit'>
-        <TemplateConfig />
-      </div>
-    );
+    return <div class='template-edit' />;
   }
 }
