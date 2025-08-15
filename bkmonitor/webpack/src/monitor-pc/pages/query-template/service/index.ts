b756a52f46ad94244d34946e3dc9fd2ed096fa34
liangling0628 <lingliang1100@gmail.com>
2025-08-15 16:17:23 +0800

    feat: 查询模板开发 # Reviewed, transaction id: 54362

--- bkmonitor/webpack/src/monitor-pc/pages/query-template/service/index.ts
+++ bkmonitor/webpack/src/monitor-pc/pages/query-template/service/index.ts
@@ -23,13 +23,6 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-
-export * from './create';
-export * from './detail';
-export * from './edit';
-export * from './expression';
+export * from './dimension';
 export * from './function';
 export * from './metric';
-export * from './query-config';
-export * from './table';
-export * from './variables';
