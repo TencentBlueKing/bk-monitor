eeb9ee37c011a1d4bba2705f7b7f2991b0d36bf2
Austin <1344583166@qq.com>
2025-08-15 10:50:35 +0800

    feat: 日志聚类列表重构与新交互开发 --Story=126603105 (#7598)

---
+++ bklog/web/src/views/retrieve-v3/search-result/log-clustering/log-table/content-table/cluster-popover/regex-match/regex-table/index.tsx
@@ -0,0 +1,205 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+import _ from 'lodash';
+import { defineComponent, ref } from 'vue';
+import useLocale from '@/hooks/use-locale';
+import { random } from '@/common/util';
+import ValidateInput from './validate-input';
+import './index.scss';
+
+export interface RowData {
+  rowKey: string;
+  pattern: string;
+  occupy: string;
+  highlight: string;
+}
+
+export default defineComponent({
+  name: 'RegexTable',
+  components: {
+    ValidateInput,
+  },
+  setup(props, { expose, emit }) {
+    const { t } = useLocale();
+
+    const tableRef = ref(null);
+    const tableRenderKey = ref(0);
+    const inputColumnsMapRef = ref({
+      regex: [],
+      occupy: [],
+    });
+    const tableData = ref<RowData[]>([]);
+
+    const regexRules = [
+      {
+        validator: (value: string) => !!value,
+        message: t('不能为空'),
+      },
+    ];
+
+    const occupyRules = [
+      {
+        validator: (value: string) => !!value,
+        message: t('不能为空'),
+      },
+      {
+        validator: (value: string) => /^[A-Z]+$/.test(value),
+        message: t('{n}不规范, 包含特殊符号.', { n: t('占位符') }),
+      },
+    ];
+
+    let currentIndex = -1;
+    let hoverRowIndex = -1;
+
+    const handleDragStart = (index: number) => {
+      currentIndex = index;
+    };
+
+    const handleDragOver = (index: number) => {
+      hoverRowIndex = index;
+    };
+
+    const handleDragEnd = () => {
+      if (hoverRowIndex === -1 || hoverRowIndex === currentIndex) {
+        return;
+      }
+
+      const list = _.cloneDeep(tableData.value);
+      [list[currentIndex], list[hoverRowIndex]] = [list[hoverRowIndex], list[currentIndex]];
+      tableData.value = list;
+      currentIndex = -1;
+      hoverRowIndex = -1;
+      tableRenderKey.value += 1;
+      emit('change', tableData.value);
+    };
+
+    const handleDeleteItem = (index: number) => {
+      const currentRow = tableData.value[index];
+      emit('delete', currentRow);
+      tableData.value.splice(index, 1);
+      emit('change', tableData.value);
+    };
+
+    const setItemRef = (type: string, index: number) => (el: HTMLElement | null) => {
+      inputColumnsMapRef.value[type][index] = el;
+    };
+
+    expose({
+      appendItem: (item: RowData) => {
+        tableData.value.push({
+          rowKey: random(),
+          ...item,
+        });
+        emit('change', tableData.value);
+        setTimeout(() => {
+          tableRef.value.scrollTop = tableRef.value.scrollHeight;
+        });
+      },
+      getData: async () => {
+        await Promise.all(inputColumnsMapRef.value.regex.map(el => el.getValue()));
+        await Promise.all(inputColumnsMapRef.value.occupy.map(el => el.getValue()));
+        return tableData.value;
+      },
+    });
+
+    return () => (
+      <table
+        class='regex-table-main'
+        ref={tableRef}
+        key={tableRenderKey.value}
+      >
+        <thead>
+          <tr>
+            <th style='width:42px'></th>
+            <th style='width:68px'>{t('生效顺序')}</th>
+            <th style='width:42px'>{t('高亮')}</th>
+            <th style='width:500px'>{t('正则表达式')}</th>
+            <th style='width:223px'>{t('占位符')}</th>
+            <th style='width:42px'>{t('操作')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.value.map((row, index) => (
+            <tr
+              key={`${row.rowKey}_${index}`}
+              draggable='true'
+              on-dragstart={() => handleDragStart(index)}
+              on-dragover={() => handleDragOver(index)}
+              on-dragend={handleDragEnd}
+            >
+              <td>
+                <div class='drag-column'>
+                  <log-icon type='drag-dots' />
+                </div>
+              </td>
+              <td>
+                <div class='index-column'>{index + 1}</div>
+              </td>
+              <td>
+                <div class='highlight-column'>
+                  <div
+                    class='rect'
+                    style={{ background: row.highlight }}
+                  ></div>
+                </div>
+              </td>
+              <td>
+                <div class='regex-column'>
+                  <ValidateInput
+                    ref={setItemRef('regex', index)}
+                    value={row.pattern}
+                    rules={regexRules}
+                    on-input={value => (row.pattern = value.trim())}
+                  />
+                </div>
+              </td>
+              <td>
+                <div class='occupy-column'>
+                  <ValidateInput
+                    ref={setItemRef('occupy', index)}
+                    value={row.occupy}
+                    rules={occupyRules}
+                    on-input={value => (row.occupy = value.trim())}
+                  />
+                </div>
+              </td>
+              <td>
+                <div class='action-column'>
+                  <span on-click={() => handleDeleteItem(index)}>
+                    <log-icon
+                      type='circle-minus-filled'
+                      class='delete-icon'
+                    />
+                  </span>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  },
+});
