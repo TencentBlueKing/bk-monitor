483831bf4c40dc783ca28fcd3de5c7987346ad16
LivySara <1936808975@qq.com>
2025-08-18 09:34:11 +0800

    feat: 新增oxlint校验代码 --story=126558017 # Reviewed, transaction id: 54447

--- bklog/web/eslint.config.js
+++ bklog/web/eslint.config.js
@@ -23,6 +23,8 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+const oxlintPlugin = require('eslint-plugin-oxlint');
+
 module.exports = [
   ...require('@blueking/bkui-lint/eslint'),
   {
@@ -61,4 +63,5 @@ module.exports = [
       'prettier/prettier': 'off', // 关闭此规则
     },
   },
+  ...oxlintPlugin.buildFromOxlintConfigFile('./.oxlintrc.json'),
 ];
