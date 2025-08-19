4dc2e10d54965998fbacce4a4c04908d93fabe59
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2025-08-19 20:11:48 +0800

    feat: doris路由协议优化 #1010158081126709496 # Reviewed, transaction id: 54814

--- bklog/apps/log_search/views/index_set_views.py
+++ bklog/apps/log_search/views/index_set_views.py
@@ -443,17 +443,18 @@ class IndexSetViewSet(ModelViewSet):
         for index_set in index_set_list:
             if doris_table_id := index_set["doris_table_id"]:
                 doris_result_table = doris_table_id.rsplit(".", maxsplit=1)[0]
-                base_doris_router.update(
+                doris_router = base_doris_router.copy()
+                doris_router.update(
                     {
                         "space_type": index_set["space_uid"].split("__")[0],
                         "space_id": index_set["space_uid"].split("__")[-1],
                         "bkbase_table_id": doris_result_table,
                         "data_label": f"bklog_index_set_{index_set['index_set_id']}_analysis",
-                        "table_id": f"bklog_index_set_{index_set['index_set_id']}_{doris_result_table}.__default__",
+                        "table_id": f"bklog_index_set_{index_set['index_set_id']}_{doris_result_table}.__analysis__",
                         "query_alias_settings": index_set["query_alias_settings"],
                     }
                 )
-                router_list.append(base_doris_router)
+                router_list.append(doris_router)
         return Response({"total": total, "list": router_list})
 
     def retrieve(self, request, *args, **kwargs):
