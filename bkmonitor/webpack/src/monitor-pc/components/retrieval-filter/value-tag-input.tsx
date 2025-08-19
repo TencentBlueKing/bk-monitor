e5627fd6e4f9010ef1f494d5f5d931c2b30fd6d3
xlc <33691777+xiaolicheng-github@users.noreply.github.com>
2025-08-19 15:59:11 +0800

    feat: 查询模版配置开发 (#7640)

--- bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/value-tag-input.tsx
+++ bkmonitor/webpack/src/monitor-pc/components/retrieval-filter/value-tag-input.tsx
@@ -31,6 +31,7 @@ import { getCharLength } from './utils';
 import './value-tag-input.scss';
 
 interface IProps {
+  hasEdit?: boolean;
   isOneRow?: boolean;
   value: string;
   onChange?: (v: string) => void;
@@ -42,6 +43,7 @@ interface IProps {
 export default class ValueTagInput extends tsc<IProps> {
   @Prop({ type: String, default: '' }) value: string;
   @Prop({ type: Boolean, default: false }) isOneRow: boolean;
+  @Prop({ type: Boolean, default: true }) hasEdit: boolean;
   @Ref('input') inputRef: HTMLInputElement;
 
   localValue = '';
@@ -55,6 +57,9 @@ export default class ValueTagInput extends tsc<IProps> {
   }
 
   handleClick() {
+    if (!this.hasEdit) {
+      return;
+    }
     this.isEdit = true;
     this.inputFocus();
   }
