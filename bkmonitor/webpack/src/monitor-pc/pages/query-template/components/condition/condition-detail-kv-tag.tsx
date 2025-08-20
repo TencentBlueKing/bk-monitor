1f12793f4224813986109cb12c7f7b6d12bccd4c
JayCC1 <85379453+JayCC1@users.noreply.github.com>
2025-08-20 10:21:24 +0800

    feat: 查询模板开发 (#7655)

---
+++ bkmonitor/webpack/src/monitor-pc/pages/query-template/components/condition/condition-detail-kv-tag.tsx
@@ -0,0 +1,187 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+import { Component, Prop, Watch } from 'vue-property-decorator';
+import { Component as tsc } from 'vue-tsx-support';
+
+import { ConditionMethodAliasMap } from '../../constants';
+import { type AggCondition, type ConditionDetailTagItem } from '../../typings';
+import { getTemplateSrv } from '../../variables/template/template-srv';
+import { type QueryVariablesTransformResult, QueryVariablesTool } from '../utils/query-variable-tool';
+import VariableSpan from '../utils/variable-span';
+
+import './condition-detail-kv-tag.scss';
+
+interface IProps {
+  /** 是否是条件变量渲染 */
+  isConditionVariable?: boolean;
+  /** 条件对象 */
+  value: AggCondition;
+  variableMap?: Record<string, any>;
+  /** 条件变量名称 */
+  variableName?: string;
+  /** 维度值允许显示个数，多出显示省略号(不传默认全显示) */
+  visibleItemLimit?: number;
+}
+@Component
+export default class ConditionDetailKvTag extends tsc<IProps> {
+  /** 条件对象 */
+  @Prop({ type: Object, default: () => null }) value: AggCondition;
+  /** 维度值允许显示个数，多出显示省略号(不传或假值时默认全显示) */
+  @Prop({ type: Number, default: 0 }) visibleItemLimit: number;
+  /** 是否是条件变量渲染 */
+  @Prop({ type: Boolean, default: false }) isConditionVariable: boolean;
+  /** 条件变量名称 */
+  @Prop({ type: String, default: '' }) variableName: string;
+  @Prop({ type: Object, default: () => {} }) variableMap: Record<string, any>;
+
+  hideCount = 0;
+  variablesToolInstance = new QueryVariablesTool();
+  templateSrv = getTemplateSrv();
+
+  get tipContent() {
+    return `<div style="max-width: 600px;">${this.value.key} ${ConditionMethodAliasMap[this.value.method]} ${this.localValue11.map(v => v.id).join(' OR ')}<div>`;
+  }
+
+  get DomTag() {
+    return this.isConditionVariable ? VariableSpan : 'span';
+  }
+
+  get dimensionValueToVariableModel(): AggCondition['value'] | ConditionDetailTagItem['value'] {
+    if (!this.value.value?.length) {
+      return [];
+    }
+    if (this.isConditionVariable) {
+      return this.value.value;
+    }
+    return this.value.value.reduce((prev, curr) => {
+      const result = this.variablesToolInstance.transformVariables(curr);
+      if (!result.value) {
+        return prev;
+      }
+      prev.push(result);
+      return prev;
+    }, []);
+  }
+
+  get localValue11() {
+    if (this.isConditionVariable) {
+      return this.dimensionValueToVariableModel.map(v => ({
+        id: v,
+        name: this.showNameSlice(v),
+        isVariable: false,
+      }));
+    }
+    return (this.dimensionValueToVariableModel as ConditionDetailTagItem['value'])?.reduce?.((prev, curr) => {
+      if (!curr.isVariable) {
+        prev.push({
+          id: curr.value,
+          name: this.showNameSlice(curr.value),
+          isVariable: curr.isVariable,
+        });
+        return prev;
+      }
+      let varValue = '';
+      const result = this.templateSrv.replace(`\${${curr.variableName}:json}` as string, this.variableMap);
+      try {
+        varValue = JSON.parse(result);
+      } catch {
+        varValue = '';
+      }
+      if (Array.isArray(varValue)) {
+        prev.push(
+          ...varValue.map(v => ({
+            id: v,
+            name: this.showNameSlice(v),
+            isVariable: curr.isVariable,
+          }))
+        );
+        return prev;
+      }
+      prev.push({
+        id: varValue || curr.value,
+        name: this.showNameSlice(varValue || curr.value),
+        isVariable: curr.isVariable,
+      });
+      return prev;
+    }, []);
+  }
+
+  showNameSlice(showName: string) {
+    return showName.length > 20 ? `${showName.slice(0, 20)}...` : showName;
+  }
+
+  valueWrapRenderer() {
+    return (
+      <div class='value-wrap'>
+        {this.localValue11.map((item, index) => {
+          const valueNameDomTag = item?.isVariable ? VariableSpan : this.DomTag;
+          return [
+            index > 0 && (
+              <this.DomTag
+                key={`${index}_condition`}
+                class='value-condition'
+              >
+                OR
+              </this.DomTag>
+            ),
+            <valueNameDomTag
+              key={`${index}_key`}
+              class='value-name'
+            >
+              {item.name || '""'}
+            </valueNameDomTag>,
+          ];
+        })}
+        {this.hideCount > 0 && <span class='value-condition'>{`+${this.hideCount}`}</span>}
+      </div>
+    );
+  }
+
+  render() {
+    return this.value ? (
+      <div class='condition-detail-kv-tag-component'>
+        <div
+          id={this.variableName}
+          key={this.tipContent}
+          class='condition-detail-kv-tag-component-wrap'
+          v-bk-tooltips={{
+            content: this.tipContent,
+            delay: [300, 0],
+            allowHTML: true,
+          }}
+        >
+          <div class='key-wrap'>
+            <this.DomTag class='key-name'>{this.value.dimension_name}</this.DomTag>
+            <this.DomTag class={['key-method', this.value.method]}>
+              {ConditionMethodAliasMap[this.value.method]}
+            </this.DomTag>
+          </div>
+          {this.valueWrapRenderer()}
+        </div>
+      </div>
+    ) : undefined;
+  }
+}
