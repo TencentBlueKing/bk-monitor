c0631f36afe3d6725f9ed8a3ed353735ae461609
crayon <873217631@qq.com>
2025-08-20 16:34:25 +0800

    feat: 查询模板接口层代码整合 #1010158081126742534

--- bkmonitor/packages/monitor_web/query_template/constants.py
+++ bkmonitor/packages/monitor_web/query_template/constants.py
@@ -1,28 +1,9 @@
-import enum
-
-
-class QueryTemplateVariableType(enum.Enum):
-    GROUP_BY = "GROUP_BY"
-    METHOD = "METHOD"
-    TAG_VALUES = "TAG_VALUES"
-    CONDITIONS = "CONDITIONS"
-    FUNCTIONS = "FUNCTIONS"
-    CONSTANTS = "CONSTANTS"
-
-    @classmethod
-    def choices(cls):
-        return [(item.name, item.value) for item in cls]
-
-
-class QueryTemplateVariableDataType(enum.Enum):
-    INTERGER = "INTERGER"
-    STRING = "STRING"
-
-    @classmethod
-    def choices(cls):
-        return [(item.name, item.value) for item in cls]
-
-
-class QueryTemplateRelationType(enum.Enum):
-    ALERT_POLICY = "ALERT_POLICY"
-    DASHBOARD = "DASHBOARD"
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+Copyright (C) 2017-2022 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
