6e80f4298e7623ed2f0b7ecbfb7266f36e33d95f
ctenetlau <1505674928@qq.com>
2025-08-21 16:03:20 +0800

    feat: AIAgents: 小鲸逻辑抽象,提取SDK交互句柄,移动至项目外层以供日志平台使用 --story=126726655 (#7671)
    Co-authored-by: NeoLing <pegasusljn@gmail.com>

--- bkmonitor/ai_whale/tests/resources/test_switch_agent_by_command.py
+++ bkmonitor/ai_whale/tests/resources/test_switch_agent_by_command.py
@@ -8,14 +8,14 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from ai_agents.resources.resources import (
+from ai_whale.resources.resources import (
     CreateChatCompletionResource,
     CreateChatSessionContentResource,
     CreateChatSessionResource,
     DestroyChatSessionResource,
 )
 from aidev_agent.api.bk_aidev import BKAidevApi
-from ai_agents.utils import generate_uuid, collect_streaming_response
+from ai_whale.utils import generate_uuid, collect_streaming_response
 from django.conf import settings
 from aidev_agent.services.config_manager import AgentConfigManager
 
