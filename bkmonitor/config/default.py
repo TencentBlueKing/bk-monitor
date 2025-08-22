372643c861f13f482882531f6dd6b9bbe5bba734
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-08-22 21:30:14 +0800

    feat: Metadata: 数据链路配置整理 #1010158081126808637

--- bkmonitor/config/default.py
+++ bkmonitor/config/default.py
@@ -1445,8 +1445,8 @@ APIGW_MANAGERS = f"[{','.join(os.getenv('BKAPP_APIGW_MANAGERS', 'admin').split('
 ENABLE_V2_VM_DATA_LINK = os.getenv("ENABLE_V2_VM_DATA_LINK", "true").lower() == "true"
 # 插件数据是否启用接入V4链路，默认开启
 ENABLE_PLUGIN_ACCESS_V4_DATA_LINK = os.getenv("ENABLE_PLUGIN_ACCESS_V4_DATA_LINK", "true").lower() == "true"
-# 是否启用influxdb，默认关闭
-ENABLE_INFLUXDB_STORAGE = os.getenv("BKAPP_ENABLE_INFLUXDB_STORAGE", "false").lower() == "true"
+# 是否启用influxdb，默认开启
+ENABLE_INFLUXDB_STORAGE = os.getenv("BKAPP_ENABLE_INFLUXDB_STORAGE", "true").lower() == "true"
 
 # 创建 vm 链路资源所属的命名空间
 DEFAULT_VM_DATA_LINK_NAMESPACE = "bkmonitor"
