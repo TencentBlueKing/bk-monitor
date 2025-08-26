f3fd45744445bbe7d1f13bf783a4bb616c103e6b
lai <11598235+unique0lai@users.noreply.github.com>
2025-08-26 12:02:30 +0800

    fix: 公共插件查询报错问题 --bug=147298567 (#7781)

--- bkmonitor/packages/monitor_web/commons/data_access.py
+++ bkmonitor/packages/monitor_web/commons/data_access.py
@@ -153,6 +153,7 @@ class DataAccessor:
 
     def __init__(
         self,
+        bk_tenant_id: str,
         bk_biz_id,
         db_name,
         tables,
@@ -171,7 +172,7 @@ class DataAccessor:
         :param operator: 操作人
         """
         self.bk_biz_id = bk_biz_id
-        self.bk_tenant_id = bk_biz_id_to_bk_tenant_id(bk_biz_id)
+        self.bk_tenant_id = bk_tenant_id
         self.db_name = db_name.lower()
         self.data_label = data_label.lower() if data_label else self.db_name
         self.tables = tables
@@ -474,6 +475,7 @@ class PluginDataAccessor(DataAccessor):
         else:
             etl_config = "bk_exporter"
         super().__init__(
+            bk_tenant_id=plugin_version.plugin.bk_tenant_id,
             bk_biz_id=plugin_version.plugin.bk_biz_id,
             db_name=db_name,
             tables=tables,
@@ -628,7 +630,7 @@ class PluginDataAccessor(DataAccessor):
 class EventDataAccessor:
     def __init__(self, current_version, operator):
         self.bk_biz_id = current_version.plugin.bk_biz_id
-        self.bk_tenant_id = bk_biz_id_to_bk_tenant_id(self.bk_biz_id)
+        self.bk_tenant_id = current_version.plugin.bk_tenant_id
         self.name = f"{current_version.plugin.plugin_type}_{current_version.plugin_id}"
         self.label = current_version.plugin.label
         self.operator = operator
