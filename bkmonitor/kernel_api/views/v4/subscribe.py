9e6804727ad620317213bed4d086377ef6c1fcc9
zichengxu <1790666569@qq.com>
2025-08-27 15:16:43 +0800

    feat: 新增告警订阅模型及接口 #1010158081126607022

---
+++ bkmonitor/kernel_api/views/v4/subscribe.py
@@ -0,0 +1,30 @@
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from core.drf_resource.viewsets import ResourceRoute, ResourceViewSet
+from monitor_web.strategies.resources import (
+    SaveStrategySubscribeResource,
+    DeleteStrategySubscribeResource,
+    ListStrategySubscribeResource,
+    DetailStrategySubscribeResource,
+)
+
+
+class SubscribeViewSet(ResourceViewSet):
+    """
+    策略订阅API
+    """
+
+    resource_routes = [
+        ResourceRoute("GET", ListStrategySubscribeResource, endpoint="search"),
+        ResourceRoute("GET", DetailStrategySubscribeResource, endpoint="detail"),
+        ResourceRoute("POST", SaveStrategySubscribeResource, endpoint="save"),
+        ResourceRoute("POST", DeleteStrategySubscribeResource, endpoint="delete"),
+    ]
