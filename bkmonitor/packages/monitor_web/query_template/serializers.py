23d94e9fbfeac678b0a882fde9170ed71b3ab62f
crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
2025-08-29 15:09:13 +0800

    fix: 查询模板问题修复 --bug=147457501 (#7879)

--- bkmonitor/packages/monitor_web/query_template/serializers.py
+++ bkmonitor/packages/monitor_web/query_template/serializers.py
@@ -140,6 +140,8 @@ class QueryTemplateModelSerializer(QueryTemplateBaseModelSerializer):
         # 如果这是一个全局模板，则将 bk_biz_id 设置为 0
         bk_biz_id = validated_data["bk_biz_id"]
         if self.is_global_template(instance.bk_biz_id):
+            # 全局模板 bk_biz_id & namespace 不允许被修改。
+            validated_data["namespace"] = instance.namespace
             validated_data["bk_biz_id"] = bk_biz_id = GLOBAL_BIZ_ID
         elif bk_biz_id not in validated_data["space_scope"]:
             raise serializers.ValidationError(_("生效范围必须包含当前业务 ID"))
