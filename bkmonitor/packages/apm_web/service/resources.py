0333ea6fd72eb7dbd1b7a001693b819512dd6662
happy <hhuang1231@gmail.com>
2025-08-29 10:56:05 +0800

    fix: 原来那个log_relation不用了可以直接去掉

--- bkmonitor/packages/apm_web/service/resources.py
+++ bkmonitor/packages/apm_web/service/resources.py
@@ -227,8 +227,6 @@ class ServiceInfoResource(Resource):
         )
         instance_res = pool.apply_async(RelationMetricHandler.list_instances, kwds=query_instance_param)
         app_relation = pool.apply_async(self.get_app_relation_info, args=(bk_biz_id, app_name, service_name))
-        # 2025.8.26 如果不再需要返回这个 log_relation 数据可以删除相关代码从而精简接口
-        log_relation = pool.apply_async(self.get_log_relation_info, args=(bk_biz_id, app_name, service_name)) 
         log_relation_list = pool.apply_async(self.get_log_relation_info_list, args=(bk_biz_id, app_name, service_name))
         cmdb_relation = pool.apply_async(self.get_cmdb_relation_info, args=(bk_biz_id, app_name, service_name))
         event_relation = pool.apply_async(self.get_event_relation_info, args=(bk_biz_id, app_name, service_name))
@@ -252,7 +250,6 @@ class ServiceInfoResource(Resource):
                 service_info.update(service)
 
         app_relation_info = app_relation.get()
-        log_relation_info = log_relation.get()
         log_relation_info_list = log_relation_list.get()
         cmdb_relation_info = cmdb_relation.get()
         event_relation_info = event_relation.get()
@@ -263,7 +260,7 @@ class ServiceInfoResource(Resource):
             [
                 apdex_info,
                 app_relation_info,
-                log_relation_info, # log_relation_info_list 和 log_relation_info 是一样的操作记录
+                log_relation_info_list[0] if log_relation_info_list else {},
                 cmdb_relation_info,
                 *event_relation_info,
                 *uri_relation_info,
@@ -276,7 +273,6 @@ class ServiceInfoResource(Resource):
 
         service_info["relation"] = {
             "app_relation": app_relation_info,
-            "log_relation": log_relation_info,
             "log_relation_list": log_relation_info_list,
             "cmdb_relation": cmdb_relation_info,
             "event_relation": event_relation_info,
