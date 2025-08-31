80d829376ef63aed94f4cc95e6dc1ac4afb76123
loongjiang <xiaoj655@gmail.com>
2025-08-31 09:32:59 +0000

    feat: AI 分析 profiling, 添加 DOT 格式转换器等 --story=126931253

--- bkmonitor/ai_whale/local_command_handlers.py
+++ bkmonitor/ai_whale/local_command_handlers.py
@@ -9,12 +9,23 @@ specific language governing permissions and limitations under the License.
 """
 
 import logging
-from ai_agent.services.local_command_handler import local_command_handler, CommandHandler
+from typing import Self
+
+from pydantic import BaseModel, Field, field_validator, model_validator
+
+from ai_agent.services.local_command_handler import (
+    CommandHandler,
+    local_command_handler,
+)
+from ai_whale.utils import get_nested_value
+from apm_web.profile.diagrams.dotgraph import DOTDiagrammer
+from apm_web.profile.diagrams.tree_converter import TreeConverter
 from core.drf_resource import api
 from core.errors.api import BKAPIError
-from ai_whale.utils import get_nested_value
 
 logger = logging.getLogger("ai_agents")
+
+USER_GUIDE_MSG = "当前页面发起不正确, 礼貌并捎带歉意提示用户前往 '{0}' 使用该功能"
 
 
 # TODO:平台暂时不支持MCP，若需要对输入数据进行特殊处理，临时在业务逻辑中实现LocalCommandHandler,实现process_content和get_template方法
@@ -65,4 +76,81 @@ class TracingAnalysisCommandHandler(CommandHandler):
         结果要求: 确保分析准确无误，无需冗余回答内容
         如果缺少任意参数,请告知用户前往数据探索->Tracing检索->Trace详情页面使用该功能
         切记不要告诉用户缺少了参数，你需要礼貌的提示用户前往对应页面使用该功能
+        """
+
+
+@local_command_handler("profiling_analysis")
+class ProfilingAnalysisCommandHandler(CommandHandler):
+    class QueryProfilingParameter(BaseModel):
+        data_type: str
+        service_name: str
+        bk_biz_id: int
+        app_name: str
+        agg_method: str
+        filter_labels: dict[str, str] = Field(default_factory=dict)
+
+        start_time: int | None = Field(default=None, description="second unit")
+        start: int | None = Field(default=None, description="microsecond unit")
+        end_time: int | None = Field(default=None, description="second unit")
+        end: int | None = Field(default=None, description="microsecond unit")
+
+        @field_validator("filter_labels", mode="before")
+        def default_on_error(cls, v):
+            if not isinstance(v, dict):
+                return {}
+            return v
+
+        @model_validator(mode="after")
+        def check_time_fields(self) -> Self:
+            has_seconds = self.start_time and self.end_time
+            has_microseconds = self.start and self.end
+
+            if not (has_seconds or has_microseconds):
+                raise ValueError("start_time/end_time or start/end must be provided")
+
+            if has_seconds and not has_microseconds:
+                self.start = self.start_time * 1_000_000
+                self.end = self.end_time * 1_000_000
+
+            if self.start >= self.end:  # type: ignore
+                raise ValueError("start must be less than end")
+
+            return self
+
+    def process_content(self, context: list[dict]) -> str:
+        from apm_web.profile.views import ProfileQueryViewSet
+
+        template = self.get_template()
+        variables = self.extract_context_vars(context)
+
+        try:
+            variables = self.QueryProfilingParameter.model_validate(variables)
+        except ValueError as e:
+            logger.info(f"ProfilingAnalysisCommandHandler: invalid params {e},will remind user")
+            return USER_GUIDE_MSG.format("数据探索->Profiling检索->Profiling详情页面")
+
+        # 查询 profiling 数据并取得 TreeConvert 转换器类实例
+        validate_data, essentials, extra_params = ProfileQueryViewSet.get_query_params(
+            variables.model_dump(exclude={"start_time", "end_time"})
+        )
+        tree_converter = ProfileQueryViewSet.converter_query(essentials, validate_data, extra_params)
+
+        # 判空
+        if not (isinstance(tree_converter, TreeConverter) and getattr(tree_converter, "tree")):
+            logger.info("ProfilingAnalysisCommandHandler: tree_converter.tree is empty,will remind user")
+            return USER_GUIDE_MSG.format("数据探索->Profiling检索->Profiling详情页面")
+
+        dot_graph = DOTDiagrammer().draw(tree_converter)["dot_graph"]
+
+        logger.info("ProfilingAnalysisCommandHandler: all params are valid,will render template")
+        return self.jinja_env.render(
+            template, {"profiling_data": dot_graph, "app_name": variables.app_name, "bk_biz_id": variables.bk_biz_id}
+        )
+
+    def get_template(self) -> str:
+        return """
+        请帮助我分析 Profiling 数据(DOT 描述): {{ profiling_data }}.
+        应用名称: {{ app_name }}
+        业务ID: {{ bk_biz_id }}
+        结果要求: 确保分析准确无误，无需冗余回答内容
         """
