247a2c1f723dbb2c6f680277850957cb9c4654fa
loongjiang <xiaoj655@gmail.com>
2025-09-02 15:12:37 +0000

    feat: 简化前端参数

--- bkmonitor/ai_whale/local_command_handlers.py
+++ bkmonitor/ai_whale/local_command_handlers.py
@@ -8,6 +8,7 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+import json
 import logging
 from typing import Self
 
@@ -18,14 +19,10 @@ from ai_agent.services.local_command_handler import (
     local_command_handler,
 )
 from ai_whale.utils import get_nested_value
-from apm_web.profile.diagrams.dotgraph import DOTDiagrammer
-from apm_web.profile.diagrams.tree_converter import TreeConverter
 from core.drf_resource import api
 from core.errors.api import BKAPIError
 
 logger = logging.getLogger("ai_agents")
-
-USER_GUIDE_MSG = "当前页面发起不正确, 礼貌并捎带歉意提示用户前往 '{0}' 使用该功能"
 
 
 # TODO:平台暂时不支持MCP，若需要对输入数据进行特殊处理，临时在业务逻辑中实现LocalCommandHandler,实现process_content和get_template方法
@@ -79,55 +76,78 @@ class TracingAnalysisCommandHandler(CommandHandler):
         """
 
 
+class QueryProfilingParameter(BaseModel):
+    """
+    ProfilingAnalysisCommandHandler 的数据模型
+    NOTE: convert_json: 把前端的 json 字符串(如果是)转回字典
+    """
+
+    data_type: str
+    service_name: str
+    bk_biz_id: int
+    app_name: str
+    agg_method: str
+    filter_labels: dict[str, str] = Field(default_factory=dict)
+
+    # 前端传了, 但暂时没用的参数, 可能会干扰查出的 profiling 数据, 暂不开启
+    # diff_filter_labels: dict
+
+    start_time: int | None = Field(default=None, description="second unit")
+    start: int | None = Field(default=None, description="microsecond unit")
+    end_time: int | None = Field(default=None, description="second unit")
+    end: int | None = Field(default=None, description="microsecond unit")
+
+    @field_validator("filter_labels", mode="before")
+    def validate_filter_labels_type(cls, v):
+        if not isinstance(v, dict):
+            return {}
+        return v
+
+    @model_validator(mode="before")
+    @classmethod
+    def convert_json(cls, data):
+        if isinstance(data, str):
+            try:
+                return json.loads(data)
+            except json.JSONDecodeError as e:
+                raise ValueError("invalid JSON string") from e
+        return data
+
+    @model_validator(mode="after")
+    def check_time_fields(self) -> Self:
+        has_seconds = self.start_time is not None and self.end_time is not None
+        has_microseconds = self.start is not None and self.end is not None
+
+        if not (has_seconds or has_microseconds):
+            raise ValueError("start_time/end_time or start/end must be provided")
+
+        if has_seconds and not has_microseconds:
+            self.start = self.start_time * 1_000_000  # type: ignore
+            self.end = self.end_time * 1_000_000  # type: ignore
+
+        if self.start >= self.end:  # type: ignore
+            raise ValueError("start must be less than end")
+
+        return self
+
+
 @local_command_handler("profiling_analysis")
 class ProfilingAnalysisCommandHandler(CommandHandler):
-    class QueryProfilingParameter(BaseModel):
-        data_type: str
-        service_name: str
-        bk_biz_id: int
-        app_name: str
-        agg_method: str
-        filter_labels: dict[str, str] = Field(default_factory=dict)
-
-        start_time: int | None = Field(default=None, description="second unit")
-        start: int | None = Field(default=None, description="microsecond unit")
-        end_time: int | None = Field(default=None, description="second unit")
-        end: int | None = Field(default=None, description="microsecond unit")
-
-        @field_validator("filter_labels", mode="before")
-        def default_on_error(cls, v):
-            if not isinstance(v, dict):
-                return {}
-            return v
-
-        @model_validator(mode="after")
-        def check_time_fields(self) -> Self:
-            has_seconds = self.start_time and self.end_time
-            has_microseconds = self.start and self.end
-
-            if not (has_seconds or has_microseconds):
-                raise ValueError("start_time/end_time or start/end must be provided")
-
-            if has_seconds and not has_microseconds:
-                self.start = self.start_time * 1_000_000
-                self.end = self.end_time * 1_000_000
-
-            if self.start >= self.end:  # type: ignore
-                raise ValueError("start must be less than end")
-
-            return self
+    class Variables(BaseModel):
+        query_params: QueryProfilingParameter
 
     def process_content(self, context: list[dict]) -> str:
+        from apm_web.profile.diagrams.dotgraph import DOTDiagrammer
+        from apm_web.profile.diagrams.tree_converter import TreeConverter
         from apm_web.profile.views import ProfileQueryViewSet
 
         template = self.get_template()
+
+        # variables like: {'query_params': {'bk_biz_id': 0, ... } }
         variables = self.extract_context_vars(context)
 
-        try:
-            variables = self.QueryProfilingParameter.model_validate(variables)
-        except ValueError as e:
-            logger.info(f"ProfilingAnalysisCommandHandler: invalid params {e},will remind user")
-            return USER_GUIDE_MSG.format("数据探索->Profiling检索->Profiling详情页面")
+        variables = self.Variables.model_validate(variables)
+        variables = variables.query_params
 
         # 查询 profiling 数据并取得 TreeConvert 转换器类实例
         validate_data, essentials, extra_params = ProfileQueryViewSet.get_query_params(
@@ -137,8 +157,7 @@ class ProfilingAnalysisCommandHandler(CommandHandler):
 
         # 判空
         if not (isinstance(tree_converter, TreeConverter) and getattr(tree_converter, "tree")):
-            logger.info("ProfilingAnalysisCommandHandler: tree_converter.tree is empty,will remind user")
-            return USER_GUIDE_MSG.format("数据探索->Profiling检索->Profiling详情页面")
+            raise ValueError("[ProfilingAnalysisCommandHandler] tree_converter is not TreeConverter or is empty")
 
         dot_graph = DOTDiagrammer().draw(tree_converter)["dot_graph"]
 
