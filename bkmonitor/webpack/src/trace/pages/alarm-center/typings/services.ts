31a9ce1f04f8b252f2800af36a7b838c3dc5f7ec
gc_huwumiao <2199202809@qq.com>
2025-09-02 18:49:15 +0800

    feat: 告警中心告警分析功能代码优化

--- bkmonitor/webpack/src/trace/pages/alarm-center/typings/services.ts
+++ bkmonitor/webpack/src/trace/pages/alarm-center/typings/services.ts
@@ -28,416 +28,419 @@ import type { ACTION_STORAGE_KEY } from '../services/action-services';
 import type { ALERT_STORAGE_KEY } from '../services/alert-services';
 import type { INCIDENT_STORAGE_KEY } from '../services/incident-services';
 
-export type CommonCondition = {
-  key: string;
-  value: number[] | string[];
-  condition?: string;
-  method?: string;
-};
-export type CommonFilterParams = {
-  bk_biz_ids: number[];
-  status: string[];
-  conditions: CommonCondition[];
-  query_string: string;
-  start_time: number;
-  end_time: number;
-  page: number;
-  page_size: number;
-  record_history: boolean;
-  bk_biz_id: number;
-  ordering: string[];
-};
-
-export type QuickFilterItem = {
-  id: string;
-  name: string;
-  count?: number;
-  icon?: string;
-  textColor?: string;
-  iconColor?: string;
-  children?: QuickFilterItem[];
-};
-
-/** 告警 -- 关联事件数接口返回数据类型 */
-export type AlertEventCountResult = Record<string, number>;
-export interface AlertExtendInfoItem {
-  data_label: string;
-  result_table_id: string;
-  type?: string;
-  hostname?: string;
-  topo_info?: string;
-}
-/** 告警 -- 关联告警信息接口返回数据类型 */
-export type AlertExtendInfoResult = Record<string, AlertExtendInfoItem>;
-
-/**
- * 告警 table 数据
- */
-export type AlertTableItem = {
-  id: string;
-  alert_name: string;
-  status: string;
-  description: string;
-  severity: number;
-  metric: string[];
-  labels: string[];
-  bk_biz_id: number;
-  ip: null | string;
-  ipv6: null | string;
-  bk_host_id: null | string;
-  bk_cloud_id: null | string;
-  bk_service_instance_id: null | string;
-  bk_topo_node: null | string;
-  assignee: string[];
-  appointee: string[];
-  supervisor: null | string;
-  follower: null | string[];
-  is_ack: boolean | null;
-  is_shielded: boolean;
-  shield_left_time: string;
-  shield_id: null | string;
-  is_handled: boolean;
-  is_blocked: boolean;
-  strategy_id: number;
-  create_time: number;
-  update_time: number;
-  begin_time: number;
-  end_time: null | number;
-  latest_time: number;
-  first_anomaly_time: number;
-  target_type: string;
-  target: null | string;
-  category: string;
-  tags: Array<{
-    value: string;
-    key: string;
-  }>;
-  category_display: string;
-  duration: string;
-  ack_duration: null | string;
-  data_type: string;
-  converge_id: string;
-  event_id: string;
-  plugin_id: string;
-  plugin_display_name: string;
-  strategy_name: string;
-  stage_display: string;
-  dimensions: Array<{
-    display_value: string;
-    display_key: string;
-    value: string;
-    key: string;
-  }>;
-  seq_id: number;
-  dedupe_md5: string;
-  dedupe_keys: string[];
-  dimension_message: string;
-  metric_display: Array<{
-    id: string;
-    name: string;
-  }>;
-  target_key: string;
-  ack_operator: string;
-  shield_operator: string[];
-  bk_biz_name: string;
-  event_count?: number;
-  extend_info?: AlertExtendInfoItem;
-  followerDisabled?: boolean;
-};
-
 /**
  * 处理记录 table数据
  */
 export type ActionTableItem = {
-  id: number | string;
-  converge_id: number;
-  is_converge_primary: boolean;
-  status: string;
-  failure_type: null | string;
-  ex_data: {
-    message: string;
-    [key: string]: any;
-  };
-  strategy_id: number;
-  strategy_name: string;
-  signal: string;
-  alert_id: string[];
-  alert_level: number;
-  operator: string[];
-  inputs: {
-    notice_way: string;
-    notice_receiver: string;
-    followed: boolean;
-    alert_latest_time: number;
-    is_alert_shielded: boolean;
-    shield_detail: string;
-    is_unshielded: boolean;
-    notice_type: string;
-    time_range: string;
-    notify_info: Record<string, string[]>;
-    [key: string]: any;
-  };
-  outputs: {
-    title: string;
-    message: string;
-    [key: string]: any;
-  };
-  execute_times: number;
-  action_plugin_type: string;
-  action_plugin: {
+  action_config: {
+    app: string;
+    bk_biz_id: number;
+    create_time: string;
+    create_user: string;
+    desc: string;
+    execute_config: Record<string, any>;
+    hash: string;
     id: number;
-    name: string;
-    plugin_type: string;
-    plugin_key: string;
-    update_user: string;
-    update_time: string;
+    is_builtin: boolean;
+    is_deleted: boolean;
     is_enabled: boolean;
-    config_schema: Record<string, any>;
+    name: string;
+    path: string;
+    plugin_id: number;
+    plugin_name: string;
+    plugin_type: string;
+    snippet: string;
+    update_time: string;
+    update_user: string;
+  };
+  action_config_id: number;
+  action_name: string;
+  action_plugin: {
     backend_config: Array<{
       function: string;
       name: string;
     }>;
-  };
-  action_name: string;
-  action_config: {
+    config_schema: Record<string, any>;
     id: number;
-    plugin_id: number;
-    desc: string;
-    execute_config: Record<string, any>;
-    name: string;
-    bk_biz_id: number;
     is_enabled: boolean;
-    is_deleted: boolean;
-    create_user: string;
-    create_time: string;
-    update_user: string;
-    update_time: string;
-    is_builtin: boolean;
-    app: string;
-    path: string;
-    hash: string;
-    snippet: string;
-    plugin_name: string;
+    name: string;
+    plugin_key: string;
     plugin_type: string;
+    update_time: string;
+    update_user: string;
   };
-  action_config_id: number;
-  is_parent_action: boolean;
-  related_action_ids: null | number[];
-  parent_action_id: number;
-  create_time: number;
-  update_time: number;
-  end_time: number;
-  bk_target_display: string;
+  action_plugin_type: string;
+  action_plugin_type_display: string;
+  alert_id: string[];
+  alert_level: number;
   bk_biz_id: number | string;
   bk_biz_name: string;
-  bk_set_ids: null | string[];
-  bk_set_names: null | string[];
   bk_module_ids: null | string[];
   bk_module_names: null | string[];
-  raw_id: number;
-  duration: string;
-  operate_target_string: string;
+  bk_set_ids: null | string[];
+  bk_set_names: null | string[];
+  bk_target_display: string;
   content: {
-    text: string;
-    action_plugin_type: string;
-    url: string;
     [key: string]: any;
+    action_plugin_type: string;
+    text: string;
+    url: string;
   };
+  converge_count: number;
+  converge_id: number;
+  create_time: number;
+  dimension_string: string;
   dimensions: Array<{
-    key: string;
-    value: string;
     display_key: string;
     display_value: string;
+    key: string;
+    value: string;
   }>;
-  dimension_string: string;
-  status_tips: string;
-  converge_count: number;
-  action_plugin_type_display: string;
+  duration: string;
+  end_time: number;
+  ex_data: {
+    [key: string]: any;
+    message: string;
+  };
+  execute_times: number;
+  failure_type: null | string;
+  id: number | string;
+  inputs: {
+    [key: string]: any;
+    alert_latest_time: number;
+    followed: boolean;
+    is_alert_shielded: boolean;
+    is_unshielded: boolean;
+    notice_receiver: string;
+    notice_type: string;
+    notice_way: string;
+    notify_info: Record<string, string[]>;
+    shield_detail: string;
+    time_range: string;
+  };
+  is_converge_primary: boolean;
+  is_parent_action: boolean;
+  operate_target_string: string;
+  operator: string[];
+  outputs: {
+    [key: string]: any;
+    message: string;
+    title: string;
+  };
+  parent_action_id: number;
+  raw_id: number;
+  related_action_ids: null | number[];
+  signal: string;
   signal_display: string;
+  status: string;
+  status_tips: string;
+  strategy_id: number;
+  strategy_name: string;
+  update_time: number;
+};
+export type AlarmStorageKey = typeof ACTION_STORAGE_KEY | typeof ALERT_STORAGE_KEY | typeof INCIDENT_STORAGE_KEY;
+
+/** 告警 -- 关联事件数接口返回数据类型 */
+export type AlertEventCountResult = Record<string, number>;
+
+export interface AlertExtendInfoItem {
+  data_label: string;
+  hostname?: string;
+  result_table_id: string;
+  topo_info?: string;
+  type?: string;
+}
+/** 告警 -- 关联告警信息接口返回数据类型 */
+export type AlertExtendInfoResult = Record<string, AlertExtendInfoItem>;
+/**
+ * 告警 table 数据
+ */
+export type AlertTableItem = {
+  ack_duration: null | string;
+  ack_operator: string;
+  alert_name: string;
+  appointee: string[];
+  assignee: string[];
+  begin_time: number;
+  bk_biz_id: number;
+  bk_biz_name: string;
+  bk_cloud_id: null | string;
+  bk_host_id: null | string;
+  bk_service_instance_id: null | string;
+  bk_topo_node: null | string;
+  category: string;
+  category_display: string;
+  converge_id: string;
+  create_time: number;
+  data_type: string;
+  dedupe_keys: string[];
+  dedupe_md5: string;
+  description: string;
+  dimension_message: string;
+  dimensions: Array<{
+    display_key: string;
+    display_value: string;
+    key: string;
+    value: string;
+  }>;
+  duration: string;
+  end_time: null | number;
+  event_count?: number;
+  event_id: string;
+  extend_info?: AlertExtendInfoItem;
+  first_anomaly_time: number;
+  follower: null | string[];
+  followerDisabled?: boolean;
+  id: string;
+  ip: null | string;
+  ipv6: null | string;
+  is_ack: boolean | null;
+  is_blocked: boolean;
+  is_handled: boolean;
+  is_shielded: boolean;
+  labels: string[];
+  latest_time: number;
+  metric: string[];
+  metric_display: Array<{
+    id: string;
+    name: string;
+  }>;
+  plugin_display_name: string;
+  plugin_id: string;
+  seq_id: number;
+  severity: number;
+  shield_id: null | string;
+  shield_left_time: string;
+  shield_operator: string[];
+  stage_display: string;
+  status: string;
+  strategy_id: number;
+  strategy_name: string;
+  supervisor: null | string;
+  tags: Array<{
+    key: string;
+    value: string;
+  }>;
+  target: null | string;
+  target_key: string;
+  target_type: string;
+  update_time: number;
 };
 
-/**
- * 故障处理 table 数据
- */
-export type IncidentTableItem = {
+// 单个分桶
+export type AnalysisBucket = {
+  count: number;
   id: string;
-  incident_id: number;
-  incident_name: string;
-  incident_reason: null | string;
-  bk_biz_id: string;
-  status: string;
-  status_order: number;
-  level: string;
-  assignees: string[];
-  handlers: string[];
-  labels: string[];
-  create_time: number;
-  update_time: number;
-  begin_time: number;
-  end_time: number;
-  snapshot: {
-    id: string;
-    incident_id: number;
-    bk_biz_ids: number[];
-    status: string;
-    alerts: string[];
-    create_time: number;
-    content: {
-      timestamp: number;
-      anomaly_time: number;
-      incident_name: string;
-      incident_alerts: Array<{
-        alert_status: string;
-        alert_time: number;
-        strategy_id: string;
-        id: string;
-        entity_id: string;
-      }>;
-      incident_root: string[];
-      incident_name_template: {
-        template: string;
-        elements: any[];
-        affected_types: any[];
-      };
-      rca_summary: {
-        graph_snapshot_info: Record<string, any>;
-        properties: Array<{
-          target_type: string;
-          target_id: null | string;
-          tags: Record<string, any>;
-        }>;
-        k8s_events_summary: string;
-        k8s_warning_events: any[];
-        incident_summary: string;
-        incident_handling_suggestions: string;
-        bk_biz_ids: string[];
-        incident_bk_biz_id: string;
-        root_cause: string;
-        rca_mode: string;
-        rca_graph_version: {
-          start_time_ms: number;
-          end_time_ms: number;
-          periods_set: number[][];
-        };
-        incident_alert_ids: {
-          related: string[];
-          unrelated: string[];
-          all: string[];
-        };
-        status: number;
-        graph_latest_time: number;
-        incident_name: string;
-        incident_label: string;
-        timestamp: number;
-        rca_snapshot_info: {
-          snapshot_id: string;
-        };
-        action: string;
-      };
-      incident_alert_ids: {
-        related: string[];
-        unrelated: string[];
-        all: string[];
-      };
-      product_hierarchy_category: Record<
-        string,
-        {
-          category_id: number;
-          category_name: string;
-          category_alias: string;
-          layer_name: string;
-          layer_alias: string;
-        }
-      >;
-      product_hierarchy_rank: Record<
-        string,
-        {
-          rank_id: number;
-          rank_name: string;
-          rank_alias: string;
-          rank_category: string;
-        }
-      >;
-      incident_propagation_graph: {
-        entities: Array<{
-          is_anomaly: boolean;
-          rank_name: string;
-          entity_name: string;
-          bk_biz_id: null | string;
-          observe_time_rage: {
-            end_at: number;
-            start_at: number;
-          };
-          entity_id: string;
-          is_root: boolean;
-          tags: Record<string, any>;
-          component_type: string;
-          entity_type: string;
-          is_on_alert: boolean;
-          properties: Record<string, any>;
-          dimensions: Record<string, any>;
-        }>;
-        edges: Array<{
-          edge_cluster_id?: null | string;
-          anomaly_score: number;
-          component_type: string;
-          is_anomaly: boolean;
-          target_type: string;
-          source_type: string;
-          target_id: string;
-          source_id: string;
-          edge_type: string;
-          events: Array<{
-            event_type: string;
-            metric_name: string;
-            time_series: number[][];
-            event_name: string;
-            event_time: number;
-            direction: string;
-          }>;
-        }>;
-      };
-      bk_biz_id: string;
-      snapshot_id: string;
-    };
-    fpp_snapshot_id: string;
-    events: any[];
-    update_time: null | number;
-    extra_info: Record<string, any>;
-  };
-  alert_count: number;
-  status_alias: string;
-  level_alias: string;
-  duration: string;
-  bk_biz_name: string;
+  name: string;
 };
-export type FilterTableResponse<T> = {
-  total: number;
-  data: T[];
+
+// 某个分析字段的聚合结果
+export type AnalysisFieldAggItem = {
+  bucket_count: number; // 分桶总数
+  buckets: AnalysisBucket[]; // 分桶明细
+  field: string; // 字段名，如 bk_biz_id
+  is_char: boolean; // 是否为字符型
 };
+
+export interface AnalysisListItem extends AnalysisFieldAggItem {
+  buckets: AnalysisListItemBucket[];
+  name: string;
+}
+export type AnalysisListItemBucket = AnalysisBucket & { percent: number };
 
 export type AnalysisTopNDataResponse<T> = {
   doc_count: number;
   fields: T[];
 };
 
-// 单个分桶
-export type AnalysisBucket = {
+export type CommonCondition = {
+  condition?: string;
+  key: string;
+  method?: string;
+  value: number[] | string[];
+};
+
+export type CommonFilterParams = {
+  bk_biz_id: number;
+  bk_biz_ids: number[];
+  conditions: CommonCondition[];
+  end_time: number;
+  ordering: string[];
+  page: number;
+  page_size: number;
+  query_string: string;
+  record_history: boolean;
+  start_time: number;
+  status: string[];
+};
+
+export type FilterTableResponse<T> = {
+  data: T[];
+  total: number;
+};
+
+/**
+ * 故障处理 table 数据
+ */
+export type IncidentTableItem = {
+  alert_count: number;
+  assignees: string[];
+  begin_time: number;
+  bk_biz_id: string;
+  bk_biz_name: string;
+  create_time: number;
+  duration: string;
+  end_time: number;
+  handlers: string[];
+  id: string;
+  incident_id: number;
+  incident_name: string;
+  incident_reason: null | string;
+  labels: string[];
+  level: string;
+  level_alias: string;
+  snapshot: {
+    alerts: string[];
+    bk_biz_ids: number[];
+    content: {
+      anomaly_time: number;
+      bk_biz_id: string;
+      incident_alert_ids: {
+        all: string[];
+        related: string[];
+        unrelated: string[];
+      };
+      incident_alerts: Array<{
+        alert_status: string;
+        alert_time: number;
+        entity_id: string;
+        id: string;
+        strategy_id: string;
+      }>;
+      incident_name: string;
+      incident_name_template: {
+        affected_types: any[];
+        elements: any[];
+        template: string;
+      };
+      incident_propagation_graph: {
+        edges: Array<{
+          anomaly_score: number;
+          component_type: string;
+          edge_cluster_id?: null | string;
+          edge_type: string;
+          events: Array<{
+            direction: string;
+            event_name: string;
+            event_time: number;
+            event_type: string;
+            metric_name: string;
+            time_series: number[][];
+          }>;
+          is_anomaly: boolean;
+          source_id: string;
+          source_type: string;
+          target_id: string;
+          target_type: string;
+        }>;
+        entities: Array<{
+          bk_biz_id: null | string;
+          component_type: string;
+          dimensions: Record<string, any>;
+          entity_id: string;
+          entity_name: string;
+          entity_type: string;
+          is_anomaly: boolean;
+          is_on_alert: boolean;
+          is_root: boolean;
+          observe_time_rage: {
+            end_at: number;
+            start_at: number;
+          };
+          properties: Record<string, any>;
+          rank_name: string;
+          tags: Record<string, any>;
+        }>;
+      };
+      incident_root: string[];
+      product_hierarchy_category: Record<
+        string,
+        {
+          category_alias: string;
+          category_id: number;
+          category_name: string;
+          layer_alias: string;
+          layer_name: string;
+        }
+      >;
+      product_hierarchy_rank: Record<
+        string,
+        {
+          rank_alias: string;
+          rank_category: string;
+          rank_id: number;
+          rank_name: string;
+        }
+      >;
+      rca_summary: {
+        action: string;
+        bk_biz_ids: string[];
+        graph_latest_time: number;
+        graph_snapshot_info: Record<string, any>;
+        incident_alert_ids: {
+          all: string[];
+          related: string[];
+          unrelated: string[];
+        };
+        incident_bk_biz_id: string;
+        incident_handling_suggestions: string;
+        incident_label: string;
+        incident_name: string;
+        incident_summary: string;
+        k8s_events_summary: string;
+        k8s_warning_events: any[];
+        properties: Array<{
+          tags: Record<string, any>;
+          target_id: null | string;
+          target_type: string;
+        }>;
+        rca_graph_version: {
+          end_time_ms: number;
+          periods_set: number[][];
+          start_time_ms: number;
+        };
+        rca_mode: string;
+        rca_snapshot_info: {
+          snapshot_id: string;
+        };
+        root_cause: string;
+        status: number;
+        timestamp: number;
+      };
+      snapshot_id: string;
+      timestamp: number;
+    };
+    create_time: number;
+    events: any[];
+    extra_info: Record<string, any>;
+    fpp_snapshot_id: string;
+    id: string;
+    incident_id: number;
+    status: string;
+    update_time: null | number;
+  };
+  status: string;
+  status_alias: string;
+  status_order: number;
+  update_time: number;
+};
+
+export type QuickFilterItem = {
+  children?: QuickFilterItem[];
+  count?: number;
+  icon?: string;
+  iconColor?: string;
   id: string;
   name: string;
-  count: number;
+  textColor?: string;
 };
-
-// 某个分析字段的聚合结果
-export type AnalysisFieldAggItem = {
-  field: string; // 字段名，如 bk_biz_id
-  is_char: boolean; // 是否为字符型
-  bucket_count: number; // 分桶总数
-  buckets: AnalysisBucket[]; // 分桶明细
-};
-
-export type AnalysisListItemBucket = { percent: number } & AnalysisBucket;
-
-export type AnalysisListItem = { name: string; buckets: AnalysisListItemBucket[] } & AnalysisFieldAggItem;
-
-export type AlarmStorageKey = typeof ACTION_STORAGE_KEY | typeof ALERT_STORAGE_KEY | typeof INCIDENT_STORAGE_KEY;
