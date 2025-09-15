024a68a5b33bf78482de128966bf5e04f7227439
JayCC1 <85379453+JayCC1@users.noreply.github.com>
2025-09-15 21:02:57 +0800

    feat: apm告警模板开发 (#8130)

--- bkmonitor/webpack/src/apm/pages/alarm-template/alarm-template.tsx
+++ bkmonitor/webpack/src/apm/pages/alarm-template/alarm-template.tsx
@@ -27,11 +27,84 @@
 import { Component } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 
+import StatusTab from 'monitor-ui/chart-plugins/plugins/table-chart/status-tab';
+
+import AlarmTemplateSearch from './components/alarm-templte-search/alarm-template-search';
+import BatchOperations from './components/batch-operations/batch-operations';
+import { ALARM_TEMPLATE_QUICK_FILTER_LIST } from './constant';
+import { type AlarmTemplateConditionParamItem, type BatchOperationTypeEnumType } from './typeing';
+
 import './alarm-template.scss';
 
 @Component
 export default class AlarmTemplate extends tsc<object> {
+  /** 模板类型快速筛选tab */
+  quickStatus = 'all';
+  /** 搜索关键字 */
+  searchKeyword: AlarmTemplateConditionParamItem[] = [];
+  /** 表格已勾选的数据行id */
+  selectedRowKeys: string[] = [];
+
+  /**
+   * @description 模板类型快捷筛选值改变后回调
+   */
+  handleQuickStatusChange(status: string) {
+    this.quickStatus = status;
+  }
+  /**
+   * @description 批量操作按钮点击事件
+   */
+  handleBatchOperationClick(operationType: BatchOperationTypeEnumType) {
+    console.log('================ operationType ================', operationType);
+  }
+
+  handleSearchChange(keyword: AlarmTemplateConditionParamItem[]) {
+    this.searchKeyword = keyword;
+    // this.setRouterParams();
+  }
+
   render() {
-    return <div class='alarm-template'>告警模板</div>;
+    return (
+      <div class='alarm-template'>
+        <div class='alarm-template-header'>
+          <div class='alarm-template-header-operations'>
+            <BatchOperations
+              disabled={!!this.selectedRowKeys?.length}
+              onOperationClick={this.handleBatchOperationClick}
+            />
+            <StatusTab
+              class='alarm-template-header-filter-tab'
+              v-model={this.quickStatus}
+              needAll={false}
+              statusList={ALARM_TEMPLATE_QUICK_FILTER_LIST}
+              onChange={this.handleQuickStatusChange}
+            />
+          </div>
+          <div class='alarm-template-header-search'>
+            <AlarmTemplateSearch
+              class='search-input'
+              searchKeyword={this.searchKeyword}
+              onChange={this.handleSearchChange}
+            />
+          </div>
+        </div>
+        <div class='alarm-template-main'>
+          {/* <QueryTemplateTable
+            current={this.current}
+            emptyType={this.searchKeyword?.length ? 'search-empty' : 'empty'}
+            loading={this.tableLoading}
+            pageSize={this.pageSize}
+            sort={this.sort}
+            tableData={this.tableData}
+            total={this.total}
+            onClearSearch={() => this.handleSearchChange([])}
+            onCurrentPageChange={this.handleCurrentPageChange}
+            onDeleteTemplate={this.deleteTemplateById}
+            onPageSizeChange={this.handlePageSizeChange}
+            onSortChange={this.handleSortChange}
+          /> */}
+        </div>
+      </div>
+    );
   }
 }
