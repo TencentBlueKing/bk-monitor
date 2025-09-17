6881cb0f431d554c1ee9dd629188974b2b1a0944
JayCC1 <85379453+JayCC1@users.noreply.github.com>
2025-09-17 09:54:47 +0800

    feat: apm告警模板开发 (#8154)

--- bkmonitor/webpack/src/apm/pages/alarm-template/typing/table.ts
+++ bkmonitor/webpack/src/apm/pages/alarm-template/typing/table.ts
@@ -24,71 +24,38 @@
  * IN THE SOFTWARE.
  */
 
-/** 表格分页属性 */
-export interface IPagination {
-  count: number;
-  current: number;
-  limit: number;
-  showTotalCount: boolean;
+import type { DetectionAlgorithmLevelEnumType } from './constants';
+
+export interface AlarmAlgorithmItem {
+  level: DetectionAlgorithmLevelEnumType;
+  method: string;
+  threshold: number;
+  type: string;
 }
 
-/** 表格设置变更事件对象 */
-export interface ITableSettingChangeEvent {
-  fields: ITableSettingFieldItem[];
-  size: ITableSettingSize;
+export interface AlarmTemplateConditionParamItem {
+  key: string;
+  value: string[];
 }
 
-/** 表格设置字段项 */
-export interface ITableSettingFieldItem {
-  disabled: boolean;
-  id: string;
-  name: string;
-}
-
-/** 表格尺寸主题 */
-export type ITableSettingSize = 'large' | 'medium' | 'small';
-
-/** 表格排序属性 */
-export interface ITableSort {
-  order: 'ascending' | 'descending' | null;
-  prop: string;
-}
-
-export interface QueryListRequestParams {
-  /** 业务ID */
-  bk_biz_id?: number;
-  /** 搜索，支持模板名称，模板说明，创建人，更新人 */
-  conditions?: { key: string; value: string[] }[];
-  /** 排序字段 例： ["id"] or ["-id"] */
-  order_by?: string[];
-  /** 页码 */
-  page?: number;
-  /** 每页条数 */
-  page_size?: number;
-}
-export interface QueryTemplateListItem {
+export interface AlarmTemplateListItem {
+  algorithms: AlarmAlgorithmItem[];
+  applied_service_names: string[];
+  category: string;
   create_time: string;
   create_user: string;
-  description: string;
-  id: number | string;
+  id: number;
+  is_auto_apply: boolean;
+  is_enabled: boolean;
   name: string;
-  relation_config_count?: number;
+  system: string;
+  type: string;
   update_time: string;
   update_user: string;
-}
-export interface QueryTemplateRelationItem {
-  query_template_id: QueryTemplateListItem['id'];
-  relation_config_count: number;
+  user_group_list: AlarmUserGroupItem[];
 }
 
-export interface QueryTemplateRelationsRequestParams {
-  query_template_ids: QueryTemplateListItem['id'][];
-}
-
-export interface SearchSelectItem {
-  id: string;
-  multiple?: boolean;
+export interface AlarmUserGroupItem {
+  id: number;
   name: string;
-  type?: 'text';
-  values?: Omit<SearchSelectItem, 'type' | 'values'>[];
 }
