8edb0f969d4ef665555a55a65c8ac00729a3dfe3
crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
2025-09-18 09:51:55 +0800

    feat: 提供 APM 内置策略模板及注册 --story=127519993 (#8164)

--- bkmonitor/packages/apm_web/strategy/constants.py
+++ bkmonitor/packages/apm_web/strategy/constants.py
@@ -1,6 +1,6 @@
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
-Copyright (C) 2017-2025 Tencent. All rights reserved.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://opensource.org/licenses/MIT
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
@@ -8,12 +8,166 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+from constants.apm import CachedEnum
 
-class ThresholdLevel:
+
+from django.utils.functional import cached_property
+
+from django.utils.translation import gettext_lazy as _
+
+
+DEFAULT_ROOT_ID: int = 0
+
+DEFAULT_DETECT_TYPE: str = "default"
+
+BUILTIN_USER_GROUP_ID: int = -9999
+
+
+class ThresholdLevel(CachedEnum):
+    """告警级别"""
+
     FATAL = 1
     WARNING = 2
     REMINDER = 3
 
     @classmethod
-    def choices(cls):
-        return [(cls.FATAL, "致命"), (cls.WARNING, "预警"), (cls.REMINDER, "提醒")]
+    def choices(cls) -> list[tuple[int, str]]:
+        return [
+            (cls.FATAL.value, cls.FATAL.label),
+            (cls.WARNING.value, cls.WARNING.label),
+            (cls.REMINDER.value, cls.REMINDER.label),
+        ]
+
+    @cached_property
+    def label(self) -> str:
+        return str({self.FATAL: _("致命"), self.WARNING: _("预警"), self.REMINDER: _("提醒")}.get(self, self.value))
+
+
+class StrategyTemplateType(CachedEnum):
+    """策略模板类型"""
+
+    APP_TEMPLATE = "app"
+    BUILTIN_TEMPLATE = "builtin"
+
+    @classmethod
+    def choices(cls) -> list[tuple[str, str]]:
+        return [
+            (cls.APP_TEMPLATE.value, cls.APP_TEMPLATE.label),
+            (cls.BUILTIN_TEMPLATE.value, cls.BUILTIN_TEMPLATE.label),
+        ]
+
+    @cached_property
+    def label(self) -> str:
+        return str({self.APP_TEMPLATE: _("克隆模板"), self.BUILTIN_TEMPLATE: _("内置模板")}.get(self, self.value))
+
+
+class StrategyTemplateSystem(CachedEnum):
+    """策略模板类型"""
+
+    RPC = "RPC"
+    K8S = "K8S"
+    LOG = "LOG"
+    TRACE = "TRACE"
+    EVENT = "EVENT"
+    METRIC = "METRIC"
+
+    @classmethod
+    def choices(cls) -> list[tuple[str, str]]:
+        return [
+            (cls.RPC.value, cls.RPC.label),
+            (cls.K8S.value, cls.K8S.label),
+            (cls.LOG.value, cls.LOG.label),
+            (cls.TRACE.value, cls.TRACE.label),
+            (cls.EVENT.value, cls.EVENT.label),
+            (cls.METRIC.value, cls.METRIC.label),
+        ]
+
+    @cached_property
+    def label(self) -> str:
+        return str(
+            {
+                self.RPC: _("调用分析"),
+                self.K8S: _("容器"),
+                self.LOG: _("日志"),
+                self.TRACE: _("调用链"),
+                self.EVENT: _("事件"),
+                self.METRIC: _("指标"),
+            }.get(self, self.value)
+        )
+
+    @classmethod
+    def get_default(cls, value) -> "StrategyTemplateType":
+        default = super().get_default(value)
+        default.label = value
+        return default
+
+
+class StrategyTemplateCategory(CachedEnum):
+    """策略模板分类"""
+
+    DEFAULT = "DEFAULT"
+    RPC_CALLEE = "RPC_CALLEE"
+    RPC_CALLER = "RPC_CALLER"
+
+    @classmethod
+    def choices(cls) -> list[tuple[str, str]]:
+        return [
+            (cls.DEFAULT.value, cls.DEFAULT.label),
+            (cls.RPC_CALLEE.value, cls.RPC_CALLEE.label),
+            (cls.RPC_CALLER.value, cls.RPC_CALLER.label),
+        ]
+
+    @cached_property
+    def label(self) -> str:
+        return str(
+            {self.DEFAULT: _("默认"), self.RPC_CALLEE: _("被调"), self.RPC_CALLER: _("主调")}.get(self, self.value)
+        )
+
+    @classmethod
+    def get_default(cls, value) -> "StrategyTemplateCategory":
+        default = super().get_default(value)
+        default.label = value
+        return default
+
+
+class StrategyTemplateMonitorType(CachedEnum):
+    """策略模板数据类型"""
+
+    # DEFAULT
+    DEFAULT = "DEFAULT"
+
+    # RED
+    P99 = "P99"
+    AVG = "AVG"
+    SUCCESS_RATE = "SUCCESS_RATE"
+
+    # 事件
+    CICD = "CICD"
+
+    @classmethod
+    def choices(cls) -> list[tuple[str, str]]:
+        return [
+            (cls.DEFAULT.value, cls.DEFAULT.label),
+            (cls.P99.value, cls.P99.label),
+            (cls.AVG.value, cls.AVG.label),
+            (cls.SUCCESS_RATE.value, cls.SUCCESS_RATE.label),
+            (cls.CICD.value, cls.CICD.label),
+        ]
+
+    @cached_property
+    def label(self) -> str:
+        return str(
+            {
+                self.DEFAULT: _("默认"),
+                self.P99: _("耗时分位"),
+                self.AVG: _("平均耗时"),
+                self.SUCCESS_RATE: _("成功率"),
+                self.CICD: _("CICD"),
+            }.get(self, self.value)
+        )
+
+    @classmethod
+    def get_default(cls, value) -> "StrategyTemplateMonitorType":
+        default = super().get_default(value)
+        default.label = value
+        return default
