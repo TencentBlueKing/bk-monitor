6dc7624f5696e5576d7284ba5d46bee213d5b7f0
joker-joker-yuan <blackpig163@163.com>
2025-09-23 11:03:12 +0000

    feat: 【APM 策略模板】option values 接口 #1010158081127639544 # Reviewed, transaction id: 58973

--- bkmonitor/packages/apm_web/strategy/handler.py
+++ bkmonitor/packages/apm_web/strategy/handler.py
@@ -4,6 +4,7 @@ from collections.abc import Iterable
 from django.db.models import QuerySet
 
 from bkmonitor.models import UserGroup
+from constants.apm import CachedEnum
 from apm_web.strategy.constants import StrategyTemplateSystem, StrategyTemplateIsEnabled, StrategyTemplateIsAutoApply
 
 
@@ -15,40 +16,47 @@ def get_user_groups(user_group_ids: Iterable[int]) -> dict[int, dict[str, int | 
 
 
 class OptionValues:
-    SUPPORT_FIELDS = []
-    FIELD_ALIAS: dict[str, dict[str, str]] = {}
+    _SUPPORT_FIELDS: list[str] = []
+    _FIELD_ALIAS: dict[str, CachedEnum] = {}
 
     def __init__(self, queryset: QuerySet):
         self.queryset = queryset
 
     @classmethod
     def is_matched(cls, field_name: str) -> bool:
-        return field_name in cls.SUPPORT_FIELDS
+        return field_name in cls._SUPPORT_FIELDS
 
     def _get_default(self, field_name: str) -> list[dict[str, Any]]:
-        return [
-            {"value": v, "alias": self.FIELD_ALIAS.get(field_name, {}).get(v, v)}
-            for v in self.queryset.values_list(field_name).distinct()
-        ]
+        option_values: list[dict[str, Any]] = []
+        for field_value in self.queryset.values_list(field_name, flat=True).distinct():
+            alias_enum = self._FIELD_ALIAS.get(field_name)
+            option_values.append(
+                {
+                    "value": field_value,
+                    "alias": field_value if alias_enum is None else alias_enum.from_value(field_value).label,
+                }
+            )
+        return option_values
 
     def get_fields_option_values(self, fields: Iterable[str]) -> dict[str, list[dict[str, Any]]]:
         return {field_name: getattr(self, f"get_{field_name}")() for field_name in fields}
 
 
 class StrategyTemplateOptionValues(OptionValues):
-    SUPPORT_FIELDS = ["system", "user_group_id", "update_user", "is_enabled", "is_auto_apply"]
-    FIELD_ALIAS: dict[str, dict[str, str]] = {
-        "system": dict(StrategyTemplateSystem.choices()),
-        "is_enabled": dict(StrategyTemplateIsEnabled.choices()),
-        "is_auto_apply": dict(StrategyTemplateIsAutoApply.choices()),
+    _SUPPORT_FIELDS: list[str] = ["system", "user_group_id", "update_user", "is_enabled", "is_auto_apply"]
+    _FIELD_ALIAS: dict[str, CachedEnum] = {
+        "system": StrategyTemplateSystem,
+        "is_enabled": StrategyTemplateIsEnabled,
+        "is_auto_apply": StrategyTemplateIsAutoApply,
     }
 
-    def get_user_group_id(self) -> list[dict[str, str]]:
-        ids_list = self.queryset.values_list("user_group_ids", flat=True).distinct()
-        flat_ids = set([_id for ids in ids_list for _id in ids])
+    def get_user_group_id(self) -> list[dict[str, Any]]:
+        user_group_ids: set[int] = set()
+        for partial_user_group_ids in self.queryset.values_list("user_group_ids", flat=True):
+            user_group_ids.update(partial_user_group_ids)
         return [
-            {"value": _id, "alias": user_group_dict["name"]}
-            for _id, user_group_dict in get_user_groups(flat_ids).items()
+            {"value": user_group_id, "alias": user_group["name"]}
+            for user_group_id, user_group in get_user_groups(user_group_ids).items()
         ]
 
     def get_system(self) -> list[dict[str, str]]:
@@ -57,15 +65,15 @@ class StrategyTemplateOptionValues(OptionValues):
     def get_update_user(self) -> list[dict[str, str]]:
         return self._get_default("update_user")
 
-    def get_is_enabled(self) -> list[dict[str, str]]:
+    def get_is_enabled(self) -> list[dict[str, Any]]:
         return self._get_default("is_enabled")
 
-    def get_is_auto_apply(self) -> list[dict[str, str]]:
+    def get_is_auto_apply(self) -> list[dict[str, Any]]:
         return self._get_default("is_auto_apply")
 
 
 class StrategyInstanceOptionValues(OptionValues):
-    SUPPORT_FIELDS = ["applied_service_name"]
+    _SUPPORT_FIELDS: list[str] = ["applied_service_name"]
 
     def get_applied_service_name(self) -> list[dict[str, str]]:
         return self._get_default("service_name")
