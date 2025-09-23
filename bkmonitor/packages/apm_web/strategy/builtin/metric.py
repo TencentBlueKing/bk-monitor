47bfb3ddc8f4c3848481fa3f937f6c8935c442b2
crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
2025-09-23 22:27:03 +0800

    feat: 提供查询模板工厂类 --story=127655727 (#8231)

--- bkmonitor/packages/apm_web/strategy/builtin/metric.py
+++ bkmonitor/packages/apm_web/strategy/builtin/metric.py
@@ -9,43 +9,14 @@ specific language governing permissions and limitations under the License.
 """
 
 from typing import Any
-from functools import cached_property
-from constants.apm import CachedEnum
 
-from constants.query_template import GLOBAL_BIZ_ID
-from bkmonitor.query_template.builtin.apm import APMQueryTemplateName
-
-from django.utils.translation import gettext as _
-
-from . import base, utils
+from . import base
 from .. import constants
-
-
-class MetricStrategyTemplateCode(CachedEnum):
-    METRIC_RPC_PANIC = "metric_rpc_panic"
-
-    @cached_property
-    def label(self) -> str:
-        return str({self.METRIC_RPC_PANIC: _("[RPC 框架] Panic 告警")}.get(self, self.value))
-
-
-METRIC_RPC_PANIC_STRATEGY_TEMPLATE: dict[str, Any] = {
-    "code": MetricStrategyTemplateCode.METRIC_RPC_PANIC.value,
-    "name": MetricStrategyTemplateCode.METRIC_RPC_PANIC.label,
-    "category": constants.StrategyTemplateCategory.DEFAULT.value,
-    "monitor_type": constants.StrategyTemplateMonitorType.DEFAULT.value,
-    "detect": utils.detect_config(5, 1, 1),
-    "algorithms": [
-        utils.fatal_threshold_algorithm_config(method="gte", threshold=1),
-    ],
-    "query_template": {"bk_biz_id": GLOBAL_BIZ_ID, "name": APMQueryTemplateName.CUSTOM_METRIC_PANIC.value},
-    "context": {},
-}
 
 
 class MetricStrategyTemplateSet(base.StrategyTemplateSet):
     SYSTEM: constants.StrategyTemplateSystem = constants.StrategyTemplateSystem.METRIC
 
-    ENABLED_CODES: list[str] = [MetricStrategyTemplateCode.METRIC_RPC_PANIC.value]
+    ENABLED_CODES: list[str] = []
 
-    STRATEGY_TEMPLATES: list[dict[str, Any]] = [METRIC_RPC_PANIC_STRATEGY_TEMPLATE]
+    STRATEGY_TEMPLATES: list[dict[str, Any]] = []
