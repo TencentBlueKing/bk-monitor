db13cb11031ed41b49ff8d45c76b70e2e2d868ba
yiqiwang-17 <66986742+yiqiwang-17@users.noreply.github.com>
2025-09-26 11:19:06 +0800

    minor: merge master--no-verify # Reviewed, transaction id: 59315

--- bkmonitor/bkmonitor/query_template/tests/test_serializers.py
+++ bkmonitor/bkmonitor/query_template/tests/test_serializers.py
@@ -7,24 +7,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from blueapps.account import get_user_model
-from django.core.management import BaseCommand
-
-<<<<<<<< HEAD:bklog/apps/log_databus/management/commands/init_admin.py
-
-class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument("--username", type=str, help="admin user name")
-
-    def handle(self, **options):
-        try:
-            username = options.get("username")
-            user = get_user_model()
-            user.objects.filter(username=username).update(is_superuser=True, is_staff=True)
-            print("[Init Admin Auth] operate SUCCESS!")
-        except Exception as e:
-            print(f"[Init Admin Auth] operate FAILED! details: {str(e)}")
-========
 from typing import Any
 
 import pytest
@@ -39,4 +21,3 @@ class TestSerializers:
     def test_query_template_serializers(self, template: dict[str, Any]):
         serializer = serializers.QueryTemplateSerializer(data=template)
         serializer.is_valid(raise_exception=True)
->>>>>>>> 957c9c1868eb754f38840f152c1b848a1bd07afd:bkmonitor/bkmonitor/query_template/tests/test_serializers.py
