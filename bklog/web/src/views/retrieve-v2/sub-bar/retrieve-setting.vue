1f6f43c7ee0bc173f85d5a04f7e4d531467ab27c
LeeXT <xintaoLi@users.noreply.github.com>
2025-09-28 15:13:50 +0800

    feat: 索引列表获取当前索引信息逻辑调整 --story=127764645 (#8291)

--- bklog/web/src/views/retrieve-v2/sub-bar/retrieve-setting.vue
+++ bklog/web/src/views/retrieve-v2/sub-bar/retrieve-setting.vue
@@ -19,11 +19,11 @@
   // const spaceUid = computed(() => store.state.spaceUid);
   const indexSetId = computed(() => store.state.indexId);
   const indexSetItem = computed(() =>
-    store.state.retrieve.indexSetList.find(item => item.index_set_id === `${indexSetId.value}`),
+    store.state.retrieve.flatIndexSetList.find(item => item.index_set_id === `${indexSetId.value}`),
   );
   const isPopoverShow = ref(false);
   const spaceUid = computed(() => {
-    const indexSetList = store.state.retrieve.indexSetList;
+    const indexSetList = store.state.retrieve.flatIndexSetList;
     const indexSetId = route.params?.indexId;
     const currentIndexSet = indexSetList.find(item => `${item.index_set_id}` == indexSetId);
     return currentIndexSet?.space_uid;
