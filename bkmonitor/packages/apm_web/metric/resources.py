c4a8b398e3ef314ca00de05943dababa79f1dea7
liuwenping <haha.rober@gmail.com>
2025-10-14 15:37:52 +0800

    feat: APM 临时去掉派生指标 bk_apm_duration_bucket，以及页面的 pXX 等展示 --story=127932656 (#8395)

--- bkmonitor/packages/apm_web/metric/resources.py
+++ bkmonitor/packages/apm_web/metric/resources.py
@@ -704,24 +704,25 @@ class ServiceListResource(PageListResource):
                 asyncable=True,
                 min_width=160,
             ),
-            NumberTableFormat(
-                id="p50",
-                name=_lazy("P50"),
-                checked=True,
-                unit="ns",
-                decimal=2,
-                asyncable=True,
-                width=80,
-            ),
-            NumberTableFormat(
-                id="p90",
-                name=_lazy("P90"),
-                checked=True,
-                unit="ns",
-                decimal=2,
-                asyncable=True,
-                width=80,
-            ),
+            # 2025-10-13 临时去掉 bk_apm_duration_bucket 指标，以及页面对应的pXX展示，待新方案上线后再放开，预计半年后
+            # NumberTableFormat(
+            #     id="p50",
+            #     name=_lazy("P50"),
+            #     checked=True,
+            #     unit="ns",
+            #     decimal=2,
+            #     asyncable=True,
+            #     width=80,
+            # ),
+            # NumberTableFormat(
+            #     id="p90",
+            #     name=_lazy("P90"),
+            #     checked=True,
+            #     unit="ns",
+            #     decimal=2,
+            #     asyncable=True,
+            #     width=80,
+            # ),
             # 四个数据状态 ↓
             DataStatusTableFormat(
                 id="metric_data_status",
