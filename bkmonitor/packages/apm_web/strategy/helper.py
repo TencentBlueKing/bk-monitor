d6711c7d438bc66d8d8ae46dc6d77f34e63f1439
crayon <42019787+ZhuoZhuoCrayon@users.noreply.github.com>
2025-10-22 19:18:50 +0800

    feat: APM 策略模板下发逻辑优化 --story=128079815 (#8539)

--- bkmonitor/packages/apm_web/strategy/helper.py
+++ bkmonitor/packages/apm_web/strategy/helper.py
@@ -17,6 +17,7 @@ from apm_web.models import StrategyTemplate
 
 from .query_template import QueryTemplateWrapperFactory
 
+from core.drf_resource import resource
 from bkmonitor.models import UserGroup
 
 from constants.query_template import GLOBAL_BIZ_ID
@@ -25,6 +26,7 @@ from constants.query_template import GLOBAL_BIZ_ID
 def format2strategy_template_detail(
     strategy_template_obj: StrategyTemplate, serializer: type[serializers.Serializer]
 ) -> dict[str, Any]:
+    """生成策略模板详情数据"""
     strategy_template_data: dict[str, Any] = serializer(strategy_template_obj).data
     query_template_data: dict[str, Any] = strategy_template_data["query_template"]
     qtw = QueryTemplateWrapperFactory.get_wrapper(
@@ -36,7 +38,21 @@ def format2strategy_template_detail(
 
 
 def get_user_groups(user_group_ids: Iterable[int]) -> dict[int, dict[str, int | str]]:
+    """获取用户组 ID 到 用户组简要信息的映射关系"""
     return {
         user_group["id"]: user_group
         for user_group in UserGroup.objects.filter(id__in=user_group_ids).values("id", "name")
+    }
+
+
+def get_id_strategy_map(bk_biz_id: int, ids: Iterable[int]) -> dict[int, dict[str, Any]]:
+    """获取策略 ID 到 策略简要信息的映射关系"""
+    if not ids:
+        return {}
+
+    strategies: list[dict[str, Any]] = resource.strategies.plain_strategy_list_v2(
+        {"bk_biz_id": bk_biz_id, "ids": list(ids)}
+    )
+    return {
+        strategy_dict["id"]: {"id": strategy_dict["id"], "name": strategy_dict["name"]} for strategy_dict in strategies
     }
