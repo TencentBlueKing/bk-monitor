fc1a256013988fbf71d5a44efe7024c2902d2848
JayCC1 <85379453+JayCC1@users.noreply.github.com>
2025-10-29 11:25:24 +0800

    feat: 告警中心-告警详情-主机tab开发 (#8615)

--- bkmonitor/webpack/src/trace/pages/alarm-center/detail-common/components/panel-host/index.tsx
+++ bkmonitor/webpack/src/trace/pages/alarm-center/detail-common/components/panel-host/index.tsx
@@ -25,11 +25,14 @@
  */
 import { type PropType, computed, defineComponent, onMounted, provide, shallowRef, watch } from 'vue';
 
+import { get } from '@vueuse/core';
 import { random } from 'monitor-common/utils';
 import { echartsConnect } from 'monitor-ui/monitor-echarts/utils';
+import { storeToRefs } from 'pinia';
 
 import { DEFAULT_TIME_RANGE } from '../../../../../components/time-range/utils';
 import { createAutoTimeRange } from '../../../../../plugins/charts/failure-chart/failure-alarm-chart';
+import { useAlarmCenterDetailStore } from '../../../../../store/modules/alarm-center-detail';
 import AiHighlightCard from '../../../components/ai-highlight-card/ai-highlight-card';
 import { getHostSceneView } from '../../../services/alarm-detail';
 import PanelHostDashboard from './components/panel-host-dashboard/panel-host-dashboard';
@@ -45,35 +48,67 @@ export default defineComponent({
   props: {
     detail: {
       type: Object as PropType<AlarmDetail>,
-      default: () => ({}),
     },
   },
   setup(props) {
     /** 图表联动Id */
     const dashboardId = random(10);
 
+    /** 业务ID */
+    const { bizId } = storeToRefs(useAlarmCenterDetailStore());
     /** host 场景指标视图配置信息 */
     const hostSceneData = shallowRef<IBookMark>({ id: '', panels: [], name: '' });
-    /** 图表请求参数变量 */
-    const viewOptions = shallowRef<Record<string, any>>({});
     /** 是否处于请求加载状态 */
     const loading = shallowRef(false);
     /** 是否立即刷新图表数据 */
     const refreshImmediate = shallowRef('');
 
-    /** 数据时间间隔 */
+    /** 图表数据的时间间隔 */
+    const interval = computed(
+      () => props.detail?.extra_info?.strategy?.items?.[0]?.query_configs?.[0]?.agg_interval || 60
+    );
+    /** 默认监控的目标配置 */
+    const defaultCurrentTarget = computed(() => {
+      const currentTarget: Record<string, any> = {
+        bk_target_ip: '0.0.0.0',
+        bk_target_cloud_id: '0',
+      };
+      for (const item of props.detail?.dimensions ?? []) {
+        if (item.key === 'bk_host_id') {
+          currentTarget.bk_host_id = item.value;
+        }
+        if (['bk_target_ip', 'ip', 'bk_host_id'].includes(item.key)) {
+          currentTarget.bk_target_ip = item.value;
+        }
+        if (['bk_cloud_id', 'bk_target_cloud_id', 'bk_host_id'].includes(item.key)) {
+          currentTarget.bk_target_cloud_id = item.value;
+        }
+      }
+      return currentTarget;
+    });
+    /** 数据时间范围 */
     const timeRange = computed(() => {
-      const interval = props.detail.extra_info?.strategy?.items?.[0]?.query_configs?.[0]?.agg_interval || 60;
-      const { startTime, endTime } = createAutoTimeRange(props.detail?.begin_time, props.detail?.end_time, interval);
+      const { startTime, endTime } = createAutoTimeRange(
+        props.detail?.begin_time,
+        props.detail?.end_time,
+        get(interval)
+      );
       return startTime && endTime ? [startTime, endTime] : DEFAULT_TIME_RANGE;
     });
+    /** 图表请求参数变量 */
+    const viewOptions = computed(() => {
+      const currentTarget = get(defaultCurrentTarget);
 
-    watch(
-      () => props.detail,
-      () => {
-        init();
-      }
-    );
+      return {
+        method: 'AVG',
+        interval: get(interval),
+        group_by: [],
+        current_target: currentTarget,
+        ip: currentTarget?.bk_target_ip,
+        bk_cloud_id: currentTarget?.bk_target_cloud_id,
+        ...currentTarget,
+      };
+    });
 
     provide('timeRange', timeRange);
     provide('refreshImmediate', refreshImmediate);
@@ -82,55 +117,11 @@ export default defineComponent({
     });
 
     /**
-     * @description 初始化 数据时间间隔 & 图表请求参数变量
-     */
-    function init() {
-      const currentTarget: Record<string, any> = {
-        bk_target_ip: '0.0.0.0',
-        bk_target_cloud_id: '0',
-      };
-      const variables: Record<string, any> = {
-        bk_target_ip: '0.0.0.0',
-        bk_target_cloud_id: '0',
-        ip: '0.0.0.0',
-        bk_cloud_id: '0',
-      };
-
-      for (const item of props.detail?.dimensions ?? []) {
-        if (item.key === 'bk_host_id') {
-          variables.bk_host_id = item.value;
-          currentTarget.bk_host_id = item.value;
-        }
-        if (['bk_target_ip', 'ip', 'bk_host_id'].includes(item.key)) {
-          variables.bk_target_ip = item.value;
-          variables.ip = item.value;
-          currentTarget.bk_target_ip = item.value;
-        }
-        if (['bk_cloud_id', 'bk_target_cloud_id', 'bk_host_id'].includes(item.key)) {
-          variables.bk_target_cloud_id = item.value;
-          variables.bk_cloud_id = item.value;
-          currentTarget.bk_target_cloud_id = item.value;
-        }
-      }
-
-      const interval = props.detail.extra_info?.strategy?.items?.[0]?.query_configs?.[0]?.agg_interval || 60;
-
-      viewOptions.value = {
-        method: 'AVG',
-        variables,
-        interval,
-        group_by: [],
-        current_target: currentTarget,
-      };
-    }
-
-    /**
      * @description 获取仪表盘数据数组
      */
     async function getDashboardPanels() {
       loading.value = true;
-      const sceneView = await getHostSceneView(props.detail?.bk_biz_id);
-
+      const sceneView = await getHostSceneView(bizId.value);
       hostSceneData.value = sceneView;
       echartsConnect(dashboardId);
       loading.value = false;
@@ -141,25 +132,32 @@ export default defineComponent({
      */
     function handleToPerformance() {
       const currentTarget = viewOptions.value?.current_target;
-
       const ip = currentTarget?.bk_target_ip ?? '0.0.0.0';
       const cloudId = currentTarget?.bk_target_cloud_id ?? '0';
       const bkHostId = currentTarget?.bk_host_id ?? 0;
-
       // 跳转至容器监控时的详情Id
       const detailId = bkHostId ? bkHostId : `${ip}-${cloudId}`;
-      window.open(
-        `${location.origin}${location.pathname}?bizId=${props.detail.bk_biz_id}#/performance/detail/${detailId}`
-      );
+      window.open(`${location.origin}${location.pathname}?bizId=${bizId.value}#/performance/detail/${detailId}`);
     }
-    return { hostSceneData, dashboardId, viewOptions, handleToPerformance };
+
+    /**
+     * @description 创建骨架屏 dom 元素
+     */
+    function createSkeletonDom() {
+      return <div class='alarm-detail-panel-host-skeleton-dom skeleton-element' />;
+    }
+
+    return { loading, hostSceneData, dashboardId, viewOptions, handleToPerformance, createSkeletonDom };
   },
   render() {
     return (
-      <div class='alarm-center-detail-panel-host'>
+      <div class={['alarm-center-detail-panel-host', this.loading ? 'is-loading' : '']}>
         <div class='panel-host-white-bg-container'>
           <div class='host-selector-wrap'>
-            <PanelHostSelector class='host-selector' />
+            <div class='host-selector-container'>
+              <PanelHostSelector class='host-selector' />
+              {this.createSkeletonDom()}
+            </div>
             <div
               class='host-explore-link-btn'
               onClick={this.handleToPerformance}
@@ -168,10 +166,13 @@ export default defineComponent({
               <i class='icon-monitor icon-mc-goto' />
             </div>
           </div>
-          <AiHighlightCard
-            content='该模块哈哈哈哈哈，我是一段随意的文本占位。'
-            title={`${window.i18n.t('AI 分析结论')}：`}
-          />
+          <div class='ai-hight-card-wrap'>
+            <AiHighlightCard
+              content='该模块哈哈哈哈哈，我是一段随意的文本占位。'
+              title={`${window.i18n.t('AI 分析结论')}：`}
+            />
+            {this.createSkeletonDom()}
+          </div>
         </div>
         <div class='panel-host-chart-wrap'>
           <PanelHostDashboard
