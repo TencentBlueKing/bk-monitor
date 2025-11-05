79b081936e9a4da5d787c4c4a20469e10d761a1a
xintaoLi <li18566230175@gmail.com>
2025-11-05 19:17:25 +0800

    fix: 修复iframe嵌入参数丢失问题

--- bklog/web/src/main.js
+++ bklog/web/src/main.js
@@ -57,6 +57,7 @@ import './static/font-face/index.css';
 import './static/style.css';
 import '@blueking/bk-user-selector/vue2/vue2.css';
 import { BK_LOG_STORAGE } from './store/store.type.ts';
+import { urlArgs } from './store/default-values.ts';
 
 // import { localSettings } from './local.po';
 
@@ -148,10 +149,10 @@ const mountedVueInstance = () => {
               this.$router.push({
                 path: '/un-authorized',
                 query: {
-                  ...this.$route.query,
                   type: 'space',
                   spaceUid: spaceUid ?? store.state.storage[BK_LOG_STORAGE.BK_SPACE_UID],
                   bkBizId: bkBizId ?? store.state.storage[BK_LOG_STORAGE.BK_BIZ_ID],
+                  from: urlArgs.from,
                 },
               });
 
