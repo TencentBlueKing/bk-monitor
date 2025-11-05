bb1b8af4ea278f61c398d98d8122f29f9186e000
Hywabwm <Hywabwm@outlook.com>
2025-11-05 09:42:58 +0000

    fix: 修复 采集项停用时, 编辑保存会触发采集下发 问题 #1010158081149933490

--- bklog/apps/log_databus/handlers/collector/k8s.py
+++ bklog/apps/log_databus/handlers/collector/k8s.py
@@ -599,7 +599,7 @@ class K8sCollectorHandler(CollectorHandler):
                 data_name=self.build_bk_data_name(self.data.get_bk_biz_id(), data["collector_config_name_en"]),
                 description=collector_config_params["description"],
                 encoding=META_DATA_ENCODING,
-                bk_biz_id=self.data.get_bk_biz_id()
+                bk_biz_id=self.data.get_bk_biz_id(),
             )
             self.data.task_id_list = list(
                 ContainerCollectorConfig.objects.filter(collector_config_id=self.collector_config_id).values_list(
@@ -983,7 +983,7 @@ class K8sCollectorHandler(CollectorHandler):
             if collector_config_params["description"]
             else collector_config_params["collector_config_name_en"],
             encoding=META_DATA_ENCODING,
-            bk_biz_id=self.data.bk_biz_id
+            bk_biz_id=self.data.bk_biz_id,
         )
         self.data.save()
 
@@ -1614,11 +1614,13 @@ class K8sCollectorHandler(CollectorHandler):
                 )
                 container_config.save()
                 container_configs.append(container_config)
-            self.create_container_release(container_config=container_config)
-        delete_container_configs = container_configs[config_length::]
-        for config in delete_container_configs:
-            # 增量比对后，需要真正删除配置
-            self.delete_container_release(config, delete_config=True)
+            if self.data.is_active:
+                self.create_container_release(container_config=container_config)
+        if self.data.is_active:
+            delete_container_configs = container_configs[config_length::]
+            for config in delete_container_configs:
+                # 增量比对后，需要真正删除配置
+                self.delete_container_release(config, delete_config=True)
 
     def create_container_release(self, container_config: ContainerCollectorConfig, **kwargs):
         """
