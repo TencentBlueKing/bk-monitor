511f4c77fb132187f6481d2f354d6d0e6836c128
Jiahua Wu <553544693@qq.com>
2025-11-10 17:23:04 +0800

    feat: 优化 entity 接口鉴权逻辑 --story=128533199 (#8824)

--- bkmonitor/kernel_api/views/v4/entity.py
+++ bkmonitor/kernel_api/views/v4/entity.py
@@ -8,11 +8,34 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+from django.conf import settings
+from rest_framework.status import HTTP_401_UNAUTHORIZED
+
+from bkmonitor.models import ApiAuthToken, AuthType
+from bkmonitor.views.renderers import MonitorJSONRenderer
 from core.drf_resource.viewsets import ResourceRoute, ResourceViewSet
 from metadata import resources as resource
 
 
+RENDER_CLASSES = (MonitorJSONRenderer,)
+
+
 class EntityViewSet(ResourceViewSet):
+    renderer_classes = RENDER_CLASSES
+
+    def check_permissions(self, request):
+        # 中间件已经判断了 token 的有效性，为避免调用方通过非 token 的方式访问，此处只需校验 token 是否存在
+        token_obj = None
+        token = getattr(request._request, "token", None)
+        if token:
+            token_obj = ApiAuthToken.objects.filter(token=token, type=AuthType.Entity).first()
+        if not token_obj:
+            self.permission_denied(
+                request,
+                message=f"API Token is required, please use your browser to access the address {settings.BK_MONITOR_HOST}/rest/v2/commons/token_manager/get_api_token/?type=entity&bk_biz_id={{your_biz_id}} to get your API Token",
+                code=HTTP_401_UNAUTHORIZED,
+            )
+
     resource_routes = [
         ResourceRoute("POST", resource.ApplyEntityResource, endpoint="apply"),
         # ResourceRoute("GET", resource.GetEntityResource, endpoint="get"),
