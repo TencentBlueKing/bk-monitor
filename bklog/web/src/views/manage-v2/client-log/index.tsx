1b13b9c627f0c2950e51d94ab164f6f696e6f36c
cheng2023112 <2247760239@qq.com>
2025-11-13 14:54:04 +0800

    feat: 客户端日志模块 #1010158081128581994

--- bklog/web/src/views/manage-v2/client-log/index.tsx
+++ bklog/web/src/views/manage-v2/client-log/index.tsx
@@ -27,44 +27,91 @@
 import { defineComponent, ref } from 'vue';
 
 import './index.scss';
+import { t } from '@/hooks/use-locale';
+
+// tab类型常量定义
+const TAB_TYPES = {
+  COLLECT: t('采集下发'),
+  REPORT: t('用户上报'),
+} as const;
+
+type TabType = (typeof TAB_TYPES)[keyof typeof TAB_TYPES];
 
 export default defineComponent({
   name: 'ClientLog',
   setup() {
     const tabs = ref([
       {
-        title: '采集下发',
-        count: 453,
+        title: TAB_TYPES.COLLECT,
+        count: '453',
       },
       {
-        title: '用户上报',
+        title: TAB_TYPES.REPORT,
         count: 87,
       },
     ]);
-    const activeTab = ref('采集下发');
+    const activeTab = ref<TabType>(TAB_TYPES.COLLECT);
 
     // tab点击事件
-    const handleTabClick = (title: string) => {
+    const handleTabClick = (title: TabType) => {
       activeTab.value = title;
     };
+
     return () => (
       <div class='client-log-main'>
+        {/* tab部分 */}
         <div class='tabs'>
           {tabs.value.map(tab => (
             <div
               class={['tab-item', activeTab.value === tab.title && 'active']}
               onClick={() => {
-                handleTabClick(tab.title);
+                handleTabClick(tab.title as TabType);
               }}
             >
-              {tab.title}
-              <span class='num'>{tab.count}</span>
+              <span class='tab-item-title'>{tab.title}</span>
+              <span class='tab-item-num'>{tab.count}</span>
             </div>
           ))}
         </div>
         <div class='client-log-container'>
-          <div>搜索区域</div>
-          <div>表格内容</div>
+          {/* 按钮、搜索、alter提示区域 */}
+          {activeTab.value === TAB_TYPES.COLLECT ? (
+            <div class='deploy-header'>
+              {/* 采集下发 */}
+              <div>
+                <bk-button theme='primary'>{t('新建采集')}</bk-button>
+                <bk-button>{t('清洗配置')}</bk-button>
+              </div>
+              <div>
+                <bk-input
+                  placeholder={t('搜索 任务 ID、任务名称、openID、创建方式、任务状态、任务阶段、创建人')}
+                  clearable
+                  right-icon={'bk-icon icon-search'}
+                ></bk-input>
+              </div>
+            </div>
+          ) : (
+            <div>
+              {/* 用户上报 */}
+              <bk-alert
+                class='alert-info'
+                type='info'
+                title={t('Alert 文案占位，用于说明如果用 SDK 上报。')}
+              ></bk-alert>
+              <div class='operating-area'>
+                <bk-button>{t('清洗配置')}</bk-button>
+                <div>
+                  <bk-input
+                    placeholder={t('搜索 任务 ID、任务名称、openID、创建方式、任务状态、任务阶段、创建人')}
+                    clearable
+                    right-icon={'bk-icon icon-search'}
+                  ></bk-input>
+                </div>
+              </div>
+            </div>
+          )}
+          {/* 表格内容区域 */}
+          <section>表格内容</section>
         </div>
       </div>
     );
