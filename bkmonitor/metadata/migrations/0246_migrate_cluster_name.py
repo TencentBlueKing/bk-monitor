d5b4f18845c4b0f38c390e7d6ab941636fe80acc
unique0lai <11598235+unique0lai@users.noreply.github.com>
2025-11-21 16:46:55 +0800

    feat: 集群管理支持与bkbase同步 #1010158081128782206

--- bkmonitor/metadata/migrations/0246_migrate_cluster_name.py
+++ bkmonitor/metadata/migrations/0246_migrate_cluster_name.py
@@ -5,6 +5,7 @@ def migrate_cluster_name(apps, schema_editor):
     ClusterInfo = apps.get_model("metadata", "ClusterInfo")
 
     for cluster in ClusterInfo.objects.all():
+        # 设置显示名称
         cluster.display_name = cluster.cluster_name
 
         # 检查cluster_name是否符合[a-zA-Z][a-zA-Z0-9_]*格式
@@ -14,6 +15,7 @@ def migrate_cluster_name(apps, schema_editor):
         # 替换为新的cluster_name，体现自动生成的含义
         cluster.cluster_name = f"auto_cluster_name_{cluster.cluster_id}"
 
+        # 如果集群类型为VM或注册来源系统为BKDATA，则默认标记为已注册到bkbase平台
         if (
             cluster.cluster_type == ClusterInfo.TYPE_VM
             or cluster.registered_system == ClusterInfo.BKDATA_REGISTERED_SYSTEM
