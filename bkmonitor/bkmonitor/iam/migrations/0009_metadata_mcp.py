225d2d78bbafcbfb3495566d49056e639c5f5e2b
EASYGOING45 <1505674928@qq.com>
2025-12-02 20:45:52 +0800

    feat: MCP: Metadata MCP服务支持,业务信息模糊匹配 & 业务下BCS集群信息获取 --story=129043758 # Reviewed, transaction id: 66546

--- bkmonitor/bkmonitor/iam/migrations/0009_metadata_mcp.py
+++ bkmonitor/bkmonitor/iam/migrations/0009_metadata_mcp.py
@@ -2,11 +2,11 @@ from iam.contrib.iam_migration.migrator import IAMMigrator
 from bkmonitor.migrate import BaseMigration
 
 
-def add_log_alarm_mcp_permission(*args, **kwargs):
-    # 新增日志MCP和告警MCP权限
+def add_metadata_mcp_permission(*args, **kwargs):
+    # 新增元数据MCP权限
     IAMMigrator("0009_metadata_mcp.json").migrate()
 
 
 class Migration(BaseMigration):
     dependencies = ["0008_log_alarm_mcp"]
-    operations = [add_log_alarm_mcp_permission]
+    operations = [add_metadata_mcp_permission]
