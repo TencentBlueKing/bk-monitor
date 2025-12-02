fef3a8015fd9db3d12a8766b31628696d999e69f
EmilyMei <447693773@qq.com>
2025-12-02 15:02:12 +0800

    feat: 采集管理 - 动态拓扑采集下发 --story=128400685 # Reviewed, transaction id: 66442

--- bklog/web/src/views/manage-v2/log-collection/components/create-operation/step2-configuration.tsx
+++ bklog/web/src/views/manage-v2/log-collection/components/create-operation/step2-configuration.tsx
@@ -56,7 +56,7 @@ import InputAddGroup from '../common-comp/input-add-group'; // 输入框组组�
 import AppendLogTags from '../business-comp/step2/container-collection/append-log-tags'; // 附加日志标签组件
 import ConfigurationItemList from '../business-comp/step2/container-collection/configuration-item-list'; // 配置项组件
 import { HOST_COLLECTION_CONFIG, CONTAINER_COLLECTION_CONFIG } from './defaultConfig'; // 默认配置
-import IndexConfigImportDialog from '../business-comp/step2/index-config-import-dialog';
+// import IndexConfigImportDialog from '../business-comp/step2/index-config-import-dialog';
 import $http from '@/api'; // API请求封装
 
 import './step2-configuration.scss'; // 样式文件
@@ -1023,17 +1023,17 @@ export default defineComponent({
         title: t('源日志信息'),
         key: 'sourceLogInfo',
         renderFn: renderSourceLogInfo,
-        subTitle: () => (
-          <span
-            class='config-import'
-            on-click={() => {
-              isIndexConfigImport.value = true;
-            }}
-          >
-            {t('索引配置导入')}
-            <i class='bklog-icon bklog-enter-3 config-import-icon' />
-          </span>
-        ),
+        // subTitle: () => (
+        //   <span
+        //     class='config-import'
+        //     on-click={() => {
+        //       isIndexConfigImport.value = true;
+        //     }}
+        //   >
+        //     {t('索引配置导入')}
+        //     <i class='bklog-icon bklog-enter-3 config-import-icon' />
+        //   </span>
+        // ),
       },
       {
         title: t('链路配置'),
@@ -1409,12 +1409,12 @@ export default defineComponent({
             {t('取消')}
           </bk-button>
         </div>
-        <IndexConfigImportDialog
+        {/* <IndexConfigImportDialog
           showDialog={isIndexConfigImport.value}
           on-cancel={(val: boolean) => {
             isIndexConfigImport.value = val;
           }}
-        />
+        /> */}
       </div>
     );
   },
