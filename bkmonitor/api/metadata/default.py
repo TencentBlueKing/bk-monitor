18de0e7bcd902542faa0d7a144a6a00ad7b98dbf
happy <hhuang1231@gmail.com>
2025-12-08 17:42:36 +0800

    fix:序列化器校验不允许为空

--- bkmonitor/api/metadata/default.py
+++ bkmonitor/api/metadata/default.py
@@ -521,7 +521,7 @@ class CreateOrUpdateTimeSeriesMetricResource(MetaDataAPIGWResource):
             field_config = serializers.DictField(required=False, label="字段其他配置", allow_null=True)
             label = serializers.CharField(required=False, label="指标监控对象", max_length=255, allow_null=True)
             scope_id = serializers.IntegerField(required=False, label="指标分组ID", allow_null=True)
-            scope_name = serializers.CharField(required=False, label="指标分组名", max_length=255, allow_null=True)
+            scope_name = serializers.CharField(required=False, label="指标分组名", max_length=255, allow_blank=True)
 
         group_id = serializers.IntegerField(required=True, label="自定义时序数据源ID")
         metrics = serializers.ListField(
