cc1219f9370033577ebbe4c304b78b068135ba25
hongq <1070293319@qq.com>
2025-12-24 18:30:06 +0800

    fix: 告警明细表格高度优化 --bug=150974836 # Reviewed, transaction id: 69401

--- bkmonitor/webpack/src/trace/pages/failure/alarm-detail/alarm-detail.tsx
+++ bkmonitor/webpack/src/trace/pages/failure/alarm-detail/alarm-detail.tsx
@@ -190,7 +190,6 @@ export default defineComponent({
     const enableCreateChatGroup = deepRef(window.enable_create_chat_group || false);
     const alertIdsData = deepRef(props.alertIdsObject);
     const alarmDetailRef = deepRef(null);
-    const alarmDetailHeight = deepRef(0);
     if (enableCreateChatGroup.value) {
       tableToolList.value.push({
         id: 'chat',
@@ -798,16 +797,30 @@ export default defineComponent({
           // 异常状态赋值
           exceptionData.value.isError = true;
           exceptionData.value.errorMsg = err.message || '';
-        });
+        })
       // const data = await incidentAlertList(Object.assign(params, props.filterSearch));
 
       // const list = alertData.value.find(item => item.alerts.length > 0);
       // collapseId.value = list ? list.id : '';n
     };
-    onMounted(() => {
-      if (alarmDetailRef.value) {
-        alarmDetailHeight.value = alarmDetailRef.value.offsetHeight;
+
+    // 表格最大高度
+    const tableMaxHeight = computed(() => {
+      if (!alarmDetailRef.value) return 0;
+
+      if (alertData.value.length > 1) {
+        const total = alertData.value.filter(f => f.alerts.length > 0).length;
+        // 273px: 固定的静态高度，150px: 每个折叠项的最小高度
+        const staticHeight = 273;
+        const itemHeight = 162;
+        return `calc(100vh - ${staticHeight}px - ${(total - 1) * itemHeight}px)`;
+      } else {
+        // 外层父容器高度
+        return alarmDetailRef.value.offsetHeight - 100;
       }
+    });
+
+    onMounted(() => {
       document.body.addEventListener('click', handleHideMoreOperate);
     });
     onUnmounted(() => {
@@ -898,7 +911,7 @@ export default defineComponent({
       refresh,
       disableKey,
       alarmDetailRef,
-      alarmDetailHeight,
+      tableMaxHeight,
     };
   },
   render() {
@@ -995,10 +1008,10 @@ export default defineComponent({
                         .filter(item => !this.disableKey.includes(item.colKey))
                         .map(item => item.colKey),
                     }}
-                    scroll={{ type: 'virtual' }}
                     columns={this.columns}
                     data={item.alerts}
-                    maxHeight={alertData.length > 1 ? 616 : this.alarmDetailHeight - 100}
+                    maxHeight={this.tableMaxHeight}
+                    scroll={{ type: 'virtual' }}
                     tooltip-config={{ showAll: false }}
                     onRowMouseenter={this.handleEnter}
                     onRowMouseleave={() => {
