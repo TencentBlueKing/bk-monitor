e0af1ad98926d4780356fa45b9157d2ed9a6757c
joker-joker-yuan <blackpig163@163.com>
2026-01-04 02:49:38 +0000

    feat: 【apm 日志】log 字段名称变更为 * #1010158081129715537 # Reviewed, transaction id: 70275

--- bkmonitor/packages/apm_web/log/resources.py
+++ bkmonitor/packages/apm_web/log/resources.py
@@ -15,6 +15,7 @@ from rest_framework import serializers
 
 from apm_web.handlers.log_handler import ServiceLogHandler, get_biz_index_sets_with_cache
 from apm_web.handlers.service_handler import ServiceHandler
+from apm_web.constants import DEFAULT_APM_LOG_SEARCH_FIELD_NAME
 from bkmonitor.utils.cache import CacheType, using_cache
 from bkmonitor.utils.thread_backend import ThreadPool
 from constants.apm import Vendor, FIVE_MIN_SECONDS
@@ -58,7 +59,7 @@ def process_service_relation(bk_biz_id, app_name, service_name, indexes_mapping,
             )
         if index_info:
             if overwrite_method:
-                index_info["addition"] = overwrite_method(overwrite_key="log")
+                index_info["addition"] = overwrite_method()
 
             result.append(index_info)
     return result
@@ -81,7 +82,7 @@ def process_span_host(bk_biz_id, app_name, span_id, span_detail, indexes_mapping
             # 默认查询: 机器 IP
             index_info["addition"] = item.get("addition", [])
             if overwrite_method:
-                index_info["addition"] = overwrite_method(overwrite_key="log")
+                index_info["addition"] = overwrite_method()
         result.append(index_info)
     return result
 
@@ -138,7 +139,7 @@ def process_metric_relations(
             if index_info:
                 index_info["addition"] = r["addition"]
                 if overwrite_method:
-                    index_info["addition"] = overwrite_method(overwrite_key="log")
+                    index_info["addition"] = overwrite_method()
                 result.append(index_info)
     return result
 
@@ -167,7 +168,9 @@ def log_relation_list(bk_biz_id, app_name, service_name, span_id=None, start_tim
             info = {"span_id": span_id}
             if span_detail and span_detail.get("trace_id"):
                 info["trace_id"] = span_detail["trace_id"]
-            overwrite_method = functools.partial(overwrite_with_span_addition, info=info)
+            overwrite_method = functools.partial(
+                overwrite_with_span_addition, info=info, overwrite_key=DEFAULT_APM_LOG_SEARCH_FIELD_NAME
+            )
 
         # 并行执行所有任务
         index_set_ids = set()
