5c4120a05744e0bdd62106a105d391790ba64362
chenguo <chenguo367@163.com>
2026-01-04 19:44:06 +0800

    refactor(fta_action): 统一使用 plugin_key 属性获取插件标识符

--- bkmonitor/alarm_backends/service/fta_action/__init__.py
+++ bkmonitor/alarm_backends/service/fta_action/__init__.py
@@ -176,11 +176,21 @@ class BaseActionProcessor:
         """
         raise NotImplementedError
 
+    @property
+    def plugin_key(self):
+        """
+        获取plugin_key(plugin_type)
+        """
+        plugin_key = self.action.action_plugin.get("plugin_key")
+        if not plugin_key:
+            plugin_key = self.action.action_plugin.get("plugin_type")
+        return plugin_key
+
     def can_func_call(self):
         """
         检查是否可以调用
         """
-        plugin_type = self.action.action_plugin.get("plugin_type")
+        plugin_type = self.plugin_key
         # 检查是否命中熔断规则
         logger.info(
             f"[circuit breaking] [{plugin_type}] begin action({self.action.id}) strategy({self.action.strategy_id})"
@@ -200,7 +210,7 @@ class BaseActionProcessor:
         :param skip_notice_check: 是否跳过通知类型检查，默认 True（执行阶段），False（通知阶段）
         :return: True 表示命中熔断规则，False 表示未命中
         """
-        plugin_type = plugin_type or self.action.action_plugin.get("plugin_type")
+        plugin_type = plugin_type or self.plugin_key
 
         # message_queue 类型在创建阶段已经检查过熔断，这里跳过
         if plugin_type == ActionPluginType.MESSAGE_QUEUE:
@@ -244,7 +254,7 @@ class BaseActionProcessor:
         :param plugin_type: 插件类型，默认使用 action 的插件类型
         :return: True 表示命中熔断规则，False 表示未命中
         """
-        plugin_type = plugin_type or self.action.action_plugin.get("plugin_type")
+        plugin_type = plugin_type or self.plugin_key
 
         # 创建熔断管理器实例
         circuit_breaking_manager = ActionCircuitBreakingManager()
@@ -278,7 +288,7 @@ class BaseActionProcessor:
 
         :return: True 表示命中熔断规则，False 表示未命中
         """
-        plugin_type = self.action.action_plugin.get("plugin_type")
+        plugin_type = self.plugin_key
 
         # message_queue 类型在创建阶段已经检查过熔断，这里跳过
         if plugin_type == ActionPluginType.MESSAGE_QUEUE:
@@ -502,7 +512,7 @@ class BaseActionProcessor:
             # 开始执行任务的通知，仅在开始执行的时候发送
             try:
                 if (
-                    self.action.action_plugin.get("plugin_type")
+                    self.plugin_key
                     not in [
                         ActionPluginType.NOTICE,
                         ActionPluginType.WEBHOOK,
@@ -667,7 +677,7 @@ class BaseActionProcessor:
 
         self.action.insert_alert_log(notice_way_display=getattr(self, "notice_way_display", ""))
 
-        if self.action.action_plugin.get("plugin_type") != ActionPluginType.NOTICE:
+        if self.plugin_key != ActionPluginType.NOTICE:
             notify_result = self.notify(STATUS_NOTIFY_DICT.get(to_status), need_update_context=True)
             if notify_result:
                 execute_notify_result = self.action.outputs.get("execute_notify_result") or {}
