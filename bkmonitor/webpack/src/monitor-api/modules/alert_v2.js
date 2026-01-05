c18b4c8327a6da17b6e7ee0018f109ff0ad1ed7b
wuw-Mercury <wwnotetip@gmail.com>
2026-01-05 15:00:52 +0800

    feat: 【告警中心】告警关联事件气泡详情 --story=129683771 (#9388)

--- bkmonitor/webpack/src/monitor-api/modules/alert_v2.js
+++ bkmonitor/webpack/src/monitor-api/modules/alert_v2.js
@@ -41,6 +41,7 @@ export const alertDetail = request('GET', 'fta/alert/v2/alert/detail/');
 export const alertEvents = request('POST', 'fta/alert/v2/alert/events/');
 export const alertEventTotal = request('GET', 'fta/alert/v2/alert/event_total/');
 export const alertEventTs = request('POST', 'fta/alert/v2/alert/event_ts/');
+export const alertEventTagDetail = request('POST', 'fta/alert/v2/alert/event_tag_detail/');
 export const alertK8sScenarioList = request('GET', 'fta/alert/v2/alert/k8s_scenario_list/');
 export const alertK8sMetricList = request('GET', 'fta/alert/v2/alert/k8s_metric_list/');
 export const alertK8sTarget = request('GET', 'fta/alert/v2/alert/k8s_target/');
@@ -92,6 +93,7 @@ export default {
   alertEvents,
   alertEventTotal,
   alertEventTs,
+  alertEventTagDetail,
   alertK8sScenarioList,
   alertK8sMetricList,
   alertK8sTarget,
