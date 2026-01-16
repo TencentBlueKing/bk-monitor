e23704dc33bece927b2fcde5eed55fb466e84238
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2026-01-16 10:25:31 +0800

    fix: 1. 解决告警详情接口请求多次的问题 2. 告警详情视图图表移除检测算法类型判断逻辑 (#9492)

--- bkmonitor/webpack/src/trace/pages/alarm-center/common-detail/components/alarm-view/chart-wrapper.tsx
+++ bkmonitor/webpack/src/trace/pages/alarm-center/common-detail/components/alarm-view/chart-wrapper.tsx
@@ -23,27 +23,25 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { type PropType, computed, defineComponent } from 'vue';
+import { type PropType, defineComponent } from 'vue';
 
-import { typeTools } from 'monitor-common/utils';
-import { type IDetectionConfig, MetricType } from 'monitor-pc/pages/strategy-config/strategy-config-set-new/typings';
-
-import AiopsCharts from './echarts/aiops-charts';
 import AlarmCharts from './echarts/alarm-charts';
-import IntelligenceScene from './echarts/intelligence-scene';
-import OutlierDetectionChart from './echarts/outlier-detection-chart';
-import TimeSeriesForecastingChart from './echarts/time-series-forecasting-chart';
 
 import type { AlarmDetail } from '@/pages/alarm-center/typings';
+// import AiopsCharts from './echarts/aiops-charts';
+// import IntelligenceScene from './echarts/intelligence-scene';
+// import OutlierDetectionChart from './echarts/outlier-detection-chart';
+// import TimeSeriesForecastingChart from './echarts/time-series-forecasting-chart';
+// import type { IDetectionConfig } from 'monitor-pc/pages/strategy-config/strategy-config-set-new/typings';
 
 import './chart-wrapper.scss';
 
 /** 算法类型常量 */
-const ALGORITHM_TYPES = {
-  INTELLIGENT_DETECT: 'IntelligentDetect',
-  ABNORMAL_CLUSTER: 'AbnormalCluster',
-  TIME_SERIES_FORECASTING: 'TimeSeriesForecasting',
-} as const;
+// const ALGORITHM_TYPES = {
+//   INTELLIGENT_DETECT: 'IntelligentDetect',
+//   ABNORMAL_CLUSTER: 'AbnormalCluster',
+//   TIME_SERIES_FORECASTING: 'TimeSeriesForecasting',
+// } as const;
 
 export default defineComponent({
   name: 'ChartWrapper',
@@ -55,99 +53,94 @@ export default defineComponent({
   },
   setup(props) {
     /** 是否为主机智能场景检测视图 */
-    const isHostAnomalyDetection = computed(() => {
-      return props.detail?.extra_info?.strategy?.items?.[0]?.algorithms?.[0].type === MetricType.HostAnomalyDetection;
-    });
+    // const isHostAnomalyDetection = computed(() => {
+    //   return props.detail?.extra_info?.strategy?.items?.[0]?.algorithms?.[0].type === MetricType.HostAnomalyDetection;
+    // });
 
-    const detectionConfig = computed<IDetectionConfig>(() => {
-      const strategy = props.detail.extra_info?.strategy;
-      const algorithms = strategy?.items?.[0]?.algorithms;
-      if (!algorithms?.length) return null;
-      const result = {
-        unit: algorithms[0].unit_prefix,
-        // @ts-expect-error
-        unitType: strategy.items?.[0]?.query_configs?.[0]?.unit || '',
-        unitList: [],
-        connector: strategy.detects?.[0]?.connector as 'and' | 'or',
-        data: algorithms.map(({ unit_prefix, ...item }) => displayDetectionRulesConfig(item)),
-        query_configs: strategy?.items?.[0]?.query_configs,
-      };
-      return result;
-    });
+    // const detectionConfig = computed<IDetectionConfig>(() => {
+    //   const strategy = props.detail.extra_info?.strategy;
+    //   const algorithms = strategy?.items?.[0]?.algorithms;
+    //   if (!algorithms?.length) return null;
+    //   const result = {
+    //     unit: algorithms[0].unit_prefix,
+    //     // @ts-expect-error
+    //     unitType: strategy.items?.[0]?.query_configs?.[0]?.unit || '',
+    //     unitList: [],
+    //     connector: strategy.detects?.[0]?.connector as 'and' | 'or',
+    //     data: algorithms.map(({ unit_prefix, ...item }) => displayDetectionRulesConfig(item)),
+    //     query_configs: strategy?.items?.[0]?.query_configs,
+    //   };
+    //   return result;
+    // });
 
     /** 是否含有智能检测算法 */
-    const hasAIOpsDetection = computed(
-      () =>
-        hasAlgorithmType(ALGORITHM_TYPES.INTELLIGENT_DETECT) &&
-        detectionConfig.value?.query_configs?.[0]?.intelligent_detect?.result_table_id
-    );
+    // const hasAIOpsDetection = computed(
+    //   () =>
+    //     hasAlgorithmType(ALGORITHM_TYPES.INTELLIGENT_DETECT) &&
+    //     detectionConfig.value?.query_configs?.[0]?.intelligent_detect?.result_table_id
+    // );
 
     /** 是否含有离群检测算法 */
-    const hasOutlierDetection = computed(() => hasAlgorithmType(ALGORITHM_TYPES.ABNORMAL_CLUSTER));
+    // const hasOutlierDetection = computed(() => hasAlgorithmType(ALGORITHM_TYPES.ABNORMAL_CLUSTER));
 
     /** 是否含有时序预测算法 */
-    const hasTimeSeriesForecasting = computed(() => hasAlgorithmType(ALGORITHM_TYPES.TIME_SERIES_FORECASTING));
+    // const hasTimeSeriesForecasting = computed(() => hasAlgorithmType(ALGORITHM_TYPES.TIME_SERIES_FORECASTING));
 
     /**
      * @description 检查是否包含指定类型的算法
      * @param {string} type 算法类型
      * @returns 是否包含该类型算法
      */
-    const hasAlgorithmType = (type: string) => detectionConfig.value?.data?.some?.(item => item.type === type);
+    // const hasAlgorithmType = (type: string) => detectionConfig.value?.data?.some?.(item => item.type === type);
 
     /**
      * @description 处理检测规则配置，对配置项进行标准化处理
      * @param item 检测算法配置项
      * @returns 处理后的配置项
      */
-    const displayDetectionRulesConfig = item => {
-      const { config } = item;
-      if (item.type === ALGORITHM_TYPES.INTELLIGENT_DETECT && !config.anomaly_detect_direct) {
-        config.anomaly_detect_direct = 'all';
-      }
-      if (typeTools.isArray(config)) return item;
+    // const displayDetectionRulesConfig = item => {
+    //   const { config } = item;
+    //   if (item.type === ALGORITHM_TYPES.INTELLIGENT_DETECT && !config.anomaly_detect_direct) {
+    //     config.anomaly_detect_direct = 'all';
+    //   }
+    //   if (typeTools.isArray(config)) return item;
 
-      for (const key of Object.keys(config)) {
-        if (config[key] === null) config[key] = '';
-      }
-      return item;
-    };
+    //   for (const key of Object.keys(config)) {
+    //     if (config[key] === null) config[key] = '';
+    //   }
+    //   return item;
+    // };
 
     /**
      * @description 根据告警类型获取对应的图表组件
      * @returns 对应的图表组件 JSX
      */
     const getSeriesViewComponent = () => {
-      // 主机智能场景检测
-      if (isHostAnomalyDetection.value) {
-        console.log('主机智能场景检测');
-        return <IntelligenceScene detail={props.detail} />;
-      }
-      // 智能检测算法图表
-      if (hasAIOpsDetection.value) {
-        console.log('智能检测算法图表');
-        return (
-          <AiopsCharts
-            detail={props.detail}
-            detectionConfig={detectionConfig.value}
-          />
-        );
-      }
-      // 时序预测图表
-      if (hasTimeSeriesForecasting.value) {
-        console.log('时序预测图表');
-        return (
-          <TimeSeriesForecastingChart
-            detail={props.detail}
-            detectionConfig={detectionConfig.value}
-          />
-        );
-      }
-      if (hasOutlierDetection.value) {
-        console.log('离群检测算法');
-        return <OutlierDetectionChart detail={props.detail} />;
-      }
-      console.log('dddddddddddddd');
+      // // 主机智能场景检测
+      // if (isHostAnomalyDetection.value) {
+      //   return <IntelligenceScene detail={props.detail} />;
+      // }
+      // // 智能检测算法图表
+      // if (hasAIOpsDetection.value) {
+      //   return (
+      //     <AiopsCharts
+      //       detail={props.detail}
+      //       detectionConfig={detectionConfig.value}
+      //     />
+      //   );
+      // }
+      // // 时序预测图表
+      // if (hasTimeSeriesForecasting.value) {
+      //   return (
+      //     <TimeSeriesForecastingChart
+      //       detail={props.detail}
+      //       detectionConfig={detectionConfig.value}
+      //     />
+      //   );
+      // }
+      // if (hasOutlierDetection.value) {
+      //   return <OutlierDetectionChart detail={props.detail} />;
+      // }
       return (
         <div class='series-view-container'>
           <AlarmCharts detail={props.detail} />
