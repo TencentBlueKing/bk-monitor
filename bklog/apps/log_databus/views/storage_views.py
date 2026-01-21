b459a5cadc400f410ef7e9928348947a50f435fb
Hywabwm <Hywabwm@outlook.com>
2026-01-21 11:11:46 +0000

    feat: 创建集群支持自定义集群英文名 #1010158081130217322

--- bklog/apps/log_databus/views/storage_views.py
+++ bklog/apps/log_databus/views/storage_views.py
@@ -298,8 +298,6 @@ class StorageViewSet(APIViewSet):
         @apiName create_storage
         @apiGroup 09_StorageCluster
         @apiParam {String} cluster_name 集群名称
-        @apiParam {String} [bkbase_cluster_en_name] 集群英文名
-        @apiParam {String} cluster_name 集群英文名称
         @apiParam {String} display_name 集群名称
         @apiParam {String} domain_name 集群域名（可以填入IP）
         @apiParam {Int} port 端口
