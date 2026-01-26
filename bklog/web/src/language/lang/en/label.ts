2cbb6609cd8ce071c0e9ba9070f7045de38ce43e
cheng2023112 <122533271+cheng2023112@users.noreply.github.com>
2026-01-26 18:17:01 +0800

    feat: 新建集群功能调整 #1010158081130383509 # Reviewed, transaction id: 72686

--- bklog/web/src/language/lang/en/label.ts
+++ bklog/web/src/language/lang/en/label.ts
@@ -951,4 +951,5 @@ export default {
   未上传: 'Not uploaded',
   英文名称: 'English name',
   '搜索名称、英文名称、地址、创建人': 'Search by name, English name, address, or creator',
+  集群英文名称: 'Cluster English Name',
 };
