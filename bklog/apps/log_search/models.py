78fb448db0b89ab1bfc1dccaf70402a1322b8a9f
JunLong-Lin <398016653@qq.com>
2026-02-27 02:28:53 +0000

    feat: esquery支持索引组 #1010158081131655802

--- bklog/apps/log_search/models.py
+++ bklog/apps/log_search/models.py
@@ -478,19 +478,6 @@ class LogIndexSet(SoftDeleteModel):
 
         return [int(child_id) for child_id in child_ids]
 
-    def get_doris_table_ids(self) -> list:
-        doris_table_ids = []
-        if self.is_group:
-            # 如果是索引组，获取所有子索引集的doris_table_id
-            child_index_set_ids = self.get_child_index_set_ids()
-            child_index_sets = LogIndexSet.objects.filter(index_set_id__in=child_index_set_ids)
-            for child_index_set in child_index_sets:
-                if child_index_set.doris_table_id:
-                    doris_table_ids.extend(child_index_set.doris_table_id.split(","))
-        elif self.doris_table_id:
-            doris_table_ids.extend(self.doris_table_id.split(","))
-        return doris_table_ids
-
     @staticmethod
     def no_data_check_time(index_set_id: str):
         result = cache.get(INDEX_SET_NO_DATA_CHECK_PREFIX + index_set_id)
@@ -504,7 +491,11 @@ class LogIndexSet(SoftDeleteModel):
         返回当前索引集下的索引列表
         :return:
         """
-        index_set_data = LogIndexSetData.objects.filter(index_set_id=self.index_set_id)
+        if self.is_group:
+            child_index_set_ids = self.get_child_index_set_ids()
+            index_set_data = LogIndexSetData.objects.filter(index_set_id__in=child_index_set_ids)
+        else:
+            index_set_data = LogIndexSetData.objects.filter(index_set_id=self.index_set_id)
         if has_applied:
             index_set_data = index_set_data.filter(apply_status=LogIndexSetData.Status.NORMAL)
         source_name = self.source_name
