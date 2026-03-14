8da58474a80340fe50f22392d648ad54b01a233c
xiaoy <1103098607@qq.com>
2026-03-14 22:25:35 +0800

    fix(metadata): import datalink module in task __init__ to register apply_apm_datalink celery task

--- bkmonitor/metadata/task/__init__.py
+++ bkmonitor/metadata/task/__init__.py
@@ -11,3 +11,4 @@ specific language governing permissions and limitations under the License.
 
 from .custom_report import *  # noqa
 from .config_refresh import *  # noqa
+from .datalink import *  # noqa
