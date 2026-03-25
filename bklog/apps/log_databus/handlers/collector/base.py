b43f80175f5cb4fe1a8b11fd929967e4e2b7f6c8
Hywabwm <Hywabwm@outlook.com>
2026-03-25 11:25:37 +0800

    feat: 存储集群列表接入 Doris 集群 --story=131219897 (#10034)

--- bklog/apps/log_databus/handlers/collector/base.py
+++ bklog/apps/log_databus/handlers/collector/base.py
@@ -61,6 +61,7 @@ from apps.log_databus.constants import (
     RunStatus,
     RETRIEVE_CHAIN,
     Environment,
+    STORAGE_CLUSTER_TYPE,
 )
 from apps.log_databus.exceptions import (
     CollectNotSuccess,
@@ -510,6 +511,7 @@ class CollectorHandler:
         etl_params=None,
         fields=None,
         storage_cluster_id=None,
+        storage_cluster_type=STORAGE_CLUSTER_TYPE,
         retention=7,
         allocation_min_days=0,
         storage_replies=1,
@@ -588,6 +590,7 @@ class CollectorHandler:
             etl_params = {
                 "table_id": table_id_name,
                 "storage_cluster_id": storage_cluster_id,
+                "storage_cluster_type": storage_cluster_type,
                 "retention": retention,
                 "es_shards": es_shards,
                 "allocation_min_days": allocation_min_days,
@@ -1325,6 +1328,7 @@ class CollectorHandler:
         etl_params=None,
         fields=None,
         storage_cluster_id=None,
+        storage_cluster_type=STORAGE_CLUSTER_TYPE,
         retention=7,
         allocation_min_days=0,
         storage_replies=1,
@@ -1425,6 +1429,7 @@ class CollectorHandler:
             params = {
                 "table_id": collector_config_name_en,
                 "storage_cluster_id": storage_cluster_id,
+                "storage_cluster_type": storage_cluster_type,
                 "retention": retention,
                 "allocation_min_days": allocation_min_days,
                 "storage_replies": storage_replies,
