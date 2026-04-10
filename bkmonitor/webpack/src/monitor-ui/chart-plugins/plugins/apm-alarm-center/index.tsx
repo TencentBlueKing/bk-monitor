0cb10b5b0fc028a2a52e0bc388c3a4cbe54f2ca8
Austin <1344583166@qq.com>
2026-04-10 17:02:35 +0800

    feat: 【APM】应用支持告警中心功能 --Story=133016626 (#10255)
    # Reviewed, transaction id: 78139

--- bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/apm-alarm-center/index.tsx
+++ bkmonitor/webpack/src/monitor-ui/chart-plugins/plugins/apm-alarm-center/index.tsx
@@ -23,7 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { Component, InjectReactive, Watch } from 'vue-property-decorator';
+import { Component, InjectReactive, Watch, Inject } from 'vue-property-decorator';
 import { Component as tsc } from 'vue-tsx-support';
 import QuickAddStrategy from 'apm/pages/alarm-template/quick-add-strategy/quick-add-strategy';
 import AlarmCenter from './alarm-center';
@@ -38,6 +38,11 @@ import './index.scss';
 export default class ApmAlarmCenter extends tsc<any, any> {
   // 图表特殊参数
   @InjectReactive('viewOptions') readonly viewOptions!: IViewOptions;
+  // dashboardId
+  @Inject('handlePageTabChange') handlePageTabChange: (
+    id: string,
+    customRouterQuery: Record<string, number | string>
+  ) => void;
   // 图表的数据时间间隔
   // @InjectReactive('timeRange') readonly timeRange: [string, string];
   // 告警关联下拉是否展示
@@ -69,6 +74,10 @@ export default class ApmAlarmCenter extends tsc<any, any> {
 
   /** 由内置筛选接口生成的查询串，传给嵌入的告警中心作为初始条件 */
   queryString = '';
+  /** 本地缓存的ui查询条件 */
+  localConditions: unknown[] = [];
+  /** 本地缓存的语句模式查询串 */
+  localQueryString = '';
   /** ui条件转换成的语句模式查询串 */
   conditionsToQueryString = '';
 
@@ -78,6 +87,11 @@ export default class ApmAlarmCenter extends tsc<any, any> {
       .filter(item => item.checked)
       .map(item => item.name)
       .join(', ');
+  }
+
+  /** 是否是 APM 服务 */
+  get isApmService() {
+    return this.viewOptions.filters?.app_name && this.viewOptions.filters?.service_name;
   }
 
   @Watch('alarmRelateList', { immediate: true, deep: true })
@@ -135,6 +149,7 @@ export default class ApmAlarmCenter extends tsc<any, any> {
     try {
       const data = await alertBuiltinFilter(params);
       this.queryString = data.query_string;
+      this.conditionsToQueryString = this.queryString;
     } finally {
       this.$nextTick(() => {
         this.pageLoading = false;
@@ -154,19 +169,44 @@ export default class ApmAlarmCenter extends tsc<any, any> {
   }
 
   /** 嵌入告警中心（V3）透出的事件；conditionChange 为 UI 条件变化，其余可扩展处理查询变更等 */
-  async handleV3EventChange(eventName: string, params: unknown) {
+  async handleV3EventChange(eventName: string, params: string | unknown[]) {
+    if (eventName === 'filterModeChange') {
+      if (params === 'ui') {
+        // 语句模式切换为ui模式
+        const queryString = await generateQueryString({
+          conditions: this.localConditions,
+        });
+        this.conditionsToQueryString = `${this.queryString} AND (${queryString})`;
+      } else {
+        // ui模式切换为语句模式
+        this.conditionsToQueryString = `${this.queryString} AND (${this.localQueryString})`;
+      }
+      return;
+    }
+    if (params.length === 0) {
+      this.conditionsToQueryString = this.queryString;
+      return;
+    }
     if (eventName === 'conditionChange') {
+      this.localConditions = params as unknown[];
       const queryString = await generateQueryString({
         conditions: params,
       });
-      this.conditionsToQueryString = queryString;
-      return
+      this.conditionsToQueryString = `${this.queryString} AND (${queryString})`;
+      return;
     }
-    this.conditionsToQueryString = params as string;
+    this.localQueryString = params as string;
+    this.conditionsToQueryString = `${this.queryString} AND (${params as string})`;
+  }
+
+  handleAddAlarmStrategy() {
+    this.handlePageTabChange('alarm_template', {});
   }
 
   created() {
-    this.getAlarmStatus();
+    if (this.isApmService) {
+      this.getAlarmStatus();
+    }
   }
 
   render() {
@@ -177,36 +217,39 @@ export default class ApmAlarmCenter extends tsc<any, any> {
       >
         <div class='apm-alarm-center-header'>
           <div class='header-left'>
-            <div class='alarm-relate-main'>
-              <span>{this.$t('告警关联')}：</span>
-              <bk-dropdown-menu
-                trigger='click'
-                onHide={() => this.handleDropdownShow(false)}
-                onShow={() => this.handleDropdownShow(true)}
-              >
-                <span slot='dropdown-trigger'>
-                  <span class='alarm-relate-text'>{this.alarmRelateListCheckedDisplay}</span>
-                  <i class={['bk-icon icon-angle-down', { 'icon-flip': this.isDropdownShow }]} />
-                </span>
-                <ul
-                  class='bk-dropdown-list alarm-relate-dropdown-list'
-                  slot='dropdown-content'
+            {this.isApmService && (
+              <div class='alarm-relate-main'>
+                <span>{this.$t('告警关联')}：</span>
+                <bk-dropdown-menu
+                  trigger='click'
+                  onHide={() => this.handleDropdownShow(false)}
+                  onShow={() => this.handleDropdownShow(true)}
                 >
-                  {this.alarmRelateList.map(item => (
-                    <li
-                      key={item.id}
-                      class='alarm-relate-dropdown-item'
-                    >
-                      <bk-checkbox
-                        v-model={item.checked}
-                        disabled={item.id === 'APM-SERVICE'}>
-                        {item.name}
-                      </bk-checkbox>
-                    </li>
-                  ))}
-                </ul>
-              </bk-dropdown-menu>
-            </div>
+                  <span slot='dropdown-trigger'>
+                    <span class='alarm-relate-text'>{this.alarmRelateListCheckedDisplay}</span>
+                    <i class={['bk-icon icon-angle-down', { 'icon-flip': this.isDropdownShow }]} />
+                  </span>
+                  <ul
+                    class='bk-dropdown-list alarm-relate-dropdown-list'
+                    slot='dropdown-content'
+                  >
+                    {this.alarmRelateList.map(item => (
+                      <li
+                        key={item.id}
+                        class='alarm-relate-dropdown-item'
+                      >
+                        <bk-checkbox
+                          v-model={item.checked}
+                          disabled={item.id === 'APM-SERVICE'}
+                        >
+                          {item.name}
+                        </bk-checkbox>
+                      </li>
+                    ))}
+                  </ul>
+                </bk-dropdown-menu>
+              </div>
+            )}
             <span
               class='alarm-center-jump-main'
               onClick={this.handleToAlarmCenter}
@@ -215,31 +258,43 @@ export default class ApmAlarmCenter extends tsc<any, any> {
               <span>{this.$t('告警中心')}</span>
             </span>
           </div>
-          <div class='header-right'>
-            <span
-              style='margin-right: 18px;'
-              onClick={this.handleToStrategyListAndEvnetCenter}
-            >
-              <i18n
-                path='该服务已关联{0}个告警策略'
-                tag='span'
+          {this.isApmService ? (
+            <div class='header-right'>
+              <span
+                style='margin-right: 18px;'
+                onClick={this.handleToStrategyListAndEvnetCenter}
               >
-                <span
-                  v-bk-tooltips={{ content: this.$t('查看策略列表') }}
-                  style='color: #3a84ff;font-weight: 700;margin: 0 4px;'
+                <i18n
+                  path='该服务已关联{0}个告警策略'
+                  tag='span'
                 >
-                  {this.strategyCount}
-                </span>
-              </i18n>
-            </span>
-            <span
-              class='add-strategy-main'
-              onClick={() => this.handleShowQuickAddStrategyChange(true)}
+                  <span
+                    v-bk-tooltips={{ content: this.$t('查看策略列表') }}
+                    style='color: #3a84ff;font-weight: 700;margin: 0 4px;'
+                  >
+                    {this.strategyCount}
+                  </span>
+                </i18n>
+              </span>
+              <span
+                class='add-strategy-main'
+                onClick={() => this.handleShowQuickAddStrategyChange(true)}
+              >
+                <i class='icon-monitor icon-gaojing2' />
+                <span>{this.$t('一键添加策略')}</span>
+              </span>
+            </div>
+          ) : (
+            <div
+              class='header-right'
+              onClick={this.handleAddAlarmStrategy}
             >
-              <i class='icon-monitor icon-gaojing2' />
-              <span>{this.$t('一键添加策略')}</span>
-            </span>
-          </div>
+              <span class='add-strategy-main'>
+                <i class='icon-monitor icon-mc-add-strategy' />
+                <span>{this.$t('添加告警策略')}</span>
+              </span>
+            </div>
+          )}
         </div>
         <div
           class='apm-alarm-center-content'
