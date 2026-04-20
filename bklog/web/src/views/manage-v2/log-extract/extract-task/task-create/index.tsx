4c14ea3b17e217972f1ca15f1cf509d0466d9798
xintaoLi <li18566230175@gmail.com>
2026-04-20 11:12:16 +0800

    feat: update ip-selector panel-list

--- bklog/web/src/views/manage-v2/log-extract/extract-task/task-create/index.tsx
+++ bklog/web/src/views/manage-v2/log-extract/extract-task/task-create/index.tsx
@@ -422,7 +422,7 @@ export default defineComponent({
               height={670}
               mode='dialog'
               original-value={ipSelectorOriginalValue.value}
-              panel-list={['staticTopo', 'dynamicTopo', 'serviceTemplate']}
+              panel-list={['staticTopo', 'dynamicTopo']}
               show-dialog={showSelectDialog.value}
               show-view-diff={isClone.value}
               value={selectorNodes.value}
