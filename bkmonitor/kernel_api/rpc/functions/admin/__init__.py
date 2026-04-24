1a0a4a8179215aa4415be9b738bca5dd00df22da
unique0lai <11598235+unique0lai@users.noreply.github.com>
2026-04-24 22:33:34 +0800

    feat: rpc增加admin支持接口

--- bkmonitor/kernel_api/rpc/functions/admin/__init__.py
+++ bkmonitor/kernel_api/rpc/functions/admin/__init__.py
@@ -8,6 +8,6 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from . import datasource, result_table
+from . import datasource, result_table, tenant
 
-__all__ = ["datasource", "result_table"]
+__all__ = ["datasource", "result_table", "tenant"]
