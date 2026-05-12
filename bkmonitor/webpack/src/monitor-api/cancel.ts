ceecfba9e3393552f2e5263188505ae203232a80
liangling0628 <lingliang1100@gmail.com>
2026-05-12 15:09:35 +0800

    feat: CVE 工单依赖更新 --Story=134213684 # Reviewed, transaction id: 79997

--- bkmonitor/webpack/src/monitor-api/cancel.ts
+++ bkmonitor/webpack/src/monitor-api/cancel.ts
@@ -23,6 +23,5 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import CancelToken from 'axios/lib/cancel/CancelToken';
-
-export { CancelToken };
+import axios from 'axios';
+export const CancelToken = axios.CancelToken;
