18eaf5f21620f457e3ddbe8b7226126b8e874d1b
cheng2023112 <122533271+cheng2023112@users.noreply.github.com>
2026-05-25 17:49:01 +0800

    feat: 展示ext扩展信息字段

--- bklog/web/src/views/client-log-search/log-detail-panel/index.tsx
+++ bklog/web/src/views/client-log-search/log-detail-panel/index.tsx
@@ -912,6 +912,14 @@ export default defineComponent({
             )}
           </header>
 
+          {props.selectedLogItem?.ext && (
+            <div class='ext-content'>
+              <i class='bklog-icon bklog-miaoshu'></i>
+              <span class='ext-label'>{t('扩展信息')}:</span>
+              <span class='ext-value'>{props.selectedLogItem.ext}</span>
+            </div>
+          )}
+
           {renderStatusContent()}
         </div>
       </div>
