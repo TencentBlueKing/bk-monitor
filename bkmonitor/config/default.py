19f827e38c7f868220b0658aaf94d0bd4da603f0
liuwenping <haha.rober@gmail.com>
2026-06-18 17:22:56 +0800

    feat: APM Metric 默认使用 BKData V4 链路，且开启指标维度分组 --story=135369013 (#11145)
    close #1010158081135369013

--- bkmonitor/config/default.py
+++ bkmonitor/config/default.py
@@ -1794,6 +1794,9 @@ APM_SERVICE_CACHE_APPLICATIONS = []
 # 企业微信模块化（layouts）消息通知灰度业务列表
 WECOM_LAYOUTS_BIZ_LIST = []
 
+# 是否默认开启 APM 指标维度分组接入
+APM_METRIC_GROUP_DIMENSIONS_ENABLED = False
+
 # 允许 APM 配置指标分组维度的白名单，格式：["2"](整业务) 或 ["2-app_name"](单应用)
 APM_METRIC_GROUP_DIMENSIONS_WHITELIST = []
 
