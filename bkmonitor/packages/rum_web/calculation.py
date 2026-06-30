24d93948c8943c279882f01a3ba86c559bdb1d2d
joker-joker-yuan <blackpig163@163.com>
2026-06-30 08:35:19 +0000

    fix: 移除无用代码 # Reviewed, transaction id: 82624

--- bkmonitor/packages/rum_web/calculation.py
+++ bkmonitor/packages/rum_web/calculation.py
@@ -8,12 +8,6 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from collections import defaultdict
-
-from opentelemetry.trace import StatusCode
-
-from rum_web.constants import Apdex
-
 
 class Calculation:
     @classmethod
@@ -34,85 +28,3 @@ class Calculation:
     @classmethod
     def calculate(cls, *data):
         return data[0]
-
-
-class ApdexCalculation(Calculation):
-    SATISFIED_RATE = 0.75
-    TOLERATING_RATE = 0.5
-    FRUSTRATING_RATE = 0.25
-
-    @classmethod
-    def instance_cal(cls, metric_result):
-        return cls.common_unify_result_cal(metric_result)
-
-    @classmethod
-    def range_cal(cls, metric_result):
-        """
-        Apdex: (满意数 + 可容忍 / 2) / 总样本量
-        """
-        if not metric_result:
-            return {"metrics": [], "series": []}
-        datapoint_map = defaultdict(lambda: {"satisfied": 0, "tolerating": 0, "frustrated": 0})
-        for item in metric_result.get("series", []):
-            if Apdex.DIMENSION_KEY not in item["dimensions"]:
-                continue
-            apdex_type = item["dimensions"][Apdex.DIMENSION_KEY]
-            for point in item.get("datapoints"):
-                if not point[0]:
-                    continue
-                datapoint_map[point[1]][apdex_type] += point[0]
-
-        res = []
-        for timestamp in sorted(datapoint_map.keys()):
-            info = datapoint_map[timestamp]
-
-            satisfied_count = info["satisfied"]
-            tolerating_count = info["tolerating"]
-            frustrated_count = info["frustrated"]
-
-            total = satisfied_count + tolerating_count + frustrated_count
-            if not total:
-                # 无数据不显示此柱
-                res.append((None, timestamp))
-            else:
-                res.append((round((satisfied_count + tolerating_count) / total, 2), timestamp))
-
-        return {
-            "metrics": [],
-            "series": [{"datapoints": res, "dimensions": {}, "target": "apdex", "type": "bar", "unit": ""}],
-        }
-
-    @classmethod
-    def common_unify_result_cal(cls, metric_result):
-        if not metric_result:
-            return None
-
-        error_count = sum(i["_result_"] for i in metric_result if i.get("status_code") == str(StatusCode.ERROR.value))
-        total_count = 0
-        satisfied_count = 0
-        tolerating_count = 0
-        frustrated_count = 0
-        for serie in metric_result:
-            if Apdex.DIMENSION_KEY not in serie:
-                continue
-            total_count += serie["_result_"]
-            if serie[Apdex.DIMENSION_KEY] == Apdex.SATISFIED:
-                satisfied_count += serie["_result_"]
-            if serie[Apdex.DIMENSION_KEY] == Apdex.TOLERATING:
-                tolerating_count += serie["_result_"]
-            if serie[Apdex.DIMENSION_KEY] == Apdex.FRUSTRATED:
-                frustrated_count += serie["_result_"]
-        return cls.calculate(satisfied_count, tolerating_count, frustrated_count, error_count, total_count)
-
-    @classmethod
-    def calculate(cls, *data):
-        satisfied_count, tolerating_count, frustrated_count, error_count, total_count = data
-        if not total_count:
-            return None
-
-        apdex_rate = (satisfied_count * 1 + tolerating_count * 0.5 + (tolerating_count + error_count) * 0) / total_count
-        if apdex_rate > cls.SATISFIED_RATE:
-            return Apdex.SATISFIED
-        if apdex_rate > cls.TOLERATING_RATE:
-            return Apdex.TOLERATING
-        return Apdex.FRUSTRATED
