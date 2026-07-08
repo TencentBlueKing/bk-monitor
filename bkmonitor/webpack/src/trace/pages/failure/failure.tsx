9281fdbc36623ab5bab199baacf7cd571d5da9bd
hongq <1070293319@qq.com>
2026-07-08 15:34:19 +0800

    feat: 提供按服务相似性聚合的能力 --story=127565073 (#11415)

--- bkmonitor/webpack/src/trace/pages/failure/failure.tsx
+++ bkmonitor/webpack/src/trace/pages/failure/failure.tsx
@@ -149,6 +149,7 @@ export default defineComponent({
     const operationTypes = deepRef([]);
     const refContent = deepRef<InstanceType<typeof FailureContent>>();
     const failureNavRef = deepRef<InstanceType<typeof FailureNav>>();
+    const failureHeaderRef = deepRef<InstanceType<typeof FailureHeader>>();
     const topoNodeId = deepRef<string>();
     const incidentResultList = deepRef({});
     const incidentResultStatus = deepRef('');
@@ -444,6 +445,12 @@ export default defineComponent({
     const handleCollapseChange = val => {
       isCollapsed.value = val;
     };
+    const handleOpenEdit = () => {
+      if (failureHeaderRef.value) {
+        failureHeaderRef.value.autoFocusReason = true;
+        failureHeaderRef.value.isShow = true;
+      }
+    };
     return {
       incidentDetailData,
       getIncidentDetail,
@@ -476,6 +483,8 @@ export default defineComponent({
       alarmDetailShow,
       alarmDetailData,
       idResolving,
+      handleOpenEdit,
+      failureHeaderRef,
     };
   },
   render() {
@@ -491,10 +500,14 @@ export default defineComponent({
         class='failure-wrapper'
         tabindex='0'
       >
-        <FailureHeader onEditSuccess={this.getIncidentDetail} />
+        <FailureHeader
+          ref='failureHeaderRef'
+          onEditSuccess={this.getIncidentDetail}
+        />
         <FailureTags
           onChooseNode={this.nodeClick}
           onChooseTag={this.handleChooseTag}
+          onOpenEdit={this.handleOpenEdit}
           onToSpan={this.handleToSpan}
         />
         <ResizeLayout
