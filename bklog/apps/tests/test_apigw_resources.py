cd8a0663b3ecf95015a969204916c83604a96288
Hywabwm <Hywabwm@outlook.com>
2026-08-03 08:43:54 +0000

    feat: 【日志平台】放开日志聚类标注API接口 #1010158081136559620

--- bklog/apps/tests/test_apigw_resources.py
+++ bklog/apps/tests/test_apigw_resources.py
@@ -39,12 +39,15 @@ PUBLIC_RESOURCES = {
     ("POST", "/esquery_search/"),
     ("POST", "/index_set/"),
     ("POST", "/pattern/{index_set_id}/search/"),
+    ("POST", "/pattern/{index_set_id}/remark/"),
     ("POST", "/query/ts/"),
     ("POST", "/query/ts/raw/"),
     ("POST", "/query/ts/reference/"),
     ("PUT", "/index_set/{index_set_id}/"),
+    ("PUT", "/pattern/{index_set_id}/update_remark/"),
     ("DELETE", "/databus_collectors/{collector_config_id}/"),
     ("DELETE", "/index_set/{index_set_id}/"),
+    ("DELETE", "/pattern/{index_set_id}/delete_remark/"),
 }
 
 
