ca1bc939b4eeadcd36e686075512fb3d3ac3c5e5
jayjiahua <553544693@qq.com>
2026-08-10 14:39:58 +0800

    feat: 【验收问题】采集接入列表补齐 is_display 过滤，客户端日志采集项不再展示 --story=136895130

--- bklog/apps/log_databus/handlers/collector_handler/log.py
+++ bklog/apps/log_databus/handlers/collector_handler/log.py
@@ -481,11 +481,12 @@ class LogCollectorHandler:
             # 非日志采集查询，直接返回
             return []
 
+        # 采集插件通过 is_display 批量隐藏旗下采集项，且默认不可见，列表须与旧接口口径一致
         if self.space_type_id == SpaceTypeEnum.BKCC.value and include_related_spaces:
             query_bk_biz_ids = self.get_query_ids_by_collector_source(collector_source, is_bk_biz_id=True)
-            qs = CollectorConfig.objects.filter(bk_biz_id__in=query_bk_biz_ids)
+            qs = CollectorConfig.objects.filter(bk_biz_id__in=query_bk_biz_ids, is_display=True)
         else:
-            qs = CollectorConfig.objects.filter(bk_biz_id=self.bk_biz_id)
+            qs = CollectorConfig.objects.filter(bk_biz_id=self.bk_biz_id, is_display=True)
 
         if exclude_not_completed:
             qs = qs.filter(table_id__isnull=False)
@@ -917,7 +918,7 @@ class LogCollectorHandler:
 
     def get_collector_count(self):
         """获取采集项总数"""
-        collector_count = CollectorConfig.objects.filter(bk_biz_id=self.bk_biz_id).count()
+        collector_count = CollectorConfig.objects.filter(bk_biz_id=self.bk_biz_id, is_display=True).count()
         index_set_count = (
             LogIndexSet.objects.filter(collector_config_id__isnull=True, space_uid=self.space_uid)
             .exclude(scenario_id=Scenario.LOG)
@@ -974,14 +975,15 @@ class LogCollectorHandler:
         :param include_related_spaces: 是否包含关联空间中的采集项
         :return: 包含创建人和更新人枚举值的字典
         """
+        # 枚举须与列表同源，否则筛选项里会出现列表中不存在的采集项
         if self.space_type_id == SpaceTypeEnum.BKCC.value and include_related_spaces:
-            query_collector_condition = {"bk_biz_id__in": self.all_related_bk_biz_ids}
+            query_collector_condition = {"bk_biz_id__in": self.all_related_bk_biz_ids, "is_display": True}
             query_index_set_condition = {
                 "collector_config_id__isnull": True,
                 "space_uid__in": self.all_related_space_uids,
             }
         else:
-            query_collector_condition = {"bk_biz_id": self.bk_biz_id}
+            query_collector_condition = {"bk_biz_id": self.bk_biz_id, "is_display": True}
             query_index_set_condition = {"collector_config_id__isnull": True, "space_uid": self.space_uid}
 
         collector_fields = list(
