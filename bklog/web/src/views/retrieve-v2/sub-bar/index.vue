771105b4a8790c8f365b096082e87356f00ca526
cheng2023112 <122533271+cheng2023112@users.noreply.github.com>
2026-08-11 16:35:32 +0800

    feat: 【日志平台】PO环境不展示IaaS化tab和请求 #1010158081137030102

--- bklog/web/src/views/retrieve-v2/sub-bar/index.vue
+++ bklog/web/src/views/retrieve-v2/sub-bar/index.vue
@@ -54,6 +54,7 @@ const router = useRouter();
 const store = useStore();
 
 const isSceneMode = computed(() => store.getters.isSceneMode);
+const isExternal = computed(() => store.state.isExternal);
 const isSceneRetrieveEnabled = computed(() => !isExternal.value && isFeatureToggleOn('scene_search', [String(store.state.bkBizId), String(store.state.spaceUid)]));
 const retrieveType = computed(() => (isSceneMode.value ? RetrieveType.Scene : RetrieveType.Normal));
 const fieldSettingRef = ref(null);
@@ -106,8 +107,6 @@ const hasCollectorConfigId = computed(() => {
   const currentIndexSet = flatIndexSetList.value.find(item => item.index_set_id == indexSetId);
   return currentIndexSet?.collector_config_id;
 });
-
-const isExternal = computed(() => store.state.isExternal);
 
 const isFieldSettingShow = computed(() => {
   return !store.getters.isUnionSearch && !isExternal.value;
