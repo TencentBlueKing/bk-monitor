ed8b314da9da6036e05a1b7ad383db584ddfd73a
unique0lai <11598235+unique0lai@users.noreply.github.com>
2026-08-23 21:25:30 +0800

    chore: kernel_rpc支持v4指标迁移页面

--- bkmonitor/kernel_api/rpc/tests/test_admin_rpc.py
+++ bkmonitor/kernel_api/rpc/tests/test_admin_rpc.py
@@ -132,6 +132,7 @@ def test_admin_rpc_functions_registered_by_builtin_loader():
 
     assert {
         "admin.datasource.list",
+        "admin.datasource.metric_migration_list",
         "admin.datasource.detail",
         "admin.space.list",
         "admin.space.detail",
