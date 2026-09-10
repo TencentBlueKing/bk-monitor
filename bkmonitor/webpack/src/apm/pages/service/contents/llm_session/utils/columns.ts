249ecea53e47adf5484d5f63f3da3be87558ced2
Austin <1344583166@qq.com>
2026-09-10 10:45:21 +0800

    feat: LLM会话支持查看Trace详情 --Story=138108337 # Reviewed, transaction id: 85604

--- bkmonitor/webpack/src/apm/pages/service/contents/llm_session/utils/columns.ts
+++ bkmonitor/webpack/src/apm/pages/service/contents/llm_session/utils/columns.ts
@@ -1,3 +1,28 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) available.
+ *
+ * Copyright (C) 2017-2025 Tencent.  All rights reserved.
+ *
+ * 蓝鲸智云PaaS平台 (BlueKing PaaS) is licensed under the MIT License.
+ *
+ * License for 蓝鲸智云PaaS平台 (BlueKing PaaS):
+ *
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 import type { ILlmColumn } from '../typings';
 
 /**
@@ -13,7 +38,7 @@ export function getSessionColumns(): ILlmColumn[] {
     {
       id: 'sessionId',
       label: window.i18n.tc('会话 ID'),
-      cellType: 'link',
+      cellType: 'text',
       className: 'is-no-padding-left',
       minWidth: 240,
     },
@@ -49,7 +74,7 @@ export function getTraceColumns(): ILlmColumn[] {
   return [
     { id: 'traceId', label: 'Trace ID', cellType: 'link', minWidth: 260 },
     { id: 'userId', label: 'User ID', cellType: 'text', width: 99 },
-    { id: 'sessionId', label: window.i18n.tc('会话 ID'), cellType: 'link', width: 180 },
+    { id: 'sessionId', label: window.i18n.tc('会话 ID'), cellType: 'text', width: 180 },
     { id: 'startTimeText', label: window.i18n.tc('开始时间'), cellType: 'text', width: 156, sortField: 'start_time' },
     { id: 'ioSummary', label: window.i18n.tc('输入 / 输出摘要'), cellType: 'text', minWidth: 240 },
     { id: 'elapsedText', label: window.i18n.tc('耗时'), cellType: 'text', width: 100, sortField: 'elapsed_time' },
